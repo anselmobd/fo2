c42f9ef628aeb81d2ee733683449ad1090db87ab
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2017-06-01 07:33:33 -0300

    pass

--- src/fo2/production_settings.py
+++ src/fo2/production_settings.py
@@ -2,7 +2,18 @@
 
 from .settings import *
 
+from .db_password import DBPASS
+
 DEBUG = False
 
 TEMPLATE_DEBUG = DEBUG
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': "tussor_fo2_production",
+        'USER': "tussor_fo2",
+        'PASSWORD': DBPASS,
+    }
+}
 
