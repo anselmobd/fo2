43149b9ba53321e68db97731dba0931727e3172f
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2017-06-01 14:17:01 -0300

    pep8

--- src/fo2/urls.py
+++ src/fo2/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 from .views import index
+
+
 urlpatterns = [
     url(r'^$', index, name='index'),
     # url(r'^', admin.site.urls),
