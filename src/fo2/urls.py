0fb0bec2a3d29a72795ff7164ab9dd6ee2ff416c
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2017-06-01 07:32:44 -0300

    index

--- src/fo2/urls.py
+++ src/fo2/urls.py
@@ -17,5 +17,7 @@ from django.conf.urls import url
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^$', 'fo2.views.index'),
+    # url(r'^', admin.site.urls),
     url(r'^admin/', admin.site.urls),
 ]
