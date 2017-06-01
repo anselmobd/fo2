9e31766e55f953853423388991c647871e236c01
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2017-06-01 08:14:46 -0300

    bug

--- src/fo2/urls.py
+++ src/fo2/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-
+from .views import index
 urlpatterns = [
-    url(r'^$', 'fo2.views.index'),
+    url(r'^$', index, name='index'),
     # url(r'^', admin.site.urls),
     url(r'^admin/', admin.site.urls),
 ]
