945017452534212c87c6fbd8ca86b288562be1cf
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2017-06-06 18:41:25 -0300

    stat_nivel

--- src/produto/views.py
+++ src/produto/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.db import connections
+from django.http import JsonResponse
 
 
 def rows_to_dict_list(cursor):
@@ -21,3 +22,27 @@ def index(request):
         'quant': row['QUANT'],
     }
     return render(request, 'produto/index.html', context)
+
+
+def stat_nivel(request):
+    cursor = connections['so'].cursor()
+    sql = '''
+        SELECT
+          p.NIVEL_ESTRUTURA nivel
+        , count(*) quant
+        FROM BASI_030 p
+        GROUP BY
+          p.NIVEL_ESTRUTURA
+        ORDER BY
+          p.NIVEL_ESTRUTURA
+    '''
+    cursor.execute(sql)
+    data = cursor.fetchall()
+    print(data)
+    # data = {
+    #     1: 123,
+    #     2: 12,
+    #     9: 1234,
+    # }
+    # return JsonResponse(data)
+    return JsonResponse(data, safe=False)
