9b3b02deb7bcdc5746b784c0e41b6dc22573f8b2
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2017-06-06 12:37:30 -0300

    production settings the right way

--- src/fo2/production_settings.py
+++ src/fo2/production_settings.py
@@ -14,6 +14,13 @@ DATABASES = {
         'NAME': "tussor_fo2_production",
         'USER': "tussor_fo2",
         'PASSWORD': DBPASS,
+    },
+    'so': {  # Systextil Oficial
+        'ENGINE': 'django.db.backends.oracle',
+        'NAME': 'xe',
+        'USER': 'systextil',
+        'PASSWORD': 'oracle',
+        'HOST': '192.168.1.93',
+        'PORT': '1521',
     }
 }
-
