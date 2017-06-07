dcd9e981db90dfae39f97585fa03523f5d873d25
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2017-06-07 19:00:42 -0300

    forms.TextInput to solve input of only numbers, also solve translation problem

--- src/lotes/forms.py
+++ src/lotes/forms.py
@@ -2,4 +2,5 @@ from django import forms
 
 
 class LoteForm(forms.Form):
-    lote = forms.CharField(label='Lote', max_length=9, min_length=9)
+    lote = forms.CharField(label='Lote', max_length=9, min_length=9,
+                           widget=forms.TextInput(attrs={'type': 'number'}))
