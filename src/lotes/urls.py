3a6b36c693b46f0795ca1d16f32c6d06afa07590
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2017-06-08 20:56:41 -0300

    detalhes_lote

--- src/lotes/urls.py
+++ src/lotes/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^posicao/$', views.posicao, name='posicao'),
+    url(r'^posicao/ajax/detalhes_lote/(\d{9})/$',
+        views.detalhes_lote, name='detalhes_lote'),
 ]
