7ac1747a13aa8eb8d735e17058aedc51a6da9fdb
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2017-06-12 15:36:45 -0300

    version

--- src/fo2/__init__.py
+++ src/fo2/__init__.py
@@ -1,2 +1,10 @@
-__version__ = '0.1.0'
-__version__date__ = '2017/06/07'
+__version__ = '0.1.1'
+__version__date__ = '2017/06/12'
+
+# histórico
+# - 0.1.0 - 2017/06/07
+#   primeira versão divulgada
+#
+# - 0.1.1 - 2017/06/12
+#   . melhorias na visualização das informações do lote
+#   . mostra cadastro de depósitos
