c481a0ca0948dfcf83aaa54ffe8c633a830e0b5b
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2017-06-12 12:31:51 -0300

    deposito

--- src/geral/views.py
+++ src/geral/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from django.db import connections
 
-# Create your views here.
+
+def rows_to_dict_list(cursor):
+    columns = [i[0] for i in cursor.description]
+    return [dict(zip(columns, row)) for row in cursor]
+
+
+def index(request):
+    context = {}
+    return render(request, 'geral/index.html', context)
+
+
+def deposito(request):
+    cursor = connections['so'].cursor()
+    sql = '''
+        SELECT
+          d.CODIGO_DEPOSITO COD
+        , d.DESCRICAO DESCR
+        FROM BASI_205 d
+        ORDER BY
+          d.CODIGO_DEPOSITO
+    '''
+    cursor.execute(sql)
+    data = rows_to_dict_list(cursor)
+    context = {
+        'headers': ('Depósito', 'Descrição'),
+        'fields': ('COD', 'DESCR'),
+        'data': data,
+    }
+    return render(request, 'geral/deposito.html', context)
