f5489cbffa00507facc6b66ec91ca405ae7e196f
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2017-06-13 14:57:01 -0300

    estágios

--- src/geral/urls.py
+++ src/geral/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^deposito/$', views.deposito, name='deposito'),
+    url(r'^estagio/$', views.estagio, name='estagio'),
 ]
