d0e3656c75d654a360a8d53def30bbbcef30d844
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2017-06-13 14:57:01 -0300

    estágios

--- src/geral/views.py
+++ src/geral/views.py
@@ -56,3 +56,30 @@ def deposito(request):
         'data': data,
     }
     return render(request, 'geral/tabela_geral.html', context)
+
+
+def estagio(request):
+    cursor = connections['so'].cursor()
+    sql = '''
+        SELECT
+          e.CODIGO_ESTAGIO EST
+        , e.DESCRICAO DESCR
+        , CASE WHEN e.CODIGO_DEPOSITO = 0 THEN ' '
+          ELSE e.CODIGO_DEPOSITO || '-' || d.DESCRICAO
+          END DEP
+        , e.LEED_TIME LT
+        FROM MQOP_005 e
+        LEFT JOIN BASI_205 d
+          ON d.CODIGO_DEPOSITO = e.CODIGO_DEPOSITO
+        ORDER BY
+          e.CODIGO_ESTAGIO
+    '''
+    cursor.execute(sql)
+    data = rows_to_dict_list(cursor)
+    context = {
+        'titulo': 'Estágios',
+        'headers': ('Estágio', 'Descrição', 'Lead time', 'Depósito'),
+        'fields': ('EST', 'DESCR', 'LT', 'DEP'),
+        'data': data,
+    }
+    return render(request, 'geral/tabela_geral.html', context)
