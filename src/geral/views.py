0137d80ba7bdc8b234f0bd3b57935487e78491fd
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2017-06-13 09:55:12 -0300

    depositos

--- src/geral/views.py
+++ src/geral/views.py
@@ -18,15 +18,40 @@ def deposito(request):
         SELECT
           d.CODIGO_DEPOSITO COD
         , d.DESCRICAO DESCR
+        , d.TIP_PROPRIEDADE_DEPOSITO PROP
+        , d.CNPJ9
+        , d.CNPJ4
+        , d.CNPJ2
+        , CASE WHEN d.CNPJ9 = 0 THEN ' '
+          ELSE coalesce(coalesce(f.NOME_FANTASIA, f.NOME_FORNECEDOR), '#')
+          END FORN
         FROM BASI_205 d
+        LEFT JOIN SUPR_010 f
+          ON f.FORNECEDOR9 = d.CNPJ9
+         AND f.FORNECEDOR4 = d.CNPJ4
+         AND f.FORNECEDOR2 = d.CNPJ2
         ORDER BY
           d.CODIGO_DEPOSITO
     '''
     cursor.execute(sql)
     data = rows_to_dict_list(cursor)
+    propriedades = {
+        1: 'Próprio',
+        2: 'Em terceiros',
+        3: 'De terceiros',
+    }
+    for row in data:
+        if row['FORN'] == ' ':
+            row['CNPJ'] = ''
+        else:
+            row['CNPJ'] = \
+                '{CNPJ9:09}/{CNPJ4:04}-{CNPJ2:02} - {FORN}'.format(**row)
+        if row['PROP'] in propriedades:
+            row['PROP'] = '{} - {}'.format(
+                row['PROP'], propriedades[row['PROP']])
     context = {
-        'headers': ('Depósito', 'Descrição'),
-        'fields': ('COD', 'DESCR'),
+        'headers': ('Depósito', 'Descrição', 'Propriedade', 'Terceiro'),
+        'fields': ('COD', 'DESCR', 'PROP', 'CNPJ'),
         'data': data,
     }
     return render(request, 'geral/deposito.html', context)
