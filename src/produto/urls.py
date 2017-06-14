26f0303e662b5c0a4f83914533e27936dcf77fed
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2017-06-14 13:55:16 -0300

    stat de produtos com informação de cores e tamanhos

--- src/produto/urls.py
+++ src/produto/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^ajax/stat_nivel/$', views.stat_nivel, name='stat_nivel'),
-    url(r'^ajax/stat_niveis/([129]{1})/$', views.stat_niveis, name='stat_niveis'),
+    url(r'^ajax/stat_niveis/([129]{1}.*)/$',
+        views.stat_niveis, name='stat_niveis'),
 ]
