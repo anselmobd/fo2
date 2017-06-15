0870a09ac41aa26873f2fb15344c8fa75e4757fb
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2017-06-14 22:23:02 -0300

    version

--- src/fo2/__init__.py
+++ src/fo2/__init__.py
@@ -1,10 +1,19 @@
-__version__ = '0.1.1'
-__version__date__ = '2017/06/12'
+__version__ = '0.1.2'
+__version__date__ = '2017/06/14'
 
 # histórico
-# - 0.1.0 - 2017/06/07
-#   primeira versão divulgada
+#
+# - ?.?.? - 20??/??/??
+#
+# - 0.1.2 - 2017/06/14
+#   . consulta de responsável por estágio
+#   . mostra cadastro de estágios
+#   . em Produtos, mostra tamanhos e cores
+#   . em Produtos, nível 1, mostra alternativas e roteiros
 #
 # - 0.1.1 - 2017/06/12
 #   . melhorias na visualização das informações do lote
 #   . mostra cadastro de depósitos
+#
+# - 0.1.0 - 2017/06/07
+#   primeira versão divulgada
