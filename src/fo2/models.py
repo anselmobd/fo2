a227458272bded19c4be3eedeae598adc2b4739f
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2017-06-22 21:46:45 -0300

    generic function

---
+++ src/fo2/models.py
@@ -0,0 +1,9 @@
+# from django.db import models
+
+
+def rows_to_dict_list(cursor):
+    columns = [i[0] for i in cursor.description]
+    return [dict(zip(columns, row)) for row in cursor]
+
+
+# Create your models here.
