bda6c09bbe903cdf31279de1a88d70dd5377106b
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2017-06-23 08:24:18 -0300

    default .old

--- src/lotes/urls.py
+++ src/lotes/urls.py
@@ -5,8 +5,8 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^posicao/$', views.posicao, name='posicao'),
-    url(r'^posicaoOri/$', views.posicaoOri, name='posicaoOri'),
-    url(r'^posicaoOri/ajax/detalhes_lote/(\d{9})/$',
+    url(r'^posicao.old/$', views.posicaoOri, name='posicaoOri'),
+    url(r'^posicao.old/ajax/detalhes_lote/(\d{9})/$',
         views.detalhes_lote, name='detalhes_lote'),
     url(r'^respons/$', views.respons, name='respons'),
     url(r'^op/$', views.op, name='op'),
