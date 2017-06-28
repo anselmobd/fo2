520e2134aaa0c540ec62682c29dff6fd68c962c0
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2017-06-28 17:20:54 -0300

    posicao class

--- src/lotes/urls.py
+++ src/lotes/urls.py
@@ -4,10 +4,13 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^posicao/$', views.posicao, name='posicao'),
-    url(r'^posicao.old/$', views.posicaoOri, name='posicaoOri'),
-    url(r'^posicao.old/ajax/detalhes_lote/(\d{9})/$',
-        views.detalhes_lote, name='detalhes_lote'),
+    url(r'^posicao/$', views.Posicao.as_view(), name='posicao'),
     url(r'^respons/$', views.respons, name='respons'),
     url(r'^op/$', views.op, name='op'),
+
+    # OLD OLD OLD OLD OLD OLD OLD OLD OLD OLD OLD OLD OLD OLD OLD OLD OLD OLD
+
+    url(r'^posicao.old/$', views.posicaoOri, name='posicao.old'),
+    url(r'^posicao.old/ajax/detalhes_lote/(\d{9})/$',
+        views.detalhes_lote, name='detalhes_lote'),
 ]
