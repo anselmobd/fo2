665259ddf37ecad75cc24f4870c12985214dc149
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2017-06-29 11:40:49 -0300

    autofocus and choice initial value

--- src/lotes/forms.py
+++ src/lotes/forms.py
@@ -2,25 +2,31 @@ from django import forms
 
 
 class LoteForm(forms.Form):
-    lote = forms.CharField(label='Lote', max_length=9, min_length=9,
-                           widget=forms.TextInput(attrs={'type': 'number'}))
+    lote = forms.CharField(
+        label='Lote', max_length=9, min_length=9,
+        widget=forms.TextInput(attrs={'type': 'number',
+                               'autofocus': 'autofocus'}))
 
 
 class OpForm(forms.Form):
-    op = forms.CharField(label='OP',
-                         widget=forms.TextInput(attrs={'type': 'number'}))
+    op = forms.CharField(
+        label='OP',
+        widget=forms.TextInput(attrs={'type': 'number',
+                               'autofocus': 'autofocus'}))
 
 
 class ResponsPorEstagioForm(forms.Form):
     estagio = forms.CharField(
         label='Estágio', max_length=2, required=False,
-        widget=forms.TextInput(attrs={'type': 'number'}))
+        widget=forms.TextInput(attrs={'type': 'number',
+                               'autofocus': 'autofocus'}))
     usuario = forms.CharField(
         label='Usuário', max_length=20, required=False,
         widget=forms.TextInput(attrs={'type': 'string'}))
     CHOICES = [('e', 'Por estágio'),
                ('u', 'Por usuário')]
-    ordem = forms.ChoiceField(choices=CHOICES, widget=forms.RadioSelect())
+    ordem = forms.ChoiceField(
+        choices=CHOICES, initial='e', widget=forms.RadioSelect())
 
     def clean_usuario(self):
         return self.cleaned_data['usuario'].upper()
