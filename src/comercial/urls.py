d7876763f4873f914c9dd38c673a5f20d69941b8
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2017-07-04 21:31:08 -0300

    better ficha_cliente view

--- src/comercial/urls.py
+++ src/comercial/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^ficha/$', views.FichaCliente.as_view(), name='ficha_cliente'),
+    url(r'^ficha/(\d+)/$', views.FichaCliente.as_view(),
+        name='ficha_cliente_cnpj'),
 ]
