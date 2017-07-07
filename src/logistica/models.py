75cb4b9b75922b6334bc973e27a7ec16aa408138
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2017-07-07 10:00:07 -0300

    verbose names

--- src/logistica/models.py
+++ src/logistica/models.py
@@ -2,12 +2,15 @@ from django.db import models
 
 
 class NotaFiscal(models.Model):
-    numero = models.IntegerField(unique=True)
+    numero = models.IntegerField(unique=True, verbose_name='número')
     ativa = models.BooleanField(default=True)
-    saida = models.DateField(null=True, blank=True)
+    saida = models.DateField(null=True, blank=True, verbose_name='saída')
     entrega = models.DateField(null=True, blank=True)
     confirmada = models.BooleanField(default=False)
-    observacao = models.TextField(null=True, blank=True)
+    observacao = models.TextField(
+        null=True, blank=True, verbose_name='observação')
 
     class Meta:
         db_table = "fo2_fat_nf"
+        verbose_name = "Nota Fiscal"
+        verbose_name_plural = "Notas Fiscais"
