22283ceae6cc8f73892c9a9ad77db18db35d47a2
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2017-07-10 16:40:35 -0300

    nfe status

--- src/logistica/admin.py
+++ src/logistica/admin.py
@@ -5,14 +5,17 @@ from .models import NotaFiscal
 
 
 class NotaFiscalAdmin(admin.ModelAdmin):
-    list_display = ['numero', 'faturamento', 'ativa',
+    list_display = ['numero', 'faturamento',
+                    'cod_status', 'msg_status', 'ativa',
                     'saida', 'entrega', 'confirmada', 'observacao']
-    list_filter = ('ativa', 'saida', 'entrega', 'confirmada', 'faturamento')
+    list_filter = ['ativa', 'saida', 'entrega', 'confirmada',
+                   'faturamento', 'cod_status']
     search_fields = ['numero', 'observacao']
     ordering = ['-numero']
-    fields = (('numero', 'faturamento', 'ativa'),
+    fields = (('numero', 'faturamento', 'cod_status', 'msg_status', 'ativa'),
               'saida', 'entrega', 'confirmada', 'observacao')
-    readonly_fields = ['numero', 'faturamento', 'ativa']
+    readonly_fields = ['numero', 'faturamento',
+                       'cod_status', 'msg_status', 'ativa']
 
 
 intr_adm_site.register(NotaFiscal, NotaFiscalAdmin)
