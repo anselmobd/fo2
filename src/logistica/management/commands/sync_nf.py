f5079c28c012bd27c40d907b39e3511b16561932
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2017-07-10 16:43:54 -0300

    nfe status sync

--- src/logistica/management/commands/sync_nf.py
+++ src/logistica/management/commands/sync_nf.py
@@ -2,6 +2,7 @@ from pprint import pprint
 
 from django.core.management.base import BaseCommand, CommandError
 from django.db import connections
+from django.utils import timezone
 
 from fo2.models import rows_to_dict_list
 import logistica.models as models
@@ -14,78 +15,66 @@ class Command(BaseCommand):
         try:
             cursor = connections['so'].cursor()
 
-            # get last included
-            last_nf = models.NotaFiscal.objects.order_by('-numero').\
-                values_list('numero').first()
-            if last_nf is None:
-                last_nf = 0
-            else:
-                last_nf = last_nf[0]
-            # self.stdout.write('last_nf = {}'.format(last_nf))
-
-            # get since 100 before last included
+            # sync all
             sql = '''
                 SELECT
                   f.NUM_NOTA_FISCAL NF
                 , f.DATA_AUTORIZACAO_NFE FATURAMENTO
+                , CAST( COALESCE( f.COD_STATUS, '0' ) AS INT ) COD_STATUS
+                , COALESCE( f.MSG_STATUS, ' ' ) MSG_STATUS
+                , f.SITUACAO_NFISC SITUACAO
                 FROM FATU_050 f
-                WHERE f.NUM_NOTA_FISCAL > %s
+                --WHERE rownum = 1
                 ORDER BY
-                  f.NUM_NOTA_FISCAL
-            '''
-            cursor.execute(sql, [last_nf-100])
-            nfs_st = rows_to_dict_list(cursor)
-
-            nfs = list(models.NotaFiscal.objects.values_list('numero'))
-
-            for row in nfs_st:
-                if (row['NF'],) not in nfs:
-                    self.stdout.write('sync_nf - insert {}'.format(row['NF']))
-                    nf = models.NotaFiscal(
-                        numero=row['NF'], faturamento=row['FATURAMENTO'])
-                    nf.save()
-
-            # get all canceled
-            sql = '''
-                SELECT
-                  f.NUM_NOTA_FISCAL NF
-                FROM FATU_050 f
-                WHERE f.SITUACAO_NFISC = 2
-                ORDER BY
-                  f.NUM_NOTA_FISCAL
+                  f.NUM_NOTA_FISCAL DESC
             '''
             cursor.execute(sql)
             nfs_st = rows_to_dict_list(cursor)
+            # self.stdout.write('len(nfs_st) = {}'.format(len(nfs_st)))
 
-            nfs = list(models.NotaFiscal.objects.filter(ativa=True).
-                       values_list('numero'))
+            nfs_fo2 = list(models.NotaFiscal.objects.values_list('numero'))
+            # self.stdout.write('len(nfs_fo2) = {}'.format(len(nfs_fo2)))
 
-            for row in nfs_st:
-                if (row['NF'],) in nfs:
-                    self.stdout.write('sync_nf - cancela {}'.format(row['NF']))
-                    nf = models.NotaFiscal.objects.get(numero=row['NF'])
-                    nf.ativa = False
-                    nf.save()
+            for row_st in nfs_st:
+                if row_st['FATURAMENTO'] is None:
+                    faturamento = None
+                else:
+                    faturamento = timezone.make_aware(
+                        row_st['FATURAMENTO'], timezone.get_current_timezone())
+                edit = True
+                if (row_st['NF'],) in nfs_fo2:
+                    nf_fo2 = models.NotaFiscal.objects.get(numero=row_st['NF'])
+                    if nf_fo2.faturamento == faturamento \
+                            and nf_fo2.cod_status == row_st['COD_STATUS'] \
+                            and nf_fo2.msg_status == row_st['MSG_STATUS'] \
+                            and nf_fo2.ativa == (row_st['SITUACAO'] == 1):
+                        edit = False
+                    else:
+                        self.stdout.write(
+                            'sync_nf - update {}'.format(row_st['NF']))
+                else:
+                    self.stdout.write(
+                        'sync_nf - insert {}'.format(row_st['NF']))
+                    nf_fo2 = models.NotaFiscal(numero=row_st['NF'])
+                if edit:
+                    # pprint(row_st)
+                    self.stdout.write(
+                        'NF = {}'.format(row_st['NF']))
+                    self.stdout.write(
+                        'date = {}'.format(faturamento))
+                    nf_fo2.faturamento = faturamento
 
-            # get data de faturamento
-            sql = '''
-                SELECT
-                  f.DATA_AUTORIZACAO_NFE FATURAMENTO
-                FROM FATU_050 f
-                WHERE f.NUM_NOTA_FISCAL = %s
-            '''
-            nfs = list(models.NotaFiscal.objects.filter(
-                faturamento=None, ativa=True).values_list('numero'))
-            for row in nfs:
-                # pprint(row)
-                cursor.execute(sql, [row[0]])
-                nfs_st = rows_to_dict_list(cursor)
-                # pprint(nfs_st)
-                if len(nfs_st) > 0:
-                    nf_st = nfs_st[0]
-                    nf = models.NotaFiscal.objects.get(numero=row[0])
-                    nf.faturamento = nf_st['FATURAMENTO']
-                    nf.save()
+                    self.stdout.write('cod = {}'.format(row_st['COD_STATUS']))
+                    nf_fo2.cod_status = row_st['COD_STATUS']
+
+                    self.stdout.write('msg = {}'.format(row_st['MSG_STATUS']))
+                    nf_fo2.msg_status = row_st['MSG_STATUS']
+
+                    self.stdout.write('sit = {}'.format(row_st['SITUACAO']))
+                    nf_fo2.ativa = (row_st['SITUACAO'] == 1)
+
+                    nf_fo2.save()
+                    self.stdout.write('saved')
 
         except Exception as e:
-            raise CommandError('Error syncing NF {}'.format(e))
+            raise CommandError('Error syncing NF "{}"'.format(e))
