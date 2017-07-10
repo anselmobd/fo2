8a1adb64d4596d7a04afc58e8d86b3eed68e0eff
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2017-07-10 17:33:06 -0300

    pedido view

--- src/contabil/models.py
+++ src/contabil/models.py
@@ -13,15 +13,23 @@ def infadprod_pro_pedido(cursor, pedido):
         , i.CD_IT_PE_ITEM COR
         , i.CD_IT_PE_SUBGRUPO TAM
         , coalesce( ip.REF_CLIENTE, '-') INFADPROD
+        , rtc.CODIGO_BARRAS EAN
+        , rtc.NARRATIVA
         FROM PEDI_100 p
         LEFT JOIN PEDI_010 c
           ON c.CGC_9 = p.CLI_PED_CGC_CLI9
          AND c.CGC_4 = p.CLI_PED_CGC_CLI4
         JOIN PEDI_110 i
           ON i.PEDIDO_VENDA = p.PEDIDO_VENDA
+        JOIN BASI_010 rtc
+          on rtc.NIVEL_ESTRUTURA = i.CD_IT_PE_NIVEL99
+         AND rtc.GRUPO_ESTRUTURA = i.CD_IT_PE_GRUPO
+         AND rtc.SUBGRU_ESTRUTURA = i.CD_IT_PE_SUBGRUPO
+         AND rtc.ITEM_ESTRUTURA = i.CD_IT_PE_ITEM
         LEFT JOIN ESTQ_400 ip
           ON ip.CLIENTE9 = CLI_PED_CGC_CLI9
          AND ip.CLIENTE4 = CLI_PED_CGC_CLI4
+         AND ip.NIVEL_ESTRUTURA = i.CD_IT_PE_NIVEL99
          AND ip.GRUPO_ESTRUTURA = i.CD_IT_PE_GRUPO
          AND ip.SUBGRUPO_ESTRUTURA = i.CD_IT_PE_SUBGRUPO
          AND ip.ITEM_ESTRUTURA = i.CD_IT_PE_ITEM
