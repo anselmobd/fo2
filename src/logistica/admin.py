e3297376ef05945456bb1d50258e55d20970cad7
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2017-07-11 18:33:22 -0300

    new fields

--- src/logistica/admin.py
+++ src/logistica/admin.py
@@ -5,18 +5,26 @@ from .models import NotaFiscal
 
 
 class NotaFiscalAdmin(admin.ModelAdmin):
-    list_display = ['numero', 'faturamento',
-                    'cod_status', 'msg_status', 'ativa',
+    list_display = ['numero', 'dest_cnpj', 'dest_nome', 'uf',
+                    'natu_descr', 'natu_venda',
+                    'faturamento', 'cod_status', 'msg_status', 'ativa',
                     'saida', 'entrega', 'confirmada', 'observacao']
-    list_filter = ['ativa', 'saida', 'entrega', 'confirmada',
-                   'faturamento', 'cod_status']
-    search_fields = ['numero', 'observacao']
+    list_filter = ['ativa', 'natu_venda', 'saida',
+                   'entrega', 'confirmada',
+                   'faturamento', 'cod_status', 'uf']
+    search_fields = ['numero', 'observacao',
+                     'dest_cnpj', 'dest_nome', 'natu_descr']
     ordering = ['-numero']
     fields = (('numero', 'ativa'),
               ('faturamento', 'cod_status', 'msg_status'),
+              ('dest_cnpj', 'dest_nome', 'uf'),
+              ('natu_descr', 'natu_venda'),
               'saida', 'entrega', 'confirmada', 'observacao')
     readonly_fields = ['numero', 'faturamento',
-                       'cod_status', 'msg_status', 'ativa']
+                       'cod_status', 'msg_status', 'ativa',
+                       'dest_cnpj', 'dest_nome', 'uf',
+                       'natu_descr', 'natu_venda',
+                       ]
 
 
 intr_adm_site.register(NotaFiscal, NotaFiscalAdmin)
