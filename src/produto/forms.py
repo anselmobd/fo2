ef742c4915d53d4764dace99aa9db5581e93dfbe
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2017-08-28 14:06:08 -0300

    show upper value and set length

--- src/produto/forms.py
+++ src/produto/forms.py
@@ -3,6 +3,13 @@ from django import forms
 
 class RefForm(forms.Form):
     ref = forms.CharField(
-        label='Referência',
+        label='Referência', max_length=5, min_length=5,
         widget=forms.TextInput(attrs={'type': 'string',
                                'autofocus': 'autofocus'}))
+
+    def clean_ref(self):
+        ref = self.cleaned_data['ref'].upper()
+        data = self.data.copy()
+        data['ref'] = ref
+        self.data = data
+        return ref
