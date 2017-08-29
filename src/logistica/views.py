016db455b2757f4634fe18ce19e0c07621c6a28c
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2017-08-29 15:03:54 -0300

    adapt to new templates and delete unused view

--- src/logistica/views.py
+++ src/logistica/views.py
@@ -8,13 +8,3 @@ import logistica.models as models
 def index(request):
     context = {}
     return render(request, 'logistica/index.html', context)
-
-
-class DataSaida(View):
-    template_name = 'logistica/data_saida.html'
-    titulo = 'Controle de data de saída de NF'
-
-    def get(self, request, *args, **kwargs):
-        context = {'titulo': self.titulo}
-        sync_nf()
-        return render(request, self.template_name, context)
