65c46dd549f17f6ca6e175d1d2e67bc3271b3dae
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2017-09-04 11:57:55 -0300

    admin log view

--- src/fo2/admin.py
+++ src/fo2/admin.py
@@ -1,3 +1,6 @@
+from django.contrib import admin
+from django.contrib.admin.models import LogEntry
+
 from django.contrib.admin import AdminSite
 
 
@@ -6,3 +9,25 @@ class IntrAdmSite(AdminSite):
 
 
 intr_adm_site = IntrAdmSite(name='intradm')
+
+
+class LogEntryAdmin(admin.ModelAdmin):
+    search_fields = ('object_repr',)
+    list_filter = ('action_time', 'content_type',)
+    list_display = ('action_time', 'user', 'content_type', 'tipo',
+                    'object_repr')
+    fields = ('action_time', 'user', 'content_type', 'object_id',
+              'object_repr', 'action_flag', 'change_message', )
+    readonly_fields = ('action_time', 'user', 'content_type', 'object_id',
+                       'object_repr', 'action_flag', 'tipo', 'change_message',)
+
+    def tipo(self, obj):
+        if obj.is_addition():
+            return u"Adicionado"
+        elif obj.is_change():
+            return u"Modificado"
+        elif obj.is_deletion():
+            return u"Deletado"
+
+
+admin.site.register(LogEntry, LogEntryAdmin)
