ea0680c61595119cdfe9d33cc630330dc7c5c5ee
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2017-09-12 13:43:21 -0300

    RecordTracking

--- src/geral/admin.py
+++ src/geral/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from fo2.admin import intr_adm_site
+from .models import RecordTracking
+
+
+class RecordTrackingAdmin(admin.ModelAdmin):
+    list_display = ['table', 'record_id', 'iud', 'log']
+    search_fields = ['table', 'log']
+    ordering = ['record_id']
+    fields = ['table', 'record_id', 'iud', 'log']
+    readonly_fields = ['table', 'record_id', 'iud', 'log']
+
+
+intr_adm_site.register(RecordTracking, RecordTrackingAdmin)
