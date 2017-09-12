c9c3d3c9830af187fc8bf09ccd60287baf8b94aa
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2017-09-12 15:16:56 -0300

    clean code

--- src/utils/middlewares.py
+++ src/utils/middlewares.py
@@ -6,34 +6,6 @@ except ImportError:
 from .classes import LoggedInUser
 
 
-# def logged_in_user_middleware(get_response):
-#     '''
-#         Insert this middleware after
-#         django.contrib.auth.middleware.AuthenticationMiddleware
-#     '''
-#     def middleware(request):
-#         '''
-#             Returned None for continue request
-#         '''
-#         logged_in_user = LoggedInUser()
-#         logged_in_user.set_user(request)
-#         return None
-#     return middleware
-
-
-# class LoggedInUserMiddleware(object):
-#     '''
-#         Insert this middleware after
-#         django.contrib.auth.middleware.AuthenticationMiddleware
-#     '''
-#     def process_request(self, request):
-#         '''
-#             Returned None for continue request
-#         '''
-#         logged_in_user = LoggedInUser()
-#         logged_in_user.set_user(request)
-#         return None
-
 class LoggedInUserMiddleware(MiddlewareMixin):
     '''
         Insert this middleware after
