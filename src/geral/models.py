fcd5f52576442c51afec90e73b35dd44cb8386f5
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2017-09-12 13:43:21 -0300

    RecordTracking

--- src/geral/models.py
+++ src/geral/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class RecordTracking(models.Model):
+    user = models.CharField(
+        max_length=64, verbose_name='Usuário')
+    time = models.DateTimeField(
+        auto_now_add=False)
+    table = models.CharField(
+        max_length=64, verbose_name='Tabela')
+    record_id = models.IntegerField(
+        verbose_name='Id do registro')
+    iud = models.CharField(
+        max_length=1, verbose_name='Ação')
+    log = models.CharField(
+        max_length=2048, verbose_name='Log')
+
+    class Meta:
+        db_table = "fo2_ger_record_tracking"
+        verbose_name = "log de alteração de registro"
+        verbose_name_plural = "logs de alterações de registros"
