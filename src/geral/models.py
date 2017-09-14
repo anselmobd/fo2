1f27fc1e9b71749301be7a9d2f6c521842f07047
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2017-09-14 12:13:30 -0300

    let user null

--- src/geral/models.py
+++ src/geral/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class RecordTracking(models.Model):
     user = models.CharField(
+        null=True, blank=True,
         max_length=64, verbose_name='Usuário')
     time = models.DateTimeField(
         null=True, blank=True,
