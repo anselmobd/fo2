cabce649bbd1c845628b9c8a0324c432e77d71e7
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2017-09-26 10:40:11 -0300

    app insumo

---
+++ src/insumo/models.py
@@ -0,0 +1,80 @@
+from django.db import models
+from django.db import connections
+
+import produto.models
+
+
+def rows_to_dict_list(cursor):
+    columns = [i[0] for i in cursor.description]
+    return [dict(zip(columns, row)) for row in cursor]
+
+
+def item_count_nivel(cursor, ref, nivel=None):
+    # verifica existêcia, unicidade e nível
+    param = [ref, ]
+    sql = """
+        SELECT
+          count(*) COUNT
+        , min(r.NIVEL_ESTRUTURA) NIVEL
+        FROM basi_030 r
+        WHERE r.REFERENCIA = %s
+    """
+    if nivel is not None:
+        sql = sql + """
+            AND r.NIVEL_ESTRUTURA = %s
+        """
+        param.append(nivel)
+    cursor.execute(sql, param)
+    return rows_to_dict_list(cursor)
+
+
+def ref_inform(cursor, nivel, ref):
+    # Informações básicas
+    sql = """
+        SELECT
+          r.DESCR_REFERENCIA DESCR
+        , r.UNIDADE_MEDIDA || ' - ' || um.DESCR_UNIDADE UM
+        , r.CONTA_ESTOQUE || ' - ' || ce.DESCR_CT_ESTOQUE CONTA_ESTOQUE
+        , r.CLASSIFIC_FISCAL || ' - ' || cf.DESCR_CLASS_FISC NCM
+        , r.CODIGO_CONTABIL || ' - ' || cc.DESCRICAO CODIGO_CONTABIL
+        FROM basi_030 r
+        JOIN basi_200 um
+          ON um.unidade_medida = r.UNIDADE_MEDIDA
+        JOIN BASI_150 ce
+          ON ce.CONTA_ESTOQUE = r.CONTA_ESTOQUE
+        JOIN BASI_240 cf
+          ON cf.CLASSIFIC_FISCAL = r.CLASSIFIC_FISCAL
+        JOIN CONT_540 cc
+          ON cc.TIPO_CONTABIL = 3
+         AND cc.CODIGO_CONTABIL = r.CODIGO_CONTABIL
+        WHERE r.NIVEL_ESTRUTURA = %s
+         AND r.REFERENCIA = %s
+    """
+    cursor.execute(sql, [nivel, ref])
+    return rows_to_dict_list(cursor)
+
+
+def ref_cores(cursor, nivel, ref):
+    return produto.models.prod_cores(cursor, nivel, ref)
+
+
+def ref_tamanhos(cursor, nivel, ref):
+    return produto.models.prod_tamanhos(cursor, nivel, ref)
+
+
+def ref_parametros(cursor, nivel, ref):
+    # Informações básicas
+    sql = """
+        SELECT
+          p.SUBGRU_ESTRUTURA TAM
+        , p.ITEM_ESTRUTURA COR
+        , p.CODIGO_DEPOSITO || ' - ' || d.DESCRICAO DEPOSITO
+        , p.ESTOQUE_MINIMO
+        FROM BASI_015 p
+        JOIN BASI_205 d
+          ON d.CODIGO_DEPOSITO = p.CODIGO_DEPOSITO
+        WHERE p.NIVEL_ESTRUTURA = %s
+          AND p.GRUPO_ESTRUTURA = %s
+    """
+    cursor.execute(sql, [nivel, ref])
+    return rows_to_dict_list(cursor)
---
+++ src/insumo/views.py
@@ -0,0 +1,107 @@
+from django.shortcuts import render
+from django.db import connections
+from django.views import View
+
+from .forms import RefForm
+import insumo.models as models
+
+
+def index(request):
+    return render(request, 'insumo/index.html')
+
+
+class Ref(View):
+    Form_class = RefForm
+    template_name = 'insumo/ref.html'
+    title_name = 'Insumos'
+
+    def mount_context(self, cursor, item):
+        context = {'item': item}
+
+        if len(item) == 5:
+            data = models.item_count_nivel(cursor, item)
+            row = data[0]
+            if row['COUNT'] > 1:
+                context.update({
+                    'msg_erro':
+                        'Referência de insumo ambígua. Informe o nível.',
+                })
+                return context
+            elif row['COUNT'] == 1:
+                nivel = row['NIVEL']
+                ref = item
+        else:
+            nivel = item[0]
+            ref = item[-5:]
+            print(nivel, ref)
+            data = models.item_count_nivel(cursor, ref, nivel)
+            row = data[0]
+        if row['COUNT'] == 0:
+            context.update({
+                'msg_erro': 'Referência de insumo não encontrada',
+            })
+            return context
+        context.update({
+            'nivel': nivel,
+            'ref': ref,
+        })
+
+        # Informações básicas
+        data = models.ref_inform(cursor, nivel, ref)
+        context.update({
+            'headers': ('Descrição', 'Unidade de medida', 'Conta de estoque',
+                        'NCM', 'Código Contábil'),
+            'fields': ('DESCR', 'UM', 'CONTA_ESTOQUE',
+                       'NCM', 'CODIGO_CONTABIL'),
+            'data': data,
+        })
+
+        # Cores
+        c_data = models.ref_cores(cursor, nivel, ref)
+        if len(c_data) != 0:
+            context.update({
+                'c_headers': ('Cor', 'Descrição'),
+                'c_fields': ('COR', 'DESCR'),
+                'c_data': c_data,
+            })
+
+        # Tamanhos
+        t_data = models.ref_tamanhos(cursor, nivel, ref)
+        if len(t_data) != 0:
+            context.update({
+                't_headers': ('Tamanho', 'Descrição', 'Complemento'),
+                't_fields': ('TAM', 'DESCR', 'COMPL'),
+                't_data': t_data,
+            })
+
+        # Parametros
+        p_data = models.ref_parametros(cursor, nivel, ref)
+        if len(p_data) != 0:
+            context.update({
+                'p_headers': ('Tamanho', 'Cor', 'Depósito', 'Estóque mínimo'),
+                'p_fields': ('TAM', 'COR', 'DEPOSITO', 'ESTOQUE_MINIMO'),
+                'p_data': p_data,
+            })
+
+        return context
+
+    def get(self, request, *args, **kwargs):
+        if 'item' in kwargs:
+            return self.post(request, *args, **kwargs)
+        else:
+            context = {'titulo': self.title_name}
+            form = self.Form_class()
+            context['form'] = form
+            return render(request, self.template_name, context)
+
+    def post(self, request, *args, **kwargs):
+        context = {'titulo': self.title_name}
+        form = self.Form_class(request.POST)
+        if 'item' in kwargs:
+            form.data['item'] = kwargs['item']
+        if form.is_valid():
+            item = form.cleaned_data['item']
+            cursor = connections['so'].cursor()
+            context.update(self.mount_context(cursor, item))
+        context['form'] = form
+        return render(request, self.template_name, context)
