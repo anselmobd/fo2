bbf098b90457262ef276f3bbe5e859f710a65609
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2017-09-27 12:34:59 -0300

    class TermalPrint

--- src/utils/classes.py
+++ src/utils/classes.py
@@ -1,3 +1,9 @@
+import copy
+from subprocess import Popen, PIPE
+
+from django.template import Template, Context
+
+
 class SingletonMeta(type):
     '''
         Singleton pattern requires for GetUser class
@@ -47,3 +53,61 @@ class LoggedInUser(SingletonBaseMeta):
     @property
     def have_user(self):
         return user is not None
+
+
+class TermalPrint:
+    _termal_print_controls = {
+        'soh_': b'\x01',
+        'stx_': b'\x02',
+        'ctr_': b'\x0d',
+        'esc_': b'\x1b',
+    }
+
+    def __init__(self):
+        self._print_started = False
+        self.lp()
+        self.printer()
+
+    def __del__(self):
+        if self._print_started:
+            self.printer_end()
+
+    def lp(self, lp='/usr/bin/lp'):
+        self._lp = copy.copy(lp)
+
+    def printer(self, p='SuporteTI_SuporteTI'):
+        self._p = copy.copy(p)
+
+    def template(self, t, limpa):
+        tt = copy.copy(t)
+        if limpa:
+            for l in limpa:
+                tt = tt.replace(l, '')
+        self._template = Template(tt)
+
+    def context(self, c):
+        cc = copy.copy(c)
+        cc.update(self._termal_print_controls)
+        self._context = Context(cc)
+
+    def render(self):
+        commands = self._template.render(self._context)
+        return commands.encode('utf-8')
+
+    def printer_init(self):
+        self._lpr = Popen([self._lp, "-d"+self._p, "-"], stdin=PIPE)
+        self._print_started = True
+
+    def printer_end(self):
+        self._lpr.stdin.close()
+        self._lpr.wait()
+
+    def printer_send(self):
+        self._lpr.stdin.write(self.render())
+
+    def printer_send1(self):
+        self.printer_init()
+        try:
+            self.printer_send()
+        finally:
+            self.printer_end()
