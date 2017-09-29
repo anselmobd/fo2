592e0726529ae42d2416fde99288bd9e6815907d
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2017-09-29 16:43:04 -0300

    link encerrar

--- src/fo2/views.py
+++ src/fo2/views.py
@@ -1,5 +1,12 @@
 from django.views.generic import TemplateView
+from django.contrib.auth import logout
+from django.shortcuts import redirect
 
 
 class IndexView(TemplateView):
     template_name = "index.html"
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('/')
