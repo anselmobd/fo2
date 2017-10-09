6e5144ff14dc41bb8bbaac235afafdbc20bad785
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2017-10-09 19:36:00 -0300

    generic FiltroForm

--- src/produto/forms.py
+++ src/produto/forms.py
@@ -20,18 +20,3 @@ class ModeloForm(forms.Form):
         label='Modelo', max_length=4, min_length=3,
         widget=forms.TextInput(attrs={'type': 'number',
                                'autofocus': 'autofocus'}))
-
-
-class ListaProdutoForm(forms.Form):
-    busca = forms.CharField(
-        label='Filtro', required=False,
-        help_text='Busca vários valores separados por espaço.',
-        widget=forms.TextInput(attrs={'type': 'string',
-                               'autofocus': 'autofocus'}))
-
-    def clean_busca(self):
-        busca = self.cleaned_data['busca'].upper()
-        data = self.data.copy()
-        data['busca'] = busca
-        self.data = data
-        return busca
