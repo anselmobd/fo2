ef913cd45b52cfe1d6249a7b1eb15d15b522f89e
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2017-10-26 22:05:06 -0200

    if no span, no group

--- src/fo2/template.py
+++ src/fo2/template.py
@@ -1,6 +1,7 @@
 
 
 def group_rowspan(data, group):
+    noSpan = True
     for i in range(len(data)):
         data[i]['rowspan'] = 1
     inferior = []
@@ -8,6 +9,9 @@ def group_rowspan(data, group):
     for i in range(len(data)-1, -1, -1):
         atual = [data[i][f] for f in group]
         if atual == inferior:
+            noSpan = False
             data[i]['rowspan'] = data[i]['rowspan'] + data[i+1]['rowspan']
             data[i+1]['rowspan'] = 0
         inferior = atual[:]
+    if noSpan:
+        del group[:]
