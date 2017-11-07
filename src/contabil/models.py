5f5d480411f47fd20ed3acf63e9a66ddb24ab6c9
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2017-11-07 07:23:41 -0200

    correcting ordering by size

--- src/contabil/models.py
+++ src/contabil/models.py
@@ -19,26 +19,26 @@ def infadprod_pro_pedido(cursor, pedido):
         , coalesce( ip.REF_CLIENTE, '-') INFADPROD
         , coalesce( rtc.CODIGO_BARRAS, ' ') EAN
         , rtc.NARRATIVA
-        FROM PEDI_100 p
+        FROM PEDI_100 p -- pedido de venda
         LEFT JOIN PEDI_010 c
           ON c.CGC_9 = p.CLI_PED_CGC_CLI9
          AND c.CGC_4 = p.CLI_PED_CGC_CLI4
-        JOIN PEDI_110 i
+        JOIN PEDI_110 i -- item de pedido de venda
           ON i.PEDIDO_VENDA = p.PEDIDO_VENDA
-        JOIN BASI_010 rtc
+        JOIN BASI_010 rtc -- item (ref+tam+cor)
           on rtc.NIVEL_ESTRUTURA = i.CD_IT_PE_NIVEL99
          AND rtc.GRUPO_ESTRUTURA = i.CD_IT_PE_GRUPO
          AND rtc.SUBGRU_ESTRUTURA = i.CD_IT_PE_SUBGRUPO
          AND rtc.ITEM_ESTRUTURA = i.CD_IT_PE_ITEM
-        LEFT JOIN ESTQ_400 ip
+        LEFT JOIN ESTQ_400 ip -- item - informações por cliente
           ON ip.CLIENTE9 = CLI_PED_CGC_CLI9
          AND ip.CLIENTE4 = CLI_PED_CGC_CLI4
          AND ip.NIVEL_ESTRUTURA = i.CD_IT_PE_NIVEL99
          AND ip.GRUPO_ESTRUTURA = i.CD_IT_PE_GRUPO
          AND ip.SUBGRUPO_ESTRUTURA = i.CD_IT_PE_SUBGRUPO
          AND ip.ITEM_ESTRUTURA = i.CD_IT_PE_ITEM
-        LEFT JOIN BASI_220 t
-          ON t.TAMANHO_REF = ip.SUBGRUPO_ESTRUTURA
+        LEFT JOIN BASI_220 t -- tamanhos
+          ON t.TAMANHO_REF = i.CD_IT_PE_SUBGRUPO
         WHERE p.PEDIDO_VENDA = %s
         ORDER BY
           p.PEDIDO_VENDA
