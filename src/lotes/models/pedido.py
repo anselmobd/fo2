38d0b47bbb184bc161113665bd3d894911d82c49
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2017-11-08 15:29:24 -0200

    show OPs

--- src/lotes/models/pedido.py
+++ src/lotes/models/pedido.py
@@ -40,3 +40,39 @@ def ped_inform(cursor, pedido):
     """
     cursor.execute(sql, [pedido])
     return rows_to_dict_list(cursor)
+
+
+def ped_op(cursor, pedido):
+    # OPs
+    sql = """
+        SELECT
+          o.PEDIDO_VENDA
+        , o.ORDEM_PRODUCAO
+        , CASE
+          when o.REFERENCIA_PECA <= '99999' then 'PA'
+          when o.REFERENCIA_PECA <= 'B9999' then 'PG'
+          when o.REFERENCIA_PECA >= 'Z0000' then 'MP'
+          else 'MD'
+          END TIPO
+        , o.REFERENCIA_PECA
+        , o.ORDEM_PRINCIPAL
+        , ( SELECT
+              SUM( l.QTDE_PECAS_PROG )
+            FROM pcpc_040 l
+            WHERE l.ORDEM_PRODUCAO = o.ORDEM_PRODUCAO
+              AND l.SEQ_OPERACAO = (
+                SELECT
+                  MAX( ls.SEQ_OPERACAO )
+                FROM pcpc_040 ls
+                WHERE ls.ORDEM_PRODUCAO = l.ORDEM_PRODUCAO
+              )
+          ) QTD
+        FROM PCPC_020 o -- OP
+        WHERE o.PEDIDO_VENDA = %s
+          AND o.SITUACAO <> 9
+        ORDER BY
+          o.ORDEM_PRINCIPAL
+        , o.ORDEM_PRODUCAO
+    """
+    cursor.execute(sql, [pedido])
+    return rows_to_dict_list(cursor)
