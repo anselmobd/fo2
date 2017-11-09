4e92f2c67c562e9eed797639b9b88411b0e42ee8
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2017-11-09 13:58:24 -0200

    em get_os a qtd de peças na OS era a programada, agora é a_produzir ou total_processadas

--- src/lotes/models/base.py
+++ src/lotes/models/base.py
@@ -188,7 +188,16 @@ def get_os(cursor, os='', op='', periodo='', oc=''):
           END SITUACAO
         , os.COD_CANC_ORDEM || '-' || c.DESCR_CANC_ORDEM CANC
         , count(l.ORDEM_CONFECCAO) LOTES
-        , sum(l.QTDE_PECAS_PROG) QTD
+        , sum(
+            CASE WHEN l.QTDE_A_PRODUZIR_PACOTE <> 0
+            THEN l.QTDE_A_PRODUZIR_PACOTE
+            ELSE --l.QTDE_PECAS_PROG
+              QTDE_PECAS_PROD
+            + QTDE_CONSERTO
+            + QTDE_PECAS_2A
+            + QTDE_PERDAS
+            END
+          ) QTD
         , os.DATA_EMISSAO
         , os.DATA_ENTREGA
         , os.OBSERVACAO
