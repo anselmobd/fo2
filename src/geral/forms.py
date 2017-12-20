d1b008d0ab1262d718d60b3e56b2b1b06d43f810
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2017-12-20 16:45:58 -0200

    informtivo view

--- src/geral/forms.py
+++ src/geral/forms.py
@@ -1,15 +1,22 @@
 from django import forms
 
 
-class PainelForm(forms.ModelForm):
+class PainelModelForm(forms.ModelForm):
     layout = forms.CharField(
         label='Layout',
         widget=forms.Textarea(
             attrs={'max_length': 4096, 'rows': 10, 'cols': 79}))
 
 
-class InformacaoModuloForm(forms.ModelForm):
+class InformacaoModuloModelForm(forms.ModelForm):
     texto = forms.CharField(
         label='texto',
         widget=forms.Textarea(
             attrs={'max_length': 4096, 'rows': 10, 'cols': 79}))
+
+
+class InformacaoModuloForm(forms.Form):
+    chamada = forms.CharField(
+        widget=forms.Textarea(
+            attrs={'max_length': 200, 'rows': 5, 'cols': 39,
+                   'style': 'vertical-align:top;'}))
--- src/geral/urls.py
+++ src/geral/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     url(r'^estagio/$', views.estagio, name='estagio'),
     url(r'^painel/(?P<painel>[^/]*)/?$',
         views.PainelView.as_view(), name='ger_painel'),
+    url(r'^informativo/(?P<modulo>[^/]+)/(?P<id>[^/]+)?/?$',
+        views.InformativoView.as_view(), name='ger_informativo_modulo'),
 ]
