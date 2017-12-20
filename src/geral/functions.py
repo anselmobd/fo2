b5815b6f38b0f4d9b54a9deaab5a818f6acfb0a7
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2017-12-20 16:39:41 -0200

    list paineis in menu

--- src/geral/functions.py
+++ src/geral/functions.py
@@ -1,7 +1,10 @@
-from .models import Painel
+from .models import Painel, PainelModulo
 
 
 # http://kkabardi.me/post/dynamic-menu-navigation-django/
 def get_list_geral_paineis(context):
-    p = Painel.objects.all()
-    return {'list_geral_paineis': p}
+    paineis = Painel.objects.all()
+    modulos = PainelModulo.objects.all()
+    return {'list_geral_paineis': paineis,
+            'list_geral_modulos': modulos,
+            }
