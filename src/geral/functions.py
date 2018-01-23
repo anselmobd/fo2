8d13d2ab860051691b39ae60d7df2d89157e8aa3
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2018-01-23 17:23:36 -0200

    order

--- src/geral/functions.py
+++ src/geral/functions.py
@@ -4,7 +4,7 @@ from .models import Painel, PainelModulo
 # http://kkabardi.me/post/dynamic-menu-navigation-django/
 def get_list_geral_paineis(context):
     paineis = Painel.objects.all()
-    modulos = PainelModulo.objects.all()
+    modulos = PainelModulo.objects.all().order_by('nome')
     return {'list_geral_paineis': paineis,
             'list_geral_modulos': modulos,
             }
