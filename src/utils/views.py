887fb0db9f826204eac54adea9ab1e0b4f2b253a
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2018-01-23 08:19:21 -0200

    register template filter word_slice

--- src/utils/views.py
+++ src/utils/views.py
@@ -6,6 +6,30 @@ def get_item(dictionary, key):
     return dictionary.get(key)
 
 
+@register.filter
+def word_slice(text, slice):
+
+    def adjust_cut(text, valor, limit=1):
+        val = valor
+        while text[val-1] != ' ' and val > 1:
+            val -= 1
+        if val <= limit:
+            val = valor
+        while text[val] == ' ' and val < len(text):
+            val += 1
+        return val
+
+    slices = slice.split(':')
+    inicio = int(slices[0]) if slices[0] != '' else 0
+    inicio = adjust_cut(text,  inicio)
+
+    fim = int(slices[1]) if slices[1] != '' else len(text)
+    if fim < len(text):
+        fim = adjust_cut(text,  fim)
+
+    return text[inicio:fim]
+
+
 def totalize_data(data, config):
     totrow = data[0].copy()
     for key in totrow:
