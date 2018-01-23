6b319332d27a9deeed637a929f9a294f410cba0b
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2018-01-23 10:47:44 -0200

    estágio como coluna na tabela

--- src/lotes/views/imprime_lotes.py
+++ src/lotes/views/imprime_lotes.py
@@ -69,7 +69,6 @@ class ImprimeLotes(LoginRequiredMixin, View):
             return context
 
         ref = l_data[0]['ref']
-        estagio = l_data[0]['est']
 
         # busca informação de OP mãe
         opi_data = models.op_inform(cursor, op)
@@ -109,11 +108,10 @@ class ImprimeLotes(LoginRequiredMixin, View):
             'qtd_lotes': qtd_lotes,
             'oc_inicial': oc_inicial,
             'oc_final': oc_final,
-            'estagio': estagio,
             'headers': ('Tamanho', 'Cor', 'Período', 'OC', '1º', 'Quant.',
-                        'Lote', 'Unidade'),
+                        'Lote', 'Estágio', 'Unidade'),
             'fields': ('tam', 'cor', 'periodo', 'oc', 'prim', 'qtd',
-                       'lote', 'descricao_divisao'),
+                       'lote', 'est', 'descricao_divisao'),
             'data': data,
         })
 
