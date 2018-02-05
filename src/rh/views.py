6ff31f2928fd24d0cc716b1575ef9f5b7f637b9e
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2018-02-05 10:41:52 -0200

    novo esquema de apresentação de dicas do RH

--- src/rh/views.py
+++ src/rh/views.py
@@ -1,8 +1,34 @@
+from datetime import datetime
+
 from django.shortcuts import render
 
 
+def limpa_data_futura(lista, campo):
+    for item in lista:
+        if item[campo] > datetime.now().date():
+            item[campo] = None
+
+
 def index(request):
-    context = {}
+    dicas = [
+      {
+        'data': datetime.strptime('07/02/2018', '%d/%m/%Y').date(),
+        'chamada': 'Movidos pela motivação',
+        'link': '/media/rh/DUOMO-05-02-Dica-da-Semana.jpg',
+      },
+      {
+        'data': datetime.strptime('05/02/2018', '%d/%m/%Y').date(),
+        'chamada': 'O quesito da harmonia',
+        'link': '/media/rh/DUOMO-05-02-Dica-da-Semana-segunda.jpg',
+      },
+      {
+        'data': datetime.strptime('02/02/2018', '%d/%m/%Y').date(),
+        'chamada': 'A Arte de se reinventar',
+        'link': '/media/rh/2018-02-02_Carnaval_Feliz_Sexta.pdf',
+      },
+    ]
+    limpa_data_futura(dicas, 'data')
+    context = {'dicas': dicas}
     return render(request, 'rh/index.html', context)
 
 
