423df4e9d2e2f76579c7ee5d6c66055541571368
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2018-02-07 16:56:12 -0200

    get observação

--- src/lotes/models/op.py
+++ src/lotes/models/op.py
@@ -81,6 +81,7 @@ def op_inform(cursor, op):
         , o.PEDIDO_VENDA PEDIDO
         , COALESCE(ped.COD_PED_CLIENTE, ' ') PED_CLIENTE
         , r.NUMERO_MOLDE MOLDE
+        , o.OBSERVACAO
         FROM PCPC_020 o
         JOIN PCPC_010 p
           ON p.AREA_PERIODO = 1
