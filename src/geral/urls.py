a27b1d25d276b3cd741319332b43904d3b1d9bf9
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2018-03-12 18:33:23 -0300

    POP page

--- src/geral/urls.py
+++ src/geral/urls.py
@@ -10,4 +10,6 @@ urlpatterns = [
         views.PainelView.as_view(), name='ger_painel'),
     url(r'^informativo/(?P<modulo>[^/]+)/(?P<id>[^/]+)?/?$',
         views.InformativoView.as_view(), name='ger_informativo_modulo'),
+    url(r'^pop/$', views.pop, name='pop'),
+    url(r'^pop/(?P<id>\d+)/?$', views.pop, name='pop_id'),
 ]
