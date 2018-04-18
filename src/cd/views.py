90b76913e707bef6db930d7041cb9a89e5c58a95
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2018-04-18 17:43:55 -0300

    evoluindo lote_local para primeira versão funcional

--- src/cd/views.py
+++ src/cd/views.py
@@ -1,10 +1,14 @@
+from pprint import pprint
+
 from django.shortcuts import render
 from django.db import connections
 from django.views import View
 
-from cd.forms import LoteForm
+from fo2.models import rows_to_dict_list_lower
 
 import lotes.models
+
+from cd.forms import LoteForm
 
 
 def index(request):
@@ -17,20 +21,73 @@ class LotelLocal(View):
     template_name = 'cd/lote_local.html'
     title_name = 'Bipa lote indicando local'
 
-    def mount_context(self, cursor, lote):
-        context = {'lote': lote}
-        periodo = lote[:4]
-        ordem_confeccao = lote[-5:]
+    def mount_context(self, cursor, form):
+        endereco = form.cleaned_data['endereco']
+        lote = form.cleaned_data['lote']
+        identificado = form.cleaned_data['identificado']
 
-        data = lotes.models.posicao_get_op(
-            cursor, periodo, ordem_confeccao)
-        if len(data) == 0:
+        context = {'endereco': endereco}
+
+        try:
+            lote_rec = lotes.models.Lote.objects.get(lote=lote)
+        except lotes.models.Lote.DoesNotExist:
             context.update({'erro': 'Lote não encontrado'})
             return context
+        context.update({
+            'op': lote_rec.op,
+            'referencia': lote_rec.referencia,
+            'cor': lote_rec.cor,
+            'tamanho': lote_rec.tamanho,
+            'qtd_produzir': lote_rec.qtd_produzir,
+            'local': lote_rec.local,
+            })
 
-        row = data[0]
-        op = row['OP']
-        context.update({'op': op})
+        # print('identificado={}'.format(identificado))
+        if identificado:
+            # print('if identificado')
+            form.data['identificado'] = None
+            form.data['lote'] = None
+            if lote != identificado:
+                context.update({
+                    'erro': 'A confirmação é bipando o mesmo lote. '
+                            'Identifique o lote novamente.'})
+                return context
+
+            try:
+                lote_rec = lotes.models.Lote.objects.get(lote=lote)
+            except lotes.models.Lote.DoesNotExist:
+                context.update({
+                    'erro': 'Lote não encontrado no banco de dados'})
+                return context
+
+            lote_rec.local = endereco
+            lote_rec.save()
+
+            context['identificado'] = identificado
+        else:
+            # print('if not identificado')
+            context['lote'] = lote
+            # periodo = lote[:4]
+            # ordem_confeccao = lote[-5:]
+
+            # data = lotes.models.posicao_get_op(
+            #     cursor, periodo, ordem_confeccao)
+            # if len(data) == 0:
+            #     context.update({'erro': 'Lote não encontrado'})
+            #     return context
+            # row = data[0]
+            # op = row['OP']
+
+            # print('lote_rec.local = "{}" endereco = "{}"'.format(
+            #     lote_rec.local, endereco))
+            if lote_rec.local != endereco:
+                context['confirma'] = True
+                form.data['identificado'] = form.data['lote']
+            form.data['lote'] = None
+
+        # lotes_rec = lotes.models.Lote.objects.get(local=endereco)
+        # data = rows_to_dict_list_lower(lotes_rec)
+        # pprint(data)
 
         return context
 
@@ -49,9 +106,9 @@ class LotelLocal(View):
         if 'lote' in kwargs:
             form.data['lote'] = kwargs['lote']
         if form.is_valid():
-            lote = form.cleaned_data['lote']
+            # pprint(request.POST)
             cursor = connections['so'].cursor()
-            data = self.mount_context(cursor, lote)
+            data = self.mount_context(cursor, form)
             context.update(data)
         context['form'] = form
         return render(request, self.template_name, context)
