3245c744234f61ed803a7f8e68a5f30da3a03bf9
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2018-04-27 10:42:57 -0300

    ligando tracking para Lote

--- src/lotes/signals.py
+++ src/lotes/signals.py
@@ -2,14 +2,17 @@ from django.db.models.signals import post_init, post_save, post_delete
 
 from fo2.signals import \
     post_init_tracking, post_save_tracking, post_delete_tracking
-from .models import ModeloTermica, ImpressoraTermica
+from .models import ModeloTermica, ImpressoraTermica, Lote
 
 
 post_init.connect(post_init_tracking, sender=ModeloTermica)
 post_init.connect(post_init_tracking, sender=ImpressoraTermica)
+post_init.connect(post_init_tracking, sender=Lote)
 
 post_save.connect(post_save_tracking, sender=ModeloTermica)
 post_save.connect(post_save_tracking, sender=ImpressoraTermica)
+post_save.connect(post_save_tracking, sender=Lote)
 
 post_delete.connect(post_delete_tracking, sender=ModeloTermica)
 post_delete.connect(post_delete_tracking, sender=ImpressoraTermica)
+post_delete.connect(post_delete_tracking, sender=Lote)
