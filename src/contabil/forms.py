8cec7218bbff39d269c797db22aa2290d5c23414
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2018-04-27 14:49:11 -0300

    mais filtros

--- src/contabil/forms.py
+++ src/contabil/forms.py
@@ -17,3 +17,33 @@ class RemessaIndustrForm(forms.Form):
         label='Data final', required=False,
         help_text='(Se não informar, assume igual a inicial.)',
         widget=forms.DateInput(attrs={'type': 'date'}))
+
+    faccao = forms.CharField(
+        label='Facção', required=False,
+        widget=forms.TextInput(attrs={'type': 'string'}))
+
+    cliente = forms.CharField(
+        label='Cliente', required=False,
+        widget=forms.TextInput(attrs={'type': 'string'}))
+
+    pedido = forms.CharField(
+        label='Pedido Tussor', required=False,
+        widget=forms.TextInput(attrs={'type': 'number'}))
+
+    pedido_cliente = forms.CharField(
+        label='Pedido de cliente', required=False,
+        widget=forms.TextInput(attrs={'type': 'string'}))
+
+    def clean_faccao(self):
+        faccao = self.cleaned_data['faccao'].upper()
+        data = self.data.copy()
+        data['faccao'] = faccao
+        self.data = data
+        return faccao
+
+    def clean_cliente(self):
+        cliente = self.cleaned_data['cliente'].upper()
+        data = self.data.copy()
+        data['cliente'] = cliente
+        self.data = data
+        return cliente
