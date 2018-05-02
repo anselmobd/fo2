71cdd74c0ae9b1daf1af870c0e33adde980fe799
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2018-05-02 15:24:30 -0300

    ordering

--- src/cd/urls.py
+++ src/cd/urls.py
@@ -10,6 +10,6 @@ urlpatterns = [
         views.Estoque.as_view(), name='cd_estoque_filtro'),
     url(r'^troca_local/$', views.TrocaLocal.as_view(), name='cd_troca_local'),
     url(r'^inconsist/$', views.Inconsistencias.as_view(), name='cd_inconsist'),
-    url(r'^inconsist/(?P<opini>\d+)/$', views.Inconsistencias.as_view(),
-        name='cd_inconsist_opini'),
+    url(r'^inconsist/(?P<ordem>.-?)/(?P<opini>\d+)/$',
+        views.Inconsistencias.as_view(), name='cd_inconsist_opini'),
 ]
