14eefb6153a73ded55e8f27d48ac1f31a24b2a36
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2018-05-03 11:32:16 -0300

    iniciado conferência iniciado e estudado mapa

--- src/cd/urls.py
+++ src/cd/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='cd'),
     url(r'^lote_local/$', views.LotelLocal.as_view(), name='cd_lote_local'),
+    url(r'^conferencia/$', views.Conferencia.as_view(), name='cd_conferencia'),
     url(r'^estoque/$', views.Estoque.as_view(), name='cd_estoque'),
     url(r'^estoque/(?P<ordem>.)/(?P<filtro>.+)/$',
         views.Estoque.as_view(), name='cd_estoque_filtro'),
@@ -12,4 +13,5 @@ urlpatterns = [
     url(r'^inconsist/$', views.Inconsistencias.as_view(), name='cd_inconsist'),
     url(r'^inconsist/(?P<ordem>.-?)/(?P<opini>\d+)/$',
         views.Inconsistencias.as_view(), name='cd_inconsist_opini'),
+    url(r'^mapa/$', views.Mapa.as_view(), name='cd_mapa'),
 ]
