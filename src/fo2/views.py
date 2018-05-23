72a9f75edb7a9dbaf3c67f3587bad73a5043875e
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2018-05-23 10:50:37 -0300

    test page

--- src/fo2/views.py
+++ src/fo2/views.py
@@ -1,6 +1,6 @@
 from django.views.generic import TemplateView
 from django.contrib.auth import logout
-from django.shortcuts import redirect
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
 
 from utils.functions import get_client_ip
@@ -8,6 +8,11 @@ from utils.functions import get_client_ip
 
 def index_view(request):
     return redirect('apoio_ao_erp')
+
+
+def test_view(request):
+    context = {}
+    return render(request, 'test.html', context)
 
 
 class ApoioAoErpView(TemplateView):
