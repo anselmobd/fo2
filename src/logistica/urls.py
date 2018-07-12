504cd39fa1e81392cca98b36c4cb922ab711fbfc
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2018-07-12 16:13:16 -0300

    namespace logistica

--- src/logistica/urls.py
+++ src/logistica/urls.py
@@ -3,9 +3,10 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='logistica'),
+    url(r'^$', views.index, name='index'),
+
     url(r'^notafiscal_rel/$',
         views.NotafiscalRel.as_view(), name='notafiscal_rel'),
     url(r'^notafiscal_rel/(?P<dia>\d+)/(?P<mes>\d+)/(?P<ano>\d+)/$',
-        views.NotafiscalRel.as_view(), name='notafiscal_rel_dt'),
+        views.NotafiscalRel.as_view(), name='notafiscal_rel__get'),
 ]
