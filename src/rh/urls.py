26e80c83b12d42989298d9b518c53b8d9e61c633
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2018-07-12 15:59:49 -0300

    namespace rh

--- src/rh/urls.py
+++ src/rh/urls.py
@@ -3,15 +3,22 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='rh'),
-    url(r'^campanhas/$', views.campanhas, name='rh_campanhas'),
-    url(r'^datas/$', views.datas, name='rh_datas'),
-    url(r'^fotos/$', views.fotos, name='rh_fotos'),
-    url(r'^videos/$', views.videos, name='rh_videos'),
+    url(r'^$', views.index, name='index'),
+
+    url(r'^campanhas/$', views.campanhas, name='campanhas'),
+
+    url(r'^datas/$', views.datas, name='datas'),
+
+    url(r'^fotos/$', views.fotos, name='fotos'),
+
+    url(r'^videos/$', views.videos, name='videos'),
+
     url(r'^fotos_homenagem20180614/$', views.fotos_homenagem20180614,
-        name='rh_fotos_homenagem20180614'),
+        name='fotos_homenagem20180614'),
+
     url(r'^fotos_brigadista2018/$', views.fotos_brigadista2018,
-        name='rh_fotos_brigadista2018'),
+        name='fotos_brigadista2018'),
+
     url(r'^videos_brigadista2018/$', views.videos_brigadista2018,
-        name='rh_videos_brigadista2018'),
+        name='videos_brigadista2018'),
 ]
