1a6f71b00f944b554de10dc9f8cc740eac59c854
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2018-07-19 17:42:32 -0300

    evoluindo - nivel, ref, cor, tam

--- src/insumo/models.py
+++ src/insumo/models.py
@@ -1479,15 +1479,31 @@ def insumos_cor_tamanho_usados(cursor):
     return rows_to_dict_list_lower(cursor)
 
 
-def compras_periodo_insumo(cursor, nivel, ref):
+def compras_periodo_insumo(cursor, nivel, ref, cor, tam):
     sql = """
         SELECT
           r.NIVEL_ESTRUTURA NIVEL
         , r.REFERENCIA REF
         , r.DESCR_REFERENCIA DESCR
-        FROM BASI_030 r
+        , t.TAMANHO_REF TAM
+        , t.DESCR_TAM_REFER DESCR_TAM
+        , tam.ORDEM_TAMANHO ORDEM_TAM
+        , c.ITEM_ESTRUTURA COR
+        , c.DESCRICAO_15 DESCR_COR
+        FROM BASI_030 r -- referencia
+        JOIN BASI_020 t -- tamanho
+          ON t.BASI030_NIVEL030 = r.NIVEL_ESTRUTURA
+         AND t.BASI030_REFERENC = r.REFERENCIA
+        LEFT JOIN BASI_220 tam -- cadastro de tamanhos
+          ON tam.TAMANHO_REF = t.TAMANHO_REF
+        JOIN BASI_010 c -- cor
+          ON c.NIVEL_ESTRUTURA = r.NIVEL_ESTRUTURA
+         AND c.GRUPO_ESTRUTURA = r.REFERENCIA
+         AND c.SUBGRU_ESTRUTURA = t.TAMANHO_REF
         WHERE r.NIVEL_ESTRUTURA = %s
           AND r.REFERENCIA = %s
+          AND c.ITEM_ESTRUTURA = %s
+          AND t.TAMANHO_REF = %s
     """
-    cursor.execute(sql, [nivel, ref])
+    cursor.execute(sql, [nivel, ref, cor, tam])
     return rows_to_dict_list_lower(cursor)
