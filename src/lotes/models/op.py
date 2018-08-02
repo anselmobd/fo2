c177a71930d921dff3fb4c84e598e8eb68af8b71
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2018-08-02 14:39:44 -0300

    criado op_tam_cor_qtd que busca tam, cor e quant. de produtos da OP, contados nos lotes

--- src/lotes/models/op.py
+++ src/lotes/models/op.py
@@ -630,3 +630,38 @@ def op_os_ref(cursor, op):
     """
     cursor.execute(sql, [op])
     return rows_to_dict_list(cursor)
+
+
+def op_tam_cor_qtd(cursor, op):
+    sql = """
+        SELECT
+          lote.PROCONF_SUBGRUPO TAM
+        , lote.PROCONF_ITEM COR
+        , sum(lote.QTDE_PECAS_PROG ) QTD
+        FROM PCPC_040 lote
+        LEFT JOIN BASI_220 tam
+          ON tam.TAMANHO_REF = lote.PROCONF_SUBGRUPO
+        WHERE lote.ORDEM_PRODUCAO = %s
+          AND lote.SEQUENCIA_ESTAGIO
+              = COALESCE(
+                  (
+                    SELECT
+                      MIN(ulote.SEQUENCIA_ESTAGIO)
+                    FROM PCPC_040 ulote
+                    WHERE ulote.ORDEM_PRODUCAO = lote.ORDEM_PRODUCAO
+                      AND ulote.ORDEM_CONFECCAO = lote.ORDEM_CONFECCAO
+                    GROUP BY
+                      ulote.ORDEM_PRODUCAO
+                    , ulote.ORDEM_CONFECCAO
+                  )
+                , 0)
+        GROUP BY
+          tam.ORDEM_TAMANHO
+        , lote.PROCONF_SUBGRUPO
+        , lote.PROCONF_ITEM
+        ORDER BY
+          tam.ORDEM_TAMANHO
+        , lote.PROCONF_ITEM
+    """
+    cursor.execute(sql, [op])
+    return rows_to_dict_list(cursor)
