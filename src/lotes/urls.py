6ebecfb23f7d3e5344c91e062a5fad6eb195a14c
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2018-08-16 09:12:16 -0300

    busca OP aceita ref no GET

--- src/lotes/urls.py
+++ src/lotes/urls.py
@@ -14,6 +14,9 @@ urlpatterns = [
     url(r'^responsavel/$', views.responsTodos, name='respons_todos'),
 
     url(r'^busca_op/$', views.BuscaOP.as_view(), name='busca_op'),
+    url(r'^busca_op/(?P<ref>\d+)/$', views.BuscaOP.as_view(),
+        name='busca_op__get'),
+
     url(r'^op/$', views.Op.as_view(), name='op'),
     url(r'^op/(?P<op>\d+)/$', views.Op.as_view(), name='op__get'),
 
