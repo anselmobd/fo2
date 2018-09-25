72d5b835f395f47840f86241262d8fbd65baeff2
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2018-09-25 09:53:59 -0300

    mostrando op, ref, tam e cor

--- src/lotes/models/distribuicao.py
+++ src/lotes/models/distribuicao.py
@@ -10,10 +10,18 @@ def distribuicao(cursor, estagio, data_de, data_ate, familia):
           TO_CHAR(h.DATA_INSERCAO, 'YYYY/MM/DD') DATA_SORT
         , TO_CHAR(h.DATA_INSERCAO, 'DD/MM/YYYY') DATA
         , h.CODIGO_FAMILIA FAMILIA
-        , count(DISTINCT h.ORDEM_PRODUCAO) OPS
+        , h.ORDEM_PRODUCAO OP
+        , l.PROCONF_GRUPO REF
+        , l.PROCONF_SUBGRUPO TAM
+        , l.PROCONF_ITEM COR
+        --, count(DISTINCT h.ORDEM_PRODUCAO) OPS
         , count(*) LOTES
         , sum(h.QTDE_PRODUZIDA) PECAS
         FROM PCPC_045 h
+        JOIN pcpc_040 l
+          ON l.PERIODO_PRODUCAO = h.PCPC040_PERCONF
+         AND l.ORDEM_CONFECCAO = h.PCPC040_ORDCONF
+         AND l.CODIGO_ESTAGIO = h.PCPC040_ESTCONF
         WHERE h.PCPC040_ESTCONF = %s
           AND TO_DATE(h.DATA_INSERCAO) >= %s
           AND TO_DATE(h.DATA_INSERCAO)-1 <= %s
@@ -22,9 +30,17 @@ def distribuicao(cursor, estagio, data_de, data_ate, familia):
           TO_CHAR(h.DATA_INSERCAO, 'YYYY/MM/DD')
         , TO_CHAR(h.DATA_INSERCAO, 'DD/MM/YYYY')
         , h.CODIGO_FAMILIA
+        , h.ORDEM_PRODUCAO
+        , l.PROCONF_GRUPO
+        , l.PROCONF_SUBGRUPO
+        , l.PROCONF_ITEM
         ORDER BY
           1
         , h.CODIGO_FAMILIA
+        , h.ORDEM_PRODUCAO
+        , l.PROCONF_GRUPO
+        , l.PROCONF_SUBGRUPO
+        , l.PROCONF_ITEM
     '''
     cursor.execute(sql, [estagio, data_de, data_ate, familia, familia])
     return rows_to_dict_list(cursor)
