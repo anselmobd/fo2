721a682b029933a25257243dcab3b73876b44b65
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2018-09-25 09:18:40 -0300

    model Familia direct from systextil data base

--- src/lotes/models/models.py
+++ src/lotes/models/models.py
@@ -3,6 +3,22 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+class Familia(models.Model):
+    divisao_producao = models.IntegerField(primary_key=True)
+    descricao = models.CharField(
+        max_length=20,
+        verbose_name='Descrição')
+
+    def __str__(self):
+        return '{}-{}'.format(self.divisao_producao, self.descricao)
+
+    class Meta:
+        managed = False
+        app_label = 'systextil'
+        db_table = "BASI_180"
+        verbose_name = "Família"
+
+
 class Impresso(models.Model):
     nome = models.CharField(
         max_length=100, null=True, blank=True,
