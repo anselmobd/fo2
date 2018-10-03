de9aa9499a25fcccc3449c60380e3392e80f0ecc
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2018-10-03 11:28:10 -0300

    mostrando lista de aniversariantes do banco de daods Persona com adicionais

--- src/rh/views.py
+++ src/rh/views.py
@@ -1,9 +1,12 @@
 from pprint import pprint
 from datetime import datetime
 
+from django.db import connections
 from django.shortcuts import render
 
 from utils.functions import inc_month
+
+import rh.models
 
 
 def limpa_data_futura(lista, campo):
@@ -179,148 +182,34 @@ def aniversariantes(request, ano, mes):
     datames = datetime(intano, intmes, 1)
 
     aniversariantes = [
-        ['03/08', 'Simone da Silva Saraiva', 'Recursos Humanos'],
-        ['03/08', 'Veronica Cordeiro Bezerra dos Santos', 'Tecelagem'],
-        ['06/08', 'Carlos Alberto de Sousa Carvalho Junior', 'Expedição'],
-        ['06/08', 'Jonas Martins', 'Tecelagem'],
-        ['07/08', 'Jorge Marcelo de Almeida Gargaglione', 'Vendas'],
-        ['07/08', 'Pedro José Nascimento de Oliveira', 'Jovem Aprendiz'],
-        ['09/08', 'Luzia Vieira da Silva Araujo', 'Tecelagem'],
-        ['09/08', 'Sonia Maria do Nascimento Reis', 'Producao'],
-        ['10/08', 'Maria Aparecida Tiburcio', 'Tecelagem'],
-        ['11/08', 'Tamires Fernanda dos Santos Marins', 'Expedição'],
-        ['12/08', 'Douglas dos Santos Alves', 'Facilites'],
-        ['13/08', 'Fabiana Abreu de Jesus', 'Tecelagem'],
-        ['14/08', 'Adriana Maria de Souza Monteiro', 'Pcp'],
-        ['15/08', 'Andreza Silva de Brito', 'Embalagem/Tecelagem'],
-        ['15/08', 'Claudia Teixeira da Silva Avelino',
-            'Desenvolvimento de Produtos'],
-        ['17/08', 'Leiliane Maria da Silva', 'Embalagem/Tecelagem'],
-        ['17/08', 'Regina Maria Sandra dos Santos', 'Tecelagem'],
-        ['18/08', 'Patricia de Oliveira', 'Embalagem/Produção'],
-        ['18/08', 'Valquiria Evangelista da Silva', 'Financeiro'],
-        ['19/08', 'Erika Cristina Arigony Antonio', 'Tecelagem'],
-        ['20/08', 'Fabiana Vasques Gonçalves', 'Expedição'],
-        ['21/08', 'Fabiola Segundo da Motta Teixeira', 'Qualidade'],
-        ['21/08', 'Maria da Gloria Miranda da Cruz', 'Producao'],
-        ['25/08', 'Lorraine Cristine Martins de Andrade', 'Recursos Humanos'],
-        ['26/08', 'Suelen Claudino Vicente', 'Tecelagem'],
         ['27/08', 'Arnaldo Silva da Costa', 'Logística'],
-        ['27/08', 'Milena Thuane da Silva Conceição', 'Qualidade'],
-        ['27/08', 'Veronica Cristina de Carvalho Soares',
-            'Embalagem/Tecelagem'],
-        ['28/08', 'Maria Lucia de Jesus Alves', 'Producao'],
-        ['28/08', 'Roberta Gomes da Silva', 'Tecelagem'],
-        ['30/08', 'Afonso Henriques de Magalhães Pereira',
-            'Desenvolvimento de Produtos'],
-        ['01/09', 'Viviane Pecanha Oliveira ', 'Assist de Expedição'],
-        ['02/09', 'Rosilene Moura dos Santos ', 'Lider de Tecelagem'],
-        ['03/09', 'Simone Lima da Cruz ', 'Costureira'],
-        ['05/09', 'Sandra Duarte Tavares Reis Pinto ', 'Costureira'],
-        ['05/09', 'Zilda Martins Costa ', 'Costureira'],
-        ['07/09', 'Lucilene Santos de Lima ', 'Costureira'],
-        ['09/09', 'Diéssica da Silva Manso ', 'Auxiliar Producao'],
-        ['10/09', 'Tania Mara da Silva do Nascimento ', 'Costureira'],
-        ['12/09', 'Alessandro dos Santos Duarte ', 'Assist. de Produção'],
-        ['12/09', 'David Cosme Barrozo ', 'Assist. de Produção'],
-        ['12/09', 'Ruth Barbosa de Lacerda ', 'Costureira'],
-        ['13/09', 'Valeria Cristina da Silva Cunha ', 'Costureira'],
-        ['14/09', 'Leonardo Andrade de Paula ', 'Assist. de Produção'],
-        ['16/09', 'Fabiana Lacerda Jesuino ', 'Assist. de Produção'],
-        ['16/09', 'Neuzeane Brando Carvalho ', 'Costureira'],
-        ['16/09', 'Érica Leandro da Silva ', 'Assist. de Produção'],
-        ['17/09', 'Cassio Flor Pereira ', 'Assist. de Produção'],
-        ['18/09', 'Jucelino Cupertino Avelino ', 'Assist. de Manut. Predial'],
-        ['20/09', 'Edinete dos Anjos da Silva ', 'Costureira'],
-        ['20/09', 'Tania Regina Gomes da Silva ', 'Costureira'],
-        ['20/09', 'Vanessa Cristina Moura dos Santos ', 'Costureira'],
-        ['21/09', 'Bruna Vieira Coelho ', 'Assist. de Produção'],
-        ['21/09',
-         'Carlos Roberto de Souza da Silva ', 'Téc de Manutenção Eletrônica'],
-        ['22/09', 'Ohana Karla da Silva Vinhas ', 'Assistente Financeiro'],
-        ['23/09', 'Adriana da Silva Almirante ', 'Assist. de Produção'],
-        ['25/09', 'Silvia Maria Duarte de Souza ', 'Costureira'],
-        ['26/09', 'Giselle Rodrigues dos Santos ', 'Analista  de Produto'],
-        ['26/09', 'Maristela Constantino da Silva Duque ', 'Costureira'],
-        ['26/09',
-         'Vanessa Fernanda Rodrigues de Miranda do Couto Bandeira ',
-         'Lider de Facilites'],
-        ['27/09', 'Bersange Galdino da Silva ', 'Gerente Industrial'],
-        ['27/09', 'Fábio Fernandes de Oliveira ', 'Analista Contábil'],
-        ['27/09', 'Raquel Damiana Santanna Ferreira ', 'Encarregada'],
-        ['28/09', 'Brenna dos Santos Queiroz Bueno ', 'Auxiliar Producao'],
-        ['28/09', 'Flavia da Silva Queiroz Bueno ', 'Assist. de Produção'],
-        ['29/09', 'Aline Machado Borges ', 'Aux. de Serv. Gerais'],
-        ['29/09', 'Fabiana Cristina da Silva ', 'Costureira'],
-        ['30/09', 'Valesca Nunes de Freitas ', 'Assist. de Produção'],
-
-        ['01/10', 'Terezinha Rozana Menezes Braga', 'Revisor de Qualidade'],
-        ['03/10', 'Adriana Martins de Andrade', 'Supervisora'],
-        ['03/10', 'Anselmo Blanco Dominguez', 'Gerente de TI'],
-        ['05/10',
-            'Leandro de Souza Bezerra', 'Operador de Maquina de Tecelagem'],
-        ['06/10',
-            'Victória Nascimento Gomes',
-            'Aprendiz Aux. Operação de Logistica'],
-        ['07/10', 'Lucas Lima Braga', 'Assist. de Produção'],
-        ['07/10', 'Monique Carla Pereira Silva', 'Costureira'],
-        ['08/10', 'Deivid Duarte Tavares Reis', 'Assist. de Produção'],
-        ['10/10', 'Felipe dos Santos Maria', 'Assist. de Produção'],
-        ['10/10', 'Joziane Rosa', 'Assist. de Produção'],
-        ['11/10', 'Bianca Vasques Gonçalves', 'Assist Adm Industrial'],
-        ['11/10', 'Thaiane Araujo Silva', 'Auxiliar Producao'],
-        ['12/10',
-            'Isaque Rodrigo dos Santos Miranda', 'Mecanico de Manutenção'],
-        ['13/10', 'Luciane Beatriz Fabri', 'Costureira'],
-        ['13/10', 'Vera Lucia Souza dos Santos', 'Costureira'],
-        ['14/10', 'Shirley de Mello Nogueira', 'Auxiliar Contabil'],
-        ['16/10',
-            'Erivelto Alves de Melo', 'Operador de Maquina de Tecelagem'],
-        ['17/10',
-            'Stephanie Melo Chinaider de Oliveira', 'Assist. de Produção'],
-        ['18/10', 'Ana Paula Pessoa de Melo', 'Faturista'],
-        ['20/10', 'Valmir Lemos', 'Operador de Maquina de Tecelagem'],
-        ['21/10',
-            'Jonatas Rocha Ferreira', 'Operador de Maquina de Tecelagem'],
-        ['21/10', 'Kelly Cristina Santos Souza', 'Assist. de Produção'],
-        ['22/10', 'Deise Oliveira da Silva', 'Costureira'],
-        ['22/10', 'Denise Bento dos Santos', 'Costureira'],
-        ['22/10',
-            'Rafaela Barroso de Carvalho',
-            'Jovem Aprendiz Oper.De Sup. Tecnico Ti'],
-        ['24/10',
-            'Elisangela Cristiane de Jesus Santos', 'Assist. de Produção'],
-        ['24/10', 'Sandra Regina Guimaraes dos Santos', 'Costureira'],
-        ['26/10',
-            'Vanessa da Silva de Brito Monteiro', 'Revisor de Qualidade'],
-        ['28/10', 'Jeane Silva de Andrade', 'Auxiliar Producao'],
-        ['28/10', 'Tatiana Pereira da Costa', 'Costureira'],
-        ['29/10', 'Luiz Claudio da Silva Vianna', 'Lider de Pcp'],
-        ['30/10', 'Deise Lucia Araujo de Oliveira', 'Costureira'],
-        ['30/10', 'Liliane Soares Jose Maria', 'Conferente de Qualidade'],
+        ['03/10', 'Anselmo Blanco Dominguez', 'TI'],
     ]
 
     anteriormes = inc_month(datames, -1)
     intmesant = anteriormes.month
-    mesant = '{:02}'.format(intmesant)
-    temmesant = False
-    for pessoa in aniversariantes:
-        if pessoa[0][-2:] == mesant:
-            temmesant = True
-            break
-    if not temmesant:
-        intmesant = None
 
     posteriosmes = inc_month(datames, 1)
     intmespos = posteriosmes.month
-    mespos = '{:02}'.format(intmespos)
-    temmespos = False
-    for pessoa in aniversariantes:
-        if pessoa[0][-2:] == mespos:
-            temmespos = True
-            break
-    if not temmespos:
-        intmespos = None
+
+    pcursor = connections['persona'].cursor()
+    data = rh.models.aniversariantes(pcursor, intmes)
+
+    aniver_db = []
+    for row in data:
+        aniver_db.append(
+           ['{:02d}/{:02d}'.format(int(row['dia']), int(row['mes'])),
+            row['nome'], row['setor']],
+        )
+
+    insere_pessoa = [
+        pessoa for pessoa in aniversariantes if pessoa[0][-2:] == mes]
+
+    for pessoa in insere_pessoa:
+        for i, row in enumerate(aniver_db):
+            if row[0] >= pessoa[0] and row[1] > pessoa[1]:
+                aniver_db.insert(i, pessoa)
+                break
 
     context = {
         'datames': datames,
@@ -328,8 +217,6 @@ def aniversariantes(request, ano, mes):
         'intanoant': anteriormes.year,
         'intmespos': intmespos,
         'intanopos': posteriosmes.year,
-        'aniversariantes': [
-            pessoa for pessoa in aniversariantes
-            if pessoa[0][-2:] == mes],
+        'aniversariantes': aniver_db,
         }
     return render(request, 'rh/aniversariantes.html', context)
