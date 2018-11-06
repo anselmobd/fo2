9a8d867a45f0c1c60e775b81d5618ccad11058d8
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2018-11-06 10:58:11 -0200

    model op_perda recebe data_de, data_ate e detalhe

--- src/lotes/models/op.py
+++ src/lotes/models/op.py
@@ -708,31 +708,50 @@ def op_conserto(cursor):
     return rows_to_dict_list(cursor)
 
 
-def op_perda(cursor):
+def op_perda(cursor, data_de, data_ate, detalhe):
     sql = """
         SELECT
           lote.PROCONF_GRUPO REF
-        , lote.PROCONF_ITEM COR
-        , lote.PROCONF_SUBGRUPO TAM
+    """
+    if detalhe == 'c':
+        sql += """
+            , lote.PROCONF_ITEM COR
+            , lote.PROCONF_SUBGRUPO TAM
+        """
+    sql += """
         , lote.ORDEM_PRODUCAO OP
         , sum(lote.QTDE_PERDAS ) QTD
         FROM PCPC_040 lote
+        JOIN PCPC_020 o
+          ON o.ORDEM_PRODUCAO = lote.ORDEM_PRODUCAO
         LEFT JOIN BASI_220 tam
           ON tam.TAMANHO_REF = lote.PROCONF_SUBGRUPO
+        WHERE o.DATA_ENTRADA_CORTE >= %s
+          AND o.DATA_ENTRADA_CORTE <= %s
         GROUP BY
           lote.PROCONF_GRUPO
-        , lote.PROCONF_ITEM
-        , tam.ORDEM_TAMANHO
-        , lote.PROCONF_SUBGRUPO
+    """
+    if detalhe == 'c':
+        sql += """
+            , lote.PROCONF_ITEM
+            , tam.ORDEM_TAMANHO
+            , lote.PROCONF_SUBGRUPO
+        """
+    sql += """
         , lote.ORDEM_PRODUCAO
         HAVING
           sum(lote.QTDE_PERDAS ) > 0
         ORDER BY
           lote.PROCONF_GRUPO
-        , lote.PROCONF_ITEM
-        , tam.ORDEM_TAMANHO
-        , lote.PROCONF_SUBGRUPO
+    """
+    if detalhe == 'c':
+        sql += """
+            , lote.PROCONF_ITEM
+            , tam.ORDEM_TAMANHO
+            , lote.PROCONF_SUBGRUPO
+        """
+    sql += """
         , lote.ORDEM_PRODUCAO
     """
-    cursor.execute(sql)
+    cursor.execute(sql, [data_de, data_ate])
     return rows_to_dict_list(cursor)
