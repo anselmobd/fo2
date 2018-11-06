49847d813aae7bd1384b25c2a6f837dec836ff3e
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2018-11-06 18:50:55 -0200

    criando tabelas para armazenar sugestões de compra

--- src/insumo/models.py
+++ src/insumo/models.py
@@ -45,3 +45,47 @@ class Periodo(models.Model):
         app_label = 'systextil'
         db_table = "PCPC_010"
         verbose_name = "Período"
+
+
+class SugestaoCompra(models.Model):
+    nivel = models.CharField(
+        db_index=True, max_length=1,
+        verbose_name='Nível')
+    referencia = models.CharField(
+        db_index=True, max_length=5,
+        verbose_name='Referência')
+    tamanho = models.CharField(
+        db_index=True, max_length=3,
+        verbose_name='Tamanho')
+    ordem_tamanho = models.IntegerField(
+        db_index=True, default=0,
+        verbose_name='ordem tamanho')
+    cor = models.CharField(
+        db_index=True, max_length=6,
+        verbose_name='Cor')
+    data = models.DateTimeField(
+        db_index=True,
+        verbose_name='Data do calculo')
+
+    class Meta:
+        db_table = "fo2_sugestao_compra"
+        verbose_name = "Sugestão de compra"
+
+
+class SugestaoCompraDatas(models.Model):
+    sugestao = models.ForeignKey(
+        SugestaoCompra, on_delete=models.CASCADE,
+        db_index=True,
+        verbose_name='Sugestão de compra')
+    data_compra = models.DateField(
+        db_index=True,
+        verbose_name='Data de compra')
+    data_recepcao = models.DateField(
+        verbose_name='Data de recepção')
+    qtd = models.DecimalField(
+        max_digits=13, decimal_places=3,
+        verbose_name='Quantidade')
+
+    class Meta:
+        db_table = "fo2_sugestao_compra_datas"
+        verbose_name = "Datas da sugestão de compra"
