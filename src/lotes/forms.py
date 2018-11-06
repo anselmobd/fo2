9173a2892333d14ca81c189778caae8e9d3eb1a4
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2018-11-06 10:47:45 -0200

    create OpPerdaForm com filtro de data de corte e seleção e visualização de cor e tamanho

--- src/lotes/forms.py
+++ src/lotes/forms.py
@@ -1,3 +1,5 @@
+from datetime import datetime, timedelta
+
 from django import forms
 
 from produto.models import Colecao
@@ -376,3 +378,41 @@ class ExpedicaoForm(forms.Form):
             self._errors['cliente'] = self.error_class(list_msg)
 
         return clean_ef
+
+
+class OpPerdaForm(forms.Form):
+    data_de = forms.DateField(
+        label='Data do Corte: De', required=False,
+        help_text='Padrão: Hoje',
+        widget=forms.DateInput(attrs={
+            'type': 'date',
+            'value': datetime.now().date()}))
+    data_ate = forms.DateField(
+        label='Até', required=False,
+        help_text='Padrão: Período de um mês',
+        widget=forms.DateInput(attrs={'type': 'date'}))
+
+    CHOICES = [('r', 'Por referência'),
+               ('c', 'Por referência-cor-tamanho')]
+    detalhe = forms.ChoiceField(
+        label='Detalhe', choices=CHOICES, initial='c')
+
+    def clean_data_de(self):
+        data_de = self.cleaned_data['data_de']
+        if data_de is None:
+            data_de = datetime.now().date()
+            data = self.data.copy()
+            data['data_de'] = data_de
+            self.data = data
+        return data_de
+
+    def clean_data_ate(self):
+        data_ate = self.cleaned_data['data_ate']
+        if data_ate is None:
+            data_ate = self.cleaned_data['data_de']
+            data_ate = data_ate.replace(
+                month=data_ate.month % 12 + 1)-timedelta(days=1)
+            data = self.data.copy()
+            data['data_ate'] = data_ate
+            self.data = data
+        return data_ate
