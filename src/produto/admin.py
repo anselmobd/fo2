589a23cd7fc368c8d2b7b9b59d778857ea689bd2
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2018-11-22 12:40:55 -0200

    produto admin

--- src/produto/admin.py
+++ src/produto/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 from fo2.admin import intr_adm_site
 
-from .models import GtinRange
+from .models import GtinRange, Produto
 
 
 class GtinRangeAdmin(admin.ModelAdmin):
@@ -11,4 +11,10 @@ class GtinRangeAdmin(admin.ModelAdmin):
     fields = ['ordem', 'pais', 'codigo']
 
 
+class ProdutoAdmin(admin.ModelAdmin):
+    search_fields = ['referencia']
+    ordering = ['referencia']
+
+
 intr_adm_site.register(GtinRange, GtinRangeAdmin)
+intr_adm_site.register(Produto, ProdutoAdmin)
