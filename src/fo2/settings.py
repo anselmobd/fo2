19365d24222b4c05d2666013468d8845c0ac611d
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2018-11-22 15:56:17 -0200

    add ProdutoCorAdmin and test nested_inline

--- src/fo2/settings.py
+++ src/fo2/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.humanize',
     'django_tables2',
+    # 'nested_inline',
     # Project
     'logistica',
     'lotes',
--- src/produto/admin.py
+++ src/produto/admin.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
+# from nested_inline.admin import NestedStackedInline, NestedModelAdmin
 
 from fo2.admin import intr_adm_site
 
-from .models import GtinRange, Produto
+from .models import GtinRange, Produto, ProdutoCor
 from .forms import ProdutoForm
+
+
+# class ProdutoCorInline(NestedStackedInline):
+#     model = ProdutoCor
+#     extra = 0
+#     fields = ['cor']
 
 
 class GtinRangeAdmin(admin.ModelAdmin):
@@ -12,11 +19,18 @@ class GtinRangeAdmin(admin.ModelAdmin):
     fields = ['ordem', 'pais', 'codigo']
 
 
+# class ProdutoAdmin(NestedModelAdmin):
 class ProdutoAdmin(admin.ModelAdmin):
     form = ProdutoForm
+    # inlines = [ProdutoCorInline]
     search_fields = ['referencia']
     ordering = ['referencia']
 
 
+class ProdutoCorAdmin(admin.ModelAdmin):
+    search_fields = ['produto__referencia', 'cor']
+
+
 intr_adm_site.register(GtinRange, GtinRangeAdmin)
 intr_adm_site.register(Produto, ProdutoAdmin)
+intr_adm_site.register(ProdutoCor, ProdutoCorAdmin)
