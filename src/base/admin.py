e589e2c1bd130b21c32f38865a7e63c137b1d2fb
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2018-11-26 14:53:31 -0200

    TipoImagem admin

--- src/base/admin.py
+++ src/base/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from fo2.admin import intr_adm_site
+
+from .models import TipoImagem
+
+
+class TipoImagemAdmin(admin.ModelAdmin):
+    fields = ["nome", "slug", "descricao"]
+    readonly_fields = ['slug']
+
+
+intr_adm_site.register(TipoImagem, TipoImagemAdmin)
