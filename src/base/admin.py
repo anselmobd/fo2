0863e697abd5421f844a50700b4fe5a60eb2ec03
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2018-11-26 15:51:47 -0200

    class ImagemAdmin

--- src/base/admin.py
+++ src/base/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 from fo2.admin import intr_adm_site
 
-from .models import TipoImagem
+from .models import TipoImagem, Imagem
 
 
 class TipoImagemAdmin(admin.ModelAdmin):
@@ -10,4 +10,11 @@ class TipoImagemAdmin(admin.ModelAdmin):
     readonly_fields = ['slug']
 
 
+class ImagemAdmin(admin.ModelAdmin):
+    list_display = ["tipo_imagem", "descricao", "slug", "imagem"]
+    fields = ["tipo_imagem", "descricao", "slug", "imagem"]
+    readonly_fields = ['slug']
+
+
 intr_adm_site.register(TipoImagem, TipoImagemAdmin)
+intr_adm_site.register(Imagem, ImagemAdmin)
