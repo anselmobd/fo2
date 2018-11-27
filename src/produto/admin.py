8d85abd5a2475a939c70a976f77439a4293b63b9
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2018-11-27 15:25:54 -0200

    class Composicao

--- src/produto/admin.py
+++ src/produto/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 
 from fo2.admin import intr_adm_site
 
-from .models import GtinRange, Produto, ProdutoCor, ProdutoTamanho
+from .models import GtinRange, Produto, ProdutoCor, ProdutoTamanho, Composicao
 from .forms import ProdutoForm
 
 
@@ -35,7 +35,12 @@ class ProdutoTamanhoAdmin(admin.ModelAdmin):
     search_fields = ['produto__referencia', 'tamanho']
 
 
+class ComposicaoAdmin(admin.ModelAdmin):
+    ordering = ['descricao']
+
+
 intr_adm_site.register(GtinRange, GtinRangeAdmin)
 intr_adm_site.register(Produto, ProdutoAdmin)
 intr_adm_site.register(ProdutoCor, ProdutoCorAdmin)
 intr_adm_site.register(ProdutoTamanho, ProdutoTamanhoAdmin)
+intr_adm_site.register(Composicao, ComposicaoAdmin)
