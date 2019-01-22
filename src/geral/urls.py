a8d7b86e7eed771b5bc87a5d6d8bc25df10b461f
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2019-01-22 20:36:18 -0200

    mend index name

--- src/geral/urls.py
+++ src/geral/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='geral'),
+    url(r'^$', views.index, name='index'),
     url(r'^deposito/$', views.deposito, name='deposito'),
     url(r'^estagio/$', views.estagio, name='estagio'),
     url(r'^painel/(?P<painel>[^/]*)/?$',
