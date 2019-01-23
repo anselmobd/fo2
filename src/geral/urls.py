9e49dfd5ef4aacb25715ddbec19040b2921d32eb
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2019-01-23 12:02:02 -0200

    corrigindo namespace e pop

--- src/geral/urls.py
+++ src/geral/urls.py
@@ -2,14 +2,19 @@ from django.conf.urls import url
 
 from . import views
 
+
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+
     url(r'^deposito/$', views.deposito, name='deposito'),
+
     url(r'^estagio/$', views.estagio, name='estagio'),
+
     url(r'^painel/(?P<painel>[^/]*)/?$',
-        views.PainelView.as_view(), name='ger_painel'),
+        views.PainelView.as_view(), name='painel'),
+
     url(r'^informativo/(?P<modulo>[^/]+)/(?P<id>[^/]+)?/?$',
-        views.InformativoView.as_view(), name='ger_informativo_modulo'),
-    url(r'^pop/$', views.pop, name='pop'),
-    url(r'^pop/(?P<id>\d+)/?$', views.pop, name='pop_id'),
+        views.InformativoView.as_view(), name='informativo_modulo'),
+
+    url(r'^pop/(?P<pop_assunto>[^/]+)/(?P<id>\d+)?$', views.pop, name='pop'),
 ]
