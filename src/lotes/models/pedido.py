6840c9de0cd687122b0be295976f2bd9db895362
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2019-02-05 18:21:11 -0200

    model to get quantidade por depósito de pedido

--- src/lotes/models/pedido.py
+++ src/lotes/models/pedido.py
@@ -256,3 +256,20 @@ def ped_expedicao(
     )
     cursor.execute(sql)
     return rows_to_dict_list(cursor)
+
+
+def ped_dep_qtd(cursor, pedido):
+    # quantidade por depósito
+    sql = """
+        SELECT
+          i.CODIGO_DEPOSITO DEPOSITO
+        , sum(i.QTDE_PEDIDA) QTD
+        FROM PEDI_110 i
+        WHERE i.PEDIDO_VENDA = %s
+        GROUP BY
+          i.CODIGO_DEPOSITO
+        ORDER BY
+          i.CODIGO_DEPOSITO
+    """
+    cursor.execute(sql, [pedido])
+    return rows_to_dict_list(cursor)
