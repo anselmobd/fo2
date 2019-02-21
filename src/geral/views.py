e3c708d43a29b9d8a4cb66cd202556afee2aa6a9
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2019-02-21 18:12:44 -0300

    completando dict_colecao_fluxos

--- src/geral/views.py
+++ src/geral/views.py
@@ -1307,23 +1307,63 @@ def dict_fluxo(id):
 
 def dict_colecao_fluxos(colecao, tipo, ref):
     cf = {
-        1: {
+        (1, 2, 3, 4, 13, 15, ): {
             'pa': {'': [1, 2, 3, 51]},
             'pg': {'': [1, 2, 3, 51]},
             'pb': {'': [1, 2, 3, 51]},
             'md': {'M': [1, 51],
                    'C': [2],
                    'R': [3],
-                   }
+                   },
+        },
+        (5, ): {
+            'md': {'M': [51],
+                   'C': [6],
+                   },
+        },
+        (6, ): {
+            'md': {'M': [51]}
+        },
+        (7, ): {
+            'pa': {'': [7, 51]},
+            'pg': {'': [7, 51]},
+        },
+        (8, ): {
+            'pa': {'': [5, '51p']},
+            'pg': {'': [5]},
+            'md': {'M': ['51p'],
+                   'C': [5],
+                   'F': [8],
+                   },
+        },
+        (9, 10, 11, 12, 16, 17, ): {
+            'pa': {'': [4]},
+            'pg': {'': [4]},
+            'pb': {'': [4]},
+            'md': {'': [4]},
+        },
+        (18, ): {
+            'pa': {'': ['1p', 5, '51p']},
+            'pg': {'': ['1p', 5]},
+            'md': {'M': ['1p', '51p'],
+                   'C': [5],
+                   },
         },
     }
+
     inicio = ''
     if tipo == 'md':
         inicio = ref[0]
         if inicio < 'C':
             inicio = ''
 
-    return cf[colecao][tipo][inicio]
+    for col_tuple in cf:
+        if colecao in col_tuple:
+            col_id = col_tuple
+            break
+    col_dict = cf[col_id]
+
+    return col_dict[tipo][inicio]
 
 
 def gera_fluxo_dot(request, destino, id):
