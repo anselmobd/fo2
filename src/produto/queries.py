47153ab36b55101e64290522481317ee0bbce257
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2019-03-27 17:38:10 -0300

    primeira versão funcional do custo de produto

--- src/produto/queries.py
+++ src/produto/queries.py
@@ -1115,22 +1115,123 @@ def por_cliente(cursor, cliente=None):
     return rows_to_dict_list(cursor)
 
 
-def ref_custo(cursor, ref=None):
+def ref_custo(cursor, ref, tam, cor, alt):
     filtra_ref = ''
     if ref != '':
         filtra_ref = '''--
-          AND r.REFERENCIA = '{}' '''.format(ref)
+          AND i.GRUPO_ESTRUTURA = '{}' '''.format(ref)
+
+    filtra_tam = ''
+    if tam != '':
+        filtra_tam = '''--
+          AND i.SUBGRU_ESTRUTURA = '{}' '''.format(tam)
+
+    filtra_cor = ''
+    if cor != '':
+        filtra_cor = '''--
+          AND i.ITEM_ESTRUTURA = '{}' '''.format(cor)
+
+    filtra_alt = ''
+    if alt != '':
+        filtra_alt = '''--
+          AND e.ALTERNATIVA_ITEM = '{}' '''.format(alt)
 
     sql = """
         SELECT
-          r.REFERENCIA REF
-        FROM BASI_030 r
-        WHERE r.NIVEL_ESTRUTURA = 1
+          e.SEQUENCIA SEQ
+        , e.NIVEL_COMP NIVEL
+        , e.GRUPO_COMP REF
+        , CASE WHEN e.SUB_COMP = '000'
+          THEN cot.SUB_COMP
+          ELSE e.SUB_COMP
+          END TAM
+        , CASE WHEN e.ITEM_COMP = '000000'
+          THEN coc.ITEM_COMP
+          ELSE e.ITEM_COMP
+          END COR
+        , icomp.NARRATIVA DESCR
+        , e.ALTERNATIVA_COMP ALT
+        , e.CONSUMO
+        , icomp.PRECO_CUSTO_INFO PRECO
+        , e.CONSUMO * icomp.PRECO_CUSTO_INFO CUSTO
+        FROM BASI_010 i
+        JOIN BASI_050 e
+          ON e.NIVEL_ITEM = i.NIVEL_ESTRUTURA
+         AND e.GRUPO_ITEM = i.GRUPO_ESTRUTURA
+        LEFT JOIN BASI_040 coc -- combinação cor
+          ON e.ITEM_COMP = '000000'
+         AND coc.NIVEL_ITEM = e.NIVEL_ITEM
+         AND coc.GRUPO_ITEM = e.GRUPO_ITEM
+         AND coc.ALTERNATIVA_ITEM = e.ALTERNATIVA_ITEM
+         AND coc.SEQUENCIA = e.SEQUENCIA
+         AND coc.SUB_ITEM = '000'
+         AND coc.ITEM_ITEM = i.ITEM_ESTRUTURA
+        LEFT JOIN BASI_040 cot -- combinação tamanho
+          ON e.SUB_COMP = '000'
+         AND cot.NIVEL_ITEM = e.NIVEL_ITEM
+         AND cot.GRUPO_ITEM = e.GRUPO_ITEM
+         AND cot.ALTERNATIVA_ITEM = e.ALTERNATIVA_ITEM
+         AND cot.SEQUENCIA = e.SEQUENCIA
+         AND cot.ITEM_ITEM = '000000'
+         AND cot.SUB_ITEM = i.SUBGRU_ESTRUTURA
+        JOIN BASI_010 icomp
+          ON icomp.NIVEL_ESTRUTURA = e.NIVEL_COMP
+         AND icomp.GRUPO_ESTRUTURA = e.GRUPO_COMP
+         AND icomp.SUBGRU_ESTRUTURA =
+              CASE WHEN e.SUB_COMP = '000'
+              THEN cot.SUB_COMP
+              ELSE e.SUB_COMP
+              END
+         AND icomp.ITEM_ESTRUTURA =
+              CASE WHEN e.ITEM_COMP = '000000'
+              THEN coc.ITEM_COMP
+              ELSE e.ITEM_COMP
+              END
+        WHERE i.NIVEL_ESTRUTURA = 1
           {filtra_ref} -- filtra_ref
+          {filtra_tam} -- filtra_tam
+          {filtra_cor} -- filtra_cor
+          {filtra_alt} -- filtra_alt
         ORDER BY
-          r.REFERENCIA
+          e.SEQUENCIA
     """.format(
         filtra_ref=filtra_ref,
+        filtra_tam=filtra_tam,
+        filtra_cor=filtra_cor,
+        filtra_alt=filtra_alt,
+    )
+    cursor.execute(sql)
+    return rows_to_dict_list(cursor)
+
+
+def item_narrativa(cursor, ref, tam, cor):
+    filtra_ref = ''
+    if ref != '':
+        filtra_ref = '''--
+          AND i.GRUPO_ESTRUTURA = '{}' '''.format(ref)
+
+    filtra_tam = ''
+    if tam != '':
+        filtra_tam = '''--
+          AND i.SUBGRU_ESTRUTURA = '{}' '''.format(tam)
+
+    filtra_cor = ''
+    if cor != '':
+        filtra_cor = '''--
+          AND i.ITEM_ESTRUTURA = '{}' '''.format(cor)
+
+    sql = """
+        SELECT
+          i.NARRATIVA
+        FROM BASI_010 i
+        WHERE i.NIVEL_ESTRUTURA = 1
+          {filtra_ref} -- filtra_ref
+          {filtra_tam} -- filtra_tam
+          {filtra_cor} -- filtra_cor
+    """.format(
+        filtra_ref=filtra_ref,
+        filtra_tam=filtra_tam,
+        filtra_cor=filtra_cor,
     )
     cursor.execute(sql)
     return rows_to_dict_list(cursor)
