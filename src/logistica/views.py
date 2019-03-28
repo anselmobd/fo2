16475ff9e11e8a009f127ec4b5a2214803700166
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2019-03-28 12:35:56 -0300

    lista Notas fiscais embarcando

--- src/logistica/views.py
+++ src/logistica/views.py
@@ -328,3 +328,78 @@ class NotafiscalChave(PermissionRequiredMixin, View):
             context.update(self.mount_context(form.cleaned_data))
         context['form'] = form
         return render(request, self.template_name, context)
+
+
+class NotafiscalEmbarcando(View):
+
+    def __init__(self):
+        self.template_name = 'logistica/notafiscal_embarcando.html'
+        self.title_name = 'Notas fiscais embarcando'
+
+    def mount_context(self):
+        fields = [f.get_attname() for f in NotaFiscal._meta.get_fields()]
+        nfs = NotaFiscal.objects.filter(posicao_id=2).order_by('numero')
+        data = list(nfs.values(*fields, 'posicao__nome'))
+        if len(data) == 0:
+            self.context.update({
+                'msg_erro': 'Nenhuma NF embarcando',
+            })
+
+        for row in data:
+            if row['saida'] is None:
+                row['saida'] = '-'
+                row['atraso'] = (
+                    timezone.now() - row['faturamento']).days
+            else:
+                row['atraso'] = (
+                    row['saida'] - row['faturamento'].date()).days
+            if row['entrega'] is None:
+                row['entrega'] = '-'
+            if row['confirmada']:
+                row['confirmada'] = 'S'
+            else:
+                row['confirmada'] = 'N'
+            if row['observacao'] is None:
+                row['observacao'] = ' '
+            if row['ped_cliente'] is None:
+                row['ped_cliente'] = ' '
+            row['atraso_order'] = -row['atraso']
+            if row['natu_venda']:
+                row['venda'] = 'Sim'
+            else:
+                row['venda'] = 'Não'
+            if row['ativa']:
+                row['ativa'] = 'Sim'
+            else:
+                row['ativa'] = 'Não'
+            if row['nf_devolucao'] is None:
+                row['nf_devolucao'] = 'Não'
+
+        self.context.update({
+            'headers': ('No.', 'Faturamento', 'Venda', 'Ativa',
+                        'Devolvida', 'Posição',
+                        'Atraso', 'Saída', 'Agendada',
+                        'Entregue', 'UF', 'CNPJ', 'Cliente',
+                        'Transp.', 'Vol.', 'Valor', 'Observação',
+                        'Pedido', 'Ped.Cliente'),
+            'fields': ('numero', 'faturamento', 'venda', 'ativa',
+                       'nf_devolucao', 'posicao__nome',
+                       'atraso', 'saida', 'entrega',
+                       'confirmada', 'uf', 'dest_cnpj', 'dest_nome',
+                       'transp_nome', 'volumes', 'valor', 'observacao',
+                       'pedido', 'ped_cliente'),
+            'data': data,
+            'quant': len(data),
+        })
+
+    def start(self, request):
+        self.request = request
+        self.context = {'titulo': self.title_name}
+
+    def end(self):
+        return render(self.request, self.template_name, self.context)
+
+    def get(self, request, *args, **kwargs):
+        self.start(request)
+        self.mount_context()
+        return self.end()
