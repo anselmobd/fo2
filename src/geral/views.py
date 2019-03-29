217105a39ffbe4bb6e8f029e57db287a8db04b62
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2019-03-29 14:32:49 -0300

    Tela de Unidades

--- src/geral/views.py
+++ src/geral/views.py
@@ -1462,3 +1462,41 @@ def roteiros_de_fluxo(request, id):
     return HttpResponse(
         pformat(roteiros, indent=4),
         content_type='text/plain')
+
+
+def unidade(request):
+    cursor = connections['so'].cursor()
+    sql = '''
+        SELECT
+          di.DIVISAO_PRODUCAO DIV
+        , di.DESCRICAO DESCR
+        , ci.ESTADO UF
+        , ci.CIDADE
+        , ' (' || lpad(fo.FORNECEDOR9, 8, '0')
+          || '/' || lpad(fo.FORNECEDOR4, 4, '0')
+          || '-' || lpad(fo.FORNECEDOR2, 2, '0')
+          || ') '
+          || fo.NOME_FORNECEDOR NOME
+        FROM BASI_180 di -- divisão / unidade
+        JOIN SUPR_010 fo -- fornacedor
+          ON fo.FORNECEDOR9 = di.FACCIONISTA9
+         AND fo.FORNECEDOR4 = di.FACCIONISTA4
+         AND fo.FORNECEDOR2 = di.FACCIONISTA2
+        JOIN BASI_160 ci -- cidade
+          ON ci.COD_CIDADE = fo.COD_CIDADE
+        WHERE di.DIVISAO_PRODUCAO > 1
+          AND di.DIVISAO_PRODUCAO < 1000
+        ORDER BY
+          di.DIVISAO_PRODUCAO
+    '''
+    cursor.execute(sql)
+    data = rows_to_dict_list(cursor)
+    context = {
+        'titulo': 'Unidades / Divisões',
+        'headers': ('Código ', 'Descrição', 'UF', 'Cidade',
+                    '(CNPJ) Razão social'),
+        'fields': ('DIV', 'DESCR', 'UF', 'CIDADE',
+                   'NOME'),
+        'data': data,
+    }
+    return render(request, 'geral/unidade.html', context)
