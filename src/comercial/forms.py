78557e65ab30afaadbd0d948ee23910da8fe2c7f
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2019-04-04 14:37:37 -0300

    Iniciado o trabalho na tela "Distribuição de vendas por cor"

--- src/comercial/forms.py
+++ src/comercial/forms.py
@@ -5,3 +5,9 @@ class ClienteForm(forms.Form):
     cnpj = forms.CharField(
         label='CNPJ (início) ou Razão Social (parte)',
         widget=forms.TextInput(attrs={'autofocus': 'autofocus'}))
+
+
+class VendasPorCorForm(forms.Form):
+    cnpj = forms.CharField(
+        label='CNPJ (início) ou Razão Social (parte)',
+        widget=forms.TextInput(attrs={'autofocus': 'autofocus'}))
--- src/comercial/views.py
+++ src/comercial/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.db import connections
 from django.views import View
 
-from .forms import ClienteForm
+import comercial.forms as forms
 import comercial.models as models
 
 
@@ -12,9 +12,99 @@ def index(request):
 
 
 class FichaCliente(View):
-    Form_class = ClienteForm
+    Form_class = forms.ClienteForm
     template_name = 'comercial/ficha_cliente.html'
     titulo = 'Ficha do Cliente (duplicatas)'
+
+    def get(self, request, *args, **kwargs):
+        if 'cnpj' not in kwargs:
+            context = {'titulo': self.titulo}
+            form = self.Form_class()
+            context['form'] = form
+            return render(request, self.template_name, context)
+        else:
+            return self.post(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        context = {'titulo': self.titulo}
+        form = self.Form_class(request.POST)
+        if 'cnpj' in kwargs:
+            form.data['cnpj'] = kwargs['cnpj']
+        context['form'] = form
+        if form.is_valid():
+            cnpj = form.cleaned_data['cnpj']
+
+            data = models.busca_clientes(cnpj)
+            if len(data) == 0:
+                context['conteudo'] = 'nada'
+
+            elif len(data) > 1:
+                context['conteudo'] = 'lista'
+                for row in data:
+                    row['CNPJ'] = row['CNPJ'].strip()
+                context['data'] = data
+
+            else:
+                cnpj = data[0]['CNPJ'].strip()
+                cliente = data[0]['CLIENTE'].strip()
+
+                context['conteudo'] = 'ficha'
+                if len(cnpj) == 14:
+                    context['cnpj'] = '{}/{}-{}'.format(
+                        cnpj[0:8],
+                        cnpj[8:12],
+                        cnpj[12:14])
+                else:
+                    context['cnpj'] = cnpj
+                context['cliente'] = cliente
+
+                data = models.ficha_cliente(cnpj)
+                if len(data) == 0:
+                    context['conteudo'] = 'zerado'
+                else:
+                    formats = {
+                        'EMISSAO': '%d/%m/%Y',
+                        'VENC_ORI': '%d/%m/%Y',
+                        'VENCIMENTO': '%d/%m/%Y',
+                        'DATA_PAGO': '%d/%m/%Y',
+                    }
+                    for row in data:
+                        for field in row:
+                            if field in formats:
+                                row[field] = \
+                                    ('{:'+formats[field]+'}').format(
+                                        row[field])
+                    cliente = data[0]
+                    context.update({
+                        'headers': (
+                            'Duplicata',
+                            'Stat.',
+                            'Pedido',
+                            'Emissão',
+                            'Venc. Orig.',
+                            'Vencimento',
+                            'P.',
+                            'Valor',
+                            'Quant.',
+                            'Quant.Fat.',
+                            'Pagamento',
+                            'Valor pago',
+                            'Juros',
+                            'Atraso',
+                            'OP',
+                            'Banco',
+                            'Desconto',
+                            'Observação',
+                            ),
+                        'data': data,
+                    })
+        return render(request, self.template_name, context)
+
+
+class VendasPorCor(View):
+    Form_class = forms.VendasPorCorForm
+    template_name = 'comercial/vendas_cor.html'
+    titulo = 'Distribuição de vendas por cor'
 
     def get(self, request, *args, **kwargs):
         if 'cnpj' not in kwargs:
