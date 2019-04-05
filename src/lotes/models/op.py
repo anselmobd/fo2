e9b5b44147c421317af1cf03708f4b2ea22d644d
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2019-04-05 08:59:11 -0300

    Na tela de OP, adicionada tabela de posição de OP.

--- src/lotes/models/op.py
+++ src/lotes/models/op.py
@@ -194,6 +194,113 @@ def busca_op(cursor, op=None, ref=None, deposito=None, tipo=None, situacao=None)
     return rows_to_dict_list(cursor)
 
 
+def posicao_op(cursor, op):
+    sql = '''
+         WITH ops AS (
+           SELECT
+             ms.ORDEM_PRODUCAO OP
+           , max(ms.SEQUENCIA_ESTAGIO) MAXSEQ
+           FROM PCPC_040 ms
+           WHERE ms.ORDEM_PRODUCAO = %s
+           GROUP BY
+             ms.ORDEM_PRODUCAO
+        )
+        (
+        SELECT
+          0 + l.SEQUENCIA_ESTAGIO SEQUENCIA
+        , sum(l.QTDE_PECAS_PROD) QTD
+        , 'FINALIZADO 1A.' TIPO
+        , l.CODIGO_ESTAGIO || ' - ' || e.DESCRICAO ESTAGIO
+        FROM ops o
+        JOIN PCPC_040 l
+          ON l.ORDEM_PRODUCAO = o.op
+        JOIN MQOP_005 e
+          ON e.CODIGO_ESTAGIO = l.CODIGO_ESTAGIO
+        WHERE (l.QTDE_PECAS_PROD) <> 0
+          AND l.SEQUENCIA_ESTAGIO = o.MAXSEQ
+        GROUP BY
+          l.SEQUENCIA_ESTAGIO
+        , l.CODIGO_ESTAGIO
+        , e.DESCRICAO
+        --
+        UNION
+        --
+        SELECT
+          0 + l.SEQUENCIA_ESTAGIO SEQUENCIA
+        , sum(l.QTDE_PECAS_PROD + QTDE_PECAS_2A) QTD
+        , 'FINALIZADO 1A.' TIPO
+        , l.CODIGO_ESTAGIO || ' - ' || e.DESCRICAO ESTAGIO
+        FROM ops o
+        JOIN PCPC_040 l
+          ON l.ORDEM_PRODUCAO = o.op
+        JOIN MQOP_005 e
+          ON e.CODIGO_ESTAGIO = l.CODIGO_ESTAGIO
+        WHERE (l.QTDE_PECAS_PROD + QTDE_PECAS_2A) <> 0
+          AND l.SEQUENCIA_ESTAGIO = o.MAXSEQ
+        GROUP BY
+          l.SEQUENCIA_ESTAGIO
+        , l.CODIGO_ESTAGIO
+        , e.DESCRICAO
+        --
+        UNION
+        --
+        SELECT
+          2000 + l.SEQUENCIA_ESTAGIO SEQUENCIA
+        , sum(l.QTDE_EM_PRODUCAO_PACOTE - l.QTDE_CONSERTO) QTD
+        , 'A PRODUZIR' TIPO
+        , l.CODIGO_ESTAGIO || ' - ' || e.DESCRICAO ESTAGIO
+        FROM ops o
+        JOIN PCPC_040 l
+          ON l.ORDEM_PRODUCAO = o.op
+        JOIN MQOP_005 e
+          ON e.CODIGO_ESTAGIO = l.CODIGO_ESTAGIO
+        WHERE (l.QTDE_EM_PRODUCAO_PACOTE - l.QTDE_CONSERTO) > 0
+        GROUP BY
+          l.SEQUENCIA_ESTAGIO
+        , l.CODIGO_ESTAGIO
+        , e.DESCRICAO
+        --
+        UNION
+        --
+        SELECT
+          3000 + l.SEQUENCIA_ESTAGIO SEQUENCIA
+        , sum(l.QTDE_CONSERTO) QTD
+        , 'EM CONSERTO' TIPO
+        , l.CODIGO_ESTAGIO || ' - ' || e.DESCRICAO ESTAGIO
+        FROM ops o
+        JOIN PCPC_040 l
+          ON l.ORDEM_PRODUCAO = o.op
+        JOIN MQOP_005 e
+          ON e.CODIGO_ESTAGIO = l.CODIGO_ESTAGIO
+        WHERE l.QTDE_CONSERTO > 0
+        GROUP BY
+          l.SEQUENCIA_ESTAGIO
+        , l.CODIGO_ESTAGIO
+        , e.DESCRICAO
+        --
+        UNION
+        --
+        SELECT
+          4000 + l.SEQUENCIA_ESTAGIO SEQUENCIA
+        , sum(l.QTDE_PERDAS) QTD
+        , 'PERDAS' TIPO
+        , l.CODIGO_ESTAGIO || ' - ' || e.DESCRICAO ESTAGIO
+        FROM ops o
+        JOIN PCPC_040 l
+          ON l.ORDEM_PRODUCAO = o.op
+        JOIN MQOP_005 e
+          ON e.CODIGO_ESTAGIO = l.CODIGO_ESTAGIO
+        WHERE l.QTDE_PERDAS > 0
+        GROUP BY
+          l.SEQUENCIA_ESTAGIO
+        , l.CODIGO_ESTAGIO
+        , e.DESCRICAO
+        )
+    '''
+    cursor.execute(sql, [op])
+    return rows_to_dict_list(cursor)
+
+
 def op_relacionamentos(cursor, op):
     sql = '''
         WITH ordemp AS
