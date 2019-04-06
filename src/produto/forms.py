c8a5d3825c24b961b416c7fa5f3190d626104450
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2019-04-05 23:04:48 -0300

    RefForm trocada por RererenciaForm

--- src/produto/forms.py
+++ src/produto/forms.py
@@ -14,20 +14,6 @@ from utils.forms import FiltroForm
 from .models import Produto, S_Produto
 
 
-class RefForm(forms.Form):
-    ref = forms.CharField(
-        label='Referência', max_length=5, min_length=5,
-        widget=forms.TextInput(attrs={'type': 'string',
-                               'autofocus': 'autofocus'}))
-
-    def clean_ref(self):
-        ref = self.cleaned_data['ref'].upper()
-        data = self.data.copy()
-        data['ref'] = ref
-        self.data = data
-        return ref
-
-
 class ModeloForm(forms.Form):
     modelo = forms.CharField(
         label='Modelo', max_length=4, min_length=3,
