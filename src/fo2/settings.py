0e896a86b902fac029207a65bdb9cdd5cd8bc0fa
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2019-04-09 16:23:24 -0300

    Liberando para acesso externo o RH em ação e suas midias

--- src/fo2/settings.py
+++ src/fo2/settings.py
@@ -79,7 +79,12 @@ MIDDLEWARE = [
 ]
 
 N2LOL_REDIRECT = 'intranet'
-N2LOL_ALLOWED_URLS = ['^/intranet/$', '^/accounts/login/.*$', ]
+N2LOL_ALLOWED_URLS = [
+    '^/intranet/$',
+    '^/accounts/login/.*$',
+    '^/rh/.*$',
+    '^/media/rh/.*$',
+]
 N2LOL_ALLOWED_IP_BLOCKS = ['^192\.168\.1\..+$', '^127\.0\.0\.1$']
 
 ROOT_URLCONF = 'fo2.urls'
