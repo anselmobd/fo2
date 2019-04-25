cde7b9199e11af544c1e2f63f0cbcba2d56019ec
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2019-04-25 15:53:23 -0300

    primeira versao apresentando dados de inventario, ainda com debug e todos os fields tratados como string

--- script/inventario.py
+++ script/inventario.py
@@ -122,8 +122,6 @@ class Inventario:
             rownum_filter=rownum_filter
         )
         self._refs = self._ora.execute(sql, nivel=self.nivel)
-        # pprint(self._refs['data'])
-        # pprint(self._refs['keys'])
 
     def param_ok(self):
         if self.ano is None or \
@@ -134,9 +132,91 @@ class Inventario:
 
     def get_invent(self):
         self.param_ok()
-        for values in self._refs['data']:
+        count = len(self._refs['data'])
+        for i, values in enumerate(self._refs['data']):
             row = dict(zip(self._refs['keys'], values))
-            print(row['REFERENCIA'])
+            print('({}/{}) {}'.format(i+1, count, row['REFERENCIA']))
+            self.get_ref_invent(row['REFERENCIA'])
+
+    def get_ref_invent(self, ref):
+        sql = """
+            SELECT
+              e.NIVEL_ESTRUTURA
+            , e.GRUPO_ESTRUTURA
+            , e.SUBGRUPO_ESTRUTURA
+            , e.ITEM_ESTRUTURA
+            , e.CODIGO_DEPOSITO
+            , e.data_movimento
+            , e.sequencia_ficha
+            , e.saldo_fisico
+            , e.saldo_financeiro
+            , e.preco_medio_unitario
+            FROM estq_310 e
+            JOIN (
+              SELECT
+                e.NIVEL_ESTRUTURA
+              , e.GRUPO_ESTRUTURA
+              , e.SUBGRUPO_ESTRUTURA
+              , e.ITEM_ESTRUTURA
+              , e.CODIGO_DEPOSITO
+              , edt.DATA_BUSCA
+              , max(e.SEQUENCIA_FICHA) SEQUENCIA_BUSCA
+              FROM estq_310 e
+              JOIN (
+                SELECT
+                  e.NIVEL_ESTRUTURA
+                , e.GRUPO_ESTRUTURA
+                , e.SUBGRUPO_ESTRUTURA
+                , e.ITEM_ESTRUTURA
+                , e.CODIGO_DEPOSITO
+                , max(e.DATA_MOVIMENTO) DATA_BUSCA
+                FROM estq_310 e
+                WHERE e.NIVEL_ESTRUTURA = 9
+                  AND e.GRUPO_ESTRUTURA = :referencia
+                  AND e.DATA_MOVIMENTO < TO_DATE('2019-01-01','YYYY-MM-DD')
+                GROUP BY
+                  e.NIVEL_ESTRUTURA
+                , e.GRUPO_ESTRUTURA
+                , e.SUBGRUPO_ESTRUTURA
+                , e.ITEM_ESTRUTURA
+                , e.CODIGO_DEPOSITO
+              ) edt
+                ON edt.codigo_deposito    = e.codigo_deposito
+               and edt.nivel_estrutura    = e.nivel_estrutura
+               and edt.grupo_estrutura    = e.grupo_estrutura
+               and edt.subgrupo_estrutura = e.subgrupo_estrutura
+               and edt.item_estrutura     = e.item_estrutura
+               and edt.data_busca         = e.data_movimento
+              group by
+                e.codigo_deposito
+              , e.nivel_estrutura
+              , e.grupo_estrutura
+              , e.subgrupo_estrutura
+              , e.item_estrutura
+              , edt.data_busca
+            ) eseq
+              ON eseq.codigo_deposito    = e.codigo_deposito
+             and eseq.nivel_estrutura    = e.nivel_estrutura
+             and eseq.grupo_estrutura    = e.grupo_estrutura
+             and eseq.subgrupo_estrutura = e.subgrupo_estrutura
+             and eseq.item_estrutura     = e.item_estrutura
+             and eseq.data_busca         = e.data_movimento
+             and eseq.SEQUENCIA_BUSCA    = e.SEQUENCIA_FICHA
+        """
+        ref_invent = self._ora.execute(sql, referencia=ref)
+
+        nkeys = len(ref_invent['keys'])
+        mask = self.make_csv_mask(nkeys)
+        for values in ref_invent['data']:
+            print(mask.format(*values))
+
+    def make_csv_mask(self, nkeys):
+        sep = ''
+        result = ''
+        for _ in range(nkeys):
+            result += sep + '"{}"'
+            sep = ';'
+        return result
 
 
 if __name__ == '__main__':
