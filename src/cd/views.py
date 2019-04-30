28cd5b0045b9c706ab052980b819202d30f8a0ec
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2019-04-30 17:28:40 -0300

    bug

--- src/cd/views.py
+++ src/cd/views.py
@@ -1698,7 +1698,7 @@ class HistoricoLote(View):
                     row['local'] = '-'
                 else:
                     row['local'] = '='
-            if row['local'] is None':
+            if row['local'] is None:
                 row['local'] = 'SAIU!'
 
         context.update({
