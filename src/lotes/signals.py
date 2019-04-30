8603433caed99305a170a1ac3ba0463d656a3fbc
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2019-04-30 19:00:12 -0300

    trakking de solicitações de lote

--- src/lotes/signals.py
+++ src/lotes/signals.py
@@ -2,17 +2,24 @@ from django.db.models.signals import post_init, post_save, post_delete
 
 from fo2.signals import \
     post_init_tracking, post_save_tracking, post_delete_tracking
-from .models import ModeloTermica, ImpressoraTermica, Lote
+from .models import ModeloTermica, ImpressoraTermica, Lote, \
+    SolicitaLote, SolicitaLoteQtd
 
 
 post_init.connect(post_init_tracking, sender=ModeloTermica)
 post_init.connect(post_init_tracking, sender=ImpressoraTermica)
 post_init.connect(post_init_tracking, sender=Lote)
+post_init.connect(post_init_tracking, sender=SolicitaLote)
+post_init.connect(post_init_tracking, sender=SolicitaLoteQtd)
 
 post_save.connect(post_save_tracking, sender=ModeloTermica)
 post_save.connect(post_save_tracking, sender=ImpressoraTermica)
 post_save.connect(post_save_tracking, sender=Lote)
+post_save.connect(post_save_tracking, sender=SolicitaLote)
+post_save.connect(post_save_tracking, sender=SolicitaLoteQtd)
 
 post_delete.connect(post_delete_tracking, sender=ModeloTermica)
 post_delete.connect(post_delete_tracking, sender=ImpressoraTermica)
 post_delete.connect(post_delete_tracking, sender=Lote)
+post_delete.connect(post_delete_tracking, sender=SolicitaLote)
+post_delete.connect(post_delete_tracking, sender=SolicitaLoteQtd)
