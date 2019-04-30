80a977358c3e311604a89a7e1a13f21311a35b0a
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2019-04-29 21:44:20 -0300

    passa tipo de saída também para o script

--- script/inventario.sh
+++ script/inventario.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-script/inventario.py s k $1 $2 2    >  $3
-script/inventario.py s k $1 $2 9 -p >> $3
-script/inventario.py f k $1 $2 1 -p >> $3
-
+script/inventario.py s $1 $2 $3 2    >  $4
+script/inventario.py s $1 $2 $3 9 -p >> $4
+script/inventario.py f $1 $2 $3 1 -p >> $4
