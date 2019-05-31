319e1db489a50ffe734c0144a52c45106e7d6c81
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2019-05-31 10:37:58 -0300

    Alterado a forma de mostrar os totais gerais

--- src/lotes/forms.py
+++ src/lotes/forms.py
@@ -483,5 +483,6 @@ class ImprimeTagForm(forms.Form):
 class QuantEstagioForm(forms.Form):
     estagio = forms.CharField(
         label='Estágio', max_length=2,
+        help_text='Informe 0 (zero) para visualizar os totais gerais',
         widget=forms.TextInput(attrs={'type': 'number',
                                'autofocus': 'autofocus'}))
