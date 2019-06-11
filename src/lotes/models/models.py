5cb3c3ff1aaf72e080e1c16462ad6a2368f14c39
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2019-06-11 16:35:31 -0300

    Muitos estudos sobre uma tabela que acumulasse versões anteriores de seus registros.

--- src/lotes/models/models.py
+++ src/lotes/models/models.py
@@ -1,3 +1,5 @@
+from pprint import pprint
+
 from django.utils import timezone
 from django.db import models
 from django.contrib.auth.models import User
@@ -259,6 +261,20 @@ class SolicitaLote(models.Model):
         verbose_name = "Solicitação de lote"
 
 
+# class EstadoSolicitacao(models.Model):
+#     nome = models.CharField(
+#         max_length=20,
+#         db_index=True, unique=True)
+#
+#     def __str__(self):
+#         return self.nome
+#
+#     class Meta:
+#         db_table = "fo2_!!pos_carga"
+#         verbose_name = "Posição de carga(NF)"
+#         verbose_name_plural = "Posições de carga(NF)"
+
+
 class SolicitaLoteQtd(models.Model):
     solicitacao = models.ForeignKey(
         SolicitaLote, on_delete=models.CASCADE, null=True, blank=True,
@@ -287,3 +303,115 @@ class SolicitaLoteQtd(models.Model):
     class Meta:
         db_table = "fo2_cd_solicita_lote_qtd"
         verbose_name = "Quantidades Solicitadas de lotes"
+
+
+# class ReservaLoteQtd(models.Model):
+#     solicitacao = models.ForeignKey(
+#         SolicitaLote, on_delete=models.CASCADE, null=True, blank=True,
+#         verbose_name='Solicitação')
+#     lote = models.ForeignKey(
+#         Lote, on_delete=models.CASCADE, null=True, blank=True,
+#         verbose_name='Lote')
+#     qtd = models.IntegerField(
+#         default=0, verbose_name='quantidade solicitada')
+#
+#     active = models.NullBooleanField(default=True)
+#     create_at = models.DateTimeField(
+#         null=True, blank=True,
+#         verbose_name='criado em')
+#     inactive_at = models.DateTimeField(
+#         null=True, blank=True,
+#         verbose_name='inativado em')
+#
+#     def save(self, *args, **kwargs):
+#         ''' On create and update, get timestamps '''
+#         if self.id:
+#             # old = deepcopy(self)
+#             old = ReservaLoteQtd.objects.get(id=self.id)
+#             old.id = None
+#             old.active = False
+#             old.save()
+#         now = timezone.now()
+#         if self.active:
+#             self.create_at = now
+#         else:
+#             self.inactive_at = now
+#         super(ReservaLoteQtd, self).save(*args, **kwargs)
+#
+#     def delete(self, *args, **kwargs):
+#         old = ReservaLoteQtd.objects.get(id=self.id)
+#         old.id = None
+#         old.active = False
+#         old.save()
+#         super(ReservaLoteQtd, self).delete(*args, **kwargs)
+#
+#     class Meta:
+#         db_table = "fo2_reserva_lote_qtd"
+#         verbose_name = "Quantidades reservadas de lotes"
+
+class TableHeapManager(models.Manager):
+    def get_queryset(self):
+        return super(MyModelManager, self).get_queryset().filter(origin_id=0)
+
+
+# Testada sem o "abstract = True" e funcionou bem
+class TableHeap(models.Model):
+    origin_id = models.IntegerField(
+        default=0,
+        verbose_name='id de origem')
+    deleted = models.NullBooleanField(
+        default=False,
+        verbose_name='apagado')
+    version = models.IntegerField(
+        default=0,
+        verbose_name='versão')
+    when = models.DateTimeField(
+        null=True, blank=True,
+        verbose_name='quando')
+    unique_aux = models.IntegerField(
+        default=0,
+        verbose_name='campo auxiliar para unique_together')
+
+    objects_all = models.Manager()
+    objects = TableHeapManager()
+
+    def save_old(self, id, deleted=False):
+        try:
+            old = TableHeap.objects.get(id=id)
+            old.origin_id = old.id
+            old.unique_aux = old.version
+            old.id = None
+            old.deleted = deleted
+            old.save()
+        except Exception:
+            pass
+
+    def save(self, *args, **kwargs):
+        if self.id:
+            self.save_old(self.id)
+        self.when = timezone.now()
+        if self.origin_id == 0:
+            self.version += 1
+        super(TableHeap, self).save(*args, **kwargs)
+
+    def delete(self, *args, **kwargs):
+        self.save_old(self.id, deleted=True)
+        super(TableHeap, self).delete(*args, **kwargs)
+
+    class Meta:
+        abstract = True
+        # db_table = "fo2_table_head"
+        verbose_name = "Tabela com pilha de versões"
+
+# Não funcionou a tabala abaixo
+# class TableCodes(TableHeap):
+#     code = models.CharField(
+#         max_length=20,
+#         verbose_name='código')
+#
+#     def __init__(self):
+#         self.unique_together = ('code', 'origin_id', 'unique_aux',)
+#
+#     class Meta:
+#         db_table = "fo2_codes"
+#         verbose_name = "Tabela herdando pilha de versões"
