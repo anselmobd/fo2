fb425e74c4f3d4d1a7c002d5bdceadd60fb0216e
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2019-06-18 21:02:23 -0300

    Totais gerais dos estágios - Apresentação nova dos estágio separados e com mais subtotais

--- src/lotes/views/quant_estagio.py
+++ src/lotes/views/quant_estagio.py
@@ -51,53 +51,139 @@ class TotalEstagio(View):
             })
             return context
 
-        total_liberado = data[0].copy()
-        total_liberado[
-            'ESTAGIO'] = 'Total em produção (apenas após 3 - PCP - LIBERACAO):'
-        total_liberado['|STYLE'] = 'font-weight: bold;'
-        total_geral = data[0].copy()
-        total_geral['ESTAGIO'] = 'Total geral (incluindo 3 - PCP - LIBERACAO):'
+        headers = ('Estágio', 'Lotes PA', 'Lotes PG',
+                   'Lotes PB', 'Lotes MD*', 'Lotes*',
+                   'Peças PA', 'Peças PG', 'Peças PB',
+                   'Peças MD*', 'Peças')
+        fields = ('ESTAGIO', 'LOTES_PA', 'LOTES_PG',
+                  'LOTES_PB', 'LOTES_MD', 'LOTES',
+                  'QUANT_PA', 'QUANT_PG', 'QUANT_PB',
+                  'QUANT_MD', 'QUANT')
+        style = {2: 'text-align: right;',
+                 3: 'text-align: right;',
+                 4: 'text-align: right;',
+                 5: 'text-align: right;',
+                 6: 'text-align: right; font-weight: bold;',
+                 7: 'text-align: right;',
+                 8: 'text-align: right;',
+                 9: 'text-align: right;',
+                 10: 'text-align: right;',
+                 11: 'text-align: right; font-weight: bold;'}
         quant_fields = [
             'LOTES_PA', 'QUANT_PA',
             'LOTES_PG', 'QUANT_PG',
             'LOTES_PB', 'QUANT_PB',
             'LOTES_MD', 'QUANT_MD',
             'LOTES', 'QUANT']
+
+        estagio_programacao = [3]
+        estagio_estoque = [57, 60, 63]
+        estagio_vendido = [66]
+        estagio_nao_producao = \
+            estagio_programacao + estagio_estoque + estagio_vendido
+
+        data_p = [
+            r for r in data if r['CODIGO_ESTAGIO'] in estagio_programacao]
+        for row in data_p:
+            for field in quant_fields:
+                if field in ['LOTES', 'QUANT']:
+                    row['{}|STYLE'.format(field)] = 'color: red;'
+        context.update({
+            'headers_p': headers,
+            'fields_p': fields,
+            'data_p': data_p,
+            'style_p': style,
+        })
+
+        total_giro = data[0].copy()
+        total_giro['ESTAGIO'] = 'Total em produção e em estoque'
+        total_giro['|STYLE'] = 'font-weight: bold;'
+        for field in quant_fields:
+            total_giro[field] = 0
+
+        data_d = [
+            r for r in data if r['CODIGO_ESTAGIO'] not in estagio_nao_producao]
+        total_producao = data[0].copy()
+        total_producao['ESTAGIO'] = 'Total em produção'
+        total_producao['|STYLE'] = 'font-weight: bold;'
+        for field in quant_fields:
+            total_producao[field] = 0
+        for row in data_d:
+            for field in quant_fields:
+                total_producao[field] += row[field]
+                if field in ['LOTES', 'QUANT']:
+                    total_producao['{}|STYLE'.format(field)] = 'color: red;'
+            for field in quant_fields:
+                total_giro[field] += row[field]
+                if field in ['LOTES', 'QUANT']:
+                    total_giro['{}|STYLE'.format(field)] = 'color: red;'
+        data_d.append(total_producao)
+        context.update({
+            'headers_d': headers,
+            'fields_d': fields,
+            'data_d': data_d,
+            'style_d': style,
+        })
+
+        data_e = [
+            r for r in data if r['CODIGO_ESTAGIO'] in estagio_estoque]
+        total_estoque = data[0].copy()
+        total_estoque['ESTAGIO'] = 'Total em estoque'
+        total_estoque['|STYLE'] = 'font-weight: bold;'
+        for field in quant_fields:
+            total_estoque[field] = 0
+        for row in data_e:
+            for field in quant_fields:
+                total_estoque[field] += row[field]
+                if field in ['LOTES', 'QUANT']:
+                    total_estoque['{}|STYLE'.format(field)] = 'color: red;'
+            for field in quant_fields:
+                total_giro[field] += row[field]
+                if field in ['LOTES', 'QUANT']:
+                    total_giro['{}|STYLE'.format(field)] = 'color: red;'
+        data_e.append(total_estoque)
+        context.update({
+            'headers_e': headers,
+            'fields_e': fields,
+            'data_e': data_e,
+            'style_e': style,
+        })
+
+        data_v = [
+            r for r in data if r['CODIGO_ESTAGIO'] in estagio_vendido]
+        for row in data_v:
+            for field in quant_fields:
+                if field in ['LOTES', 'QUANT']:
+                    row['{}|STYLE'.format(field)] = 'color: red;'
+        context.update({
+            'headers_v': headers,
+            'fields_v': fields,
+            'data_v': data_v,
+            'style_v': style,
+        })
+
+        total_geral = data[0].copy()
+        total_geral['ESTAGIO'] = 'Total geral'
+        total_geral['|STYLE'] = 'font-weight: bold;'
         for field in quant_fields:
             total_geral[field] = 0
-            total_liberado[field] = 0
         for row in data:
             for field in quant_fields:
                 total_geral[field] += row[field]
-            if row['CODIGO_ESTAGIO'] != 3:
-                for field in quant_fields:
-                    total_liberado[field] += row[field]
-                    if field in ['LOTES', 'QUANT']:
-                        total_liberado['{}|STYLE'.format(field)] = \
-                            'color: darkred;'
-
-        data.append(total_liberado)
-        data.append(total_geral)
+                if field in ['LOTES', 'QUANT']:
+                    total_geral['{}|STYLE'.format(field)] = 'color: red;'
         context.update({
-            'headers': ('Estágio', 'Lotes PA', 'Lotes PG',
-                        'Lotes PB', 'Lotes MD*', 'Lotes*',
-                        'Peças PA', 'Peças PG', 'Peças PB',
-                        'Peças MD*', 'Peças'),
-            'fields': ('ESTAGIO', 'LOTES_PA', 'LOTES_PG',
-                       'LOTES_PB', 'LOTES_MD', 'LOTES',
-                       'QUANT_PA', 'QUANT_PG', 'QUANT_PB',
-                       'QUANT_MD', 'QUANT'),
-            'data': data,
-            'style': {2: 'text-align: right;',
-                      3: 'text-align: right;',
-                      4: 'text-align: right;',
-                      5: 'text-align: right;',
-                      6: 'text-align: right; font-weight: bold;',
-                      7: 'text-align: right;',
-                      8: 'text-align: right;',
-                      9: 'text-align: right;',
-                      10: 'text-align: right;',
-                      11: 'text-align: right; font-weight: bold;'},
+            'headers_t': headers,
+            'data_t': [total_geral],
+            'fields_t': fields,
+            'style_t': style,
+        })
+
+        context.update({
+            'headers_g': headers,
+            'data_g': [total_giro],
+            'fields_g': fields,
+            'style_g': style,
         })
 
         return context
