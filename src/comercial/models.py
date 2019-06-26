f37c4849f8a763c816b6181b37c67aefa11e54cd
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2019-06-26 13:20:50 -0300

    criando ModeloPassadoPeriodo

--- src/comercial/models.py
+++ src/comercial/models.py
@@ -15,3 +15,25 @@ class ModeloPassado(models.Model):
         db_table = "fo2_mod_passado"
         verbose_name = "Modelo de análise do passado"
         verbose_name_plural = "Modelos de análise do passado"
+
+
+class ModeloPassadoPeriodo(models.Model):
+    modelo = models.ForeignKey(
+        ModeloPassado, on_delete=models.CASCADE)
+    ordem = models.IntegerField()
+    meses = models.IntegerField()
+    peso = models.IntegerField()
+
+    def __str__(self):
+        return '{}, período #{} - {} {}; peso {}'.format(
+            self.modelo.nome,
+            self.ordem,
+            self.meses,
+            ('mês' if self.meses == 1 else 'meses'),
+            self.peso,
+        )
+
+    class Meta:
+        db_table = "fo2_mod_pass_periodo"
+        verbose_name = "Período de modelo de análise do passado"
+        verbose_name_plural = "Períodos de modelo de análise do passado"
