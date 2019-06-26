66ca62c01a70486919b98428cbfd2adc9183dfb4
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2019-06-26 15:45:15 -0300

    "Estoque desejado" inicialmente apresentando apenas a "Ponderação a aplicar"

--- src/comercial/views/estoque.py
+++ src/comercial/views/estoque.py
@@ -1,16 +1,44 @@
 from pprint import pprint
+from datetime import datetime, timedelta
 
 from django.shortcuts import render
 from django.db import connections
 from django.views import View
 
-from base.views import O2BaseGetPostView
+from base.views import O2BaseGetView
+from utils.functions import dec_month, dec_months
 
-import produto.queries
-
-import comercial.forms as forms
 import comercial.models as models
 
 
-class EstoqueDesejado(O2BaseGetPostView):
-    pass
+class EstoqueDesejado(O2BaseGetView):
+
+    def __init__(self, *args, **kwargs):
+        super(EstoqueDesejado, self).__init__(*args, **kwargs)
+        self.template_name = 'comercial/estoque_desejado.html'
+        self.title_name = 'Estoque desejado'
+
+    def mount_context(self):
+        nfs = list(models.ModeloPassadoPeriodo.objects.filter(
+            modelo_id=1).order_by('ordem').values())
+        if len(nfs) == 0:
+            self.context.update({
+                'msg_erro': 'Nenhum período definido',
+            })
+            return
+
+        data = list(nfs)
+
+        hoje = datetime.today()
+        mes = dec_month(hoje, 1)
+        for row in data:
+            row['mes_fim'] = mes.strftime("%m/%Y")
+            mes = dec_months(mes, row['meses']-1)
+            row['mes_ini'] = mes.strftime("%m/%Y")
+            mes = dec_month(mes)
+
+        self.context.update({
+            'headers': ('# meses', 'Mês inicial', 'Mês final', 'Peso'),
+            'fields': ('meses', 'mes_ini', 'mes_fim', 'peso'),
+            'data': data,
+        })
