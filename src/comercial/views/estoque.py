e0e47b32586529e98493066dca739b8f7bc8d1ee
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2019-06-28 16:54:20 -0300

    primeira versão apresentando ponderamentos; reordenação de alguns códigos

--- src/comercial/views/estoque.py
+++ src/comercial/views/estoque.py
@@ -56,6 +56,87 @@ class EstoqueDesejado(O2BaseGetView):
             'modref': modref,
         })
 
+        # quantidades ponderada do modelo
+        data = []
+        for periodo in self.periodos:
+            data_periodo = queries.get_vendas(
+                self.cursor, ref=None, periodo=periodo['range'],
+                colecao=None, cliente=None, por='modelo', modelo=modref)
+            for row in data_periodo:
+                data_row = next(
+                    (dr for dr in data if dr['modelo'] == row['modelo']),
+                    False)
+                if not data_row:
+                    data_row = {
+                        'modelo': row['modelo'],
+                        'qtd': 0
+                    }
+                    data.append(data_row)
+                data_row['qtd'] += round(
+                    row['qtd'] / periodo['meses'] *
+                    periodo['peso'] / self.tot_peso)
+        self.context['modelo_ponderado'] = {
+            'headers': ['Modelo', 'Quantidade ponderada'],
+            'fields': ['modelo', 'qtd'],
+            'data': data,
+            'style': {2: 'text-align: right;'},
+        }
+
+        # quantidades ponderada do tamanho
+        data = []
+        for periodo in self.periodos:
+            data_periodo = queries.get_vendas(
+                self.cursor, ref=None, periodo=periodo['range'],
+                colecao=None, cliente=None, por='tam', modelo=modref,
+                order_qtd=False)
+            for row in data_periodo:
+                data_row = next(
+                    (dr for dr in data if dr['tam'] == row['tam']),
+                    False)
+                if not data_row:
+                    data_row = {
+                        'tam': row['tam'],
+                        'qtd': 0
+                    }
+                    data.append(data_row)
+                data_row['qtd'] += round(
+                    row['qtd'] / periodo['meses'] *
+                    periodo['peso'] / self.tot_peso)
+        self.context['tamanho_ponderado'] = {
+            'headers': ['Tamanho', 'Quantidade ponderada'],
+            'fields': ['tam', 'qtd'],
+            'data': data,
+            'style': {2: 'text-align: right;'},
+        }
+
+        # quantidades ponderada da cor
+        data = []
+        for periodo in self.periodos:
+            data_periodo = queries.get_vendas(
+                self.cursor, ref=None, periodo=periodo['range'],
+                colecao=None, cliente=None, por='cor', modelo=modref)
+            for row in data_periodo:
+                data_row = next(
+                    (dr for dr in data if dr['cor'] == row['cor']),
+                    False)
+                if not data_row:
+                    data_row = {
+                        'cor': row['cor'],
+                        'qtd': 0
+                    }
+                    data.append(data_row)
+                data_row['qtd'] += round(
+                    row['qtd'] / periodo['meses'] *
+                    periodo['peso'] / self.tot_peso)
+        self.context['cor_ponderada'] = {
+            'headers': ['Cor', 'Quantidade ponderada'],
+            'fields': ['cor', 'qtd'],
+            'data': data,
+            'style': {2: 'text-align: right;'},
+        }
+
+        # médias mensais
+        # do modelo
         data = []
         zero_data_row = {p['range']: 0 for p in self.periodos}
         for periodo in self.periodos:
@@ -77,38 +158,9 @@ class EstoqueDesejado(O2BaseGetView):
                     data.append(data_row)
                 data_row[periodo['range']] = round(
                     row['qtd'] / periodo['meses'])
-        self.context.update({
+        self.context['do_modelo'] = {
             'headers': ['Modelo', *[p['descr'] for p in self.periodos]],
             'fields': ['modelo', *[p['range'] for p in self.periodos]],
-            'data': data,
-            'style': self.style,
-        })
-
-        # Por cor
-        data = []
-        zero_data_row = {p['range']: 0 for p in self.periodos}
-        for periodo in self.periodos:
-            data_periodo = queries.get_vendas(
-                self.cursor, ref=None, periodo=periodo['range'],
-                colecao=None, cliente=None, por='cor', modelo=modref)
-            for row in data_periodo:
-                data_row = next(
-                    (dr for dr in data if dr['cor'] == row['cor']),
-                    False)
-                if not data_row:
-                    data_row = {
-                        'cor': row['cor'],
-                        'cor|LINK': reverse(
-                            'comercial:estoque_desejado__get',
-                            args=[row['cor']]),
-                        **zero_data_row
-                    }
-                    data.append(data_row)
-                data_row[periodo['range']] = round(
-                    row['qtd'] / periodo['meses'])
-        self.context['por_cor'] = {
-            'headers': ['Cor', *[p['descr'] for p in self.periodos]],
-            'fields': ['cor', *[p['range'] for p in self.periodos]],
             'data': data,
             'style': self.style,
         }
@@ -139,6 +191,35 @@ class EstoqueDesejado(O2BaseGetView):
         self.context['por_tam'] = {
             'headers': ['Tamanho', *[p['descr'] for p in self.periodos]],
             'fields': ['tam', *[p['range'] for p in self.periodos]],
+            'data': data,
+            'style': self.style,
+        }
+
+        # Por cor
+        data = []
+        zero_data_row = {p['range']: 0 for p in self.periodos}
+        for periodo in self.periodos:
+            data_periodo = queries.get_vendas(
+                self.cursor, ref=None, periodo=periodo['range'],
+                colecao=None, cliente=None, por='cor', modelo=modref)
+            for row in data_periodo:
+                data_row = next(
+                    (dr for dr in data if dr['cor'] == row['cor']),
+                    False)
+                if not data_row:
+                    data_row = {
+                        'cor': row['cor'],
+                        'cor|LINK': reverse(
+                            'comercial:estoque_desejado__get',
+                            args=[row['cor']]),
+                        **zero_data_row
+                    }
+                    data.append(data_row)
+                data_row[periodo['range']] = round(
+                    row['qtd'] / periodo['meses'])
+        self.context['por_cor'] = {
+            'headers': ['Cor', *[p['descr'] for p in self.periodos]],
+            'fields': ['cor', *[p['range'] for p in self.periodos]],
             'data': data,
             'style': self.style,
         }
@@ -187,16 +268,20 @@ class EstoqueDesejado(O2BaseGetView):
         self.data_nfs = list(nfs)
 
         self.periodos = []
+        self.tot_peso = 0
         n_mes = 0
         hoje = datetime.today()
         mes = dec_month(hoje, 1)
         self.style = {}
         for i, row in enumerate(self.data_nfs):
             periodo = {
-                'range': '{}:{}'.format(n_mes+row['meses'], n_mes),
+                'range': '{}:{}'.format(
+                    n_mes+row['meses'], n_mes),
                 'meses': row['meses'],
+                'peso': row['peso'],
             }
             n_mes += row['meses']
+            self.tot_peso += row['peso']
 
             mes_fim = mes.strftime("%m/%Y")
             mes = dec_months(mes, row['meses']-1)
