0085a83c7166ed77e776f97a14ff5413e26bac81
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2019-07-12 16:19:05 -0300

    admin usuário / tipo máquina

--- src/manutencao/admin.py
+++ src/manutencao/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 from fo2.admin import intr_adm_site
 
-from .models import TipoMaquina, UnidadeTempo, Maquina
+from .models import TipoMaquina, UnidadeTempo, Maquina, UsuarioTipoMaquina
 
 
 class TipoMaquinaAdmin(admin.ModelAdmin):
@@ -21,6 +21,14 @@ class MaquinaAdmin(admin.ModelAdmin):
     readonly_fields = ['slug']
 
 
+class UsuarioTipoMaquinaAdmin(admin.ModelAdmin):
+    list_display = ['usuario', 'tipo_maquina']
+    ordering = ['usuario__username', 'tipo_maquina__nome']
+    search_fields = ['usuario__username', 'tipo_maquina__nome']
+    fields = ['usuario', 'tipo_maquina']
+
+
 intr_adm_site.register(TipoMaquina, TipoMaquinaAdmin)
 intr_adm_site.register(UnidadeTempo, UnidadeTempoAdmin)
 intr_adm_site.register(Maquina, MaquinaAdmin)
+intr_adm_site.register(UsuarioTipoMaquina, UsuarioTipoMaquinaAdmin)
