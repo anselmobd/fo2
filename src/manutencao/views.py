ff164ec079cb7fc2533cb3e875dc07c7de20c094
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2019-07-12 19:27:04 -0300

    lista rotinas cadastradas

--- src/manutencao/views.py
+++ src/manutencao/views.py
@@ -1,11 +1,36 @@
 from django.views import View
 from django.shortcuts import render
 
+from base.views import O2BaseGetView
+
+import manutencao.models as models
+
 
 def index(request):
     context = {}
     return render(request, 'manutencao/index.html', context)
 
 
-class Rotinas(View):
-    pass
+class Rotinas(O2BaseGetView):
+
+    def __init__(self, *args, **kwargs):
+        super(Rotinas, self).__init__(*args, **kwargs)
+        self.template_name = 'manutencao/rotinas.html'
+        self.title_name = 'Rotinas'
+
+    def mount_context(self):
+        rotinas = models.Rotina.objects.all().order_by(
+            'tipo_maquina', 'nome')
+        if len(rotinas) == 0:
+            self.context.update({
+                'msg_erro': 'Nenhuma rotina cadastrada',
+            })
+            return
+
+        data = list(rotinas.values('tipo_maquina__nome', 'nome'))
+
+        self.context.update({
+            'headers': ('Tipo de máquina', 'Nome da rotina'),
+            'fields': ('tipo_maquina__nome', 'nome'),
+            'data': data,
+        })
