69371fb40dc251269d7fa3d91e8d5194e41070d9
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2019-07-12 15:35:09 -0300

    table unidade de tempo

--- src/manutencao/models.py
+++ src/manutencao/models.py
@@ -24,3 +24,26 @@ class TipoMaquina(models.Model):
     def save(self, *args, **kwargs):
         self.slug = slugify(self.nome)
         super(TipoMaquina, self).save(*args, **kwargs)
+
+
+class UnidadeTempo(models.Model):
+    codigo = models.CharField(
+        'Código',
+        db_index=True,
+        max_length=1,
+        )
+    nome = models.CharField(
+        max_length=50,
+        )
+
+    def __str__(self):
+        return '{}-{}'.format(self.codigo, self.nome)
+
+    class Meta:
+        db_table = 'fo2_man_unidade_tempo'
+        verbose_name = 'Unidade de tempo'
+        verbose_name_plural = 'Unidades de tempo'
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.nome)
+        super(UnidadeTempo, self).save(*args, **kwargs)
