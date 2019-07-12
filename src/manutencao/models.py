dc1dc95cc9fda511949faa829a3cd0464e2ba549
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2019-07-12 15:33:17 -0300

    table tipo de máquina

--- src/manutencao/models.py
+++ src/manutencao/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+
+class TipoMaquina(models.Model):
+    nome = models.CharField(
+        db_index=True,
+        max_length=20,
+        )
+    slug = models.SlugField()
+    descricao = models.CharField(
+        'Descrição',
+        max_length=250,
+        )
+
+    def __str__(self):
+        return self.descricao
+
+    class Meta:
+        db_table = 'fo2_man_tipo_maquina'
+        verbose_name = 'Tipo de máquina'
+        verbose_name_plural = 'Tipos de máquinas'
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.nome)
+        super(TipoMaquina, self).save(*args, **kwargs)
