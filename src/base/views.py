cd2cd70922fb18271aea7e49299c7a5f5ffd4264
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2019-07-15 19:55:15 -0300

    O2BaseCustomView: parâmetro de funcionamento get_args2contect; definido mount_context vazio

--- src/base/views.py
+++ src/base/views.py
@@ -6,19 +6,30 @@ class O2BaseCustomView(View):
 
     def __init__(self):
         self.get_args = []
+        self.get_args2contect = False
 
     def init_self(self, request, kwargs):
         self.request = request
         self.kwargs = kwargs
+
         self.context = {}
         if hasattr(self, 'title_name'):
             self.context.update({'titulo': self.title_name})
+
+        if self.get_args2contect:
+            for arg in self.get_args:
+                print(arg)
+                arg_value = self.get_arg(arg)
+                self.context.update({arg: arg_value})
 
     def get_arg(self, field):
         return self.kwargs[field] if field in self.kwargs else None
 
     def my_render(self):
         return render(self.request, self.template_name, self.context)
+
+    def mount_context(self):
+        pass
 
 
 class O2BaseGetPostView(O2BaseCustomView):
