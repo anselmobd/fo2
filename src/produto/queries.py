9f34bf0335211de274d0806605d96af07eb668aa
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2019-11-06 10:17:13 -0300

    função lead_de_modelo pega o lead de modelo mesmo quando o modelo tem mais de uma coleção e apenas se os leads das diferentes coleções for o mesmo

--- src/produto/queries.py
+++ src/produto/queries.py
@@ -3,6 +3,8 @@ from functools import lru_cache
 
 from fo2.models import rows_to_dict_list, rows_to_dict_list_lower
 
+import lotes.models
+
 
 def produtos_n1_basic(param):
     tipo = param[0:2]
@@ -881,7 +883,7 @@ def multiplas_colecoes(cursor):
     return rows_to_dict_list(cursor)
 
 
-def colecao_de_modelo(cursor, modelo):
+def colecoes_de_modelo(cursor, modelo):
     sql = """
         SELECT
           r.COLECAO
@@ -895,10 +897,35 @@ def colecao_de_modelo(cursor, modelo):
           r.COLECAO
     """.format(modelo)
     cursor.execute(sql)
-    rows = rows_to_dict_list(cursor)
+    return rows_to_dict_list(cursor)
+
+
+def colecao_de_modelo(cursor, modelo):
+    rows = colecoes_de_modelo(cursor, modelo)
     if len(rows) != 1:
         return -1
     return rows[0]['COLECAO']
+
+
+def lead_de_modelo(cursor, modelo):
+    lead = 0
+    rows = colecoes_de_modelo(cursor, modelo)
+    for row in rows:
+        try:
+            lc = lotes.models.LeadColecao.objects.get(colecao=row['COLECAO'])
+            lc_lead = lc.lead
+            if modelo == '526':
+                print(lc_lead)
+        except models.LeadColecao.DoesNotExist:
+            lc_lead = 0
+            break
+        if lead == 0:
+            lead = lc_lead
+        else:
+            if lead != lc_lead:
+                lead = 0
+                break
+    return lead
 
 
 def gtin(cursor, ref=None, gtin=None):
