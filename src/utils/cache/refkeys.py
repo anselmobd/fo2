97294f2200caaad0c3f09adf7f3db80d683933ce
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2019-11-08 16:29:41 -0300

    versão funcional

--- src/utils/cache/refkeys.py
+++ src/utils/cache/refkeys.py
@@ -1,60 +1,68 @@
 import hashlib
 
+from django.core.cache import cache
 
-def refkeys_hash(ref):
+from utils.functions import fo2logger
+
+
+def hash(ref):
     key_seasoned = 'keys_ref_SAUCE_{}'.format(ref)
     key_hash = 'keys_ref_{}'.format(
         hashlib.md5(key_seasoned.encode('utf-8')).hexdigest())
     return key_hash
 
 
-def refkeys_set(ref, keys, key_hash=None):
+def put(ref, keys, key_hash=None):
     if key_hash is None:
-        key_hash = refkeys_hash(ref)
+        key_hash = hash(ref)
     cache.set(key_hash, keys, timeout=60*60*9)
 
 
-def refkeys_dset(ref, dkeys, key_hash=None):
+def dset(ref, dkeys, key_hash=None):
     if key_hash is None:
-        key_hash = refkeys_hash(ref)
+        key_hash = hash(ref)
     keys = '<|>'.join(dkeys)
-    refkeys_set(ref, keys, key_hash=key_hash)
+    put(ref, keys, key_hash=key_hash)
 
 
-def refkeys_get(ref, key_hash=None):
+def get(ref, key_hash=None):
     if key_hash is None:
-        key_hash = refkeys_hash(ref)
+        key_hash = hash(ref)
     return cache.get(key_hash)
 
 
-def refkeys_dget(ref, key_hash=None):
+def dget(ref, key_hash=None):
     if key_hash is None:
-        key_hash = refkeys_hash(ref)
-    keys = refkeys_get(key_hash, key_hash=key_hash)
+        key_hash = hash(ref)
+    keys = get(key_hash, key_hash=key_hash)
     dkeys = []
     if keys is not None:
         dkeys = keys.split('<|>')
     return dkeys
 
 
-def refkeys_add(ref, key):
-    key_hash = refkeys_hash(ref)
-    keys = refkeys_get(ref, key_hash=key_hash)
-    if keys is not None:
+def add(ref, key):
+    fo2logger.info('add {} {}'.format(ref, key))
+    key_hash = hash(ref)
+    keys = get(ref, key_hash=key_hash)
+    if keys is None:
+        keys = key
+    else:
         keys = '<|>'.join([keys, key])
-    refkeys_set(ref, keys, key_hash=key_hash)
+    put(ref, keys, key_hash=key_hash)
 
 
-def refkeys_remove(ref, key):
-    key_hash = refkeys_hash(ref)
-    dkeys = refkeys_dget(ref, key_hash=key_hash)
+def remove(ref, key):
+    key_hash = hash(ref)
+    dkeys = dget(ref, key_hash=key_hash)
     try:
         dkeys.remove(key)
     except ValueError:
         pass
-    refkeys_dset(ref, dkeys, key_hash=key_hash)
+    dset(ref, dkeys, key_hash=key_hash)
 
 
-def refkeys_delete(ref):
-    key_hash = refkeys_hash(ref)
-    keys_ref = cache.delete(key_hash)
+def delete(ref):
+    fo2logger.info('delete {}'.format(ref))
+    key_hash = hash(ref)
+    cache.delete(key_hash)
