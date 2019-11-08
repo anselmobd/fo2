b17657d7771c1d873529f0b333795503d26ee74d
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2019-11-08 18:35:42 -0300

    op_sortimentos: a produzir, de pedido faturado

--- src/lotes/models/op.py
+++ src/lotes/models/op.py
@@ -1139,6 +1139,65 @@ def op_sortimentos(cursor, **kwargs):
             sql=sql,
         )
 
+    elif tipo == 'apf':  # a produzir, de pedido faturado
+        sql = '''
+            WITH opl AS
+            (
+            SELECT
+              o.ORDEM_PRODUCAO
+            , max(lote.SEQ_OPERACAO) SEQ_OPERACAO
+            FROM pcpc_040 lote
+            JOIN PCPC_020 o
+              ON o.ORDEM_PRODUCAO = lote.ORDEM_PRODUCAO
+            LEFT JOIN PEDI_100 ped -- pedido de venda
+              ON ped.PEDIDO_VENDA = o.PEDIDO_VENDA
+             AND ped.STATUS_PEDIDO <> 5 -- não cancelado
+            LEFT JOIN FATU_050 fok
+              ON fok.PEDIDO_VENDA = ped.PEDIDO_VENDA
+             AND fok.SITUACAO_NFISC <> 2  -- cancelada
+            WHERE 1=1
+              {filtro_especifico} -- filtro_especifico
+              {filtra_op} -- filtra_op
+              {filtra_modelo} -- filtra_modelo
+              {filtra_situacao} -- filtra_situacao
+              {filtro_tipo_ref} -- filtro_tipo_ref
+              {filtro_tipo_alt} -- filtro_tipo_alt
+              AND o.PEDIDO_VENDA <> 0
+              AND ped.PEDIDO_VENDA IS NOT NULL
+              AND fok.NUM_NOTA_FISCAL IS NOT NULL
+            GROUP BY
+              o.ORDEM_PRODUCAO
+            )
+            SELECT
+              l.PROCONF_SUBGRUPO TAMANHO
+            , l.PROCONF_ITEM SORTIMENTO
+            , SUM( l.QTDE_A_PRODUZIR_PACOTE ) QUANTIDADE
+            FROM pcpc_040 l
+            JOIN opl
+              ON opl.ORDEM_PRODUCAO = l.ORDEM_PRODUCAO
+             AND opl.SEQ_OPERACAO = l.SEQ_OPERACAO
+            LEFT JOIN BASI_220 tam
+              ON tam.TAMANHO_REF = l.PROCONF_SUBGRUPO
+            GROUP BY
+              tam.ORDEM_TAMANHO
+            , l.PROCONF_SUBGRUPO
+            , l.PROCONF_ITEM
+            ORDER BY
+              tam.ORDEM_TAMANHO
+            , l.PROCONF_ITEM
+        '''.format(
+            filtro_especifico=filtro_especifico,
+            filtra_op=filtra_op,
+            filtra_modelo=filtra_modelo,
+            filtra_situacao=filtra_situacao,
+            filtro_tipo_ref=filtro_tipo_ref,
+            filtro_tipo_alt=filtro_tipo_alt,
+        )
+        grade.value(
+            id='QUANTIDADE',
+            sql=sql,
+        )
+
     elif tipo == 'p':  # Perda
         # sortimento
         grade.value(
