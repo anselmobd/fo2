2a7a0e27677a1a89821d5df84f3b43d12d460db9
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2019-12-12 21:05:25 -0300

    mais um ip da velosat

--- src/fo2/settings.py
+++ src/fo2/settings.py
@@ -39,7 +39,7 @@ ALLOWED_HOSTS = [
     # production
     'intranet', 'intranet.tussor', 'intranet.tussor.com.br', '192.168.1.96',
     'intranet.cuecasduomo.com.br', 'intranet.cuecasduomo.com', '177.23.138.90',
-    '192.141.163.26',
+    '192.141.163.26', '192.141.163.25',
     # development
     '192.168.1.242', '192.168.1.225', 'localhost'
 ]
