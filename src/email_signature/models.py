e5261066b1fff6dff65e5015d0a4071c688609a9
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2019-12-26 17:59:01 -0300

    criando tabela fo2_emsign_account

--- src/email_signature/models.py
+++ src/email_signature/models.py
@@ -1,3 +1,51 @@
 from django.db import models
 
-# Create your models here.
+
+class Account(models.Model):
+    codigo = models.CharField(
+        max_length=20, unique=True, verbose_name='código')
+    nome = models.CharField(
+        null=True, blank=True,
+        max_length=100)
+    setor = models.CharField(
+        null=True, blank=True,
+        max_length=100)
+    email = models.CharField(
+        null=True, blank=True,
+        max_length=100, verbose_name='e-mail')
+    tipo_num_1 = models.CharField(
+        null=True, blank=True,
+        max_length=20, verbose_name='tipo do número 1')
+    num_1 = models.CharField(
+        null=True, blank=True,
+        max_length=20, verbose_name='número 1')
+    tipo_num_2 = models.CharField(
+        null=True, blank=True,
+        max_length=20, verbose_name='tipo do número 2')
+    num_2 = models.CharField(
+        null=True, blank=True,
+        max_length=20, verbose_name='número 2')
+    dir_servidor = models.CharField(
+        null=True, blank=True,
+        max_length=200, verbose_name='diretório no servidor')
+    dir_local = models.CharField(
+        null=True, blank=True,
+        max_length=200, verbose_name='diretório no local')
+    create_at = models.DateTimeField(
+        null=True, blank=True,
+        verbose_name='Criada em')
+    update_at = models.DateTimeField(
+        null=True, blank=True,
+        verbose_name='alterado em')
+
+    def save(self, *args, **kwargs):
+        ''' On create, get timestamps '''
+        if self.id:
+            self.update_at = now
+        else:  # At create have no "id"
+            self.create_at = timezone.now()
+        super(Account, self).save(*args, **kwargs)
+
+    class Meta:
+        db_table = "fo2_emsign_account"
+        verbose_name = "Conta"
