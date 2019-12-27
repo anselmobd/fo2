158f1b44bd880d13c32176784529ff945b277183
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2019-12-27 10:27:11 -0300

    refs_com_movimento: de models para queries

--- src/estoque/models.py
+++ src/estoque/models.py
@@ -7,31 +7,6 @@ from django.db import models
 from fo2.models import rows_to_dict_list_lower, GradeQtd
 
 
-def refs_com_movimento(cursor, data_ini=None):
-    filtro_data_ini = ''
-    if data_ini is not None:
-        filtro_data_ini = (
-            "AND ee.DATA_MOVIMENTO >= "
-            "TO_DATE('{data_ini}', 'yyyy-mm-dd')".format(data_ini=data_ini)
-        )
-
-    sql = '''
-        SELECT DISTINCT
-          ee.GRUPO_ESTRUTURA REF
-        FROM ESTQ_300_ESTQ_310 ee
-        WHERE ee.NIVEL_ESTRUTURA = 1
-          AND ee.GRUPO_ESTRUTURA < 'C0000'
-          AND ee.CODIGO_DEPOSITO IN (231, 101, 102)
-          {filtro_data_ini} -- filtro_data_ini
-        ORDER BY
-          ee.GRUPO_ESTRUTURA
-    '''.format(
-        filtro_data_ini=filtro_data_ini,
-    )
-    cursor.execute(sql)
-    return rows_to_dict_list_lower(cursor)
-
-
 def grade_estoque(
         cursor, ref=None, dep=None, data_ini=None, tipo_grade=None,
         modelo=None):
--- src/estoque/queries/__init__.py
+++ src/estoque/queries/__init__.py
@@ -1,3 +1,4 @@
 from .posicao_estoque import *
 from .referencia_deposito import *
+from .refs_com_movimento import *
 from .valor_mp import *
