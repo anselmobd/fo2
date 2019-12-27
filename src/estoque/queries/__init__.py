007f610878dee8d641e6cf8083573fd70a644277
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2019-12-27 11:24:02 -0300

    get_estoque_dep_ref_cor_tam from models to queries

--- src/estoque/queries/__init__.py
+++ src/estoque/queries/__init__.py
@@ -1,5 +1,6 @@
 from .estoque_deposito_ref_modelo import *
 from .get_transfo2_deposito_ref import *
+from .get_estoque_dep_ref_cor_tam import *
 from .grade_estoque import *
 from .posicao_estoque import *
 from .referencia_deposito import *
