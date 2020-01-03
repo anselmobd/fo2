f6e42f2e5e03dbe7161c3357a72ab1d2c7f464b3
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2020-01-03 15:32:39 -0300

    continuando desenvolvimento

--- src/estoque/management/commands/executa_ajuste.py
+++ src/estoque/management/commands/executa_ajuste.py
@@ -3,6 +3,8 @@ import datetime
 
 from django.core.management.base import BaseCommand, CommandError
 
+from estoque.views import executa_ajuste
+
 
 class Command(BaseCommand):
     help = 'Executa transação no Systêxtil'
@@ -15,17 +17,30 @@ class Command(BaseCommand):
         self.stdout.flush()
 
     def add_arguments(self, parser):
-        parser.add_argument('dep', help='101, 102 ou 231', nargs=1)
-        parser.add_argument('ref', help='5 caracteres', nargs=1)
-        parser.add_argument('cor', help='6 caracteres', nargs=1)
-        parser.add_argument('tam', help='1 a 3 caracteres', nargs=1)
-        parser.add_argument('qtd', type=int, nargs=1,
+        parser.add_argument('dep', help='101, 102 ou 231')
+        parser.add_argument('ref', help='5 caracteres')
+        parser.add_argument('cor', help='6 caracteres')
+        parser.add_argument('tam', help='1 a 3 caracteres')
+        parser.add_argument('qtd', type=int,
                             help='Indica a quantidade transacionada')
-        parser.add_argument('doc', help='número do documento', nargs=1)
+        parser.add_argument('data', help='data do inventário (AAAA-MM-DD)')
+        parser.add_argument('hora', help='hora do inventário (HHhMM)')
 
     def handle(self, *args, **options):
         self.my_println('---')
         self.my_println('{}'.format(datetime.datetime.now()))
+
+        dep = options['dep']
+        ref = options['ref']
+        cor = options['cor']
+        tam = options['tam']
+        qtd = options['qtd']
+        data = options['data']
+        hora = options['hora']
+
+        self.my_println(
+            '%s %s %s %s %s %s %s' % (dep, ref, cor, tam, qtd, data, hora))
+
         try:
             pass
         except Exception as e:
