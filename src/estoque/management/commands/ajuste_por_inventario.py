c9a8591c2f7e3db8cf7c2dd4d032b2e532d6fdba
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2020-01-07 17:21:09 -0300

    Tamanho agora é opcional. Se não definido roda o ajuste para todos os tamanhos da referência.

--- src/estoque/management/commands/ajuste_por_inventario.py
+++ src/estoque/management/commands/ajuste_por_inventario.py
@@ -1,7 +1,10 @@
 from pprint import pprint, pformat
 import datetime
 
+from django.db import connections
 from django.core.management.base import BaseCommand, CommandError
+
+import produto.queries
 
 from estoque.views import ajuste_por_inventario
 
@@ -20,7 +23,7 @@ class Command(BaseCommand):
         parser.add_argument('dep', help='101, 102 ou 231')
         parser.add_argument('ref', help='5 caracteres')
         parser.add_argument('cor', help='6 caracteres')
-        parser.add_argument('tam', help='1 a 3 caracteres')
+        parser.add_argument('tam', help='1 a 3 caracteres', nargs='?')
         parser.add_argument('qtd', type=int, help='quantidade inventariada')
         parser.add_argument('data', help='data do inventário (AAAA-MM-DD)')
         parser.add_argument('hora', help='hora do inventário (HHhMM)')
@@ -37,33 +40,64 @@ class Command(BaseCommand):
         data = options['data']
         hora = options['hora']
 
-        self.my_println(
-            '{} {} {} {} {} {} {}'.format(dep, ref, cor, tam, qtd, data, hora))
+        itens = []
 
-        try:
-            sucesso, mensagem, infos = ajuste_por_inventario(
-                dep,
-                ref,
-                tam,
-                cor,
-                qtd,
-                data,
-                hora,
+        if tam is None:
+            cursor = connections['so'].cursor()
+
+            tamanhos_list = produto.queries.ref_tamanhos(cursor, ref)
+            if len(tamanhos_list) != 0:
+                for row in tamanhos_list:
+                    itens.append({
+                        'ref': ref,
+                        'cor': cor,
+                        'tam': row['TAM'],
+                    })
+
+        if len(itens) == 0:
+            itens.append({
+                'ref': ref,
+                'cor': cor,
+                'tam': tam,
+            })
+
+        for item in itens:
+            self.my_println(
+                '{} {} {} {} {} {} {}'.format(
+                    dep,
+                    item['ref'],
+                    item['tam'],
+                    item['cor'],
+                    qtd,
+                    data,
+                    hora,
+                )
             )
-        except Exception as e:
-            sucesso = False
-            mensagem = '{}'.format(e)
-            infos = []
+            try:
+                # _ = 1/0
+                sucesso, mensagem, infos = ajuste_por_inventario(
+                    dep,
+                    item['ref'],
+                    item['tam'],
+                    item['cor'],
+                    qtd,
+                    data,
+                    hora,
+                )
+            except Exception as e:
+                sucesso = False
+                mensagem = '{}'.format(e)
+                infos = []
 
-        if not sucesso:
-            info_string = ''
-            if len(infos) != 0:
-                info_string = '\n{}'.format(pformat(infos))
-            raise CommandError(
-                'Erro [{}]{}'.format(mensagem, info_string))
-        self.my_println(
-            'Sucesso [{}]'.format(mensagem))
+            if not sucesso:
+                info_string = ''
+                if len(infos) != 0:
+                    info_string = '\n{}'.format(pformat(infos))
+                raise CommandError(
+                    'Erro [{}]{}'.format(mensagem, info_string))
+            self.my_println(
+                'Sucesso [{}]'.format(mensagem))
 
-        self.my_println(pformat(infos))
+            self.my_println(pformat(infos))
 
-        self.my_println(format(datetime.datetime.now(), '%H:%M:%S.%f'))
+            self.my_println(format(datetime.datetime.now(), '%H:%M:%S.%f'))
