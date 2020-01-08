a86a1c8fba780468237a21c8d4f221d368af5e03
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2020-01-08 20:58:10 -0300

    limpando __init__

--- src/estoque/views/__init__.py
+++ src/estoque/views/__init__.py
@@ -1,30 +1,4 @@
-import datetime
-import time
-import re
-import hashlib
-from pprint import pprint
-
-from django.db import connections
 from django.shortcuts import render
-from django.views import View
-from django.urls import reverse
-from django.http import JsonResponse
-from django.contrib.auth.mixins import PermissionRequiredMixin
-from django.contrib.auth.decorators import permission_required
-from django.shortcuts import redirect
-from django.core.exceptions import SuspiciousOperation
-
-from utils.views import (
-    totalize_data, totalize_grouped_data, TableHfs, request_hash_trail)
-from fo2.template import group_rowspan
-from geral.functions import request_user, has_permission
-import produto.queries
-
-from estoque import forms
-from estoque import models
-from estoque import queries
-from estoque.classes import TransacoesDeAjuste
-from estoque.functions import transfo2_num_doc, transfo2_num_doc_dt
 
 from .estoque_na_data import *
 from .executa_ajuste import *
