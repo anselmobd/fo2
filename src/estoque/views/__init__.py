4e9bdb0d0bd30b8871fcdc797c8cfeab5f467e7d
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2020-01-08 20:47:12 -0300

    separando referencia_deposito

--- src/estoque/views/__init__.py
+++ src/estoque/views/__init__.py
@@ -30,6 +30,7 @@ from .executa_ajuste import *
 from .edita_estoque import *
 from .mostra_estoque import *
 from .posicao_estoque import *
+from .referencia_deposito import *
 from .valor_mp import *
 
 
@@ -97,101 +98,5 @@ class RefsComMovimento(View):
             data_ini = form.cleaned_data['data_ini']
             cursor = connections['so'].cursor()
             context.update(self.mount_context(cursor, data_ini))
-        context['form'] = form
-        return render(request, self.template_name, context)
-
-
-class ReferenciaDeposito(View):
-    Form_class = forms.ReferenciasEstoqueForm
-    template_name = 'estoque/referencia_deposito.html'
-    title_name = 'Por referência em depósito'
-
-    def mount_context(self, request, cursor, deposito, modelo):
-        context = {
-            'deposito': deposito,
-            'modelo': modelo,
-            'permission': has_permission(
-                request, 'base.can_adjust_stock'),
-        }
-        try:
-            imodelo = int(modelo)
-        except Exception:
-            imodelo = None
-        if imodelo is not None:
-            anterior = None
-            posterior = None
-            get_prox = False
-            lista = produto.queries.busca_modelo(cursor)
-            for row in lista:
-                item = row['modelo']
-                if get_prox:
-                    posterior = item
-                    break
-                else:
-                    if imodelo == item:
-                        get_prox = True
-                    else:
-                        anterior = item
-            context.update({
-                'anterior': anterior,
-                'posterior': posterior,
-            })
-
-        data = queries.referencia_deposito(cursor, deposito, modelo)
-        if len(data) == 0:
-            context.update({'erro': 'Nada selecionado'})
-            return context
-
-        if has_permission(request, 'base.can_adjust_stock'):
-            for row in data:
-                row['ref|TARGET'] = '_blank'
-                row['ref|LINK'] = reverse(
-                    'estoque:mostra_estoque__get', args=[
-                        row['dep'], row['ref']])
-
-        group = ['dep']
-        tot_conf = {
-            'group': group,
-            'sum': ['estoque', 'falta', 'soma'],
-            'count': [],
-            'descr': {'ref': 'Totais:'},
-        }
-        if deposito == '-':
-            tot_conf.update({
-                'global_sum': ['estoque', 'falta', 'soma'],
-                'global_descr': {'ref': 'Totais gerais:'},
-            })
-        totalize_grouped_data(data, tot_conf)
-        group_rowspan(data, group)
-
-        context.update({
-            'headers': ['Depósito', 'Referência', 'Estoque', 'Falta', 'Soma'],
-            'fields': ['dep', 'ref', 'estoque', 'falta', 'soma'],
-            'group': group,
-            'data': data,
-            'style': {
-                3: 'text-align: right;',
-                4: 'text-align: right;',
-                5: 'text-align: right;',
-            },
-        })
-
-        return context
-
-    def get(self, request, *args, **kwargs):
-        context = {'titulo': self.title_name}
-        form = self.Form_class()
-        context['form'] = form
-        return render(request, self.template_name, context)
-
-    def post(self, request, *args, **kwargs):
-        context = {'titulo': self.title_name}
-        form = self.Form_class(request.POST)
-        if form.is_valid():
-            deposito = form.cleaned_data['deposito']
-            modelo = form.cleaned_data['modelo']
-            cursor = connections['so'].cursor()
-            context.update(self.mount_context(
-                request, cursor, deposito, modelo))
         context['form'] = form
         return render(request, self.template_name, context)
