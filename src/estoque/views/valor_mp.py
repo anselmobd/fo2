f97fae6daf1c32221cfab40756e716b637671951
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2020-01-08 20:12:38 -0300

    separando valor_mp

---
+++ src/estoque/views/valor_mp.py
@@ -0,0 +1,85 @@
+from django.db import connections
+from django.shortcuts import render
+from django.views import View
+
+from utils.views import totalize_grouped_data
+
+from estoque import forms
+from estoque import queries
+
+
+class ValorMp(View):
+    Form_class = forms.ValorForm
+    template_name = 'estoque/valor_mp.html'
+    title_name = 'Valor de estoque'
+
+    def mount_context(
+            self, cursor, nivel, positivos, zerados, negativos, preco_zerado,
+            deposito_compras):
+        context = {
+            'nivel': nivel,
+            'positivos': positivos,
+            'zerados': zerados,
+            'negativos': negativos,
+            'preco_zerado': preco_zerado,
+            'deposito_compras': deposito_compras,
+        }
+
+        data = queries.valor_mp(
+            cursor, nivel, positivos, zerados, negativos, preco_zerado,
+            deposito_compras)
+        if len(data) == 0:
+            context.update({'erro': 'Nada selecionado'})
+            return context
+
+        totalize_grouped_data(data, {
+            'group': ['nivel'],
+            'sum': ['total'],
+            'count': [],
+            'descr': {'deposito': 'Total:'}
+        })
+
+        for row in data:
+            row['qtd|DECIMALS'] = 2
+            row['preco|DECIMALS'] = 2
+            row['total|DECIMALS'] = 2
+
+        context.update({
+            'headers': ('Nível', 'Referência', 'Tamanho', 'Cor',
+                        'Conta estoque', 'Depósito',
+                        'Quantidade', 'Preço', 'Total'),
+            'fields': ('nivel', 'ref', 'tam', 'cor',
+                       'conta_estoque', 'deposito',
+                       'qtd', 'preco', 'total'),
+            'style': {
+                7: 'text-align: right;',
+                8: 'text-align: right;',
+                9: 'text-align: right;',
+            },
+            'data': data,
+        })
+
+        return context
+
+    def get(self, request, *args, **kwargs):
+        context = {'titulo': self.title_name}
+        form = self.Form_class()
+        context['form'] = form
+        return render(request, self.template_name, context)
+
+    def post(self, request, *args, **kwargs):
+        context = {'titulo': self.title_name}
+        form = self.Form_class(request.POST)
+        if form.is_valid():
+            nivel = form.cleaned_data['nivel']
+            positivos = form.cleaned_data['positivos']
+            zerados = form.cleaned_data['zerados']
+            negativos = form.cleaned_data['negativos']
+            preco_zerado = form.cleaned_data['preco_zerado']
+            deposito_compras = form.cleaned_data['deposito_compras']
+            cursor = connections['so'].cursor()
+            context.update(self.mount_context(
+                cursor, nivel, positivos, zerados, negativos, preco_zerado,
+                deposito_compras))
+        context['form'] = form
+        return render(request, self.template_name, context)
