ee788a3437d6aec6f006cef98b48cab668cfbdff
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2020-01-08 19:26:54 -0300

    separando view PosicaoEstoque

--- src/estoque/views/__init__.py
+++ src/estoque/views/__init__.py
@@ -26,6 +26,8 @@ from estoque import queries
 from estoque.classes import TransacoesDeAjuste
 from estoque.functions import transfo2_num_doc, transfo2_num_doc_dt
 
+from .posicao_estoque import *
+
 
 def index(request):
     return render(request, 'estoque/index.html')
@@ -33,134 +35,6 @@ def index(request):
 
 class EstoqueNaData(View):
     pass
-
-
-class PosicaoEstoque(View):
-    Form_class = forms.PorDepositoForm
-    template_name = 'estoque/posicao_estoque.html'
-    title_name = 'Posição de estoque'
-
-    def mount_context(
-            self, cursor, nivel, ref, tam, cor, deposito, agrupamento, tipo):
-        context = {
-            'nivel': nivel,
-            'tam': tam,
-            'cor': cor,
-            'deposito': deposito,
-            'agrupamento': agrupamento,
-            'tipo': tipo,
-            }
-        modelo = None
-        if len(ref) % 5 == 0:
-            context.update({
-                'ref': ref,
-            })
-        else:
-            modelo = ref.lstrip("0")
-            ref = ''
-            context.update({
-                'modelo': modelo,
-            })
-        data = queries.posicao_estoque(
-            cursor, nivel, ref, tam, cor, deposito, zerados=False,
-            group=agrupamento, tipo=tipo, modelo=modelo)
-        if len(data) == 0:
-            context.update({'erro': 'Nada selecionado'})
-            return context
-
-        if agrupamento == 'r':
-            totalize_data(data, {
-                'sum': ['qtd_positiva', 'qtd_negativa'],
-                'count': [],
-                'descr': {'dep_descr': 'Totais:'},
-                'row_style': 'font-weight: bold;',
-            })
-            soma = data[-1]['qtd_positiva'] + data[-1]['qtd_negativa']
-            context.update({
-                'headers': ('Nível', 'Referência', 'Depósito',
-                            'Quantidades Positivas', 'Quantidades Negativas'),
-                'fields': ('cditem_nivel99', 'cditem_grupo', 'dep_descr',
-                           'qtd_positiva', 'qtd_negativa'),
-                'style': {4: 'text-align: right;',
-                          5: 'text-align: right;'},
-                'soma': soma,
-            })
-        elif agrupamento == 'd':
-            totalize_data(data, {
-                'sum': ['qtd_positiva', 'qtd_negativa'],
-                'count': [],
-                'descr': {'dep_descr': 'Totais:'},
-                'row_style': 'font-weight: bold;',
-            })
-            soma = data[-1]['qtd_positiva'] + data[-1]['qtd_negativa']
-            context.update({
-                'headers': ('Depósito',
-                            'Quantidades Positivas', 'Quantidades Negativas'),
-                'fields': ('dep_descr',
-                           'qtd_positiva', 'qtd_negativa'),
-                'style': {2: 'text-align: right;',
-                          3: 'text-align: right;'},
-                'soma': soma,
-            })
-        elif agrupamento == 'tc':
-            totalize_data(data, {
-                'sum': ['qtd_positiva', 'qtd_negativa'],
-                'count': [],
-                'descr': {'cditem_item': 'Totais:'},
-                'row_style': 'font-weight: bold;',
-            })
-            soma = data[-1]['qtd_positiva'] + data[-1]['qtd_negativa']
-            context.update({
-                'headers': ('Nível', 'Tamanho', 'Cor',
-                            'Quantidades Positivas', 'Quantidades Negativas'),
-                'fields': ('cditem_nivel99', 'cditem_subgrupo', 'cditem_item',
-                           'qtd_positiva', 'qtd_negativa'),
-                'style': {4: 'text-align: right;',
-                          5: 'text-align: right;'},
-                'soma': soma,
-            })
-        else:
-            totalize_data(data, {
-                'sum': ['qtd'],
-                'count': [],
-                'descr': {'dep_descr': 'Total:'},
-                'row_style': 'font-weight: bold;',
-            })
-            context.update({
-                'headers': ('Nível', 'Referência', 'Tamanho',
-                            'Cor', 'Depósito', 'Quantidade'),
-                'fields': ('cditem_nivel99', 'cditem_grupo', 'cditem_subgrupo',
-                           'cditem_item', 'dep_descr', 'qtd'),
-                'style': {6: 'text-align: right;'},
-            })
-        context.update({
-            'data': data,
-        })
-
-        return context
-
-    def get(self, request, *args, **kwargs):
-        context = {'titulo': self.title_name}
-        form = self.Form_class()
-        context['form'] = form
-        return render(request, self.template_name, context)
-
-    def post(self, request, *args, **kwargs):
-        context = {'titulo': self.title_name}
-        form = self.Form_class(request.POST)
-        if form.is_valid():
-            nivel = form.cleaned_data['nivel']
-            ref = form.cleaned_data['ref']
-            tam = form.cleaned_data['tam']
-            cor = form.cleaned_data['cor']
-            deposito = form.cleaned_data['deposito']
-            agrupamento = form.cleaned_data['agrupamento']
-            tipo = form.cleaned_data['tipo']
-            cursor = connections['so'].cursor()
-            context.update(self.mount_context(
-                cursor, nivel, ref, tam, cor, deposito, agrupamento, tipo))
-        context['form'] = form
-        return render(request, self.template_name, context)
 
 
 class ValorMp(View):
