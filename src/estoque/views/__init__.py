1bccbf52be653121c5c405e2717339d93e27167d
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2020-01-08 20:51:06 -0300

    separando refs_com_movimento

--- src/estoque/views/__init__.py
+++ src/estoque/views/__init__.py
@@ -31,6 +31,7 @@ from .edita_estoque import *
 from .mostra_estoque import *
 from .posicao_estoque import *
 from .referencia_deposito import *
+from .refs_com_movimento import *
 from .valor_mp import *
 
 
@@ -40,63 +41,3 @@ def index(request):
 
 class EstoqueNaData(View):
     pass
-
-
-class RefsComMovimento(View):
-    Form_class = forms.InventarioExpedicaoForm
-    template_name = 'estoque/refs_com_movimento.html'
-    title_name = 'Referências com movimento'
-
-    def mount_context(self, cursor, data_ini):
-        context = {
-            'data_ini': data_ini,
-        }
-
-        refs = queries.refs_com_movimento(cursor, data_ini)
-        if len(refs) == 0:
-            context.update({'erro': 'Nada selecionado'})
-            return context
-
-        deps = [231, 101, 102]
-        for ref in refs:
-            ref['deps'] = []
-            for dep in deps:
-                header, fields, data, style, total = \
-                    queries.grade_estoque(
-                        cursor, ref['ref'], dep, data_ini=data_ini)
-                grade = None
-                if total != 0:
-                    grade = {
-                        'headers': header,
-                        'fields': fields,
-                        'data': data,
-                        'style': style,
-                    }
-                    ref['deps'].append({
-                        'dep': dep,
-                        'grade': grade,
-                    })
-
-        context.update({
-            'headers': ['Referência'],
-            'fields': ['ref'],
-            'refs': refs,
-        })
-
-        return context
-
-    def get(self, request, *args, **kwargs):
-        context = {'titulo': self.title_name}
-        form = self.Form_class()
-        context['form'] = form
-        return render(request, self.template_name, context)
-
-    def post(self, request, *args, **kwargs):
-        context = {'titulo': self.title_name}
-        form = self.Form_class(request.POST)
-        if form.is_valid():
-            data_ini = form.cleaned_data['data_ini']
-            cursor = connections['so'].cursor()
-            context.update(self.mount_context(cursor, data_ini))
-        context['form'] = form
-        return render(request, self.template_name, context)
---
+++ src/estoque/views/refs_com_movimento.py
@@ -0,0 +1,66 @@
+from django.db import connections
+from django.shortcuts import render
+from django.views import View
+
+from estoque import forms
+from estoque import queries
+
+
+class RefsComMovimento(View):
+    Form_class = forms.InventarioExpedicaoForm
+    template_name = 'estoque/refs_com_movimento.html'
+    title_name = 'Referências com movimento'
+
+    def mount_context(self, cursor, data_ini):
+        context = {
+            'data_ini': data_ini,
+        }
+
+        refs = queries.refs_com_movimento(cursor, data_ini)
+        if len(refs) == 0:
+            context.update({'erro': 'Nada selecionado'})
+            return context
+
+        deps = [231, 101, 102]
+        for ref in refs:
+            ref['deps'] = []
+            for dep in deps:
+                header, fields, data, style, total = \
+                    queries.grade_estoque(
+                        cursor, ref['ref'], dep, data_ini=data_ini)
+                grade = None
+                if total != 0:
+                    grade = {
+                        'headers': header,
+                        'fields': fields,
+                        'data': data,
+                        'style': style,
+                    }
+                    ref['deps'].append({
+                        'dep': dep,
+                        'grade': grade,
+                    })
+
+        context.update({
+            'headers': ['Referência'],
+            'fields': ['ref'],
+            'refs': refs,
+        })
+
+        return context
+
+    def get(self, request, *args, **kwargs):
+        context = {'titulo': self.title_name}
+        form = self.Form_class()
+        context['form'] = form
+        return render(request, self.template_name, context)
+
+    def post(self, request, *args, **kwargs):
+        context = {'titulo': self.title_name}
+        form = self.Form_class(request.POST)
+        if form.is_valid():
+            data_ini = form.cleaned_data['data_ini']
+            cursor = connections['so'].cursor()
+            context.update(self.mount_context(cursor, data_ini))
+        context['form'] = form
+        return render(request, self.template_name, context)
