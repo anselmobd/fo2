12ae8ae868085dd81c0d02f17b4e65f272de439a
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2020-01-08 21:01:35 -0300

    separando index

--- src/estoque/views/__init__.py
+++ src/estoque/views/__init__.py
@@ -1,14 +1,9 @@
-from django.shortcuts import render
-
 from .estoque_na_data import *
 from .executa_ajuste import *
 from .edita_estoque import *
+from .index import *
 from .mostra_estoque import *
 from .posicao_estoque import *
 from .referencia_deposito import *
 from .refs_com_movimento import *
 from .valor_mp import *
-
-
-def index(request):
-    return render(request, 'estoque/index.html')
