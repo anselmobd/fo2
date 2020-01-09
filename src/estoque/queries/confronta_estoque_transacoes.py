df9ee07f20c66923a694d2f09ee5d59aa7e8c422
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2020-01-09 16:31:45 -0300

    utilizando um sql um pouco mais compacto

--- src/estoque/queries/confronta_estoque_transacoes.py
+++ src/estoque/queries/confronta_estoque_transacoes.py
@@ -35,60 +35,42 @@ def confronta_estoque_transacoes(
             (
               SELECT
                 t.SALDO_FISICO
-              FROM (
-                SELECT
-                  t.NIVEL_ESTRUTURA
-                , t.CODIGO_DEPOSITO
-                , t.GRUPO_ESTRUTURA
-                , t.SUBGRUPO_ESTRUTURA
-                , t.ITEM_ESTRUTURA
-                , t.DATA_MOVIMENTO
-                , max(t.SEQUENCIA_INSERCAO) SEQUENCIA_INSERCAO
-                FROM (
-                  SELECT
-                    t.CODIGO_DEPOSITO
-                  , t.NIVEL_ESTRUTURA
-                  , t.GRUPO_ESTRUTURA
-                  , t.SUBGRUPO_ESTRUTURA
-                  , t.ITEM_ESTRUTURA
-                  , max(t.DATA_MOVIMENTO) DATA_MOVIMENTO
-                  FROM filtro
-                  JOIN ESTQ_310 t
-                    ON t.codigo_deposito = filtro.dep
-                   AND t.NIVEL_ESTRUTURA = filtro.nivel
-                   AND t.GRUPO_ESTRUTURA = filtro.ref
-                   AND t.SUBGRUPO_ESTRUTURA = filtro.tam
-                   AND t.ITEM_ESTRUTURA = filtro.cor
-                  GROUP BY
-                    t.CODIGO_DEPOSITO
-                  , t.NIVEL_ESTRUTURA
-                  , t.GRUPO_ESTRUTURA
-                  , t.SUBGRUPO_ESTRUTURA
-                  , t.ITEM_ESTRUTURA
-                ) old1
-                JOIN ESTQ_310 t
-                  ON t.CODIGO_DEPOSITO = old1.CODIGO_DEPOSITO
-                 AND t.NIVEL_ESTRUTURA = old1.NIVEL_ESTRUTURA
-                 AND t.GRUPO_ESTRUTURA = old1.GRUPO_ESTRUTURA
-                 AND t.SUBGRUPO_ESTRUTURA = old1.SUBGRUPO_ESTRUTURA
-                 AND t.ITEM_ESTRUTURA = old1.ITEM_ESTRUTURA
-                 AND t.DATA_MOVIMENTO = old1.DATA_MOVIMENTO
-                GROUP BY
-                  t.NIVEL_ESTRUTURA
-                , t.CODIGO_DEPOSITO
-                , t.GRUPO_ESTRUTURA
-                , t.SUBGRUPO_ESTRUTURA
-                , t.ITEM_ESTRUTURA
-                , t.DATA_MOVIMENTO
-              ) old2
-              JOIN ESTQ_310 t
-                ON t.CODIGO_DEPOSITO = old2.CODIGO_DEPOSITO
-               AND t.NIVEL_ESTRUTURA = old2.NIVEL_ESTRUTURA
-               AND t.GRUPO_ESTRUTURA = old2.GRUPO_ESTRUTURA
-               AND t.SUBGRUPO_ESTRUTURA = old2.SUBGRUPO_ESTRUTURA
-               AND t.ITEM_ESTRUTURA = old2.ITEM_ESTRUTURA
-               AND t.DATA_MOVIMENTO = old2.DATA_MOVIMENTO
-               AND t.SEQUENCIA_INSERCAO = old2.SEQUENCIA_INSERCAO
+              FROM ESTQ_310 t
+              WHERE t.CODIGO_DEPOSITO = filtro.dep
+                AND t.NIVEL_ESTRUTURA = filtro.nivel
+                AND t.GRUPO_ESTRUTURA = filtro.ref
+                AND t.SUBGRUPO_ESTRUTURA = filtro.tam
+                AND t.ITEM_ESTRUTURA = filtro.cor
+                AND t.DATA_MOVIMENTO =
+                    ( SELECT
+                        max(td.DATA_MOVIMENTO)
+                      FROM ESTQ_310 td
+                      WHERE td.codigo_deposito = filtro.dep
+                       AND td.NIVEL_ESTRUTURA = filtro.nivel
+                       AND td.GRUPO_ESTRUTURA = filtro.ref
+                       AND td.SUBGRUPO_ESTRUTURA = filtro.tam
+                       AND td.ITEM_ESTRUTURA = filtro.cor
+                    )
+                AND t.SEQUENCIA_INSERCAO =
+                    ( SELECT
+                        max(td.SEQUENCIA_INSERCAO)
+                      FROM ESTQ_310 td
+                      WHERE td.codigo_deposito = filtro.dep
+                        AND td.NIVEL_ESTRUTURA = filtro.nivel
+                        AND td.GRUPO_ESTRUTURA = filtro.ref
+                        AND td.SUBGRUPO_ESTRUTURA = filtro.tam
+                        AND td.ITEM_ESTRUTURA = filtro.cor
+                        AND td.DATA_MOVIMENTO =
+                            ( SELECT
+                                max(td.DATA_MOVIMENTO)
+                              FROM ESTQ_310 td
+                              WHERE td.codigo_deposito = filtro.dep
+                               AND td.NIVEL_ESTRUTURA = filtro.nivel
+                               AND td.GRUPO_ESTRUTURA = filtro.ref
+                               AND td.SUBGRUPO_ESTRUTURA = filtro.tam
+                               AND td.ITEM_ESTRUTURA = filtro.cor
+                            )
+                    )
             )
           , 0
           ) QTD_OLD
@@ -100,27 +82,25 @@ def confronta_estoque_transacoes(
                   ELSE t.QUANTIDADE
                   END
                 ) QTD
-              FROM filtro
-              JOIN ESTQ_300 t
-                ON t.CODIGO_DEPOSITO = filtro.dep
-               AND t.NIVEL_ESTRUTURA = filtro.nivel
-               AND t.GRUPO_ESTRUTURA = filtro.ref
-               AND t.SUBGRUPO_ESTRUTURA = filtro.tam
-               AND t.ITEM_ESTRUTURA = filtro.cor
+              FROM ESTQ_300 t
+              WHERE t.CODIGO_DEPOSITO = filtro.dep
+                AND t.NIVEL_ESTRUTURA = filtro.nivel
+                AND t.GRUPO_ESTRUTURA = filtro.ref
+                AND t.SUBGRUPO_ESTRUTURA = filtro.tam
+                AND t.ITEM_ESTRUTURA = filtro.cor
             )
           , 0
           ) QTD
         , COALESCE(
             ( SELECT
                 estq_040.qtde_estoque_atu
-              FROM filtro
-              JOIN estq_040
-                ON estq_040.deposito = filtro.dep
-               AND estq_040.cditem_nivel99 = filtro.nivel
-               AND estq_040.cditem_grupo = filtro.ref
-               AND estq_040.cditem_subgrupo = filtro.tam
-               AND estq_040.cditem_item = filtro.cor
-               AND estq_040.lote_acomp = 0
+              FROM estq_040
+              WHERE estq_040.deposito = filtro.dep
+                AND estq_040.cditem_nivel99 = filtro.nivel
+                AND estq_040.cditem_grupo = filtro.ref
+                AND estq_040.cditem_subgrupo = filtro.tam
+                AND estq_040.cditem_item = filtro.cor
+                AND estq_040.lote_acomp = 0
             )
           , 0
           ) STQ
