9e2075a989adac3c6ebfd1c3fa5ce8e7b92a60e4
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2020-01-10 09:57:48 -0300

    iniciada a view estoque_na_data

--- src/estoque/views/estoque_na_data.py
+++ src/estoque/views/estoque_na_data.py
@@ -1,5 +1,40 @@
+from django.db import connections
+from django.shortcuts import render
 from django.views import View
+
+from estoque import forms
 
 
 class EstoqueNaData(View):
-    pass
+
+    def __init__(self):
+        self.Form_class = forms.EstoqueNaDataForm
+        self.template_name = 'estoque/estoque_na_data.html'
+        self.title_name = 'Estoque na data'
+
+    def mount_context(self, cursor, data, hora, deposito):
+        context = {
+            'data': data,
+            'hora': hora,
+            'deposito': deposito,
+            }
+
+        return context
+
+    def get(self, request, *args, **kwargs):
+        context = {'titulo': self.title_name}
+        form = self.Form_class()
+        context['form'] = form
+        return render(request, self.template_name, context)
+
+    def post(self, request, *args, **kwargs):
+        context = {'titulo': self.title_name}
+        form = self.Form_class(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data['data']
+            hora = form.cleaned_data['hora']
+            deposito = form.cleaned_data['deposito']
+            cursor = connections['so'].cursor()
+            context.update(self.mount_context(cursor, data, hora, deposito))
+        context['form'] = form
+        return render(request, self.template_name, context)
