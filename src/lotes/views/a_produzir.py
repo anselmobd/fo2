2bff73e09969ae5db0e8e5a07463898350b053a9
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2020-01-13 19:21:17 -0300

    "A produzir" passa a considerar também estoque em depósitos

--- src/lotes/views/a_produzir.py
+++ src/lotes/views/a_produzir.py
@@ -59,6 +59,8 @@ class AProduzir(O2BaseGetView):
             data_row['meta'] = row['meta_giro'] + row['meta_estoque']
             data_row['total_op'] = 0
             data_row['total_op|CLASS'] = 'total_op-{}'.format(row['modelo'])
+            data_row['total_est'] = 0
+            data_row['total_est|CLASS'] = 'total_est-{}'.format(row['modelo'])
             data_row['total_ped'] = 0
             data_row['total_ped|CLASS'] = 'total_ped-{}'.format(row['modelo'])
             data_row['op_menos_ped'] = 0
@@ -74,8 +76,9 @@ class AProduzir(O2BaseGetView):
         data = sorted(data, key=lambda i: -i['meta'])
 
         totalize_data(data, {
-            'sum': ['meta_giro', 'meta_estoque', 'meta', 'total_op',
-                    'total_ped', 'op_menos_ped', 'a_produzir', 'excesso'],
+            'sum': ['meta_giro', 'meta_estoque', 'meta',
+                    'total_op', 'total_est', 'total_ped',
+                    'op_menos_ped', 'a_produzir', 'excesso'],
             'count': [],
             'descr': {'modelo': 'Totais:'},
             'row_style': 'font-weight: bold;',
@@ -86,13 +89,15 @@ class AProduzir(O2BaseGetView):
         self.context.update({
             'dias_alem_lead': dias_alem_lead,
             'headers': ['Modelo', 'Meta de estoque', 'Meta de giro (lead)',
-                        'Total das metas (A)', 'Total das OPs',
-                        'Carteira de pedidos', 'OPs – Pedidos (B)',
-                        'A produzir (A-B)[+]', 'Excesso (A-B)[-]'],
+                        'Total das metas(A)', 'Total das OPs',
+                        'Total nos depósitos', 'Carteira de pedidos',
+                        'OPs+Depósitos–Pedidos(B)', 'A produzir(A-B)[+]',
+                        'Excesso(A-B)[-]'],
             'fields': ['modelo', 'meta_estoque', 'meta_giro',
                        'meta', 'total_op',
-                       'total_ped', 'op_menos_ped',
-                       'a_produzir', 'excesso'],
+                       'total_est', 'total_ped',
+                       'op_menos_ped', 'a_produzir',
+                       'excesso'],
             'data': data,
             'style': {
                 2: 'text-align: right;',
@@ -103,6 +108,7 @@ class AProduzir(O2BaseGetView):
                 7: 'text-align: right;',
                 8: 'text-align: right;',
                 9: 'text-align: right;',
+                10: 'text-align: right;',
             },
         })
 
