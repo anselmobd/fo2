022ce7f3a41e638cf6ca09f61e2a222da79e9064
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2020-01-15 15:48:58 -0300

    query de deposito agora aceita filtro de somente/exceto alguns depósitos

--- src/geral/queries.py
+++ src/geral/queries.py
@@ -3,9 +3,22 @@ from django.db import connections
 from fo2.models import rows_to_dict_list, rows_to_dict_list_lower
 
 
-def deposito():
+def deposito(somente=None, *depositos):
+    filtra_depositos = ''
+    if somente is not None and depositos is not None:
+        if somente:
+            in_ = 'IN'
+        else:
+            in_ = 'NOT IN'
+        lista_depositos = ''
+        sep = ''
+        for deposito in depositos:
+                lista_depositos += f'{sep}{str(deposito)}'
+                sep = ', '
+        filtra_depositos = f'AND d.CODIGO_DEPOSITO {in_} ({lista_depositos})'
+
     cursor = connections['so'].cursor()
-    sql = '''
+    sql = f'''
         SELECT
           d.CODIGO_DEPOSITO COD
         , d.DESCRICAO DESCR
@@ -21,6 +34,8 @@ def deposito():
           ON f.FORNECEDOR9 = d.CNPJ9
          AND f.FORNECEDOR4 = d.CNPJ4
          AND f.FORNECEDOR2 = d.CNPJ2
+        WHERE 1=1
+          {filtra_depositos} -- filtra_depositos
         ORDER BY
           d.CODIGO_DEPOSITO
     '''
