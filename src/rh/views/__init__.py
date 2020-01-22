68177687f0afb3ebf4f974d4039cf94fd27735b2
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2020-01-22 10:52:35 -0300

    passando views.py para diretório

---
+++ src/rh/views/__init__.py
@@ -0,0 +1,335 @@
+from pprint import pprint
+from datetime import datetime
+
+from django.db import connections
+from django.shortcuts import render
+
+from utils.functions import inc_month
+
+import rh.models
+import rh.queries
+
+
+def limpa_data_futura(lista, campo):
+    for item in lista:
+        if item[campo] > datetime.now().date():
+            item[campo] = None
+
+
+def fotos(request):
+    context = {'titulo': 'Fotos - SIPAT 2018'}
+    return render(request, 'rh/fotos.html', context)
+
+
+def videos(request):
+    context = {'titulo': 'Vídeos - SIPAT 2018'}
+    return render(request, 'rh/videos.html', context)
+
+
+def fotos_homenagem20180614(request):
+    context = {'titulo': 'Fotos - Homenagem a Thiago e Silvio'}
+    return render(request, 'rh/fotos_homenagem20180614.html', context)
+
+
+def fotos_brigadista2018(request):
+    context = {'titulo': 'Fotos - Brigadistas 2018'}
+    return render(request, 'rh/fotos_brigadista2018.html', context)
+
+
+def videos_brigadista2018(request):
+    context = {'titulo': 'Vídeos - Brigadistas 2018'}
+    return render(request, 'rh/videos_brigadista2018.html', context)
+
+
+def fotos_20180720_dia_do_amigo(request):
+    context = {'titulo': 'Fotos - Dia do amigo 2018'}
+    return render(request, 'rh/20180720_dia_do_amigo.html', context)
+
+
+def fotos_2019_04_01_1o_cafe(request):
+    context = {'titulo': '1º Café com RH de 2019'}
+    return render(request, 'rh/fotos_2019_04_01_1o_cafe.html', context)
+
+
+def fotos_2019_03_lorena(request):
+    context = {'titulo': 'Funcionária Lorena homenageada'}
+    return render(request, 'rh/fotos_2019_03_lorena.html', context)
+
+
+def fotos_2019_05_24_bianca(request):
+    context = {'titulo': 'Funcionária Bianca Vasques homenageada'}
+    return render(request, 'rh/fotos_2019_05_24_bianca.html', context)
+
+
+def fotos_2019_06_14_adriana_leo(request):
+    context = {'titulo': 'Funcionários Adriana Martins e Leonardo Andrade '
+                         'homenageados'}
+    return render(request, 'rh/fotos_2019_06_14_adriana_leo.html', context)
+
+
+def fotos_2019_07_12_brigadistas_2019(request):
+    context = {'titulo': 'Aos brigadistas 2019'}
+    return render(
+        request, 'rh/fotos_2019_07_12_brigadistas_2019.html', context)
+
+
+def media_2019_04_09_dicas(request):
+    context = {'titulo': 'Dicas de comportamento no ambiente de trabalho'}
+    return render(request, 'rh/media_2019_04_09_dicas.html', context)
+
+
+def media_2019_07_16_sobre_amizade(request):
+    context = {'titulo': 'Poesia com Rapadura- Falando sobre amizade'}
+    return render(request, 'rh/media_2019_07_16_sobre_amizade.html', context)
+
+
+def media_2019_08_01_25_anos(request):
+    context = {'titulo': '25 anos de empresa de Rosângela e Denilson'}
+    return render(request, 'rh/media_2019_08_01_25_anos.html', context)
+
+
+def index(request):
+    pensamentos = []
+    dicas = [
+      {
+        'data': datetime.strptime('07/11/2018', '%d/%m/%Y').date(),
+        'chamada': 'Inspire Pessoas',
+        'link': '/media/rh/dicas/2018-11-07_inspire_pessoas.jpg',
+      },
+      {
+        'data': datetime.strptime('22/10/2018', '%d/%m/%Y').date(),
+        'chamada': 'Se colocar no lugar do outro',
+        'link': '/media/rh/dicas/2018-10-22_no_lugar_do_outro.jpg',
+      },
+      {
+        'data': datetime.strptime('09/10/2018', '%d/%m/%Y').date(),
+        'chamada': 'Sem coroa',
+        'link': '/media/rh/dicas/2018-10-09_lider.png',
+      },
+      {
+        'data': datetime.strptime('02/10/2018', '%d/%m/%Y').date(),
+        'chamada': 'Alta performance com suas equipes',
+        'link': '/media/rh/dica_2018-10-02_alta_performance_equipe.jpg',
+      },
+      {
+        'data': datetime.strptime('18/09/2018', '%d/%m/%Y').date(),
+        'chamada': 'Pessoas de sucesso',
+        'link': '/media/rh/dica_2018-09-18_pessoas-de-sucesso.png',
+      },
+      {
+        'data': datetime.strptime('05/09/2018', '%d/%m/%Y').date(),
+        'chamada': 'Concentre-se e desconecte-se!',
+        'link': '/media/rh/2018-09-Desconecte.pdf',
+      },
+      {
+        'data': datetime.strptime('04/09/2018', '%d/%m/%Y').date(),
+        'chamada': 'Farmácia interna',
+        'link': '/media/rh/Dica-da-Semana-2018-09-04-farmacia_interna.jpg',
+      },
+      {
+        'data': datetime.strptime('13/08/2018', '%d/%m/%Y').date(),
+        'chamada': 'Inspire pessoas',
+        'link': '/media/rh/Dica-da-Semana-2018-08-13.jpg',
+      },
+      {
+        'data': datetime.strptime('06/08/2018', '%d/%m/%Y').date(),
+        'chamada': 'No lugar do outro',
+        'link': '/media/rh/Dica-da-Semana-2018-08-06.jpg',
+      },
+      {
+        'data': datetime.strptime('30/07/2018', '%d/%m/%Y').date(),
+        'chamada': 'Desenvolvimento da equipe',
+        'link': '/media/rh/dica_2018-07-30_desenvolvimento_da_equipe.png',
+      },
+      {
+        'data': datetime.strptime('25/06/2018', '%d/%m/%Y').date(),
+        'chamada': 'Mudança',
+        'link': '/media/rh/Dica-da-Semana-2018-06-25.jpg',
+      },
+      {
+        'data': datetime.strptime('11/06/2018', '%d/%m/%Y').date(),
+        'chamada': 'Pequenas Gentilezas',
+        'link': '/media/rh/Dica-da-Semana-2018-06-11.jpg',
+      },
+      {
+        'data': datetime.strptime('14/05/2018', '%d/%m/%Y').date(),
+        'chamada': 'Arrisca',
+        'link': '/media/rh/DUOMO-2018-05-14-Pensamento.jpg',
+      },
+      {
+        'data': datetime.strptime('04/05/2018', '%d/%m/%Y').date(),
+        'chamada': 'Custa R$ 0,00',
+        'link': '/media/rh/DUOMO-2018-05-04-Dica.jpg',
+      },
+      {
+        'data': datetime.strptime('23/02/2018', '%d/%m/%Y').date(),
+        'chamada': '3 dicas essenciais',
+        'link': '/media/rh/DUOMO-23-02-Dica-da-Semana.jpg',
+      },
+      {
+        'data': datetime.strptime('22/02/2018', '%d/%m/%Y').date(),
+        'chamada': 'Haja com confiança e seja confiável',
+        'link': '/media/rh/DUOMO-22-02-Dica-da-Semana.jpg',
+      },
+      {
+        'data': datetime.strptime('21/02/2018', '%d/%m/%Y').date(),
+        'chamada': 'Seja proativo',
+        'link': '/media/rh/DUOMO-21-02-Dica-da-Semana.jpg',
+      },
+      {
+        'data': datetime.strptime('20/02/2018', '%d/%m/%Y').date(),
+        'chamada': 'Melhore a comunicação',
+        'link': '/media/rh/DUOMO-20-02-Dica-da-Semana.jpg',
+      },
+      {
+        'data': datetime.strptime('19/02/2018', '%d/%m/%Y').date(),
+        'chamada': 'Como administrar conflitos',
+        'link': '/media/rh/DUOMO-19-02-Dica-da-Semana.jpg',
+      },
+      {
+        'data': datetime.strptime('07/02/2018', '%d/%m/%Y').date(),
+        'chamada': 'Movidos pela motivação',
+        'link': '/media/rh/DUOMO-07-02-Dica-da-Semana.jpg',
+      },
+      {
+        'data': datetime.strptime('06/02/2018', '%d/%m/%Y').date(),
+        'chamada': 'Evolução das alas',
+        'link': '/media/rh/DUOMO-06-02-Dica-da-Semana.jpg',
+      },
+      {
+        'data': datetime.strptime('05/02/2018', '%d/%m/%Y').date(),
+        'chamada': 'O quesito da harmonia',
+        'link': '/media/rh/DUOMO-05-02-Dica-da-Semana.jpg',
+      },
+      {
+        'data': datetime.strptime('02/02/2018', '%d/%m/%Y').date(),
+        'chamada': 'A arte de se reinventar',
+        'link': '/media/rh/DUOMO-02-02-Dica-da-Semana.jpg',
+      },
+    ]
+    limpa_data_futura(dicas, 'data')
+    links = [
+        {'chamada': 'Escola do Trabalhador',
+         'descricao':
+            'O Ministério do Trabalho tem cursos '
+            'gratuitos em várias áreas,<br />basta se '
+            'inscrever e fazer os cursos on line.',
+         'link': 'http://escolatrabalho.gov.br/'},
+    ]
+    rota2020 = [
+        {'data': datetime.strptime('22/01/2020', '%d/%m/%Y').date(),
+         'chamada': 'Transformar juntos',
+         'link': '/rh/campanhas/2020-01-22'},
+        {'data': datetime.strptime('13/01/2020', '%d/%m/%Y').date(),
+         'chamada': 'Inovar é Transformar',
+         'link': '/media/rh/campanhas/2020/01/13/duomo-rota-sucesso-001.jpg'},
+        {'data': datetime.strptime('08/01/2020', '%d/%m/%Y').date(),
+         'chamada': 'Em 2020, vamos embarcar numa viagem rumo ao SUCESSO!',
+         'descricao':
+            'Nessa viagem, iremos carimbar o nosso passaporte com os<br />'
+            'carimbos da Inovação, Produtividade, Desenvolvimento,<br />'
+            'Experiência, Transformação, União e Excelência.<br />'
+            'Vai ser demais!<br />'
+            'Juntos vamos fazer essa incrível Rota do Sucesso!',
+         'link': '/media/rh/campanhas/2020/01/08/duomo-rota-sucesso-w650.jpg'},
+    ]
+    context = {
+        'dicas': dicas,
+        'links': links,
+        'pensamentos': pensamentos,
+        'rota2020': rota2020,
+        }
+    return render(request, 'rh/index.html', context)
+
+
+def campanhas(request, id):
+    if id is None:
+        context = {'titulo': 'Campanhas'}
+        return render(request, 'rh/campanhas.html', context)
+    elif id == '2020-01-22':
+        return render(request, 'rh/campanhas/2020-01-22-RS-IT.html', {})
+
+
+def datas(request, data):
+    if data == 'lista':
+        context = {'titulo': 'Datas comemorativas'}
+        return render(request, 'rh/datas.html', context)
+    elif data == '2019-12-natal':
+        context = {'titulo': '2019 - Feliz Natal!'}
+        return render(request, 'rh/datas/2019-12-natal.html', context)
+    elif data == '2019-12-retrospectiva':
+        context = {'titulo': 'Retrospectiva 2019!'}
+        return render(request, 'rh/datas/2019-12-retrospectiva.html', context)
+
+
+def mensagens(request):
+    context = {'titulo': 'Mensagens'}
+    return render(request, 'rh/mensagens.html', context)
+
+
+def aniversariantes(request, *args, **kwargs):
+    if 'mes' in kwargs:
+        intmes = int(kwargs['mes'])
+        intano = int(kwargs['ano'])
+    else:
+        now = datetime.now()
+        intano = now.year
+        intmes = now.month
+    mes = '{:02}'.format(intmes)
+    datames = datetime(intano, intmes, 1)
+
+    aniversariantes = [
+        ['27/08', 'Arnaldo Silva da Costa', 'Logística'],
+        ['03/10', 'Anselmo Blanco Dominguez', 'TI'],
+        ['17/02', 'Antonio Ricardo Maganhães Moraes', 'Diretor'],
+    ]
+
+    anteriormes = inc_month(datames, -1)
+    intmesant = anteriormes.month
+
+    posteriosmes = inc_month(datames, 1)
+    intmespos = posteriosmes.month
+
+    pcursor = connections['persona'].cursor()
+    data = rh.queries.aniversariantes(pcursor, intmes)
+
+    aniver_db = []
+    for row in data:
+        aniver_db.append(
+           ['{:02d}/{:02d}'.format(int(row['dia']), int(row['mes'])),
+            row['nome'], row['setor']],
+        )
+
+    insere_pessoa = [
+        pessoa for pessoa in aniversariantes if pessoa[0][-2:] == mes]
+
+    for pessoa in insere_pessoa:
+        for i, row in enumerate(aniver_db):
+            if row[0] >= pessoa[0] and row[1] > pessoa[1]:
+                aniver_db.insert(i, pessoa)
+                break
+
+    context = {
+        'datames': datames,
+        'intmesant': intmesant,
+        'intanoant': anteriormes.year,
+        'intmespos': intmespos,
+        'intanopos': posteriosmes.year,
+        'aniversariantes': aniver_db,
+        }
+    return render(request, 'rh/aniversariantes.html', context)
+
+
+def v2018_dia_rosa_fotos(request):
+    context = {'titulo': 'Fotos - Dia Rosa 2018'}
+    return render(request, 'rh/2018_dia_rosa_fotos.html', context)
+
+
+def v2018_dia_rosa_videos(request):
+    context = {'titulo': 'Vídeos - Dia Rosa 2018'}
+    return render(request, 'rh/2018_dia_rosa_videos.html', context)
+
+
+def divulga_atitudes_crise(request):
+    context = {'titulo': 'Atitudes que um líder deve ter em momentos de crise'}
+    return render(request, 'rh/divulga_atitudes_crise.html', context)
