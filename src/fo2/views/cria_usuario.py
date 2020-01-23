9b4f0a004f7d52b4bddc6e6eb40c1bbcba980fa3
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2020-01-23 15:27:07 -0300

    por ora apenas busca trabalhador pelo código

--- src/fo2/views/cria_usuario.py
+++ src/fo2/views/cria_usuario.py
@@ -7,6 +7,8 @@ from django.views import View
 
 from fo2.forms import *
 
+import rh.queries
+
 
 class CriaUsuario(View):
 
@@ -17,10 +19,28 @@ class CriaUsuario(View):
 
     def mount_context(self):
         cursor = connections['so'].cursor()
+        pcursor = connections['persona'].cursor()
 
-    def cleanned_fields_to_context(self):
+        if 'busca' in self.context:
+            data = rh.queries.trabalhadores(
+                pcursor,
+                **{f: self.context[f] for f in ['codigo']})
+            if len(data) != 1:
+                self.context.update({
+                    'erro': 'Trabalhador com esse código não encontrado',
+                })
+                return
+            pprint(data)
+            self.context.update({
+                'trabalhador': data[0],
+            })
+
+    def cleanned_fields_to_context(self, post):
         for field in self.context['form'].fields:
             self.context[field] = self.context['form'].cleaned_data[field]
+        for field in post:
+            if field not in self.context:
+                self.context[field] = post[field]
 
     def get(self, request, *args, **kwargs):
         self.context['form'] = self.Form_class()
@@ -29,7 +49,7 @@ class CriaUsuario(View):
     def post(self, request, *args, **kwargs):
         self.context['form'] = self.Form_class(request.POST)
         if self.context['form'].is_valid():
-            self.cleanned_fields_to_context()
+            self.cleanned_fields_to_context(request.POST)
             self.context['form'] = self.Form_class(self.context)
             self.mount_context()
         return render(request, self.template_name, self.context)
