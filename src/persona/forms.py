04e288082a437e31c43187a827a0beac63add2aa
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2020-01-24 14:24:42 -0300

    verificação partial OK

--- src/persona/forms.py
+++ src/persona/forms.py
@@ -13,10 +13,20 @@ class CriaUsuarioForm(forms.Form):
         }
 
     codigo = forms.CharField(
-        label='Código (matrícula)',
-        required=True, max_length=5,
+        label='Código (matrícula)', required=True, max_length=5,
         widget=forms.TextInput(attrs={
             **autofocus_attrs, **number_attrs}))
 
+    cpf = forms.CharField(
+        label='CPF', required=False, max_length=11,
+        widget=forms.HiddenInput())
+
     def clean_codigo(self):
-        return self.cleaned_data['codigo'].upper().zfill(5)
+        return self.cleaned_data['codigo'].zfill(5)
+
+    def clean_cpf(self):
+        cpf = self.cleaned_data['cpf']
+        cpf = cpf.lstrip('0')
+        if len(cpf) == 0:
+            return ''
+        return cpf.zfill(11)
--- src/persona/urls.py
+++ src/persona/urls.py
@@ -10,6 +10,6 @@ urlpatterns = [
     url(r'^aniversariantes/(?P<ano>\d{4})/(?P<mes>\d{1,2})/?$',
         views.aniversariantes, name='aniversariantes'),
 
-    url(r'^cria_usuario/', views.CriaUsuario.as_view(), name='cria_usuario'),
+    url(r'^cria_usuario/$', views.CriaUsuario.as_view(), name='cria_usuario'),
 
 ]
