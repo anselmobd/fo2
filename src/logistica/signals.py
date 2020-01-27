263dc854e94a12f8e3ab4aadace64b999e86fec9
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2020-01-27 16:52:05 -0300

    signals passou de fo2 para geral, onde já estava o model RecordTracking

--- src/logistica/signals.py
+++ src/logistica/signals.py
@@ -1,6 +1,6 @@
 from django.db.models.signals import post_init, post_save
 
-from fo2.signals import post_init_tracking, post_save_tracking
+from geral.signals import post_init_tracking, post_save_tracking
 from .models import NotaFiscal
 
 
--- src/lotes/signals.py
+++ src/lotes/signals.py
@@ -1,6 +1,6 @@
 from django.db.models.signals import post_init, post_save, post_delete
 
-from fo2.signals import \
+from geral.signals import \
     post_init_tracking, post_save_tracking, post_delete_tracking
 from .models import ModeloTermica, ImpressoraTermica, Lote, \
     SolicitaLote, SolicitaLoteQtd
