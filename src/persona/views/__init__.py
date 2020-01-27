9988477cf879a749ee3a4df12c4bdfc86b1f4640
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2020-01-27 13:43:38 -0300

    padrão em views __init__

--- src/persona/views/__init__.py
+++ src/persona/views/__init__.py
@@ -1,8 +1,3 @@
-from django.shortcuts import render
-
 from .aniversariantes import *
 from .cria_usuario import *
-
-
-def index(request):
-    return render(request, 'persona/index.html')
+from .views import *
