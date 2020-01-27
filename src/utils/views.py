e8a9659c338d824396a24977efde70cdbdc8de37
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2020-01-27 16:55:30 -0300

    WIP group_rowspan foi para utils.views

--- src/utils/views.py
+++ src/utils/views.py
@@ -208,6 +208,23 @@ def totalize_grouped_data(data, config):
     del(data[len(data)-1])
 
 
+def group_rowspan(data, group):
+    noSpan = True
+    for i in range(len(data)):
+        data[i]['rowspan'] = 1
+    inferior = []
+    atual = []
+    for i in range(len(data)-1, -1, -1):
+        atual = [data[i][f] for f in group]
+        if atual == inferior:
+            noSpan = False
+            data[i]['rowspan'] = data[i]['rowspan'] + data[i+1]['rowspan']
+            data[i+1]['rowspan'] = 0
+        inferior = atual[:]
+    if noSpan:
+        del list(group)[:]
+
+
 class TableHfs(object):
     '''
         formato do self.definition:
