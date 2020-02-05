f986d468d7c14fc9f1b930a1f253702000794e26
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2020-02-05 10:01:34 -0300

    melhoria na definição das urls

--- src/email_signature/urls.py
+++ src/email_signature/urls.py
@@ -8,17 +8,17 @@ urlpatterns = [
     url(r'^accounts/$',
         views.Accounts.as_view(), name='accounts'),
 
-    url(r'^emsign_account/$', views.AccountListView.as_view(),
-        name='emsign_account_changelist'),
+    url(r'^account/$', views.AccountListView.as_view(),
+        name='account_changelist'),
 
-    url(r'^emsign_account/add/$', views.AccountCreateView.as_view(),
-        name='emsign_account_add'),
+    url(r'^account/add/$', views.AccountCreateView.as_view(),
+        name='account_add'),
 
-    url(r'^emsign_account/(?P<pk>[^/]+)/$', views.AccountUpdateView.as_view(),
-        name='emsign_account_change'),
+    url(r'^account/(?P<pk>[^/]+)/$', views.AccountUpdateView.as_view(),
+        name='account_change'),
 
-    url(r'^emsign_account/del/(?P<pk>[^/]+)/$',
+    url(r'^account/del/(?P<pk>[^/]+)/$',
         views.AccountDeleteView.as_view(),
-        name='emsign_account_delete'),
+        name='account_delete'),
 
 ]
