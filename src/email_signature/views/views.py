eca74c19294987b84fc47a88a32ba4c9d9703b1f
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2020-02-05 16:42:07 -0300

    layout com as imagens via internet

--- src/email_signature/views/views.py
+++ src/email_signature/views/views.py
@@ -7,4 +7,11 @@ def index(request):
 
 
 def show_template(request):
-    return render(request, 'email_signature/assin-abvtex.html')
+    context = {
+        'nome': 'Nome do funcionário',
+        'setor': 'Setor',
+        'email': 'funcionario@cuecasduomo.com.br',
+        'num_1': '(21) 99999-1111',
+        'num_2': '(21) 99999-2222',
+    }
+    return render(request, 'email_signature/assin-abvtex.html', context)
