f695ac1a1d166ec9a65a6f0d15784b88c3c5bbb0
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2020-02-05 18:07:08 -0300

    evoluindo

--- src/email_signature/views/gerar_assinaturas.py
+++ src/email_signature/views/gerar_assinaturas.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views import View
 
+import email_signature.models as models
+
 
 class GerarAssinaturas(View):
 
@@ -8,8 +10,18 @@ class GerarAssinaturas(View):
         self.template_name = 'email_signature/gerar_assinaturas.html'
         self.context = {'titulo': 'Gerar assinaturas'}
 
-    def mount_context(self):
+    def gerar_assinatura(self, conta):
         pass
+
+    def mount_context(self):
+        contas = models.Account.objects.all()
+        self.context['lista'] = []
+        for conta in contas:
+            erro = self.gerar_assinatura(conta)
+            self.context['lista'].append(dict(
+                conta=conta.codigo,
+                erro=erro,
+            ))
 
     def get(self, request, *args, **kwargs):
         self.mount_context()
