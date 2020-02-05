1e450679cc25f4c8d719fc6744a4673828e3e130
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2020-02-05 16:18:52 -0300

    "mostra template" básico

--- src/email_signature/views/views.py
+++ src/email_signature/views/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'email_signature/index.html')
+
+
+def show_template(request):
+    return render(request, 'email_signature/assin-abvtex.html')
