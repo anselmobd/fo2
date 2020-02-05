29d75c820b6bb5b0b4161d1d25228f3eb82c18a0
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2020-02-05 16:02:12 -0300

    admin para Layout

--- src/email_signature/admin.py
+++ src/email_signature/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from fo2.admin import intr_adm_site
+
+from .models import Layout
+
+
+class LayoutAdmin(admin.ModelAdmin):
+    list_display = ['nome', 'slug', 'template', 'habilitado']
+    search_fields = ['nome']
+    ordering = ['nome']
+    fields = ['nome', 'slug', 'template', 'habilitado']
+    readonly_fields = ['slug']
+
+
+intr_adm_site.register(Layout, LayoutAdmin)
