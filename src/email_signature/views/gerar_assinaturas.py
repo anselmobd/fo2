31838d06e6c50cc7a7e4f75f2c4d1c354582c69a
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2020-02-05 19:07:46 -0300

    começando a carregar o template

--- src/email_signature/views/gerar_assinaturas.py
+++ src/email_signature/views/gerar_assinaturas.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from django.template import loader
 from django.views import View
 
 import email_signature.models as models
+from email_signature.views.views import get_template_file
 
 
 class GerarAssinaturas(View):
@@ -11,9 +13,24 @@ class GerarAssinaturas(View):
         self.context = {'titulo': 'Gerar assinaturas'}
 
     def gerar_assinatura(self, conta):
-        pass
+        context = {
+            'nome': conta.nome,
+            'setor': conta.setor,
+            'email': conta.email,
+            'num_1': conta.num_1,
+            'num_2': conta.num_2,
+        }
+        http_resp = HttpResponse(
+            self.template.render(context, self.request),
+            content_type='text/plain')
+        http_resp['Content-Disposition'] = \
+            'attachment; filename="index.html"'
+        # return http_resp
 
     def mount_context(self):
+        template_file = get_template_file()
+        self.template = loader.get_template(template_file)
+
         contas = models.Account.objects.all()
         self.context['lista'] = []
         for conta in contas:
@@ -22,7 +39,9 @@ class GerarAssinaturas(View):
                 conta=conta.codigo,
                 erro=erro,
             ))
+            return
 
     def get(self, request, *args, **kwargs):
+        self.request = request
         self.mount_context()
         return render(request, self.template_name, self.context)
