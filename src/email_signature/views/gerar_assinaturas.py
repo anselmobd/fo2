39d4f88e919673b7ca5075bf394df2249171655e
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2020-02-05 22:12:48 -0300

    render to string e gravando localmente prara teste

--- src/email_signature/views/gerar_assinaturas.py
+++ src/email_signature/views/gerar_assinaturas.py
@@ -1,5 +1,8 @@
+import os
+from pprint import pprint
+
 from django.shortcuts import render
-from django.template import loader
+from django.template.loader import render_to_string
 from django.views import View
 
 import email_signature.models as models
@@ -20,16 +23,17 @@ class GerarAssinaturas(View):
             'num_1': conta.num_1,
             'num_2': conta.num_2,
         }
-        http_resp = HttpResponse(
-            self.template.render(context, self.request),
-            content_type='text/plain')
-        http_resp['Content-Disposition'] = \
-            'attachment; filename="index.html"'
-        # return http_resp
+        try:
+            assinatura = render_to_string(self.template_file, context)
+            # arquivo = os.path.join(conta.dir_servidor, conta.arquivo)
+            arquivo = os.path.join('.', conta.arquivo)
+            with open(arquivo, 'w') as index:
+                index.write(assinatura)
+        except Exception:
+            return 'Erro'
 
     def mount_context(self):
-        template_file = get_template_file()
-        self.template = loader.get_template(template_file)
+        self.template_file = get_template_file()
 
         contas = models.Account.objects.all()
         self.context['lista'] = []
@@ -42,6 +46,5 @@ class GerarAssinaturas(View):
             return
 
     def get(self, request, *args, **kwargs):
-        self.request = request
         self.mount_context()
         return render(request, self.template_name, self.context)
