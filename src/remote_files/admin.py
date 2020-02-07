b55cfc0824dca221f0c95510f06ca8b7fe60b256
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2020-02-07 15:19:09 -0300

    acesso no admin para a tabela de servidores

--- src/remote_files/admin.py
+++ src/remote_files/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from fo2.admin import intr_adm_site
+
+from .models import Servidor
+
+
+class ServidorAdmin(admin.ModelAdmin):
+    list_display = ['descricao', 'hostname', 'ip4']
+    search_fields = ['descricao', 'hostname', 'ip4']
+    ordering = ['descricao']
+    fields = ['descricao', 'hostname', 'ip4']
+
+
+intr_adm_site.register(Servidor, ServidorAdmin)
