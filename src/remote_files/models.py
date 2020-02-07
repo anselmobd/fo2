d9142ea81909b4ad63236ccdf6f4a1d7482dc092
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2020-02-07 14:42:10 -0300

    criando tabale de servidores

--- src/remote_files/models.py
+++ src/remote_files/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Servidor(models.Model):
+    descricao = models.CharField(
+        unique=True, max_length=200,
+        verbose_name='descrição',
+        )
+    hostname = models.CharField(
+        unique=True, max_length=50,
+        )
+    ip4 = models.CharField(
+        unique=True, max_length=15,
+        )
+
+    def __str__(self):
+        return self.descricao
+
+    class Meta:
+        db_table = 'fo2_rfile_servidor'
+        verbose_name = 'Servidor'
+        verbose_name_plural = 'Servidores'
