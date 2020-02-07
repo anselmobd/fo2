87ddec64f44ae8cd241e6c7c2aaab1e10fc60436
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2020-02-06 21:37:45 -0300

    exige permissões nas views

--- src/email_signature/views/account.py
+++ src/email_signature/views/account.py
@@ -1,3 +1,7 @@
+from django.contrib.auth.mixins import (
+    PermissionRequiredMixin,
+    LoginRequiredMixin
+    )
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 
@@ -8,23 +12,27 @@ _fields = ('email', 'nome', 'setor', 'ddd_1', 'num_1', 'ddd_2', 'num_2',
            'dir_servidor')
 
 
-class AccountListView(ListView):
+class AccountListView(PermissionRequiredMixin, ListView):
+    permission_required = 'email_signature.can_edit_mail_signature'
     model = Account
     context_object_name = 'accounts'
 
 
-class AccountCreateView(CreateView):
+class AccountCreateView(PermissionRequiredMixin, CreateView):
+    permission_required = 'email_signature.can_edit_mail_signature'
     model = Account
     fields = _fields
     success_url = reverse_lazy('email_signature:account_list')
 
 
-class AccountUpdateView(UpdateView):
+class AccountUpdateView(PermissionRequiredMixin, UpdateView):
+    permission_required = 'email_signature.can_edit_mail_signature'
     model = Account
     fields = _fields
     success_url = reverse_lazy('email_signature:account_list')
 
 
-class AccountDeleteView(DeleteView):
+class AccountDeleteView(PermissionRequiredMixin, DeleteView):
+    permission_required = 'email_signature.can_edit_mail_signature'
     model = Account
     success_url = reverse_lazy('email_signature:account_list')
