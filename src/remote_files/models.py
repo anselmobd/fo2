0278c10ce3f94bd12ff47f8c049bf34a7a1e7d8b
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2020-02-07 16:06:48 -0300

    criando tabela de diretórios

--- src/remote_files/models.py
+++ src/remote_files/models.py
@@ -20,3 +20,19 @@ class Servidor(models.Model):
         db_table = 'fo2_rfile_servidor'
         verbose_name = 'Servidor'
         verbose_name_plural = 'Servidores'
+
+
+class Diretorio(models.Model):
+    servidor = models.ForeignKey(Servidor, on_delete=models.PROTECT)
+    descricao = models.CharField(max_length=200, verbose_name='descrição')
+    caminho = models.CharField(max_length=300)
+    file_perm = models.IntegerField(verbose_name='permissões (octal)')
+    file_usuer = models.CharField(max_length=50, verbose_name='usuário')
+    file_group = models.CharField(max_length=50, verbose_name='grupo')
+
+    def __str__(self):
+        return f'({self.servidor.descricao}) {self.descricao} - {self.caminho}'
+
+    class Meta:
+        db_table = 'fo2_rfile_folder'
+        verbose_name = 'Diretório'
