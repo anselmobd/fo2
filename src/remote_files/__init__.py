18f1a9dadc1b6323135b54993fd8739f29d2ed7f
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2020-02-07 15:17:32 -0300

    Nome da aplicação remote_files para o admin

---
+++ src/remote_files/__init__.py
@@ -0,0 +1,1 @@
+default_app_config = 'remote_files.apps.RemoteFilesConfig'
--- src/remote_files/apps.py
+++ src/remote_files/apps.py
@@ -3,3 +3,4 @@ from django.apps import AppConfig
 
 class RemoteFilesConfig(AppConfig):
     name = 'remote_files'
+    verbose_name = 'Arquivos remotos'
