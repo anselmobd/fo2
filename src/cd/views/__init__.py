b82aa8479296556795ec0daea3ce621cb000c854
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2020-02-14 09:30:49 -0300

    Separando a classe Solicitacoes

--- src/cd/views/__init__.py
+++ src/cd/views/__init__.py
@@ -27,6 +27,8 @@ from geral.functions import request_user, has_permission
 import cd.models as models
 import cd.forms
 
+from .solicitacoes import *
+
 
 def index(request):
     return render(request, 'cd/index.html')
@@ -1060,123 +1062,6 @@ class InconsistenciasDetalhe(View):
         cursor = connections['so'].cursor()
         data = self.mount_context(cursor, op)
         context.update(data)
-        return render(request, self.template_name, context)
-
-
-class Solicitacoes(LoginRequiredMixin, View):
-
-    def __init__(self):
-        self.Form_class = cd.forms.SolicitacaoForm
-        self.template_name = 'cd/solicitacoes.html'
-        self.title_name = 'Solicitações de lotes'
-        self.SL = lotes.models.SolicitaLote
-        self.id = None
-
-    def lista(self):
-        fields = ('codigo', 'ativa', 'descricao',
-                  'usuario__username', 'update_at',
-                  'total_qtd', 'total_no_cd')
-        descriptions = ('Código', 'Ativa para o usuário', 'Descrição',
-                        'Usuário', 'Última alteração', 'Qtd. total',
-                        'Qtd. do CD')
-        headers = dict(zip(fields, descriptions))
-
-        cursor_def = connection.cursor()
-        data = models.solicita_lote(cursor_def)
-        for row in data:
-            row['codigo|LINK'] = reverse(
-                'cd:solicitacao_detalhe', args=[row['id']])
-        context = {
-            'headers': headers,
-            'fields': fields,
-            'data': data,
-        }
-        return context
-
-    def get(self, request, *args, **kwargs):
-        context = {'titulo': self.title_name}
-
-        if 'id' in kwargs:
-            self.id = kwargs['id']
-
-        if self.id:
-            if self.id == 'add':
-                if has_permission(request, 'lotes.add_solicitalote'):
-                    context['form'] = self.Form_class()
-                else:
-                    context['msg_erro'] = \
-                        'Usuário não tem direito de criar solicitações.'
-                    self.id = None
-            else:
-                if has_permission(request, 'lotes.change_solicitalote'):
-                    data = self.SL.objects.filter(id=self.id)
-                    if len(data) == 0:
-                        self.id = None
-                    else:
-                        row = data[0]
-                        context['id'] = self.id
-                        context['form'] = self.Form_class(
-                            initial={'codigo': row.codigo,
-                                     'descricao': row.descricao,
-                                     'ativa': row.ativa})
-                else:
-                    context['msg_erro'] = \
-                        'Usuário não tem direito de alterar solicitações.'
-                    self.id = None
-
-        if not self.id:
-            context.update(self.lista())
-
-        return render(request, self.template_name, context)
-
-    def post(self, request, *args, **kwargs):
-        context = {'titulo': self.title_name}
-
-        if 'id' in kwargs:
-            self.id = kwargs['id']
-
-        form = self.Form_class(request.POST)
-        if form.is_valid():
-            codigo = form.cleaned_data['codigo']
-            descricao = form.cleaned_data['descricao']
-            ativa = form.cleaned_data['ativa']
-            if ativa:
-                outras_ativas = self.SL.objects.filter(
-                    usuario=request.user,
-                    ativa=True
-                )
-                if self.id != 'add':
-                    outras_ativas = outras_ativas.exclude(id=self.id)
-                if len(outras_ativas) != 0:
-                    for soli in outras_ativas:
-                        soli.ativa = False
-                        soli.save()
-            grava = True
-            if self.id == 'add':
-                try:
-                    self.SL.objects.get(codigo=codigo.upper())
-                    context['msg_erro'] = 'Já existe uma solicitação ' \
-                        'com o código "{}".'.format(codigo)
-                    grava = False
-                except self.SL.DoesNotExist:
-                    solicitacao = self.SL()
-            else:
-                solicitacao = self.SL.objects.get(id=self.id)
-
-            if grava:
-                try:
-                    solicitacao.usuario = request.user
-                    solicitacao.codigo = codigo
-                    solicitacao.descricao = descricao
-                    solicitacao.ativa = ativa
-                    solicitacao.save()
-                except IntegrityError as e:
-                    context['msg_erro'] = 'Ocorreu um erro ao gravar ' \
-                        'a solicitação. <{}>'.format(str(e))
-
-            context.update(self.lista())
-        else:
-            self.context['form'] = form
         return render(request, self.template_name, context)
 
 
