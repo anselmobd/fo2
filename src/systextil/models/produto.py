bf49f88c63a7d300b2c1569b05bde1aa9e6068a7
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2020-03-10 16:23:28 -0300

    movendo model Produto para app systextil

--- src/systextil/models/produto.py
+++ src/systextil/models/produto.py
@@ -31,3 +31,21 @@ class Familia(models.Model):
         app_label = 'systextil'
         db_table = "BASI_180"
         verbose_name = "Família"
+
+
+class Produto(models.Model):
+    nivel_estrutura = models.CharField(
+        primary_key=True, max_length=1,
+        verbose_name='Nível')
+    referencia = models.CharField(
+        primary_key=True, max_length=5,
+        verbose_name='Referência')
+
+    def __str__(self):
+        return '{}.{}'.format(self.nivel_estrutura, self.referencia)
+
+    class Meta:
+        managed = False
+        app_label = 'systextil'
+        db_table = "BASI_030"
+        verbose_name = "Produto (Systêxtil)"
