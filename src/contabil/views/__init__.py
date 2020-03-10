5471444d29e5fb635b0e69e5a5384df70d0530bc
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2020-03-10 10:56:47 -0300

    separando RemessaIndustr

--- src/contabil/views/__init__.py
+++ src/contabil/views/__init__.py
@@ -11,6 +11,8 @@ from utils.views import totalize_grouped_data, totalize_data, group_rowspan
 import contabil.forms as forms
 import contabil.queries as queries
 
+from .remeindu import *
+
 
 def index(request):
     return render(request, 'contabil/index.html')
@@ -93,89 +95,6 @@ class InfAdProd(View):
         if form.is_valid():
             pedido = form.cleaned_data['pedido']
             context.update(self.mount_context(pedido))
-        context['form'] = form
-        return render(request, self.template_name, context)
-
-
-class RemessaIndustr(View):
-    Form_class = forms.RemessaIndustrForm
-    template_name = 'contabil/remeindu.html'
-    title_name = 'Industrialização por OP'
-
-    def get(self, request):
-        context = {'titulo': self.title_name}
-        form = self.Form_class()
-        context['form'] = form
-        return render(request, self.template_name, context)
-
-    def post(self, request):
-        form = self.Form_class(request.POST)
-        context = {'titulo': self.title_name}
-        if form.is_valid():
-            data_de = form.cleaned_data['data_de']
-            data_ate = form.cleaned_data['data_ate']
-            faccao = form.cleaned_data['faccao']
-            cliente = form.cleaned_data['cliente']
-            pedido = form.cleaned_data['pedido']
-            pedido_cliente = form.cleaned_data['pedido_cliente']
-            op = form.cleaned_data['op']
-            retorno = form.cleaned_data['retorno']
-            detalhe = form.cleaned_data['detalhe']
-
-            cursor = connections['so'].cursor()
-            data = queries.reme_indu(
-                cursor, dt_saida_de=data_de, dt_saida_ate=data_ate,
-                faccao=faccao, cliente=cliente,
-                pedido=pedido, pedido_cliente=pedido_cliente, op=op,
-                retorno=retorno, detalhe=detalhe)
-            if len(data) == 0:
-                context['erro'] = 'Remessa não encontrada'
-            else:
-                total_pecas = 0
-                for row in data:
-                    total_pecas += row['QTD']
-                    row['DT'] = row['DT'].date()
-                    if row['DT_RET'] is None:
-                        row['DT_RET'] = '-'
-                        row['NF_RET'] = '-'
-                        row['QTD_RET'] = '-'
-                    else:
-                        row['DT_RET'] = row['DT_RET'].date()
-                    if row['PED'] == 0:
-                        row['PED'] = '-'
-                        row['CLI'] = '-'
-                    if row['PED_CLI'] is None:
-                        row['PED_CLI'] = '-'
-                    if row['TAM'] is None:
-                        row['TAM'] = '-'
-                    row['OP|LINK'] = reverse(
-                        'producao:op__get', args=[row['OP']])
-                    row['OS|LINK'] = reverse(
-                        'producao:os__get', args=[row['OS']])
-                    if row['PED'] != '-':
-                        row['PED|LINK'] = reverse(
-                            'producao:pedido__get', args=[row['PED']])
-                context.update({
-                    'data_de': data_de,
-                    'data_ate': data_ate,
-                    'faccao': faccao,
-                    'cliente': cliente,
-                    'pedido': pedido,
-                    'pedido_cliente': pedido_cliente,
-                    'retorno': retorno,
-                    'detalhe': detalhe,
-                    'total_pecas': total_pecas,
-                    'headers': ('OP', 'Ref.', 'Cor', 'Tam.', 'OS', 'Quant.',
-                                'Data saída', 'NF. saída', 'Facção',
-                                'Data retorno', 'NF retorno', 'Quant. retorno',
-                                'Pedido', 'Ped. cliente', 'Cliente'),
-                    'fields': ('OP', 'REF', 'COR', 'TAM', 'OS', 'QTD',
-                               'DT', 'NF', 'FACCAO',
-                               'DT_RET', 'NF_RET', 'QTD_RET',
-                               'PED', 'PED_CLI', 'CLI'),
-                    'data': data,
-                })
-
         context['form'] = form
         return render(request, self.template_name, context)
 
