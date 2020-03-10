54731ea444026f59b31d023db5449d094067440e
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2020-03-10 15:09:29 -0300

    move ContaEstoque to app systextil

--- src/insumo/models.py
+++ src/insumo/models.py
@@ -2,24 +2,6 @@ from django.utils import timezone
 from django.db import models
 
 
-class ContaEstoque(models.Model):
-    conta_estoque = models.IntegerField(
-        primary_key=True,
-        verbose_name='Código')
-    descr_ct_estoque = models.CharField(
-        max_length=100,
-        verbose_name='Descrição')
-
-    def __str__(self):
-        return '{}-{}'.format(self.conta_estoque, self.descr_ct_estoque)
-
-    class Meta:
-        managed = False
-        app_label = 'systextil'
-        db_table = "BASI_150"
-        verbose_name = "Conta de estoque"
-
-
 class PeriodoConfeccaoManager(models.Manager):
     def get_queryset(self):
         return super(PeriodoConfeccaoManager, self).get_queryset().filter(
--- src/systextil/models/estoque.py
+++ src/systextil/models/estoque.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class ContaEstoque(models.Model):
+    conta_estoque = models.IntegerField(
+        primary_key=True,
+        verbose_name='Código')
+    descr_ct_estoque = models.CharField(
+        max_length=100,
+        verbose_name='Descrição')
+
+    def __str__(self):
+        return '{}-{}'.format(self.conta_estoque, self.descr_ct_estoque)
+
+    class Meta:
+        managed = False
+        app_label = 'systextil'
+        db_table = "BASI_150"
+        verbose_name = "Conta de estoque"
