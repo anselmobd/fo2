d81799720fa37fddd0fe87ae86aeffecb1fab41d
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2020-03-10 16:19:51 -0300

    movendo model Familia para app systextil

--- src/systextil/models/produto.py
+++ src/systextil/models/produto.py
@@ -15,3 +15,19 @@ class Colecao(models.Model):
         app_label = 'systextil'
         db_table = "BASI_140"
         verbose_name = "Coleção"
+
+
+class Familia(models.Model):
+    divisao_producao = models.IntegerField(primary_key=True)
+    descricao = models.CharField(
+        max_length=20,
+        verbose_name='Descrição')
+
+    def __str__(self):
+        return '{}-{}'.format(self.divisao_producao, self.descricao)
+
+    class Meta:
+        managed = False
+        app_label = 'systextil'
+        db_table = "BASI_180"
+        verbose_name = "Família"
