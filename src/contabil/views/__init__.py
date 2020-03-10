737133b98c0b69ee9fcc7f056afb01fdca0cfdb7
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2020-03-10 11:14:35 -0300

    Separando index em views.py

--- src/contabil/views/__init__.py
+++ src/contabil/views/__init__.py
@@ -1,10 +1,5 @@
-from django.shortcuts import render
-
-from .nota_fiscal import *
 from .infadprod import *
+from .nota_fiscal import *
 from .remeindu import *
 from .remeindunf import *
-
-
-def index(request):
-    return render(request, 'contabil/index.html')
+from .views import *
