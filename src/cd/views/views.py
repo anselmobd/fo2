e92570fb1e96c7ef8d09c85d5b29b179083fa22a
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2020-03-12 09:18:31 -0300

    separando view EnderecoLote

--- src/cd/views/views.py
+++ src/cd/views/views.py
@@ -37,65 +37,6 @@ def teste_som(request):
     return render(request, 'cd/teste_som.html', context)
 
 
-class EnderecoLote(View):
-
-    def __init__(self):
-        self.Form_class = cd.forms.AskLoteForm
-        self.template_name = 'cd/endereco_lote.html'
-        self.title_name = 'Verifica endereço de Lote'
-
-    def mount_context(self, request, form):
-        lote = form.cleaned_data['lote']
-
-        context = {'lote': lote}
-
-        try:
-            lote_rec = lotes.models.Lote.objects.get(lote=lote)
-        except lotes.models.Lote.DoesNotExist:
-            context.update({'erro': 'Lote não encontrado'})
-            return context
-
-        if lote_rec.local is None or lote_rec.local == '':
-            local = 'Não endereçado'
-            lotes_no_local = -1
-        else:
-            local = lote_rec.local
-            lotes_no_local = len(lotes.models.Lote.objects.filter(
-                local=lote_rec.local))
-
-        context.update({
-            'op': lote_rec.op,
-            'referencia': lote_rec.referencia,
-            'cor': lote_rec.cor,
-            'tamanho': lote_rec.tamanho,
-            'qtd_produzir': lote_rec.qtd_produzir,
-            'local': local,
-            'q_lotes': lotes_no_local,
-            })
-
-        return context
-
-    def get(self, request, *args, **kwargs):
-        if 'lote' in kwargs and kwargs['lote'] is not None:
-            return self.post(request, *args, **kwargs)
-        else:
-            context = {'titulo': self.title_name}
-            form = self.Form_class()
-            context['form'] = form
-            return render(request, self.template_name, context)
-
-    def post(self, request, *args, **kwargs):
-        context = {'titulo': self.title_name}
-        form = self.Form_class(request.POST)
-        if 'lote' in kwargs and kwargs['lote'] is not None:
-            form.data['lote'] = kwargs['lote']
-        if form.is_valid():
-            data = self.mount_context(request, form)
-            context.update(data)
-        context['form'] = form
-        return render(request, self.template_name, context)
-
-
 class Grade(View):
 
     def __init__(self):
