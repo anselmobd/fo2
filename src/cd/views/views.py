74b020c308009d046ae80b6b79e729bad2804f60
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2020-03-12 09:11:17 -0300

    separando ajax view solicita_lote

--- src/cd/views/views.py
+++ src/cd/views/views.py
@@ -106,63 +106,6 @@ class Mapa(View):
         return render(request, self.template_name, context)
 
 
-def solicita_lote(request, solicitacao_id, lote, qtd):
-    data = {}
-
-    try:
-        solicitacao = lotes.models.SolicitaLote.objects.get(
-            id=solicitacao_id)
-    except lotes.models.SolicitaLote.DoesNotExist:
-        data.update({
-            'result': 'ERR',
-            'descricao_erro': 'Solicitação não encontrada por id',
-        })
-        return JsonResponse(data, safe=False)
-
-    try:
-        lote_rec = lotes.models.Lote.objects.get(lote=lote)
-    except lotes.models.Lote.DoesNotExist:
-        data.update({
-            'result': 'ERR',
-            'descricao_erro': 'Lote não encontrado',
-        })
-        return JsonResponse(data, safe=False)
-
-    try:
-        iqtd = int(qtd)
-    except ValueError:
-        iqtd = -1
-    if iqtd < 1 or iqtd > lote_rec.qtd:
-        data.update({
-            'result': 'ERR',
-            'descricao_erro': 'Quantidade inválida',
-        })
-        return JsonResponse(data, safe=False)
-
-    try:
-        solicita = lotes.models.SolicitaLoteQtd()
-        solicita.solicitacao = solicitacao
-        solicita.lote = lote_rec
-        solicita.qtd = iqtd
-        solicita.save()
-    except lotes.models.Lote.DoesNotExist:
-        data.update({
-            'result': 'ERR',
-            'descricao_erro':
-                'Erro ao criar registro de quantidade solicitada',
-        })
-        return JsonResponse(data, safe=False)
-
-    data = {
-        'result': 'OK',
-        'solicitacao_id': solicitacao_id,
-        'lote': lote,
-        'qtd': iqtd,
-        'solicita_qtd_id': solicita.id,
-    }
-    return JsonResponse(data, safe=False)
-
-
 class EnderecoLote(View):
 
     def __init__(self):
