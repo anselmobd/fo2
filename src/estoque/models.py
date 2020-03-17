8e25293f12831f812fea64bc931386c8bfc0c27e
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2020-03-17 18:13:02 -0300

    criando fo2_est_tipo_mov

---
+++ src/estoque/models.py
@@ -0,0 +1,23 @@
+from pprint import pprint
+
+from django.db import models
+
+
+class TipoMovStq(models.Model):
+    descricao = models.CharField(
+        max_length=100,
+        verbose_name='Descrição')
+    trans_saida = models.IntegerField(
+        default=0,
+        verbose_name='Transação de saída')
+    trans_entrada = models.IntegerField(
+        default=0,
+        verbose_name='Transação de entrada')
+
+    def __str__(self):
+        return self.descricao
+
+    class Meta:
+        db_table = "fo2_est_tipo_mov"
+        verbose_name = "Tipo de movimento de estoque"
+        verbose_name_plural = "Tipos de movimentos de estoque"
