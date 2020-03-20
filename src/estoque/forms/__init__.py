45468de88c9970eaef15ad6fc9728681d928836f
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2020-03-20 11:37:46 -0300

    Separando TransferenciaForm

--- src/estoque/forms/__init__.py
+++ src/estoque/forms/__init__.py
@@ -6,6 +6,8 @@ import geral.queries
 import geral.functions
 from base.forms import O2BaseForm, O2FieldRefForm, O2FieldModeloForm
 
+from .transferencia import *
+
 
 class PorDepositoForm(forms.Form):
     nivel = forms.IntegerField(
@@ -174,43 +176,6 @@ class MostraEstoqueForm(forms.Form):
 
 
 class ConfrontaEstoqueForm(forms.Form):
-    ref = forms.CharField(
-        label='Referência ou modelo',
-        required=False, min_length=1, max_length=5,
-        widget=forms.TextInput(attrs={'type': 'string',
-                               'autofocus': 'autofocus'}))
-
-    tam = forms.CharField(
-        label='Tamanho', required=False, min_length=1, max_length=3,
-        widget=forms.TextInput(attrs={'type': 'string'}))
-
-    cor = forms.CharField(
-        label='Cor', required=False, max_length=6,
-        widget=forms.TextInput(attrs={'type': 'string'}))
-
-    CHOICES = geral.functions.depositos_choices(only=(101, 102, 122, 231))
-    deposito = forms.ChoiceField(
-        label='Depósito', required=True,
-        choices=CHOICES, initial='')
-
-    def upper_clean(self, field_name):
-        field = self.cleaned_data[field_name].upper()
-        data = self.data.copy()
-        data[field_name] = field
-        self.data = data
-        return field
-
-    def clean_ref(self):
-        return self.upper_clean('ref')
-
-    def clean_tam(self):
-        return self.upper_clean('tam')
-
-    def clean_cor(self):
-        return self.upper_clean('cor')
-
-
-class TransferenciaForm(forms.Form):
     ref = forms.CharField(
         label='Referência ou modelo',
         required=False, min_length=1, max_length=5,
