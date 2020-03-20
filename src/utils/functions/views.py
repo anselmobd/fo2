b301a123789c27f467c00b2537ae96717d8d4f5f
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2020-03-20 16:19:38 -0300

    cleanned_fields_to_context __DOC__

--- src/utils/functions/views.py
+++ src/utils/functions/views.py
@@ -2,5 +2,9 @@ from pprint import pprint
 
 
 def cleanned_fields_to_context(self):
+    '''
+        Assume que há self.context['form'] e cria um
+        self.context['field'] para cada field no form
+    '''
     for field in self.context['form'].fields:
         self.context[field] = self.context['form'].cleaned_data[field]
