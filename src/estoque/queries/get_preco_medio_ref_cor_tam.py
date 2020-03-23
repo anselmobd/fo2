da60d1092b7c503c918673a4b09e5469b57962cc
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2020-03-23 15:18:28 -0300

    criada get_preco_medio_niv_ref_cor_tam a partir de get_preco_medio_ref_cor_tam

--- src/estoque/queries/get_preco_medio_ref_cor_tam.py
+++ src/estoque/queries/get_preco_medio_ref_cor_tam.py
@@ -1,14 +1,14 @@
 from utils.functions.models import rows_to_dict_list_lower
 
 
-def get_preco_medio_ref_cor_tam(cursor, ref, cor, tam):
+def get_preco_medio_niv_ref_cor_tam(cursor, niv, ref, cor, tam):
     filtra_tam = ''
     if tam != '000':
-        filtra_tam = """--
+        filtra_tam = f"""--
             AND rtc.SUBGRU_ESTRUTURA = '{tam}'
-        """.format(tam=tam)
+        """
 
-    sql = '''
+    sql = f'''
         SELECT
           rtc.GRUPO_ESTRUTURA REF
         , rtc.SUBGRU_ESTRUTURA TAM
@@ -16,14 +16,14 @@ def get_preco_medio_ref_cor_tam(cursor, ref, cor, tam):
         , coalesce(rtc.PRECO_MEDIO, 0) PRECO_MEDIO
         FROM BASI_010 rtc
         WHERE 1=1
-          AND rtc.NIVEL_ESTRUTURA = 1
+          AND rtc.NIVEL_ESTRUTURA = {niv}
           AND rtc.GRUPO_ESTRUTURA = '{ref}'
           {filtra_tam} -- filtra_tam
           AND rtc.ITEM_ESTRUTURA = '{cor}'
-    '''.format(
-        ref=ref,
-        cor=cor,
-        filtra_tam=filtra_tam,
-    )
+    '''
     cursor.execute(sql)
     return rows_to_dict_list_lower(cursor)
+
+
+def get_preco_medio_ref_cor_tam(cursor, ref, cor, tam):
+    return get_preco_medio_niv_ref_cor_tam(cursor, 1, ref, cor, tam)
