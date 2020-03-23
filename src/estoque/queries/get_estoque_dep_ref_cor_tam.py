6aa4ef6e1ba7c6561f2b096eda1a78998e21188d
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2020-03-23 15:47:58 -0300

    criado get_estoque_dep_niv_ref_cor_tam baseada em get_estoque_dep_ref_cor_tam

--- src/estoque/queries/get_estoque_dep_ref_cor_tam.py
+++ src/estoque/queries/get_estoque_dep_ref_cor_tam.py
@@ -1,22 +1,21 @@
 from utils.functions.models import rows_to_dict_list_lower
 
 
-def get_estoque_dep_ref_cor_tam(cursor, deposito, ref, cor, tam):
-    sql = '''
+def get_estoque_dep_niv_ref_cor_tam(cursor, deposito, niv, ref, cor, tam):
+    sql = f'''
         SELECT
           e.QTDE_ESTOQUE_ATU ESTOQUE
         FROM ESTQ_040 e
         WHERE 1=1
-          AND e.CDITEM_NIVEL99 = 1
+          AND e.CDITEM_NIVEL99 = {niv}
           AND e.CDITEM_GRUPO = '{ref}'
           AND e.CDITEM_SUBGRUPO = '{tam}'
           AND e.CDITEM_ITEM = '{cor}'
           AND e.DEPOSITO = {deposito}
-    '''.format(
-        deposito=deposito,
-        ref=ref,
-        cor=cor,
-        tam=tam,
-    )
+    '''
     cursor.execute(sql)
     return rows_to_dict_list_lower(cursor)
+
+
+def get_estoque_dep_ref_cor_tam(cursor, deposito, ref, cor, tam):
+    return get_estoque_dep_niv_ref_cor_tam(cursor, deposito, 1, ref, cor, tam)
