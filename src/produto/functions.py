ca9a9fb427024ef5ca9f2196e8f7328e2bfeda7d
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2020-03-26 15:23:45 -0300

    movendo def item_str para produto/functions

--- src/produto/functions.py
+++ src/produto/functions.py
@@ -3,3 +3,7 @@
 def papg_modelo(papg):
     modelo = ''.join(filter(str.isdigit, papg))
     return modelo.lstrip('0')
+
+
+def item_str(nivel, ref, tam, cor):
+    return f'{nivel}.{ref}.{tam}.{cor}'
