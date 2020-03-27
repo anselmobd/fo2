5428cbaf0f593dbf0afd9356b0f7c8fef0d60a02
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2020-03-27 11:42:41 -0300

    criado set_produto_cor

--- src/produto/classes.py
+++ src/produto/classes.py
@@ -22,13 +22,14 @@ class objs_produto():
         self.valid_entries()
 
         self.set_produto()
+        self.set_produto_cor()
 
     def valid_entries(self):
         s_produtos = sys_que.item(
             self.cursor, self.nivel, self.ref, self.tam, self.cor)
         if len(s_produtos) == 0:
             raise ValueError(f'Item {self.item} não encontrado.')
-        s_produto = s_produtos[0]
+        self.s_produto = s_produtos[0]
 
     def set_produto(self):
         try:
@@ -39,9 +40,27 @@ class objs_produto():
         if self.produto is None:
             self.produto = pro_mod.Produto(
                 referencia=self.ref,
-                descricao=s_produto['descr'],
+                descricao=self.s_produto['descr'],
             )
             self.produto.save()
 
+    def set_produto_cor(self):
+        try:
+            self.produto_cor = pro_mod.ProdutoCor.objects.get(
+                produto=self.produto, cor=self.cor)
+        except pro_mod.ProdutoCor.DoesNotExist as e:
+            self.produto_cor = None
+
+        if self.produto_cor is None:
+            self.produto_cor = pro_mod.ProdutoCor(
+                produto=self.produto,
+                cor=self.cor,
+                descricao=self.s_produto['descr_cor'],
+            )
+            self.produto_cor.save()
+
     def get_produto(self):
         return self.produto
+
+    def get_produto_cor(self):
+        return self.produto_cor
