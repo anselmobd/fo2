cde05256b5bddff87e7006a74184cbf5e2092962
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2020-03-27 14:39:19 -0300

    criada a tabela de "Documentos de movimentos de estoque"

--- src/estoque/admin.py
+++ src/estoque/admin.py
@@ -2,11 +2,16 @@ from django.contrib import admin
 
 from fo2.admin import intr_adm_site
 
-from .models import TipoMovStq
+from .models import TipoMovStq, DocMovStq
 
 
 class TipoMovStqAdmin(admin.ModelAdmin):
     list_display = ["codigo", "descricao"]
 
 
+class DocMovStqAdmin(admin.ModelAdmin):
+    pass
+
+
 intr_adm_site.register(TipoMovStq, TipoMovStqAdmin)
+intr_adm_site.register(DocMovStq, DocMovStqAdmin)
