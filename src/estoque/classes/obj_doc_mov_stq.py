64831606854b04dee632ddac81d68d40d43c912c
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2020-03-31 10:47:59 -0300

    reestrutura métodos

--- src/estoque/classes/obj_doc_mov_stq.py
+++ src/estoque/classes/obj_doc_mov_stq.py
@@ -17,28 +17,39 @@ class ObjDocMovStq():
 
         self.cursor = connections['so'].cursor()
 
-        self.valid_entries()
+        self.get_num_doc()
+
         self.valid_user()
 
-    def valid_entries(self):
+    def get_num_doc(self):
         if self.num_doc == '0':
-            if self.descricao.strip() == '':
-                raise ValueError(
-                    'Não é possível criar um número de documento sem uma '
-                    'descrição.')
-            self.num_doc = self.cria_num_doc()
+            self.cria_num_doc()
+        else:
+            self.valid_num_doc()
+            self.valid_user()
+
+    def valid_num_doc(self):
         self.doc_mov_stq = models.DocMovStq.objects.filter(
             num_doc=self.num_doc, usuario=self.user)
         if len(self.doc_mov_stq) != 1:
             raise ValueError('Número de documento não encontrado.')
 
     def cria_num_doc(self):
-        obj_docs = models.DocMovStq(
+        self.valid_descricao()
+        self.valid_user()
+
+        self.doc_mov_stq = models.DocMovStq(
             descricao=self.descricao,
             usuario=self.user,
         )
-        obj_docs.save()
-        return obj_docs.get_num_doc
+        self.doc_mov_stq.save()
+        self.num_doc = self.doc_mov_stq.get_num_doc
+
+    def valid_descricao(self):
+        if self.descricao.strip() == '':
+            raise ValueError(
+                'Não é possível criar um número de documento sem uma '
+                'descrição.')
 
     def valid_user(self):
         try:
