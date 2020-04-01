9c839915af7af2b290cfb0eaaf5ed5a4b1ebcb44
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2020-04-01 08:47:38 -0300

    esqueleto da view

--- src/estoque/views/lista_movs.py
+++ src/estoque/views/lista_movs.py
@@ -4,9 +4,39 @@ from django.db import connections
 from django.shortcuts import render
 from django.views import View
 
+from utils.functions.views import (
+    cleanned_fields_to_context,
+    context_to_form_post,
+    )
+
+from estoque import forms
+
 
 class ListaMovimentos(View):
 
     Form_class = forms.ListaMovimentosForm
     template_name = 'estoque/lista_movs.html'
     title_name = 'Lista de movimentações de estoque'
+
+    cleanned_fields_to_context = cleanned_fields_to_context
+    context_to_form_post = context_to_form_post
+
+    def __init__(self):
+        self.context = {'titulo': self.title_name}
+
+    def mount_context(self):
+        self.cursor = connections['so'].cursor()
+
+    def get(self, request, *args, **kwargs):
+        self.context['form'] = self.Form_class()
+        return render(request, self.template_name, self.context)
+
+    def post(self, request, *args, **kwargs):
+        self.request = request
+        self.context['form'] = self.Form_class(request.POST)
+        if self.context['form'].is_valid():
+            self.cleanned_fields_to_context()
+            self.mount_context()
+            self.context_to_form_post()
+            self.context['form'] = self.Form_class(self.context)
+        return render(request, self.template_name, self.context)
