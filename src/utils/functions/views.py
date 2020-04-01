7e294c0bc2c9c108aaca3c4ba145ec6e2e4fa068
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2020-04-01 08:46:49 -0300

    criado context_to_form_post, pois, para os cleanned values voltarem para o form, caso seja date, temos que transformar em string

--- src/utils/functions/views.py
+++ src/utils/functions/views.py
@@ -1,3 +1,4 @@
+import datetime
 from pprint import pprint
 
 
@@ -8,3 +9,9 @@ def cleanned_fields_to_context(self):
     '''
     for field in self.context['form'].fields:
         self.context[field] = self.context['form'].cleaned_data[field]
+
+
+def context_to_form_post(self):
+    for field in self.context['form'].fields:
+        if isinstance(self.context[field], datetime.date):
+            self.context[field] = self.context[field].isoformat()
