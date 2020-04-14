dbdd2ee9bc9d7c160578666cd908380acb00eb69
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2020-04-14 18:22:56 -0300

    base pages_context geting page_ip_interno e etc

---
+++ src/base/pages_context.py
@@ -0,0 +1,22 @@
+from pprint import pprint
+
+from utils.classes import AcessoInterno
+
+
+def get_origem_do_ip(context):
+    acesso_interno = AcessoInterno()
+    try:
+        ip_interno = acesso_interno.current_interno
+        ip_externo = not ip_interno
+        ip_de_acesso = True
+    except Exception:
+        ip_interno = False
+        ip_externo = False
+        ip_de_acesso = False
+    context = {
+        'base_ip_interno': ip_interno,
+        'base_ip_externo': ip_externo,
+        'base_ip_de_acesso': ip_de_acesso,
+    }
+    pprint(context)
+    return context
