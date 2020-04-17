d00171adecf5f1ea19bf150a858192f5670fdc6d
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2020-04-17 15:43:40 -0300

    separando op_movi_estagios

--- src/lotes/queries/op/__init__.py
+++ src/lotes/queries/op/__init__.py
@@ -1,6 +1,7 @@
 from .busca_op import *
 from .get_seq_est_op import *
 from .op_estagios import *
+from .op_movi_estagios import *
 from .op_pendente import *
 from .op_relacionamentos import *
 from .op_sortimento import *
