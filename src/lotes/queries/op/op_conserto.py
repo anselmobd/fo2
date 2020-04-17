965b9b926a4b2fc40b89bfa4948a050fc3a57371
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2020-04-17 15:57:11 -0300

    separando op_conserto

---
+++ src/lotes/queries/op/op_conserto.py
@@ -0,0 +1,31 @@
+from utils.functions.models import rows_to_dict_list
+
+
+def op_conserto(cursor):
+    sql = """
+        SELECT
+          lote.PROCONF_GRUPO REF
+        , lote.PROCONF_ITEM COR
+        , lote.PROCONF_SUBGRUPO TAM
+        , lote.ORDEM_PRODUCAO OP
+        , sum(lote.QTDE_CONSERTO ) QTD
+        FROM PCPC_040 lote
+        LEFT JOIN BASI_220 tam
+          ON tam.TAMANHO_REF = lote.PROCONF_SUBGRUPO
+        GROUP BY
+          lote.PROCONF_GRUPO
+        , lote.PROCONF_ITEM
+        , tam.ORDEM_TAMANHO
+        , lote.PROCONF_SUBGRUPO
+        , lote.ORDEM_PRODUCAO
+        HAVING
+          sum(lote.QTDE_CONSERTO ) > 0
+        ORDER BY
+          lote.PROCONF_GRUPO
+        , lote.PROCONF_ITEM
+        , tam.ORDEM_TAMANHO
+        , lote.PROCONF_SUBGRUPO
+        , lote.ORDEM_PRODUCAO
+    """
+    cursor.execute(sql)
+    return rows_to_dict_list(cursor)
