1171a35ebf840186e4b6a57ebe90a9ad238cafb8
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2020-04-17 15:40:32 -0300

    separando op_estagios

---
+++ src/lotes/queries/op/op_estagios.py
@@ -0,0 +1,33 @@
+from utils.functions.models import rows_to_dict_list
+
+
+def op_estagios(cursor, op):
+    # Lotes ordenados por OS + referência + estágio
+    sql = '''
+        SELECT
+          l.CODIGO_ESTAGIO || ' - ' || e.DESCRICAO EST
+        , l.CODIGO_ESTAGIO COD_EST
+        , cast( SUM( l.QTDE_PECAS_PROD ) / SUM( l.QTDE_PECAS_PROG ) * 100
+                AS NUMERIC(10,2) ) PERC
+        , SUM( l.QTDE_EM_PRODUCAO_PACOTE ) EMPROD
+        , SUM( l.QTDE_PECAS_PROD ) PROD
+        , SUM( l.QTDE_PECAS_2A ) Q2
+        , SUM( l.QTDE_PERDAS ) PERDA
+        , SUM( l.QTDE_CONSERTO ) CONSERTO
+        , SUM(
+          CASE WHEN l.QTDE_EM_PRODUCAO_PACOTE <> 0 THEN 1 ELSE 0 END
+          ) LOTES
+        FROM pcpc_040 l
+        JOIN MQOP_005 e
+          ON e.CODIGO_ESTAGIO = l.CODIGO_ESTAGIO
+        WHERE l.ORDEM_PRODUCAO = %s
+        GROUP BY
+          l.SEQ_OPERACAO
+        , l.CODIGO_ESTAGIO
+        , e.DESCRICAO
+        , l.ORDEM_PRODUCAO
+        ORDER BY
+          l.SEQ_OPERACAO
+    '''
+    cursor.execute(sql, [op])
+    return rows_to_dict_list(cursor)
--- src/lotes/views/imprime_pacote_3_lotes.py
+++ src/lotes/views/imprime_pacote_3_lotes.py
@@ -333,7 +333,7 @@ class ImprimePacote3Lotes(LoginRequiredMixin, View):
                 do_print = False
 
         if do_print:
-            e_data = models.op_estagios(cursor, op)
+            e_data = lotes.queries.op.op_estagios(cursor, op)
             estagios = []
             for e_row in e_data:
                 estagios.append(e_row['EST'])
