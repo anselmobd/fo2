338fa83a00799f52524bf7df0acae0143e0e16ca
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2020-04-20 17:43:31 -0300

    separando query ref_estrutura_comp

---
+++ src/produto/queries/ref_estrutura_comp.py
@@ -0,0 +1,107 @@
+from utils.functions.models import rows_to_dict_list
+
+
+def ref_estrutura_comp(cursor, ref, alt):
+    # Detalhando Estruturas
+    sql = """
+        SELECT DISTINCT
+          e.SEQUENCIA
+        , (
+          SELECT
+            LISTAGG(ee.ITEM_ITEM, ', ')
+              WITHIN GROUP (ORDER BY ee.ITEM_ITEM)
+          FROM BASI_050 ee
+          WHERE ee.SEQUENCIA = e.SEQUENCIA
+            AND ee.NIVEL_ITEM = e.NIVEL_ITEM
+            AND ee.GRUPO_ITEM = e.GRUPO_ITEM
+            AND ee.ALTERNATIVA_ITEM = e.ALTERNATIVA_ITEM
+            AND ee.NIVEL_COMP = e.NIVEL_COMP
+            AND ee.GRUPO_COMP = e.GRUPO_COMP
+            AND ee.SUB_COMP = e.SUB_COMP
+            AND ee.ITEM_COMP = e.ITEM_COMP
+            AND ee.ALTERNATIVA_COMP = e.ALTERNATIVA_COMP
+            AND ee.CONSUMO = e.CONSUMO
+            AND ee.ESTAGIO = e.ESTAGIO
+          ) COR_REF
+        , e.NIVEL_COMP NIVEL
+        , e.GRUPO_COMP REF
+        , r.DESCR_REFERENCIA DESCR
+        , e.SUB_COMP TAM
+        , CASE WHEN cocv.SUB_ITEM IS NULL
+          THEN
+            CASE WHEN e.ITEM_COMP = '000000'
+            THEN '= ='
+            ELSE e.ITEM_COMP
+            END
+          ELSE
+            CASE WHEN
+              ( SELECT
+                  count(DISTINCT coc1.ITEM_COMP)
+                FROM BASI_040 coc1 -- comb. cor - verifica se é sempre a mesma
+                WHERE e.ITEM_COMP = '000000'
+                  AND coc1.SUB_ITEM = '000'
+                  AND coc1.GRUPO_ITEM = e.GRUPO_ITEM
+                  AND coc1.ALTERNATIVA_ITEM = e.ALTERNATIVA_ITEM
+                  AND coc1.SEQUENCIA = e.SEQUENCIA
+              ) = 1
+            THEN '= ' ||
+              CASE WHEN coc.ITEM_COMP = '000000'
+              THEN '?'
+              ELSE coc.ITEM_COMP
+              END
+            ELSE
+              CASE WHEN e.NIVEL_COMP = 1
+              THEN coc.ITEM_ITEM
+              ELSE
+                ( SELECT
+                    LISTAGG(coc1.ITEM_ITEM, ', ')
+                      WITHIN GROUP (ORDER BY coc1.ITEM_ITEM)
+                  FROM BASI_040 coc1 -- comb. cor - com mesma tratucao
+                  WHERE e.ITEM_COMP = '000000'
+                    AND coc1.SUB_ITEM = '000'
+                    AND coc1.GRUPO_ITEM = e.GRUPO_ITEM
+                    AND coc1.ALTERNATIVA_ITEM = e.ALTERNATIVA_ITEM
+                    AND coc1.SEQUENCIA = e.SEQUENCIA
+                    AND coc1.ITEM_COMP = coc.ITEM_COMP
+                )
+              END
+              || ' -> ' ||
+              CASE WHEN coc.ITEM_COMP = '000000'
+              THEN '?'
+              ELSE coc.ITEM_COMP
+              END
+            END
+          END COR
+        , e.ALTERNATIVA_COMP ALTERN
+        , e.CONSUMO
+        , e.ESTAGIO || '-' || es.DESCRICAO ESTAGIO
+        FROM BASI_050 e
+        LEFT JOIN basi_030 r
+          ON r.NIVEL_ESTRUTURA = e.NIVEL_COMP
+         AND r.REFERENCIA = e.GRUPO_COMP
+        LEFT JOIN BASI_040 cocv -- combinação cor - verifica se todos iguais
+          ON e.ITEM_COMP = '000000'
+         AND cocv.SUB_ITEM = '000'
+         AND cocv.GRUPO_ITEM = e.GRUPO_ITEM
+         AND cocv.ALTERNATIVA_ITEM = e.ALTERNATIVA_ITEM
+         AND cocv.SEQUENCIA = e.SEQUENCIA
+         AND cocv.ITEM_ITEM != cocv.ITEM_COMP
+        LEFT JOIN BASI_040 coc -- combinação cor
+          ON e.ITEM_COMP = '000000'
+         AND cocv.SUB_ITEM IS NOT NULL
+         AND coc.SUB_ITEM = '000'
+         AND coc.GRUPO_ITEM = e.GRUPO_ITEM
+         AND coc.ALTERNATIVA_ITEM = e.ALTERNATIVA_ITEM
+         AND coc.SEQUENCIA = e.SEQUENCIA
+        LEFT JOIN MQOP_005 es
+          ON es.CODIGO_ESTAGIO = e.ESTAGIO
+        WHERE e.NIVEL_ITEM = 1
+          AND e.GRUPO_ITEM = %s
+          AND e.ALTERNATIVA_ITEM = %s
+        ORDER BY
+          e.SEQUENCIA
+        , 2
+        , 7
+    """
+    cursor.execute(sql, [ref, alt])
+    return rows_to_dict_list(cursor)
