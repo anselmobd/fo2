c725acc4d675f84071013cfb54fbf00f49deb478
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2020-04-22 18:03:12 -0300

    query op_sortimentos utilizando style de GradeQtd

--- src/lotes/queries/op/op_sortimento.py
+++ src/lotes/queries/op/op_sortimento.py
@@ -576,19 +576,11 @@ def op_sortimentos(cursor, **kwargs):
             grade.total,
         )
     else:
-        style = {}
-        right_style = 'text-align: right;'
-        bold_style = 'font-weight: bold;'
-        for i in range(2, len(fields)):
-            style[i] = right_style
-        style[len(fields)] = right_style + bold_style
-        data[-1]['|STYLE'] = bold_style
-
         result = (
             grade.table_data['header'],
             fields,
             data,
-            style,
+            grade.table_data['style'],
             grade.total,
         )
 
