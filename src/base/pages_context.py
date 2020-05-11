29ae642429fe4151b9a4e5faa96e81f26d370438
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2020-05-11 14:22:45 -0300

    pages_context get_current_users passa a retornar Colaborador ao invés de User

--- src/base/pages_context.py
+++ src/base/pages_context.py
@@ -1,6 +1,5 @@
 from pprint import pprint
 
-from django.contrib.auth.models import User
 from django.contrib.sessions.models import Session
 from django.utils import timezone
 
@@ -15,7 +14,7 @@ def get_current_users():
     for session in active_sessions:
         data = session.get_decoded()
         user_id_list.append(data.get('_auth_user_id', None))
-    return User.objects.filter(id__in=user_id_list)
+    return Colaborador.objects.filter(user__id__in=user_id_list)
 
 
 def get_origem_do_ip(request):
