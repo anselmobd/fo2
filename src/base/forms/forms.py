b707f837b782f56665f661d66dc91425be8d7484
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2020-05-22 14:06:09 -0300

    Iniciada uma função montadora de classes Form

--- src/base/forms/forms.py
+++ src/base/forms/forms.py
@@ -18,3 +18,23 @@ class DepositoForm(
 
     class Meta:
         autofocus_field = 'deposito'
+
+
+def MountForm(*args, **kwargs):
+    classes = {
+        'deposito': {
+            'class': fields.O2FieldDepositoForm,
+        }
+    }
+
+    superclasses = custom.O2BaseForm,
+    for field in args:
+        superclasses += classes[field]['class'],
+
+    Meta = type('MountedMeta', (object, ), kwargs)
+
+    return type(
+        "MountedDepositoForm",
+        superclasses,
+        {'Meta': Meta, }
+    )
