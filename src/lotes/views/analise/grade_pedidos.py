7373484216efbd56f02edc8a9bd589a4c53b130a
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2020-05-27 10:38:35 -0300

    iniciando definição e uso de DepositoDatasForm2

--- src/lotes/views/analise/grade_pedidos.py
+++ src/lotes/views/analise/grade_pedidos.py
@@ -2,7 +2,7 @@ from pprint import pprint
 
 from django.db import connections
 
-from base.forms.forms2 import DepositoForm2
+from base.forms.forms2 import DepositoDatasForm2
 from base.views import O2BaseGetPostView
 
 from lotes.queries.pedido.pedido_faturavel_sortimento \
@@ -13,7 +13,7 @@ class GradePedidos(O2BaseGetPostView):
 
     def __init__(self, *args, **kwargs):
         super(GradePedidos, self).__init__(*args, **kwargs)
-        self.Form_class = DepositoForm2
+        self.Form_class = DepositoDatasForm2
         self.template_name = 'lotes/analise/grade_pedidos.html'
         self.title_name = 'Grade de pedidos a embarcar'
         self.get_args = ['deposito']
