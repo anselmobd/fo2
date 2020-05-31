b494976ee1ef975e795b6e4dcae67c3e82cc5de7
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2020-05-31 15:03:59 -0300

    Apresenta conteúdo do pallet

--- src/cd/views/rearrumar.py
+++ src/cd/views/rearrumar.py
@@ -21,6 +21,7 @@ class Rearrumar(PermissionRequiredMixin, View):
 
     def mount_context(self, request, form):
         cursor = connections['so'].cursor()
+        context = {}
 
         rua = form.cleaned_data['rua'].upper()
         endereco = form.cleaned_data['endereco']
@@ -36,20 +37,49 @@ class Rearrumar(PermissionRequiredMixin, View):
                 form.add_error(
                     'endereco', "Endereco não pode ser alterado na confirmação"
                 )
+            if not form.is_valid():
+                return context
 
-            if form.is_valid():
-                lotes_recs = lotes.models.Lote.objects.filter(
-                    local=endereco)
-                for lote in lotes_recs:
-                    lote.local = rua
-                    lote.local_usuario = request.user
-                    lote.save()
+        data = lotes.models.Lote.objects.filter(
+            local=endereco
+        ).values(
+            'op', 'lote', 'qtd_produzir',
+            'referencia', 'cor', 'tamanho',
+            'local_at', 'local_usuario__username'
+        ).order_by('referencia', 'cor', 'ordem_tamanho', 'op', 'lote')
 
-                form.data['endereco'] = ''
+        q_lotes = len(data)
+        q_itens = 0
+        for row in data:
+            q_itens += row['qtd_produzir']
+
+        context.update({
+            'q_lotes': q_lotes,
+            'q_itens': q_itens,
+            'headers': (
+                'Referência', 'Tamanho', 'Cor', 'Quant',
+                'OP', 'Lote', 'Em', 'Por'),
+            'fields': (
+                'referencia', 'tamanho', 'cor', 'qtd_produzir',
+                'op', 'lote', 'local_at', 'local_usuario__username'),
+            'data': data,
+        })
+
+        if request.POST.get("confirma"):
+            lotes_recs = lotes.models.Lote.objects.filter(
+                local=endereco)
+            for lote in lotes_recs:
+                lote.local = rua
+                lote.local_usuario = request.user
+                lote.save()
+
+            form.data['endereco'] = ''
 
         else:  # request.POST.get("tira"):
             form.data['valid_rua'] = rua
             form.data['valid_endereco'] = endereco
+
+        return context
 
     def get(self, request, *args, **kwargs):
         context = {'titulo': self.title_name}
@@ -61,6 +91,7 @@ class Rearrumar(PermissionRequiredMixin, View):
         context = {'titulo': self.title_name}
         form = self.Form_class(request.POST)
         if form.is_valid():
-            self.mount_context(request, form)
+            data = self.mount_context(request, form)
+            context.update(data)
         context['form'] = form
         return render(request, self.template_name, context)
