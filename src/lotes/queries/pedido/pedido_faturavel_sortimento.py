06e1f8abf7c78b374b06f494ced156ba6b8aaa24
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2020-06-05 17:46:58 -0300

    Agora a tela "Grade de pedidos a embarcar" entende que um pedido faturado, mesmo quando devolvido não pode mais ser faturado.

--- src/lotes/queries/pedido/pedido_faturavel_sortimento.py
+++ src/lotes/queries/pedido/pedido_faturavel_sortimento.py
@@ -29,10 +29,8 @@ def pedido_faturavel_sortimento(
           FROM PEDI_100 ped -- pedido de venda
           WHERE ped.STATUS_PEDIDO <> 5 -- 5=cancelado
             {filtro_data_de} -- filtro_data_de
-            {filtro_data_ate} -- filtro_data_ate
-            -- filtro_embarque_de
             -- AND ped.DATA_ENTR_VENDA >= TO_DATE('2019-02-25','YYYY-MM-DD')
-            -- filtro_embarque_ate
+            {filtro_data_ate} -- filtro_data_ate
             -- AND ped.DATA_ENTR_VENDA <= TO_DATE('2020-03-29','YYYY-MM-DD')
           GROUP BY
             ped.PEDIDO_VENDA
@@ -42,21 +40,15 @@ def pedido_faturavel_sortimento(
           SELECT
             ped.PEDIDO_VENDA PEDIDO
           , fok.NUM_NOTA_FISCAL NFOK
-          , fe.DOCUMENTO NFDEV
           FROM ped_data_entr ped -- pedido de venda filtrado
           LEFT JOIN FATU_050 fok -- fatura
             ON fok.PEDIDO_VENDA = ped.PEDIDO_VENDA
            AND fok.SITUACAO_NFISC = 1  -- 1=emitida; 2=cancelada
-          LEFT JOIN OBRF_010 fe -- nota fiscal de entrada/devolução
-            ON fe.NOTA_DEV = fok.NUM_NOTA_FISCAL
-           AND fe.SITUACAO_ENTRADA = 1 -- 1=ativa
         )
         , it_ped_qtd AS -- itens de pedidos com qtd
         (
           SELECT
             pf.PEDIDO
-          , pf.NFOK
-          , pf.NFDEV
           , i.CD_IT_PE_GRUPO REF
           , i.CD_IT_PE_SUBGRUPO TAM
           , i.CD_IT_PE_ITEM COR
@@ -65,77 +57,15 @@ def pedido_faturavel_sortimento(
           JOIN PEDI_110 i -- item de pedido de venda
             ON i.PEDIDO_VENDA = pf.PEDIDO
           WHERE 1=1
+             --
+            AND pf.NFOK IS NULL
+            AND i.CD_IT_PE_NIVEL99 = 1
             {filtro_deposito} -- filtro_deposito
           GROUP BY
             pf.PEDIDO
-          , pf.NFOK
-          , pf.NFDEV
-          , i.CD_IT_PE_NIVEL99
           , i.CD_IT_PE_GRUPO
           , i.CD_IT_PE_SUBGRUPO
           , i.CD_IT_PE_ITEM
-        )
-        , it_ped_q_qfat AS -- itens de pedidos com qtd e qtd faturada
-        (
-          SELECT
-            pq.PEDIDO
-          , pq.NFOK
-          , pq.NFDEV
-          , pq.REF
-          , pq.TAM
-          , pq.COR
-          , pq.QTD
-          , sum(COALESCE(inf.QTDE_ITEM_FATUR, 0)) QTD_FAT
-          FROM it_ped_qtd pq -- itens de pedidos com qtd
-          LEFT JOIN fatu_060 inf -- item de nf de saída
-            ON inf.CH_IT_NF_NUM_NFIS = pq.NFOK
-           AND inf.PEDIDO_VENDA = pq.PEDIDO
-           AND inf.COD_CANCELAMENTO = 0
-           AND inf.NIVEL_ESTRUTURA = 1
-           AND inf.GRUPO_ESTRUTURA = pq.REF
-           AND inf.SUBGRU_ESTRUTURA = pq.TAM
-           AND inf.ITEM_ESTRUTURA = pq.COR
-          GROUP BY
-            pq.PEDIDO
-          , pq.NFOK
-          , pq.NFDEV
-          , pq.REF
-          , pq.TAM
-          , pq.COR
-          , pq.QTD
-        )
-        , it_ped_qtd_final AS -- itens de pedidos com qtd e qtd fat e devolvida
-        (
-          SELECT
-            pq.PEDIDO
-          , pq.NFOK
-          , pq.NFDEV
-          , pq.REF
-          , pq.TAM
-          , pq.COR
-          , pq.QTD
-          , pq.QTD_FAT
-          , sum(COALESCE(inf.QUANTIDADE, 0)) QTD_DEV
-          , pq.QTD - pq.QTD_FAT + sum(COALESCE(inf.QUANTIDADE, 0)) QTD_FINAL
-          FROM it_ped_q_qfat pq -- itens de pedidos com qtd
-          LEFT JOIN OBRF_015 inf -- item de nf de devolução
-            ON inf.CAPA_ENT_NRDOC = pq.NFDEV
-           AND inf.NUM_NOTA_ORIG = pq.NFOK
-           AND inf.CODITEM_NIVEL99 = 1
-           AND inf.CODITEM_GRUPO = pq.REF
-           AND inf.CODITEM_SUBGRUPO = pq.TAM
-           AND inf.CODITEM_ITEM = pq.COR
-          GROUP BY
-            pq.PEDIDO
-          , pq.NFOK
-          , pq.NFDEV
-          , pq.REF
-          , pq.TAM
-          , pq.COR
-          , pq.QTD
-          , pq.QTD_FAT
-          HAVING
-            pq.QTD - pq.QTD_FAT + sum(COALESCE(inf.QUANTIDADE, 0)) > 0
         )
     """
     if retorno == 'r':
@@ -146,8 +76,8 @@ def pedido_faturavel_sortimento(
                 pq.REF
               , pq.COR
               , pq.TAM
-              , sum(pq.QTD_FINAL) QTD
-              FROM it_ped_qtd_final pq -- itens de ped com qtd e qtd fat e dev
+              , sum(pq.QTD) QTD
+              FROM it_ped_qtd pq -- itens de ped com qtd e qtd fat e dev
               LEFT JOIN BASI_220 t -- tamanhos
                 ON t.TAMANHO_REF = pq.TAM
               GROUP BY
@@ -172,8 +102,8 @@ def pedido_faturavel_sortimento(
               SELECT
                 ped.DATA_ENTR_VENDA DATA
               , pq.PEDIDO
-              , sum(pq.QTD_FINAL) QTD
-              FROM it_ped_qtd_final pq -- itens de ped com qtd e qtd fat e dev
+              , sum(pq.QTD) QTD
+              FROM it_ped_qtd pq -- itens de ped com qtd e qtd fat e dev
               JOIN PEDI_100 ped -- pedido de venda
                 ON ped.PEDIDO_VENDA = pq.PEDIDO
               GROUP BY
