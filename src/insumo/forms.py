8409e75f6fbef59e92ada4f2640dc5287e5abcbd
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2020-06-23 21:51:42 -0300

    "Mapa de compras por semana" com opção de versão antiga ou nova

--- src/insumo/forms.py
+++ src/insumo/forms.py
@@ -393,6 +393,10 @@ class MapaPorSemanaNewForm(forms.Form):
         min_length=2, required=False,
         widget=forms.TextInput(attrs={'type': 'string'}))
 
+    CHOICES = [('A', 'Antiga'),
+               ('N', 'Nova')]
+    versao = forms.ChoiceField(label='Versão', choices=CHOICES, initial='U')
+
     def __init__(self, *args, **kwargs):
         super(MapaPorSemanaNewForm, self).__init__(*args, **kwargs)
 
