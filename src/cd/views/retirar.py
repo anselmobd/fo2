4bfe334969175886c76165447f938fa2f31f5101
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2020-07-07 15:42:52 -0300

    lista de lotes e informação de totais melhorados, seguindo o padrão da retirada parcial

--- src/cd/views/retirar.py
+++ src/cd/views/retirar.py
@@ -95,21 +95,25 @@ class Retirar(PermissionRequiredMixin, View):
             local=endereco).order_by(
                 '-local_at'
                 ).values(
-                    'op', 'lote', 'qtd',
+                    'op', 'lote', 'qtd_produzir', 'qtd', 'conserto',
                     'referencia', 'cor', 'tamanho',
                     'local_at', 'local_usuario__username')
         if lotes_no_local:
             q_itens = 0
+            q_itens_end = 0
             for row in lotes_no_local:
+                row['livre'] = row['qtd'] - row['conserto']
                 q_itens += row['qtd']
+                q_itens_end += row['conserto']
             context.update({
                 'q_lotes': len(lotes_no_local),
                 'q_itens': q_itens,
+                'q_itens_end': q_itens_end,
                 'headers': ('Bipado em', 'Bipado por',
-                            'Lote', 'Quant.',
+                            'Lote', 'Q.Ori.', 'Q.Está.', 'Q.Livre', 'Q.End.',
                             'Ref.', 'Cor', 'Tam.', 'OP'),
                 'fields': ('local_at', 'local_usuario__username',
-                           'lote', 'qtd',
+                           'lote', 'qtd_produzir', 'qtd', 'livre', 'conserto',
                            'referencia', 'cor', 'tamanho', 'op'),
                 'data': lotes_no_local,
                 })
