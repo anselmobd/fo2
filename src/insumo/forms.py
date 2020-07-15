5990d0c990e5f6598e86944ff8712561705edc79
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2020-07-15 11:22:39 -0300

    "Mapa de compras por semana" tirada a opção de "Versão". Fixada em Nova versão.

--- src/insumo/forms.py
+++ src/insumo/forms.py
@@ -393,9 +393,10 @@ class MapaPorSemanaNewForm(forms.Form):
         min_length=2, required=False,
         widget=forms.TextInput(attrs={'type': 'string'}))
 
-    CHOICES = [('A', 'Antiga'),
-               ('N', 'Nova')]
-    versao = forms.ChoiceField(label='Versão', choices=CHOICES, initial='U')
+    # CHOICES = [('A', 'Antiga'),
+    #            ('N', 'Nova')]
+    # versao = forms.ChoiceField(label='Versão', choices=CHOICES, initial='U')
+    versao = forms.CharField(initial='N', widget=forms.HiddenInput())
 
     def __init__(self, *args, **kwargs):
         super(MapaPorSemanaNewForm, self).__init__(*args, **kwargs)
