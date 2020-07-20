416c039b2e6ef4a93a6931bdbff7d6eddf40e1c8
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2020-07-20 13:00:07 -0300

    Retirando itens antigos no sub-menu Movimentação do menu CD

--- src/cd/views/views.py
+++ src/cd/views/views.py
@@ -1,6 +1,6 @@
 from pprint import pprint
 
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 
 def index(request):
@@ -13,3 +13,7 @@ def teste_som(request):
 
 def movimentacao(request):
     return render(request, 'cd/movimentacao.html')
+
+
+def menu_desligado(request):
+    return redirect('apoio_ao_erp')
