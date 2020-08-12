ba15296a5abab13e2ebfb00c501b4d8cd155a56d
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2020-08-12 18:47:52 -0300

    Dupliquei totalmente rotinas ligadas a view "Mapa de compras por semana"

---
+++ src/insumo/views/mapa_compras_semana.py
@@ -0,0 +1,110 @@
+from pprint import pprint
+from datetime import timedelta
+
+from django.db import connections
+from django.shortcuts import render
+from django.views import View
+
+import systextil.models
+
+import insumo.queries as queries
+from insumo.forms import MapaComprasSemanaForm
+
+
+class MapaComprasSemana(View):
+    Form_class = MapaComprasSemanaForm
+    template_name = 'insumo/mapa_compras_semana.html'
+    title_name = 'Mapa de compras por semana (novo)'
+
+    def mount_context_pre(self, cursor, periodo, qtd_semanas):
+        if periodo is None:
+            return {}
+
+        if qtd_semanas is None:
+            qtd_semanas = 1
+        periodo_atual = systextil.models.Periodo.confeccao.filter(
+            periodo_producao=periodo
+        ).values()
+        periodo_ini = 0
+        periodo_fim = 0
+        if periodo_atual:
+            periodo_ini = periodo_atual[0]['data_ini_periodo'].date()
+            periodo_ini += timedelta(days=1)
+            periodo_fim = periodo_ini+timedelta(weeks=qtd_semanas-1)
+        periodo_ini_int = periodo_ini.year*10000 + \
+            periodo_ini.month*100 + periodo_ini.day
+
+        context = {'periodo': periodo,
+                   'periodo_ini': periodo_ini,
+                   'periodo_fim': periodo_fim,
+                   'qtd_semanas': qtd_semanas,
+                   'periodo_ini_int': periodo_ini_int,
+                   }
+
+        return context
+
+    def mount_context(
+            self, cursor, periodo, qtd_semanas, qtd_itens, nivel, uso, insumo,
+            versao):
+        cursor = connections['so'].cursor()
+        data = queries.insumos_cor_tamanho_usados(
+            cursor, qtd_itens, nivel, uso, insumo)
+        refs = []
+        for row in data:
+            refs.append('{}.{}.{}.{}'.format(
+                row['nivel'], row['ref'], row['cor'], row['tam']))
+
+        context = {
+            'refs': refs,
+            'qtd_itens': qtd_itens,
+            'nivel': nivel,
+            'uso': uso,
+            'insumo': insumo,
+            'versao': versao,
+        }
+
+        return context
+
+    def get(self, request, *args, **kwargs):
+        if 'periodo' in kwargs:
+            return self.post(request, *args, **kwargs)
+        else:
+            context = {'titulo': self.title_name}
+            form = self.Form_class()
+            periodo = None
+            qtd_semanas = None
+            if form.fields['periodo'].initial:
+                periodo = form.fields['periodo'].initial
+            if form.fields['qtd_semanas'].initial:
+                qtd_semanas = form.fields['qtd_semanas'].initial
+            cursor = connections['so'].cursor()
+            context.update(self.mount_context_pre(
+                cursor, periodo, qtd_semanas))
+            context['form'] = form
+            return render(request, self.template_name, context)
+
+    def post(self, request, *args, **kwargs):
+        context = {'titulo': self.title_name}
+        form = self.Form_class(request.POST.copy())
+        if 'periodo' in kwargs:
+            form.data['periodo'] = kwargs['periodo']
+        if form.is_valid():
+            periodo = form.cleaned_data['periodo']
+            qtd_semanas = form.cleaned_data['qtd_semanas']
+            qtd_itens = form.cleaned_data['qtd_itens']
+            nivel = form.cleaned_data['nivel']
+            uso = form.cleaned_data['uso']
+            insumo = form.cleaned_data['insumo']
+            versao = form.cleaned_data['versao']
+
+            insumo = ' '.join(insumo.strip().upper().split())
+            form.data['insumo'] = insumo
+
+            cursor = connections['so'].cursor()
+            context.update(self.mount_context_pre(
+                cursor, periodo, qtd_semanas))
+            context.update(self.mount_context(
+                cursor, periodo, qtd_semanas, qtd_itens, nivel, uso, insumo,
+                versao))
+        context['form'] = form
+        return render(request, self.template_name, context)
---
+++ src/insumo/views/mapa_compras_semana_ref_.py
@@ -0,0 +1,130 @@
+import datetime
+import math
+from pprint import pprint
+
+from django.db import connections
+from django.http import HttpResponse
+from django.template.loader import render_to_string
+from django.core.cache import cache
+
+from utils.cache import entkeys
+from utils.functions import make_key_cache, fo2logger
+
+import insumo.functions
+
+
+def mapa_compras_semana_ref(request, item, dtini, qtdsem):
+
+    def return_result(result):
+        cached_result = result
+        cache.set(key_cache, cached_result, timeout=entkeys._DAY*10)
+        fo2logger.info('calculated '+key_cache)
+        entkeys.add(key_cache, (nivel, ref, cor, tam), timeout=entkeys._DAY*10)
+        return cached_result
+
+    key_cache = make_key_cache()
+    cached_result = cache.get(key_cache)
+    if cached_result is not None:
+        fo2logger.info('cached '+key_cache)
+        return cached_result
+
+    template_name = 'insumo/mapa_compras_semana_ref.html'
+
+    nivel = item[0]
+    ref = item[2:7]
+    cor = item[8:14]
+    tam = item[15:18]
+    context = {
+        'qtdsem': int(qtdsem),
+    }
+
+    if len(item) == 2:
+        context['th'] = True
+    else:
+        cursor = connections['so'].cursor()
+
+        data = []
+
+        datas = insumo.functions.mapa_compras_semana_ref_dados(
+            cursor, nivel, ref, cor, tam)
+
+        if 'msg_erro' in datas:
+            context.update({
+                'data': data,
+            })
+            html = render_to_string(template_name, context)
+            return HttpResponse(html)
+
+        data_id = datas['data_id']
+        drow = data_id[0]
+
+        drow['REF'] = drow['REF'] + ' (' + drow['DESCR'] + ')'
+        drow['COR'] = drow['COR'] + ' (' + drow['DESCR_COR'] + ')'
+        if drow['TAM'] != drow['DESCR_TAM']:
+            drow['TAM'] = drow['TAM'] + ' (' + drow['DESCR_TAM'] + ')'
+        semanas = math.ceil(drow['REPOSICAO'] / 7)
+        drow['REP_STR'] = '{}d.({}s.)'.format(drow['REPOSICAO'], semanas)
+        drow['QUANT'] = round(drow['QUANT'])
+
+        data_sug = datas['data_sug']
+        semana_hoje = datas['semana_hoje']
+        semana_recebimento = datas['semana_recebimento']
+        dtsem = datetime.datetime.strptime(dtini, '%Y%m%d').date()
+
+        data_adi = datas['data_adi']
+
+        for i in range(int(qtdsem)):
+            compra_atrasada = 0
+            comprar = 0
+            dt_compra = dtsem
+            dt_chegada = None
+
+            if len(data_sug) != 0:
+                for row in data_sug:
+                    if dtsem == semana_hoje and \
+                            row['SEMANA_COMPRA'] < semana_hoje:
+                        compra_atrasada += row['QUANT']
+                        dt_compra = semana_hoje
+                        dt_chegada = semana_recebimento
+                    if row['SEMANA_COMPRA'] == dtsem:
+                        comprar += row['QUANT']
+                        dt_chegada = row['SEMANA_RECEPCAO']
+                comprar = round(comprar)
+                compra_atrasada = round(compra_atrasada)
+
+            movido = round(sum(
+                item['QUANT']
+                for item in data_adi
+                if item['SEMANA_DESTINO'] == dtsem))
+
+            if dt_chegada is None:
+                dt_chegada = '-'
+
+            row = drow.copy()
+
+            row.update({
+                'nivel': nivel,
+                'ref': ref,
+                'cor': cor,
+                'tam': tam,
+                'tam_order': tam.zfill(3),
+                'i': i+1,
+                'compra_atrasada': compra_atrasada,
+                'comprar': comprar,
+                'compra_total': compra_atrasada + comprar,
+                'dt_compra': dt_compra,
+                'dt_chegada': dt_chegada,
+                'movido': movido,
+            })
+
+            data.append(row)
+
+            dtsem += datetime.timedelta(days=7)
+
+        context.update({
+            'data': data,
+        })
+
+    html = render_to_string(template_name, context)
+    result = HttpResponse(html)
+    return return_result(result)
