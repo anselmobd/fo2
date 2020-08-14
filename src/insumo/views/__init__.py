673dbb7d392abcba6d8492e90aa04da252b24dc6
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2020-08-14 10:15:53 -0300

    iniciando mapa_compras_necessidades

--- src/insumo/views/__init__.py
+++ src/insumo/views/__init__.py
@@ -1,6 +1,7 @@
 from .mapa import *
 from .mapa_compras import *
 from .mapa_novo import *
+from .mapa_compras_necessidades import *
 from .mapa_compras_semana import *
 from .mapa_compras_semana_ref_ import *
 from .ref import *
