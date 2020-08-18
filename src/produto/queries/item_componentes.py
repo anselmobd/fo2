116c044e060508b59548f6ff87bce822079d8530
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2020-08-18 16:55:23 -0300

    substituindo make_key_cache por my_make_key_cache em item_comps

--- src/produto/queries/item_componentes.py
+++ src/produto/queries/item_componentes.py
@@ -3,7 +3,7 @@ from pprint import pprint
 from django.core.cache import cache
 
 from utils.cache import entkeys
-from utils.functions import make_key_cache, fo2logger
+from utils.functions import make_key_cache, fo2logger, my_make_key_cache
 from utils.functions.models import rows_to_dict_list
 
 
@@ -24,7 +24,10 @@ def item_comps_custo(cursor, nivel, ref, tam, cor, alt):
 
 def item_comps(cursor, nivel, ref, tam, cor, alt):
 
-    key_cache = make_key_cache()
+    # key_cache = make_key_cache(ignore=['cursor'])
+    key_cache = my_make_key_cache(
+        'item_comps', nivel, ref, tam, cor, alt
+    )
     cached_result = cache.get(key_cache)
     if cached_result is not None:
         fo2logger.info('cached '+key_cache)
