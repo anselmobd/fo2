df4d92ece7e55f868307986762b67ededaaf7df2
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2020-08-24 18:35:49 -0300

    passei função calc_check_digit para utils.functions.gtin

--- src/produto/management/commands/import_ean.py
+++ src/produto/management/commands/import_ean.py
@@ -6,16 +6,7 @@ from dbfread import DBF
 from django.core.management.base import BaseCommand, CommandError
 from django.db import connections
 
-
-def calc_check_digit(number):
-    """Calculate the EAN check digit for 13-digit numbers. The number passed
-    should not have the check bit included.
-    ---
-    From:
-      https://github.com/arthurdejong/python-stdnum/blob/master/stdnum/ean.py
-    """
-    return str((10 - sum((3, 1)[i % 2] * int(n)
-                for i, n in enumerate(reversed(number)))) % 10)
+from utils.functions.gtin import calc_check_digit
 
 
 class Command(BaseCommand):
