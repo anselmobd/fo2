b2d4c906959fccebcd4d5448d7d2dd53a0bb8542
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2020-09-01 15:47:00 -0300

    query alterada para pegar estoque mínimo dos produtos controlados na Agator; aproveitei para somar estoque de lotes diferentes e pegar o depósito certo de insumos da tinturaria, que agora são da conta de estoque 77

--- src/insumo/queries/insumo_descr.py
+++ src/insumo/queries/insumo_descr.py
@@ -5,6 +5,61 @@ from utils.functions.models import rows_to_dict_list
 
 def insumo_descr(cursor, nivel, ref, cor, tam):
     sql = f"""
+        WITH item AS
+        ( SELECT
+            i.*
+          FROM BASI_010 i -- item
+          WHERE 1=1
+            AND i.NIVEL_ESTRUTURA = {nivel}
+            AND i.GRUPO_ESTRUTURA = '{ref}'
+            AND i.SUBGRU_ESTRUTURA = '{tam}'
+            AND i.ITEM_ESTRUTURA = '{cor}'
+        )
+        , parm AS
+        ( SELECT
+            p1.*
+          FROM
+          ( SELECT
+              p.*
+            FROM BASI_015 p
+            JOIN item i
+              ON i.NIVEL_ESTRUTURA = p.NIVEL_ESTRUTURA
+             AND i.GRUPO_ESTRUTURA = p.GRUPO_ESTRUTURA
+             AND i.SUBGRU_ESTRUTURA = p.SUBGRU_ESTRUTURA
+             AND i.ITEM_ESTRUTURA = p.ITEM_ESTRUTURA
+            WHERE ( p.ESTOQUE_MINIMO > 0
+                  OR p.CODIGO_EMPRESA = 1
+                  )
+            ORDER BY
+              p.ESTOQUE_MINIMO DESC
+          ) p1
+          WHERE rownum = 1
+        )
+        , estoque AS
+        (
+          SELECT
+            sum(e.QTDE_ESTOQUE_ATU) QUANT
+          , max(e.DATA_ULT_ENTRADA) ULT_ENTRADA
+          , max(e.DATA_ULT_SAIDA) ULT_SAIDA
+          , max(e.DT_INVENTARIO) DT_INVENTARIO
+          FROM item ic
+          JOIN BASI_030 i -- insumo
+            ON i.NIVEL_ESTRUTURA = ic.NIVEL_ESTRUTURA
+           AND i.REFERENCIA = ic.GRUPO_ESTRUTURA
+          LEFT JOIN ESTQ_040 e -- estoque por depósito
+            ON e.CDITEM_NIVEL99 = ic.NIVEL_ESTRUTURA
+           AND e.CDITEM_GRUPO = ic.GRUPO_ESTRUTURA
+           AND e.CDITEM_SUBGRUPO = ic.SUBGRU_ESTRUTURA
+           AND e.CDITEM_ITEM = ic.ITEM_ESTRUTURA
+           AND e.DEPOSITO =
+               CASE WHEN i.NIVEL_ESTRUTURA = 2 THEN 202
+               ELSE -- i.NIVEL_ESTRUTURA = 9
+                 CASE WHEN i.CONTA_ESTOQUE = 22 THEN 212
+                 WHEN i.CONTA_ESTOQUE = 77 THEN 731
+                 ELSE 231
+                 END
+               END
+        )
         SELECT
           i.NIVEL_ESTRUTURA NIVEL
         , i.REFERENCIA REF
@@ -17,11 +72,13 @@ def insumo_descr(cursor, nivel, ref, cor, tam):
         , coalesce(parm.TEMPO_REPOSICAO, 0) REPOSICAO
         , coalesce(parm.LOTE_MULTIPLO, 0) LOTE_MULTIPLO
         , i.UNIDADE_MEDIDA UNID
-        , COALESCE(e.QTDE_ESTOQUE_ATU, 0) QUANT
-        , e.DATA_ULT_ENTRADA ULT_ENTRADA
-        , e.DATA_ULT_SAIDA ULT_SAIDA
+        , COALESCE(e.QUANT, 0) QUANT
+        , e.ULT_ENTRADA
+        , e.ULT_SAIDA
         , e.DT_INVENTARIO
-        FROM BASI_010 ic -- insumo cor
+        FROM item ic
+        JOIN parm ON 1=1
+        JOIN estoque e ON 1=1
         JOIN BASI_020 it -- insumo tamanho
           ON it.BASI030_NIVEL030 = ic.NIVEL_ESTRUTURA
          AND it.BASI030_REFERENC = ic.GRUPO_ESTRUTURA
@@ -29,30 +86,6 @@ def insumo_descr(cursor, nivel, ref, cor, tam):
         JOIN BASI_030 i -- insumo
           ON i.NIVEL_ESTRUTURA = ic.NIVEL_ESTRUTURA
          AND i.REFERENCIA = ic.GRUPO_ESTRUTURA
-        LEFT JOIN BASI_015 parm -- parâmetros de item
-          ON parm.NIVEL_ESTRUTURA = ic.NIVEL_ESTRUTURA
-         AND parm.GRUPO_ESTRUTURA = ic.GRUPO_ESTRUTURA
-         AND parm.SUBGRU_ESTRUTURA = ic.SUBGRU_ESTRUTURA
-         AND parm.ITEM_ESTRUTURA = ic.ITEM_ESTRUTURA
-         AND parm.CODIGO_EMPRESA = 1
-        LEFT JOIN ESTQ_040 e -- estoque por depósito
-          ON e.CDITEM_NIVEL99 = ic.NIVEL_ESTRUTURA
-         AND e.CDITEM_GRUPO = ic.GRUPO_ESTRUTURA
-         AND e.CDITEM_SUBGRUPO = ic.SUBGRU_ESTRUTURA
-         AND e.CDITEM_ITEM = ic.ITEM_ESTRUTURA
-         -- vvv não tenho certeza disso, mas evita aparecer mais de um registro
-         AND e.LOTE_ACOMP = 0
-         AND e.DEPOSITO =
-             CASE WHEN i.NIVEL_ESTRUTURA = 2 THEN 202
-             ELSE -- i.NIVEL_ESTRUTURA = 9
-               CASE WHEN i.CONTA_ESTOQUE = 22 THEN 212
-               ELSE 231
-               END
-             END
-        WHERE ic.NIVEL_ESTRUTURA = {nivel}
-          AND ic.GRUPO_ESTRUTURA = '{ref}'
-          AND ic.ITEM_ESTRUTURA = '{cor}'
-          AND ic.SUBGRU_ESTRUTURA = '{tam}'
     """
     cursor.execute(sql)
     return rows_to_dict_list(cursor)
