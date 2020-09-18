6a2208ef114e14763fe0993cbb4d5d6bd8927f88
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2020-09-18 19:16:38 -0300

    voltando para fdb

--- src/base/views.py
+++ src/base/views.py
@@ -1,6 +1,6 @@
 import cx_Oracle
-import firebirdsql
-# import fdb
+# import firebirdsql
+import fdb
 # from firebird.base import DatabaseWrapper
 from pprint import pprint
 
@@ -133,23 +133,23 @@ class TestaDB(PermissionRequiredMixin, O2BaseGetView):
             db_dict = databases[db_id]
 
             # conn = DatabaseWrapper(db_dict)
-            # conn = fdb.connect(
-            #     host=db_dict['HOST'],
-            #     port=db_dict['PORT'],
-            #     database=db_dict['NAME'],
-            #     user=db_dict['USER'],
-            #     password=db_dict['PASSWORD'],
-            #     sql_dialect=db_dict['DIALECT'],
-            #     charset=db_dict['OPTIONS']['charset'],
-            # )
-            conn = firebirdsql.connect(
+            conn = fdb.connect(
                 host=db_dict['HOST'],
                 port=db_dict['PORT'],
                 database=db_dict['NAME'],
                 user=db_dict['USER'],
                 password=db_dict['PASSWORD'],
+                sql_dialect=db_dict['DIALECT'],
                 charset=db_dict['OPTIONS']['charset'],
             )
+            # conn = firebirdsql.connect(
+            #     host=db_dict['HOST'],
+            #     port=db_dict['PORT'],
+            #     database=db_dict['NAME'],
+            #     user=db_dict['USER'],
+            #     password=db_dict['PASSWORD'],
+            #     charset=db_dict['OPTIONS']['charset'],
+            # )
 
             cursor = conn.cursor()
 
