fd5913bda9bf937dddcd39052647902b43a48ce2
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2020-09-18 22:20:56 -0300

    passei connect_fcb para utils.functions.data

--- src/utils/functions/data.py
+++ src/utils/functions/data.py
@@ -1,3 +1,5 @@
+import time
+import fdb
 from pprint import pprint
 
 
@@ -18,3 +20,37 @@ def filtered_data_fields(search, data, *fields, **params):
         if search_in_dict_fields(search, row, *fields, **params):
             result.append(row)
     return result
+
+
+def connect_fdb(databases, db_id, erros=[]):
+
+    def connect(databases, db_id):
+        try:
+            db_dict = databases[db_id]
+
+            conn = fdb.connect(
+                host=db_dict['HOST'],
+                port=db_dict['PORT'],
+                database=db_dict['NAME'],
+                user=db_dict['USER'],
+                password=db_dict['PASSWORD'],
+                sql_dialect=db_dict['DIALECT'],
+                charset=db_dict['OPTIONS']['charset'],
+            )
+            return True, conn
+
+        except Exception as e:
+            return False, e
+
+    count = 0
+
+    while count < 20:
+        result, conn = connect(databases, db_id)
+        if result:
+            return conn
+        else:
+            erros.append(conn)
+        count += 1
+        time.sleep(0.5)
+
+    return None
