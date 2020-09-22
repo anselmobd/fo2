ad407d6f1f48ebcfd846506718b5fc5f638316b2
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2020-09-22 16:12:17 -0300

    separando ajax estoque_depositos_modelo

--- src/lotes/views/a_produzir.py
+++ src/lotes/views/a_produzir.py
@@ -114,31 +114,6 @@ class AProduzir(O2BaseGetView):
         })
 
 
-def estoque_depositos_modelo(request, modelo):
-    cursor = connections['so'].cursor()
-    data = {
-        'modelo': modelo,
-    }
-
-    try:
-        _, _, _, _, total_est = \
-            estoque.queries.grade_estoque(
-                cursor, dep=('101', '102', '231'), modelo=modelo)
-
-    except Exception:
-        data.update({
-            'result': 'ERR',
-            'descricao_erro': 'Erro ao buscar estoque nos depósitos',
-        })
-        return JsonResponse(data, safe=False)
-
-    data.update({
-        'result': 'OK',
-        'total_est': total_est,
-    })
-    return JsonResponse(data, safe=False)
-
-
 def op_producao_modelo(request, modelo):
     cursor = connections['so'].cursor()
     data = {
