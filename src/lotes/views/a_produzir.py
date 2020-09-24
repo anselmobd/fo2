72a3d76d1601da902a714f9ea705195d7406b76f
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2020-09-24 07:05:29 -0300

    separando ajax view op_producao_modelo

--- src/lotes/views/a_produzir.py
+++ src/lotes/views/a_produzir.py
@@ -114,41 +114,6 @@ class AProduzir(O2BaseGetView):
         })
 
 
-def op_producao_modelo(request, modelo):
-    cursor = connections['so'].cursor()
-    data = {
-        'modelo': modelo,
-    }
-
-    try:
-        data_op = lotes.queries.op.busca_op(
-            cursor, modelo=modelo, tipo='v', tipo_alt='p',
-            situacao='a', posicao='p', cached=True)
-
-        if len(data_op) == 0:
-            total_op = 0
-        else:
-            totalize_data(data_op, {
-                'sum': ['QTD_AP'],
-                'count': [],
-                'descr': {'OP': 'T:'},
-            })
-            total_op = data_op[-1]['QTD_AP']
-
-    except Exception:
-        data.update({
-            'result': 'ERR',
-            'descricao_erro': 'Erro ao buscar OP',
-        })
-        return JsonResponse(data, safe=False)
-
-    data.update({
-        'result': 'OK',
-        'total_op': total_op,
-    })
-    return JsonResponse(data, safe=False)
-
-
 def get_celula(grd, tam, cor):
     result = 0
     sortimento_field = grd['fields'][0]
--- src/lotes/views/ajax/__init__.py
+++ src/lotes/views/ajax/__init__.py
@@ -1,2 +1,3 @@
 from .estoque_depositos_modelo import *
+from .op_producao_modelo import *
 from .pedido_lead_modelo import *
