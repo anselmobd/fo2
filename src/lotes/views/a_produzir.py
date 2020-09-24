01e1ecf8bdd94b0bbf77f2cbeba6b6e545bc928b
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2020-09-24 08:45:52 -0300

    Alteração de título

--- src/lotes/views/a_produzir.py
+++ src/lotes/views/a_produzir.py
@@ -28,7 +28,7 @@ class AProduzir(O2BaseGetView):
     def __init__(self, *args, **kwargs):
         super(AProduzir, self).__init__(*args, **kwargs)
         self.template_name = 'lotes/a_produzir.html'
-        self.title_name = 'A produzir por modelo'
+        self.title_name = 'Aproduzir - Por modelo - Totais sem grade'
 
     def mount_context(self):
         cursor = connections['so'].cursor()
