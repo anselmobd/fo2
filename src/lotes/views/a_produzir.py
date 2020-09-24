38153749ee6ffcabba1bc87c1d670e926ee39362
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2020-09-24 07:02:31 -0300

    separando ajax view pedido_lead_modelo

--- src/lotes/views/a_produzir.py
+++ src/lotes/views/a_produzir.py
@@ -149,47 +149,6 @@ def op_producao_modelo(request, modelo):
     return JsonResponse(data, safe=False)
 
 
-def pedido_lead_modelo(request, modelo):
-    cursor = connections['so'].cursor()
-    data = {
-        'modelo': modelo,
-    }
-    dias_alem_lead = config_get_value('DIAS-ALEM-LEAD', default=7)
-
-    try:
-        lead = produto.queries.lead_de_modelo(cursor, modelo)
-
-        if lead == 0:
-            periodo = ''
-        else:
-            periodo = lead + dias_alem_lead
-
-        data_ped = lotes.queries.pedido.pedido_faturavel_modelo(
-            cursor, modelo=modelo, periodo=':{}'.format(periodo))
-
-        if len(data_ped) == 0:
-            total_ped = 0
-        else:
-            totalize_data(data_ped, {
-                'sum': ['QTD'],
-                'count': [],
-                'descr': {'REF': 'T:'}})
-            total_ped = data_ped[-1]['QTD']
-
-    except Exception:
-        data.update({
-            'result': 'ERR',
-            'descricao_erro': 'Erro ao buscar Pedido',
-        })
-        return JsonResponse(data, safe=False)
-
-    data.update({
-        'result': 'OK',
-        'total_ped': total_ped,
-    })
-    return JsonResponse(data, safe=False)
-
-
 def get_celula(grd, tam, cor):
     result = 0
     sortimento_field = grd['fields'][0]
--- src/lotes/views/ajax/__init__.py
+++ src/lotes/views/ajax/__init__.py
@@ -1,1 +1,2 @@
 from .estoque_depositos_modelo import *
+from .pedido_lead_modelo import *
