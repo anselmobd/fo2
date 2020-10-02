d42785f700c6ae0dc708de5df2951a33a4a4a2cc
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2020-10-01 22:36:47 -0300

    separando YamlBaseObject para utilizar com outras classes de modelos

--- src/lotes/classes.py
+++ src/lotes/classes.py
@@ -4,25 +4,29 @@ import yaml
 from django.contrib.auth.models import User
 
 
-class YamlUser(yaml.YAMLObject):
-    yaml_tag = u'!username'
+class YamlBaseObject(yaml.YAMLObject):
 
-    def __init__(self, user):
-        self.user = user
+    def __init__(self, object_instance):
+        self.object_instance = object_instance
 
     def __str__(self):
-        return f"!YAML-{self.id}-{str(self.user)}"
+        return f"!YAML-{self.object_instance.id}-{str(self.object_instance)}"
 
     @classmethod
     def from_yaml(cls, loader, node):
         user_scalar = loader.construct_scalar(node)
         user_list = user_scalar.split('-')
         id = user_list[0]
-        user = User.objects.get(id=id)
-        return YamlUser(user)
+        object_instance = cls.object_class.objects.get(id=id)
+        return YamlUser(object_instance)
 
     @classmethod
     def to_yaml(cls, dumper, data):
-        scalar = f"{data.user.id}-{data.user.username}"
+        scalar = f"{data.object_instance.id}-{str(data.object_instance)}"
         node = dumper.represent_scalar(cls.yaml_tag, scalar)
         return node
+
+
+class YamlUser(YamlBaseObject):
+    yaml_tag = u'!username'
+    object_class = User
