f7938ae2313941af17258a8876dfdbf5b061de9c
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2020-10-05 20:01:01 -0300

    separando def existe_lote

---
+++ src/lotes/queries/lote/existe_lote.py
@@ -0,0 +1,17 @@
+from pprint import pprint
+
+from utils.functions.models import rows_to_dict_list
+
+
+def existe_lote(cursor, periodo, ordem_confeccao):
+    print('existe_lote')
+    sql = '''
+        SELECT DISTINCT
+          l.PERIODO_PRODUCAO
+        , l.ORDEM_CONFECCAO
+        FROM PCPC_040 l
+        WHERE l.PERIODO_PRODUCAO = %s
+          AND l.ORDEM_CONFECCAO = %s
+    '''
+    cursor.execute(sql, [periodo, ordem_confeccao])
+    return rows_to_dict_list(cursor)
