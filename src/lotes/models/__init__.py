6940c71c3d73f12a118944e46c0264fd485f2589
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2020-10-06 11:40:33 -0300

    movendo LotesPermissions para __init__

--- src/lotes/models/__init__.py
+++ src/lotes/models/__init__.py
@@ -1,5 +1,18 @@
+from django.db import models
+
 from .impresso import *
 from .models import *
 from .op import *
 from .parametro import *
 from .solicitacao import *
+
+
+class LotesPermissions(models.Model):
+
+    class Meta:
+        managed = False
+        permissions = (
+            ("can_edit_estagio_direito", "Can edit direitos a estágios"),
+            ("can_print__solicitacao_parciais",
+             "Pode imprimir etiquetas de solicitações parciais"),
+        )
