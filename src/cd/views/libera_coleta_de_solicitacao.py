a080feb073d017fae189652020fd46f5b5559fe3
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2020-10-13 18:10:34 -0300

    evoluindo libera_coleta_de_solicitacao

--- src/cd/views/libera_coleta_de_solicitacao.py
+++ src/cd/views/libera_coleta_de_solicitacao.py
@@ -1,26 +1,49 @@
 from pprint import pprint
 
-from django.db import connections
 from django.http import JsonResponse
+
+from utils.functions.digits import fo2_digit_valid
+
+from lotes.models import SolicitaLote
 
 
 def libera_coleta_de_solicitacao(request, num):
-    id = num[:-2]
-
     data = {
         'num': num,
-        'id': id,
     }
-    erro = False
 
-    cursor = connections['so'].cursor()
-
-    if erro:
+    if not fo2_digit_valid(num):
         data.update({
             'result': 'ERR',
-            'descricao_erro': 'Erro ao alterar liberação de coleta',
+            'descricao_erro': f'Número de solicitação #{num} inválido',
         })
         return JsonResponse(data, safe=False)
+
+    id = num[:-2]
+
+    try:
+        sl = SolicitaLote.objects.get(id=id)
+    except SolicitaLote.DoesNotExist:
+        data.update({
+            'result': 'ERR',
+            'descricao_erro': f'Solicitação #{num} não encontrada',
+        })
+        return JsonResponse(data, safe=False)
+
+    if not sl.concluida:
+        data.update({
+            'result': 'ERR',
+            'descricao_erro': f'Solicitação #{num} não concluída',
+        })
+        return JsonResponse(data, safe=False)
+
+    # sl.coleta = not sl.coleta
+
+    # data.update({
+    #     'result': 'ERR',
+    #     'descricao_erro': 'Erro ao alterar liberação de coleta',
+    # })
+    # return JsonResponse(data, safe=False)
 
     data.update({
         'result': 'OK',
