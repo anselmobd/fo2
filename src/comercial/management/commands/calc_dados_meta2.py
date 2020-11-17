fb11a59f257e3d08b7b3fa0cb5ab59e5538112cd
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2020-11-17 18:46:38 -0300

    command duplicado para testar cache, com indicação de _calc_, no redis

---
+++ src/comercial/management/commands/calc_dados_meta2.py
@@ -0,0 +1,43 @@
+import datetime
+from pprint import pprint, pformat
+
+from django.core.management.base import BaseCommand, CommandError
+from django.db import connections
+
+import comercial.queries
+
+
+class Command(BaseCommand):
+    help = 'Carrega no cache dados da meta do ano'
+
+    def my_println(self, text=''):
+        self.my_print(text, ending='\n')
+
+    def my_print(self, text='', ending=''):
+        self.stdout.write(text, ending=ending)
+        self.stdout.flush()
+
+    def my_pprintln(self, object):
+        self.my_pprint(object, ending='\n')
+
+    def my_pprint(self, object, ending=''):
+        self.stdout.write(pformat(object), ending=ending)
+        self.stdout.flush()
+
+    def handle(self, *args, **options):
+        self.my_println('---')
+        self.my_println('{}'.format(datetime.datetime.now()))
+
+        try:
+            cursor = connections['so'].cursor()
+            hoje = datetime.date.today()
+
+            meses, total = comercial.queries.ddados_meta_no_ano(cursor, hoje)
+            self.my_pprintln(meses)
+            self.my_pprintln(total)
+
+        except Exception as e:
+            raise CommandError(
+                'Erro carregando no cache dados da meta do ano "{}"'.format(e))
+
+        self.my_println(format(datetime.datetime.now(), '%H:%M:%S.%f'))
