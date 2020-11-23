983ecea24183f3dd96d47ac7b57c4df32415d531
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2020-11-23 14:19:51 -0300

    dados para teste

--- src/lotes/views/pedido/historico.py
+++ src/lotes/views/pedido/historico.py
@@ -1,5 +1,7 @@
+import datetime
 from pprint import pprint
 
+from django.conf import settings
 from django.shortcuts import render
 from django.urls import reverse
 from django.views import View
@@ -19,6 +21,21 @@ class Historico(View):
         context = {'pedido': pedido}
 
         data = queries.pedido.historico(pedido)
+        if settings.DATABASES['default']['ENGINE'] == 'django.db.backends.sqlite3':
+            data = [
+                {
+                    'data_ocorr': datetime.datetime(2020, 11, 23, 11, 38, 21),
+                    'usuario': 'ALESSANDRA_COME',
+                    'maquina_rede': '192.168.1.174',
+                    'long01': '                              CAPA DO PEDIDO DE VENDAS\n\nCLIENTE: 33200056/313-70\nDATA EMBARQUE: 27/01/21\nOBSERVAÇÕES DE PRODUÇÃO: \nNAT. DE OPERAÇÃO: 2\nTRANSPORTADORA: 11874063/1-93\nTIPO FRETE: 1\nEMPRESA: 1\nMOEDA: 0\nCANCELAMENTO: 0\nSTATUS PEDIDO: 0\nSTATUS COMERCIAL: 0\nSTATUS EXPEDIÇÃO: 0\nSITUAÇÃO PEDIDO: 5\nREPRESENTANTE: 23\nADMINISTRADOR: 0\nTIPO COMISSÃO: 2\n% COMIS. REPRES.: 2\n% COMISSÃO (ADMIN): 0\nCONDIÇÃO PAGTO: 99\nENDEREÇO DE ENTREGA: 2\nEND.COBRANÇA: 2\nTABELA PREÇO: 0-0-0\n  1\n  1\n  N\nDATA PREV. RECEB.: \nPORTADOR: 0\n  \n  \nDATA BASE FATUR: '
+                },
+                {
+                    'data_ocorr': datetime.datetime(2020, 11, 23, 11, 39, 1),
+                    'usuario': 'ALESSANDRA_COME',
+                    'maquina_rede': '192.168.1.174',
+                    'long01': '                              CAPA DO PEDIDO DE VENDAS\n\nSITUAÇÃO PEDIDO: 5 - PEDIDO SUSPENSO -> 0 - PEDIDO LIBERADO\n'
+                }
+            ]
         if len(data) == 0:
             context.update({
                 'msg_erro': 'Pedido não encontrado',
@@ -29,7 +46,8 @@ class Historico(View):
             'headers': ('Data', 'Usuário',
                         'Máquina', 'Descrição'),
             'fields': ('data_ocorr', 'usuario',
-                        'maquina_rede', 'long01'),
+                       'maquina_rede', 'long01'),
+            'pre': ['long01'],
             'data': data,
         })
         return context
