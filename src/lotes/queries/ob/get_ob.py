4632fede5f6deaa9ea7a2fbe0645bf96e5c3e61a
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2021-01-05 09:24:26 -0300

    pega só OB e Obs.

--- src/lotes/queries/ob/get_ob.py
+++ src/lotes/queries/ob/get_ob.py
@@ -4,7 +4,8 @@ from utils.functions.models import rows_to_dict_list_lower
 def get_ob(cursor, os):
     sql = f"""
         SELECT 
-          b.*
+          b.ORDEM_PRODUCAO OB
+        , b.OBSERVACAO
         FROM PCPB_010 b
         WHERE b.OBSERVACAO LIKE '{os}.%'
     """
