759f4440fe715b32db86d122acc6438cd1b647b3
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2021-01-07 08:45:27 -0300

    diferenciando a url "intranet" entre agator e tussor

--- src/fo2/urls_tussor.py
+++ src/fo2/urls_tussor.py
@@ -4,6 +4,8 @@ from fo2.urls import *
 # para não acumular alterações, trabalha sempre em uma cópia nova
 urlpatterns = urlpatterns.copy()
 
-urlpatterns.append(
+urlpatterns += [
     url(r'^$', views.index_tussor_view, name='index'),
-)
+
+    url(r'^intranet/', views.IntranetTussorView.as_view(), name='intranet'),
+]
