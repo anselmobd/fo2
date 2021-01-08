5be5a7de9154271f12dc9897174c744cece2d10d
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2021-01-08 14:08:50 -0300

    criado lotes index_agator.html

--- src/lotes/views/views.py
+++ src/lotes/views/views.py
@@ -5,6 +5,7 @@ from django.template.loader import render_to_string
 from django_tables2 import RequestConfig
 
 from utils.functions.models import rows_to_dict_list
+from geral.functions import get_empresa
 
 import lotes.models as models
 import lotes.forms as forms
@@ -12,7 +13,10 @@ from lotes.tables import ImpressoraTermicaTable
 
 
 def index(request):
-    return render(request, 'lotes/index.html')
+    if get_empresa(request) == 'agator':
+        return render(request, 'lotes/index_agator.html')
+    else:
+        return render(request, 'lotes/index.html')
 
 
 def impressoraTermica(request):
