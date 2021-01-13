c6d79326542d40b62ab6dc4d0bd9e15c674f2d6b
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2021-01-13 19:07:33 -0300

    evoluindo para uso de cleanned_fields_to_context

--- src/beneficia/views/ob.py
+++ src/beneficia/views/ob.py
@@ -4,40 +4,43 @@ from django.db import connections
 from django.shortcuts import render
 from django.views import View
 
-import beneficia.forms
+from utils.functions.views import (
+    cleanned_fields_to_context,
+    context_to_form_post,
+)
 
+import beneficia.forms
 
 
 class Ob(View):
 
+    Form_class = beneficia.forms.ObForm
+    template_name = 'beneficia/ob.html'
+    title_name = 'OB'
+
+    cleanned_fields_to_context = cleanned_fields_to_context
+    context_to_form_post = context_to_form_post
+
     def __init__(self):
-        self.Form_class = beneficia.forms.ObForm
-        self.template_name = 'beneficia/ob.html'
-        self.title_name = 'OB'
+        self.context = {'titulo': self.title_name}
 
-    def mount_context(self, request, form):
-        cursor = connections['so'].cursor()
-        context = {}
+    def mount_context(self):
+        self.cursor = connections['so'].cursor()
 
-        ob = form.cleaned_data['ob']
-
-        context.update({
-            'ob': ob,
-        })
-
-        return context
+        # self.context.update({
+        #     'sucesso_msg': f"{self.context['ob']} encontrada."
+        # })
 
     def get(self, request, *args, **kwargs):
-        context = {'titulo': self.title_name}
-        form = self.Form_class()
-        context['form'] = form
-        return render(request, self.template_name, context)
+        self.context['form'] = self.Form_class()
+        return render(request, self.template_name, self.context)
 
     def post(self, request, *args, **kwargs):
-        context = {'titulo': self.title_name}
-        form = self.Form_class(request.POST)
-        if form.is_valid():
-            data = self.mount_context(request, form)
-            context.update(data)
-        context['form'] = form
-        return render(request, self.template_name, context)
+        self.request = request
+        self.context['form'] = self.Form_class(request.POST)
+        if self.context['form'].is_valid():
+            self.cleanned_fields_to_context()
+            self.mount_context()
+            self.context_to_form_post()
+            self.context['form'] = self.Form_class(self.context)
+        return render(request, self.template_name, self.context)
