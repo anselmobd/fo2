de16d533f4847b91f174258bb52c5d7d5600a1ab
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2021-01-19 19:59:37 -0300

    histórico na época do sqlite preciava de controles a mais

--- src/lotes/queries/pedido/historico.py
+++ src/lotes/queries/pedido/historico.py
@@ -8,16 +8,13 @@ from utils.functions.models import rows_to_dict_list
 
 def historico(pedido):
     cursor = connection.cursor()
-    esquema = "" if (
-        settings.DATABASES['default']['ENGINE'] == 'django.db.backends.sqlite3'
-    ) else "systextil_logs."
     sql = f"""
         select 
           h.data_ocorr
         , h.usuario
         , h.maquina_rede
         , h.long01
-        from {esquema}hist_100 h
+        from systextil_logs.hist_100 h
         where h.programa = 'pedi_f130'
           -- and h.tabela = 'PEDI_100'
           and h.num01 = {pedido}
