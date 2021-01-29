c69c516b753104eacb1ce06c33cd15486b1abba8
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2021-01-29 17:10:03 -0300

    movendo def padrao_decorator para utils

--- src/comercial/queries/dados_meta_no_ano.py
+++ src/comercial/queries/dados_meta_no_ano.py
@@ -1,6 +1,5 @@
 import datetime
 import time
-from functools import wraps, partial
 from inspect import signature
 from pprint import pprint
 
@@ -8,6 +7,7 @@ from django.core.cache import cache
 from django.db import connections
 
 from utils.cache import entkeys
+from utils.decorators import caching_function
 from utils.functions import (
     dias_mes_data,
     fo2logger,
@@ -18,78 +18,6 @@ import lotes.queries.pedido as l_q_p
 
 import comercial.models
 import comercial.queries
-
-
-def caching_function(
-        func=None, *,
-        key_cache_fields=[],
-        max_run_delay=20,
-        minutes_key_variation=None):
-    if func is None:
-        return partial(
-            caching_function,
-            key_cache_fields=key_cache_fields,
-            max_run_delay=max_run_delay,
-            minutes_key_variation=minutes_key_variation,
-        )
-
-    @wraps(func)
-    def wrapper(*args, **kwargs):
-        if minutes_key_variation is not None:
-            # nova key a cada x minutos
-            now = datetime.datetime.now()
-            key_variation = int(
-                (now.hour * 60 + now.minute) / minutes_key_variation)
-
-        my_make_key_cache_args = [func.__name__]
-
-        sig = signature(func)
-        func_params = list(sig.parameters.keys())
-        for field in key_cache_fields:
-            field_index = func_params.index(field)
-            try:
-                value = args[field_index]
-            except IndexError:
-                value = kwargs[field]
-            my_make_key_cache_args.append(value)
-
-        if minutes_key_variation is not None:
-            my_make_key_cache_args.append(key_variation)
-
-        key_cache = my_make_key_cache(*my_make_key_cache_args)
-
-        fo2logger.info('antes do while')
-
-        while True:
-            fo2logger.info('dentro do while')
-            calc_cache = cache.get(f"{key_cache}_calc_", "n")
-            if calc_cache == 's':
-                fo2logger.info('is _calc_ '+key_cache)
-                time.sleep(0.2)
-            else:
-                fo2logger.info('not _calc_ '+key_cache)
-                cached_result = cache.get(key_cache)
-                if cached_result is None:
-                    fo2logger.info('set _calc_ '+key_cache)
-                    cache.set(
-                        f"{key_cache}_calc_", "s",
-                        timeout=entkeys._SECOND * max_run_delay)
-                    break
-                else:
-                    fo2logger.info('cached '+key_cache)
-                    return cached_result
-
-        fo2logger.info('depois do while')
-
-        cached_result = func(*args, **kwargs)
-
-        cache.set(key_cache, cached_result)
-        cache.set(f"{key_cache}_calc_", "n")
-        fo2logger.info('calculated '+key_cache)
-
-        return cached_result
-
-    return wrapper
 
 
 @caching_function(key_cache_fields=['hoje'], minutes_key_variation=10)
