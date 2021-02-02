dc899432948014c18afdc25e73619c986f8cf839
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2021-02-02 13:03:15 -0300

    separating date functions

--- src/utils/functions/functions.py
+++ src/utils/functions/functions.py
@@ -22,32 +22,6 @@ def arg_def(kwargs, arg, default):
     return default
 
 
-__dow_info = {
-    0: {'name': 'segunda-feira', 'plural': 'segundas-feiras',
-        'alias': 'segunda', 'abb': 'seg'},
-    1: {'name': 'terça-feira', 'plural': 'terças-feiras',
-        'alias': 'terça', 'abb': 'ter'},
-    2: {'name': 'quarta-feira', 'plural': 'quartas-feiras',
-        'alias': 'quarta', 'abb': 'qua'},
-    3: {'name': 'quinta-feira', 'plural': 'quintas-feiras',
-        'alias': 'quinta', 'abb': 'qui'},
-    4: {'name': 'sexta-feira', 'plural': 'sextas-feiras',
-        'alias': 'sexta', 'abb': 'sex'},
-    5: {'name': 'sábado', 'plural': 'sábados',
-        'alias': 'sábado', 'abb': 'sab'},
-    6: {'name': 'domingo', 'plural': 'domingos',
-        'alias': 'domingo', 'abb': 'dom'},
-}
-
-
-def dow_info(dt, info, capitalize=False):
-    dow = dt.weekday()
-    result = __dow_info[dow][info]
-    if capitalize:
-        result = result.capitalize()
-    return result
-
-
 def inc_month(dt, months=1):
     newmonth = (((dt.month - 1) + months) % 12) + 1
     newyear = dt.year + (((dt.month - 1) + months) // 12)
