3bc137e016a1d456f53281642279dd4af380f314
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2021-02-17 14:23:51 -0300

    view mapa_por_semana_ref; versão antiga e nova - eliminando antiga

--- src/insumo/urls.py
+++ src/insumo/urls.py
@@ -79,10 +79,6 @@ urlpatterns = [
         views.MapaPorSemanaNew.as_view(),
         name='mapa_por_semana__get'),
 
-    url(r'^mapa_por_semana_ref/(?P<item>(?:.{2}|\d\..{5}\..{6}\..{1,3}))/'
-        r'(?P<dtini>\d{8})/(?P<qtdsem>(?:\d{1,2}|--))/$',
-        views.mapa_sem_ref_new,
-        name='mapa_por_semana_ref__get'),
     url(r'^mapa_novo_por_semana_ref/(?P<item>(?:.{2}|\d\..{5}\..{6}\..{1,3}))/'
         r'(?P<dtini>\d{8})/(?P<qtdsem>(?:\d{1,2}|--))/$',
         views.mapa_novo_sem_ref_new,
