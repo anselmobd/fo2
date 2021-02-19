7fa3ceda7551756d7df6369936fbf5783a34227f
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2021-02-19 13:29:01 -0300

    evoluindo sync_f1_fo2

--- src/tussor/management/commands/sync_f1_fo2.py
+++ src/tussor/management/commands/sync_f1_fo2.py
@@ -26,23 +26,48 @@ class Command(BaseCommand):
         parser.add_argument('tabela', help='cli ou dup', nargs='?')
         parser.add_argument('qtd', type=int, help='quantidade de registros', nargs='?')
 
+    def basic_params(self):
+        self.verbosity = self.options['verbosity']
+
+    def params(self):
+        self.tabela = self.options['tabela']
+        self.qtd = self.options['qtd']
+
+        dic_tabelas = {
+            None: ('cli', 'dup'),
+            'cli': ('cli', ),
+            'dup': ('dup', ),
+        }
+
+        self.tabelas = dic_tabelas[self.tabela]
+        self.my_println(f'tabelas = {self.tabelas}')
+
+        if self.qtd:
+            self.my_println(f'qtd = {self.qtd}')
+
+    def sync_cli(self):
+        self.my_println('sync_cli', v=2)
+
+    def sync_dup(self):
+        self.my_println('sync_dup', v=2)
+
+    def exec(self):
+        if 'cli' in self.tabelas:
+            self.sync_cli()
+        if 'dup' in self.tabelas:
+            self.sync_dup()
+
     def handle(self, *args, **options):
-        self.verbosity = options['verbosity']
+        self.options = options
+        self.basic_params()
 
         self.my_println('---')
         self.my_println('{}'.format(datetime.datetime.now()))
 
-        tabela = options['tabela']
-        qtd = options['qtd']
-
-        if tabela:
-            self.my_println(f'tabela = {tabela}')
-        if qtd:
-            self.my_println(f'qtd = {qtd}')
-
         try:
-            pass
+            self.params()
+            self.exec()
         except Exception as e:
-            raise CommandError(f"Exceção [{e}]")
+            raise CommandError(f"Erro! <{e}>")
 
         self.my_println(format(datetime.datetime.now(), '%H:%M:%S.%f'))
