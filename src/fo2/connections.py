cdf3d51e95260fb6c473c8da19bc828a4e4efb59
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2021-02-22 14:56:59 -0300

    se db_conn receber request, pode avaliar se deve ser utilizado connexão alternativa

--- src/fo2/connections.py
+++ src/fo2/connections.py
@@ -2,6 +2,14 @@ from pprint import pprint
 
 from django.db import connections
 
+from geral.functions import is_alternativa
 
-def db_conn(name):
+
+def db_conn(name, request=None):
+    if request and name == 'so':
+        # 2021-02-22 alternativa aponta para núvem e não alernativa para local
+        if is_alternativa(request):
+            return connections['sn']
+        else:
+            return connections['so']
     return connections[name]
