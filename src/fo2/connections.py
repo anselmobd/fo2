acef10df1b064dad27cbe989fa7882a7807dbd72
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2021-03-03 10:13:20 -0300

    criando variáveis públicas fo2_db_so e fo2_db_sn

--- src/fo2/connections.py
+++ src/fo2/connections.py
@@ -6,13 +6,15 @@ from django.db import connections
 from geral.functions import is_alternativa
 
 
+fo2_db_so = settings.FO2_DEFAULT_SYSTEXTIL_DB
+fo2_db_sn = settings.FO2_ALTER_SYSTEXTIL_DB
+
+
 def db_conn(name, request=None):
-    so = settings.FO2_DEFAULT_SYSTEXTIL_DB
-    sn = settings.FO2_ALTER_SYSTEXTIL_DB
-    if request and name == so:
+    if request and name == fo2_db_so:
         # 2021-02-22 url alternativa aponta db alernativo
         if is_alternativa(request):
-            return connections[sn]
+            return connections[fo2_db_sn]
         else:
-            return connections[so]
+            return connections[fo2_db_so]
     return connections[name]
