6542095701beeaf6c1ea4656c24132473b40e63b
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2021-03-03 20:35:29 -0300

    dict_conserto_lote_custom recebe cursor e dict_conserto_lote calcula cursor, pois recebe request

--- src/lotes/views/lote/conserto_lote.py
+++ src/lotes/views/lote/conserto_lote.py
@@ -1,8 +1,9 @@
 from pprint import pprint
 
 from django.contrib.auth.models import User
-from django.db import connections
 from django.http import JsonResponse
+
+from fo2.connections import db_cursor_so
 
 from base.models import Colaborador
 from geral.functions import request_user
@@ -10,12 +11,13 @@ from systextil.models import Usuario
 
 
 def dict_conserto_lote(request, lote, estagio, in_out, qtd_a_mover):
+    cursor = db_cursor_so(request)
     return dict_conserto_lote_custom(
-        lote, estagio, in_out, qtd_a_mover, request=request)
+        cursor, lote, estagio, in_out, qtd_a_mover, request=request)
 
 
 def dict_conserto_lote_custom(
-        lote, estagio, in_out, qtd_a_mover, request=None, username=None):
+        cursor, lote, estagio, in_out, qtd_a_mover, request=None, username=None):
     in_out = in_out.lower()
     data = {
         'lote': lote,
@@ -122,181 +124,179 @@ def dict_conserto_lote_custom(
         })
         return data
 
-    with connections['so'].cursor() as cursor:
+    if in_out == 'in':
+        qtd_var = 'l.QTDE_DISPONIVEL_BAIXA'
+    else:
+        qtd_var = 'l.QTDE_CONSERTO'
 
-        if in_out == 'in':
-            qtd_var = 'l.QTDE_DISPONIVEL_BAIXA'
-        else:
-            qtd_var = 'l.QTDE_CONSERTO'
+    sql = f"""
+        SELECT
+            {qtd_var} QTD
+        FROM PCPC_040 l
+        WHERE l.PERIODO_PRODUCAO = {lote[:4]}
+            AND l.ORDEM_CONFECCAO = {lote[4:]}
+            AND l.CODIGO_ESTAGIO = {estagio}
+    """
 
-        sql = f"""
-            SELECT
-              {qtd_var} QTD
-            FROM PCPC_040 l
-            WHERE l.PERIODO_PRODUCAO = {lote[:4]}
-              AND l.ORDEM_CONFECCAO = {lote[4:]}
-              AND l.CODIGO_ESTAGIO = {estagio}
-        """
-
-        cursor.execute(sql)
-        row = cursor.fetchone()
-        if row is None:
-            data.update({
-                'error_level': 1,
-                'msg': f'Lote {lote} sem estágio {estagio}',
-            })
-            return data
-
-        if row[0] <= 0:
-            data.update({
-                'error_level': 2,
-                'msg': f'Lote {lote} sem quantidade a mover no '
-                       f'estágio {estagio}',
-            })
-            return data
-
-        qtd_disponivel = row[0]
+    cursor.execute(sql)
+    row = cursor.fetchone()
+    if row is None:
         data.update({
-            'qtd_disponivel': qtd_disponivel,
+            'error_level': 1,
+            'msg': f'Lote {lote} sem estágio {estagio}',
         })
+        return data
 
-        if qtd_a_mover == 0:
-            qtd_a_mover = qtd_disponivel
+    if row[0] <= 0:
+        data.update({
+            'error_level': 2,
+            'msg': f'Lote {lote} sem quantidade a mover no '
+                    f'estágio {estagio}',
+        })
+        return data
 
-        if qtd_a_mover > qtd_disponivel:
-            data.update({
-                'error_level': 3,
-                'msg': f'Quantidade a mover não disponível',
-            })
-            return data
+    qtd_disponivel = row[0]
+    data.update({
+        'qtd_disponivel': qtd_disponivel,
+    })
 
-        if in_out == 'in':
-            qtd_a_mover = qtd_a_mover
-        else:
-            qtd_a_mover = -qtd_a_mover
+    if qtd_a_mover == 0:
+        qtd_a_mover = qtd_disponivel
 
-        sql = f"""
-            INSERT INTO SYSTEXTIL.PCPC_045
-            ( PCPC040_PERCONF, PCPC040_ORDCONF, PCPC040_ESTCONF, SEQUENCIA
-            , DATA_PRODUCAO, HORA_PRODUCAO, QTDE_PRODUZIDA, QTDE_PECAS_2A
-            , QTDE_CONSERTO, TURNO_PRODUCAO, TIPO_ENTRADA_ORD, NOTA_ENTR_ORDEM
-            , SERIE_NF_ENT_ORD, SEQ_NF_ENTR_ORD, ORDEM_PRODUCAO, CODIGO_USUARIO
-            , QTDE_PERDAS, NUMERO_DOCUMENTO, CODIGO_DEPOSITO, CODIGO_FAMILIA
-            , CODIGO_INTERVALO, EXECUTA_TRIGGER, DATA_INSERCAO
-            , PROCESSO_SYSTEXTIL, NUMERO_VOLUME, NR_OPERADORES
-            , ATZ_PODE_PRODUZIR, ATZ_EM_PROD, ATZ_A_PROD, EFICIENCIA_INFORMADA
-            , USUARIO_SYSTEXTIL, CODIGO_OCORRENCIA, COD_OCORRENCIA_ESTORNO
-            , SOLICITACAO_CONSERTO, NUMERO_SOLICITACAO, NUMERO_ORDEM
-            , MINUTOS_PECA, NR_OPERADORES_INFORMADO, EFICIENCIA
-            )
+    if qtd_a_mover > qtd_disponivel:
+        data.update({
+            'error_level': 3,
+            'msg': f'Quantidade a mover não disponível',
+        })
+        return data
+
+    if in_out == 'in':
+        qtd_a_mover = qtd_a_mover
+    else:
+        qtd_a_mover = -qtd_a_mover
+
+    sql = f"""
+        INSERT INTO SYSTEXTIL.PCPC_045
+        ( PCPC040_PERCONF, PCPC040_ORDCONF, PCPC040_ESTCONF, SEQUENCIA
+        , DATA_PRODUCAO, HORA_PRODUCAO, QTDE_PRODUZIDA, QTDE_PECAS_2A
+        , QTDE_CONSERTO, TURNO_PRODUCAO, TIPO_ENTRADA_ORD, NOTA_ENTR_ORDEM
+        , SERIE_NF_ENT_ORD, SEQ_NF_ENTR_ORD, ORDEM_PRODUCAO, CODIGO_USUARIO
+        , QTDE_PERDAS, NUMERO_DOCUMENTO, CODIGO_DEPOSITO, CODIGO_FAMILIA
+        , CODIGO_INTERVALO, EXECUTA_TRIGGER, DATA_INSERCAO
+        , PROCESSO_SYSTEXTIL, NUMERO_VOLUME, NR_OPERADORES
+        , ATZ_PODE_PRODUZIR, ATZ_EM_PROD, ATZ_A_PROD, EFICIENCIA_INFORMADA
+        , USUARIO_SYSTEXTIL, CODIGO_OCORRENCIA, COD_OCORRENCIA_ESTORNO
+        , SOLICITACAO_CONSERTO, NUMERO_SOLICITACAO, NUMERO_ORDEM
+        , MINUTOS_PECA, NR_OPERADORES_INFORMADO, EFICIENCIA
+        )
+        SELECT
+            *
+        FROM (
             SELECT
-              *
-            FROM (
-              SELECT
-                l.PERIODO_PRODUCAO PCPC040_PERCONF
-              , l.ORDEM_CONFECCAO PCPC040_ORDCONF
-              , l.CODIGO_ESTAGIO PCPC040_ESTCONF
-              , COALESCE( ml.SEQUENCIA + 1, 0 ) SEQUENCIA
-              , TO_DATE(CURRENT_DATE) DATA_PRODUCAO
-              , CURRENT_TIMESTAMP HORA_PRODUCAO
-              , 0 QTDE_PRODUZIDA
-              , 0 QTDE_PECAS_2A
-              , {qtd_a_mover}  QTDE_CONSERTO
-              , COALESCE( ml.TURNO_PRODUCAO, 1 ) TURNO_PRODUCAO
-              , COALESCE( ml.TIPO_ENTRADA_ORD, 0 ) TIPO_ENTRADA_ORD
-              , COALESCE( ml.NOTA_ENTR_ORDEM, 0 ) NOTA_ENTR_ORDEM
-              , ml.SERIE_NF_ENT_ORD
-              , COALESCE( ml.SEQ_NF_ENTR_ORD, 0 ) SEQ_NF_ENTR_ORD
-              , l.ORDEM_PRODUCAO ORDEM_PRODUCAO
-              , {colab.matricula} CODIGO_USUARIO
-              , 0 QTDE_PERDAS
-              , COALESCE( ml.NUMERO_DOCUMENTO, 0) NUMERO_DOCUMENTO
-              , COALESCE( ml.CODIGO_DEPOSITO, 0) CODIGO_DEPOSITO
-              , COALESCE( ml.CODIGO_FAMILIA, 0) CODIGO_FAMILIA
-              , COALESCE( ml.CODIGO_INTERVALO, 0) CODIGO_INTERVALO
-              , COALESCE( ml.EXECUTA_TRIGGER, 3) EXECUTA_TRIGGER
-              , CURRENT_TIMESTAMP DATA_INSERCAO
-              , '?' PROCESSO_SYSTEXTIL
-              , COALESCE( ml.NUMERO_VOLUME, 0) NUMERO_VOLUME
-              , COALESCE( ml.NR_OPERADORES, 0) NR_OPERADORES
-              , COALESCE( ml.ATZ_PODE_PRODUZIR, 0) ATZ_PODE_PRODUZIR
-              , COALESCE( ml.ATZ_EM_PROD, 0) ATZ_EM_PROD
-              , COALESCE( ml.ATZ_A_PROD, 0) ATZ_A_PROD
-              , COALESCE( ml.EFICIENCIA_INFORMADA, 0) EFICIENCIA_INFORMADA
-              , '?' USUARIO_SYSTEXTIL
-              , COALESCE( ml.CODIGO_OCORRENCIA, 0) CODIGO_OCORRENCIA
-              , COALESCE( ml.COD_OCORRENCIA_ESTORNO, 0) COD_OCORRENCIA_ESTORNO
-              , COALESCE( ml.SOLICITACAO_CONSERTO, 0) SOLICITACAO_CONSERTO
-              , COALESCE( ml.NUMERO_SOLICITACAO, 0) NUMERO_SOLICITACAO
-              , COALESCE( ml.NUMERO_ORDEM, 0) NUMERO_ORDEM
-              , COALESCE( ml.MINUTOS_PECA, 0) MINUTOS_PECA
-              , ml.NR_OPERADORES_INFORMADO
-              , ml.EFICIENCIA
-              FROM PCPC_040 l
-              LEFT JOIN SYSTEXTIL.PCPC_045 ml
-                ON ml.PCPC040_PERCONF = l.PERIODO_PRODUCAO
-               AND ml.PCPC040_ORDCONF = l.ORDEM_CONFECCAO
-               AND ml.PCPC040_ESTCONF = l.CODIGO_ESTAGIO
-              WHERE l.PERIODO_PRODUCAO = {lote[:4]}
-                AND l.ORDEM_CONFECCAO = {lote[4:]}
-                AND l.CODIGO_ESTAGIO = {estagio}
-              ORDER BY
-                ml.SEQUENCIA DESC
+            l.PERIODO_PRODUCAO PCPC040_PERCONF
+            , l.ORDEM_CONFECCAO PCPC040_ORDCONF
+            , l.CODIGO_ESTAGIO PCPC040_ESTCONF
+            , COALESCE( ml.SEQUENCIA + 1, 0 ) SEQUENCIA
+            , TO_DATE(CURRENT_DATE) DATA_PRODUCAO
+            , CURRENT_TIMESTAMP HORA_PRODUCAO
+            , 0 QTDE_PRODUZIDA
+            , 0 QTDE_PECAS_2A
+            , {qtd_a_mover}  QTDE_CONSERTO
+            , COALESCE( ml.TURNO_PRODUCAO, 1 ) TURNO_PRODUCAO
+            , COALESCE( ml.TIPO_ENTRADA_ORD, 0 ) TIPO_ENTRADA_ORD
+            , COALESCE( ml.NOTA_ENTR_ORDEM, 0 ) NOTA_ENTR_ORDEM
+            , ml.SERIE_NF_ENT_ORD
+            , COALESCE( ml.SEQ_NF_ENTR_ORD, 0 ) SEQ_NF_ENTR_ORD
+            , l.ORDEM_PRODUCAO ORDEM_PRODUCAO
+            , {colab.matricula} CODIGO_USUARIO
+            , 0 QTDE_PERDAS
+            , COALESCE( ml.NUMERO_DOCUMENTO, 0) NUMERO_DOCUMENTO
+            , COALESCE( ml.CODIGO_DEPOSITO, 0) CODIGO_DEPOSITO
+            , COALESCE( ml.CODIGO_FAMILIA, 0) CODIGO_FAMILIA
+            , COALESCE( ml.CODIGO_INTERVALO, 0) CODIGO_INTERVALO
+            , COALESCE( ml.EXECUTA_TRIGGER, 3) EXECUTA_TRIGGER
+            , CURRENT_TIMESTAMP DATA_INSERCAO
+            , '?' PROCESSO_SYSTEXTIL
+            , COALESCE( ml.NUMERO_VOLUME, 0) NUMERO_VOLUME
+            , COALESCE( ml.NR_OPERADORES, 0) NR_OPERADORES
+            , COALESCE( ml.ATZ_PODE_PRODUZIR, 0) ATZ_PODE_PRODUZIR
+            , COALESCE( ml.ATZ_EM_PROD, 0) ATZ_EM_PROD
+            , COALESCE( ml.ATZ_A_PROD, 0) ATZ_A_PROD
+            , COALESCE( ml.EFICIENCIA_INFORMADA, 0) EFICIENCIA_INFORMADA
+            , '?' USUARIO_SYSTEXTIL
+            , COALESCE( ml.CODIGO_OCORRENCIA, 0) CODIGO_OCORRENCIA
+            , COALESCE( ml.COD_OCORRENCIA_ESTORNO, 0) COD_OCORRENCIA_ESTORNO
+            , COALESCE( ml.SOLICITACAO_CONSERTO, 0) SOLICITACAO_CONSERTO
+            , COALESCE( ml.NUMERO_SOLICITACAO, 0) NUMERO_SOLICITACAO
+            , COALESCE( ml.NUMERO_ORDEM, 0) NUMERO_ORDEM
+            , COALESCE( ml.MINUTOS_PECA, 0) MINUTOS_PECA
+            , ml.NR_OPERADORES_INFORMADO
+            , ml.EFICIENCIA
+            FROM PCPC_040 l
+            LEFT JOIN SYSTEXTIL.PCPC_045 ml
+            ON ml.PCPC040_PERCONF = l.PERIODO_PRODUCAO
+            AND ml.PCPC040_ORDCONF = l.ORDEM_CONFECCAO
+            AND ml.PCPC040_ESTCONF = l.CODIGO_ESTAGIO
+            WHERE l.PERIODO_PRODUCAO = {lote[:4]}
+            AND l.ORDEM_CONFECCAO = {lote[4:]}
+            AND l.CODIGO_ESTAGIO = {estagio}
+            ORDER BY
+            ml.SEQUENCIA DESC
+        )
+        WHERE rownum = 1
+    """
+
+    try:
+        cursor.execute(sql)
+    except Exception:
+        data.update({
+            'error_level': 31,
+            'msg': 'Erro ao mover a quantidade',
+        })
+        return data
+
+    sql = f"""
+        UPDATE PCPC_045 ml
+        SET
+            ml.USUARIO_SYSTEXTIL = '*' || (
+            SELECT
+                u.USUARIO
+            FROM HDOC_030 u
+            WHERE u.EMPRESA = 1
+                AND u.CODIGO_USUARIO = ml.CODIGO_USUARIO
             )
-            WHERE rownum = 1
-        """
-
-        try:
-            cursor.execute(sql)
-        except Exception:
-            data.update({
-                'error_level': 31,
-                'msg': 'Erro ao mover a quantidade',
-            })
-            return data
-
-        sql = f"""
-            UPDATE PCPC_045 ml
-            SET
-              ml.USUARIO_SYSTEXTIL = '*' || (
-                SELECT
-                  u.USUARIO
-                FROM HDOC_030 u
-                WHERE u.EMPRESA = 1
-                  AND u.CODIGO_USUARIO = ml.CODIGO_USUARIO
-              )
-            --, ml.PROCESSO_SYSTEXTIL = '-'
-            WHERE
-            ( ml.PCPC040_PERCONF
+        --, ml.PROCESSO_SYSTEXTIL = '-'
+        WHERE
+        ( ml.PCPC040_PERCONF
+        , ml.PCPC040_ORDCONF
+        , ml.PCPC040_ESTCONF
+        , ml.SEQUENCIA
+        ) IN
+        ( SELECT
+            ml.PCPC040_PERCONF
             , ml.PCPC040_ORDCONF
             , ml.PCPC040_ESTCONF
             , ml.SEQUENCIA
-            ) IN
-            ( SELECT
-                ml.PCPC040_PERCONF
-              , ml.PCPC040_ORDCONF
-              , ml.PCPC040_ESTCONF
-              , ml.SEQUENCIA
-              FROM PCPC_045 ml
-              JOIN HDOC_030 u
-                ON u.EMPRESA = 1
-               AND u.CODIGO_USUARIO = ml.CODIGO_USUARIO
-              WHERE PCPC040_PERCONF = {lote[:4]}
-                AND PCPC040_ORDCONF = {lote[4:]}
-                AND PCPC040_ESTCONF = {estagio}
-                AND ml.USUARIO_SYSTEXTIL != u.USUARIO
-                AND ml.DATA_PRODUCAO = TO_DATE(CURRENT_DATE)
-            )
-        """
+            FROM PCPC_045 ml
+            JOIN HDOC_030 u
+            ON u.EMPRESA = 1
+            AND u.CODIGO_USUARIO = ml.CODIGO_USUARIO
+            WHERE PCPC040_PERCONF = {lote[:4]}
+            AND PCPC040_ORDCONF = {lote[4:]}
+            AND PCPC040_ESTCONF = {estagio}
+            AND ml.USUARIO_SYSTEXTIL != u.USUARIO
+            AND ml.DATA_PRODUCAO = TO_DATE(CURRENT_DATE)
+        )
+    """
 
-        try:
-            cursor.execute(sql)
-        except Exception:
-            data.update({
-                'error_level': 32,
-                'msg': 'Erro ao ajustar nome do usuário',
-            })
-            return data
+    try:
+        cursor.execute(sql)
+    except Exception:
+        data.update({
+            'error_level': 32,
+            'msg': 'Erro ao ajustar nome do usuário',
+        })
+        return data
 
     data.update({
         'error_level': 0,
