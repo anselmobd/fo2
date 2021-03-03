e8e641c08d602c1107fbfb1b8136a3d93a800824
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2021-03-03 09:17:16 -0300

    db_conn

--- src/cd/views/retirar.py
+++ src/cd/views/retirar.py
@@ -1,9 +1,10 @@
 from pprint import pprint
 
-from django.db import connections
 from django.contrib.auth.mixins import PermissionRequiredMixin
 from django.shortcuts import render
 from django.views import View
+
+from fo2.connections import db_conn
 
 from lotes.views.lote.conserto_lote import dict_conserto_lote
 
@@ -25,7 +26,7 @@ class Retirar(PermissionRequiredMixin, View):
             self.title_name = 'Retirar lote inteiro'
 
     def mount_context(self, request, form):
-        cursor = connections['so'].cursor()
+        cursor = db_conn('so', request).cursor()
         context = {}
 
         lote = form.cleaned_data['lote']
