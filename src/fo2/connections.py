3dec9eb7c3460b3e88fffca1af2d48eb99c31a99
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2021-03-03 09:11:51 -0300

    db_conn utilizando FO2_DEFAULT_SYSTEXTIL_DB e FO2_ALTER_SYSTEXTIL_DB

--- src/fo2/connections.py
+++ src/fo2/connections.py
@@ -1,15 +1,18 @@
 from pprint import pprint
 
+from django.conf import settings
 from django.db import connections
 
 from geral.functions import is_alternativa
 
 
 def db_conn(name, request=None):
-    if request and name == 'so':
-        # 2021-02-22 alternativa aponta para núvem e não alernativa para local
+    so = settings.FO2_DEFAULT_SYSTEXTIL_DB
+    sn = settings.FO2_ALTER_SYSTEXTIL_DB
+    if request and name == so:
+        # 2021-02-22 url alternativa aponta db alernativo
         if is_alternativa(request):
-            return connections['sn']
+            return connections[sn]
         else:
-            return connections['so']
+            return connections[so]
     return connections[name]
