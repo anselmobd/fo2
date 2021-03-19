c9e975fc9ea87fbcf3021b9752759b5226392003
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2021-03-19 16:19:17 -0300

    alterando para trabalhar com ReferenciaNoneForm

--- src/produto/views/ficha_tecnica.py
+++ src/produto/views/ficha_tecnica.py
@@ -12,16 +12,14 @@ class FichaTecnica(O2BaseGetPostView):
 
     def __init__(self, *args, **kwargs):
         super(FichaTecnica, self).__init__(*args, **kwargs)
-        self.Form_class = produto.forms.ReferenciaForm
+        self.Form_class = produto.forms.ReferenciaNoneForm
         self.template_name = 'produto/ficha_tecnica.html'
         self.title_name = 'Ficha técnica'
 
     def mount_context(self):
         ref = self.form.cleaned_data['ref']
 
-        if ref == '':
-            return
-
+        if ref != '':
         cursor = db_cursor_so(self.request)
 
         data = produto.queries.ref_inform(cursor, ref)
