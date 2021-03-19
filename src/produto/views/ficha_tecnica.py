c5db9a407137872782fb39f86ef8c5af579fbc13
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2021-03-19 15:55:05 -0300

    evoluindo view FichaTecnica

--- src/produto/views/ficha_tecnica.py
+++ src/produto/views/ficha_tecnica.py
@@ -2,6 +2,19 @@ from pprint import pprint
 
 from base.views import O2BaseGetPostView
 
+import produto.forms
+
 
 class FichaTecnica(O2BaseGetPostView):
-    pass
+
+    def __init__(self, *args, **kwargs):
+        super(FichaTecnica, self).__init__(*args, **kwargs)
+        self.Form_class = produto.forms.ReferenciaForm
+        self.template_name = 'comercial/ficha_tecnica.html'
+        self.title_name = 'Ficha técnica'
+
+    def mount_context(self):
+        ref = self.form.cleaned_data['ref']
+
+        if ref == '':
+            return
