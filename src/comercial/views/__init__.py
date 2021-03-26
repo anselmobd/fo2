44d2d84096f66ab4a914233f51b2b5602857d76b
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2021-03-26 11:15:59 -0300

    separando views de vendas_por

--- src/comercial/views/__init__.py
+++ src/comercial/views/__init__.py
@@ -6,5 +6,6 @@ from .meta_no_ano import *
 from .painel_meta_faturamento import *
 from .tabela_de_preco import *
 from .vendas import *
+from .vendas_por import *
 from .vendas_por_modelo import *
 from .views import *
