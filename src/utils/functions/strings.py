d0aa61b2ab97aec415d95fea7ba525fc65ef5e5a
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2021-03-31 18:17:18 -0300

    função join mais concisa

--- src/utils/functions/strings.py
+++ src/utils/functions/strings.py
@@ -2,14 +2,11 @@ from pprint import pprint
 
 
 def join(sep, lista):
-    str_lista = list(map(str, lista))
-    if isinstance(sep, str):
-        return sep.join(str_lista)
+    if isinstance(sep, (list, tuple)) and len(sep) == 2:
+        first = str(sep[0]).join(map(str, lista[:-1]))
+        return str(sep[1]).join([first, str(lista[-1])])
     else:
-        if len(sep) == 2:
-            result = sep[0].join(str_lista[:-1])
-            result += sep[1] + str_lista[-1]
-            return result
+        return sep.join(map(str, lista))
 
 
 def only_digits(text):
