c91277a92c478d174755026a0b103a66402f68c3
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2021-04-01 22:39:15 -0300

    voltei a versão original da query get_vendas e a versão nova ficou como get_vendas_new

--- src/comercial/queries/queries.py
+++ src/comercial/queries/queries.py
@@ -76,6 +76,222 @@ def get_modelo_dims(cursor, modelo=None, get=None):
 
 def get_vendas(
         cursor, ref=None, periodo=None, colecao=None, cliente=None, por=None,
+        modelo=None, order_qtd=True, ultimos_dias=None):
+
+    # key_cache = make_key_cache()
+    key_cache = my_make_key_cache(
+        'get_vendas', ref, periodo, colecao, cliente, por, modelo, order_qtd,
+        ultimos_dias)
+
+    cached_result = cache.get(key_cache)
+    if cached_result is not None:
+        fo2logger.info('cached '+key_cache)
+        return cached_result
+
+    if order_qtd:
+        order = '1 DESC'
+    else:
+        order = ''
+
+    def add_order(new_order):
+        nonlocal order
+        sep = "\n, " if order else ''
+        order += sep + new_order
+
+    filtra_col = ''
+    if colecao is not None:
+        ref = None
+        filtra_col = "AND v.COL = '{}'".format(colecao)
+
+    filtra_cliente = ''
+    if cliente is not None:
+        filtra_cliente = "AND v.CNPJ9 = '{}'".format(cliente)
+
+    filtra_modelo = ''
+    filtra_modelo_select_item = ''
+    pre_filtra_modelo = ''
+    if modelo is not None:
+        filtra_modelo = "AND v.MODELO = '{}'".format(modelo)
+        pre_filtra_modelo = \
+            "AND inf.GRUPO_ESTRUTURA LIKE '%{}%'".format(modelo)
+        filtra_modelo_select_item = f""" --
+          AND TRIM(
+                LEADING '0' FROM (
+                  REGEXP_REPLACE(
+                    v.GRUPO_ESTRUTURA
+                  , '^([^a-zA-Z]+)[a-zA-Z]*$'
+                  , '\\1'
+                  )
+                )
+              ) = '{modelo}'
+        """
+    filtra_ref = ''
+    pre_filtra_ref = ''
+    if ref is not None:
+        filtra_ref = "AND v.REF = '{}'".format(ref)
+        pre_filtra_ref = "AND inf.GRUPO_ESTRUTURA = '{}'".format(ref)
+
+    hoje = date.today()
+    ini_mes = hoje.replace(day=1)
+    filtra_periodo = ''
+    pre_filtra_periodo = ''
+    if periodo is not None:
+        periodo_list = periodo.split(':')
+        ini_periodo = dec_months(ini_mes, int(periodo_list[0]))
+        filtra_periodo = "  AND v.dt >= TO_DATE('{}', 'yyyy-mm-dd')".format(
+            ini_periodo.strftime('%Y-%m-%d'))
+        pre_filtra_periodo = \
+            "  AND nf.DATA_EMISSAO >= TO_DATE('{}', 'yyyy-mm-dd')".format(
+                ini_periodo.strftime('%Y-%m-%d'))
+        if periodo_list[1] != '':
+            fim_periodo = dec_months(ini_mes, int(periodo_list[1]))
+            filtra_periodo += \
+                "  AND v.dt < TO_DATE('{}', 'yyyy-mm-dd')".format(
+                    fim_periodo.strftime('%Y-%m-%d'))
+            pre_filtra_periodo += \
+                "  AND nf.DATA_EMISSAO < TO_DATE('{}', 'yyyy-mm-dd')".format(
+                    fim_periodo.strftime('%Y-%m-%d'))
+
+    filtra_ultimos_dias = ''
+    pre_filtra_ultimos_dias = ''
+    if ultimos_dias is not None:
+        filtra_ultimos_dias = \
+            "  AND v.dt >= ""TO_DATE('{}', 'yyyy-mm-dd') - {} + 1"
+        filtra_ultimos_dias = filtra_ultimos_dias.format(
+            hoje.strftime('%Y-%m-%d'), ultimos_dias)
+        pre_filtra_ultimos_dias = \
+            "  AND nf.DATA_EMISSAO >= TO_DATE('{}', 'yyyy-mm-dd') - {} + 1"
+        pre_filtra_ultimos_dias = pre_filtra_ultimos_dias.format(
+            hoje.strftime('%Y-%m-%d'), ultimos_dias)
+
+    select_por = ''
+    group_por = ''
+    if por == 'modelo':
+        select_por = ", v.MODELO"
+        # no filtro por modelo não busca modelos com qtd zerada, por EMISSAO
+        # apenas repete o valor, para, ao agrupar, não fazer diferença
+        select_item = ", '{}' MODELO".format(modelo)
+        select_global = select_por
+        group_por = ", v.MODELO"
+        add_order('v.MODELO')
+    elif por == 'ref':
+        select_por = ", v.REF"
+        select_item = ", v.GRUPO_ESTRUTURA REF"
+        select_global = select_por
+        group_por = ", v.REF"
+        add_order('v.REF')
+    elif por == 'cor':
+        select_por = ", v.COR"
+        select_item = ", v.ITEM_ESTRUTURA COR"
+        select_global = select_por
+        group_por = ", v.COR"
+        add_order('v.COR')
+    elif por == 'tam':
+        select_por = ", t.ORDEM_TAMANHO\n, v.TAM"
+        select_item = ", t.ORDEM_TAMANHO\n, v.SUBGRU_ESTRUTURA TAM"
+        select_global = ", v.ORDEM_TAMANHO\n, v.TAM"
+        group_por = ", v.ORDEM_TAMANHO\n, v.TAM"
+        add_order("v.ORDEM_TAMANHO\n, v.TAM")
+
+    sql = f"""
+        SELECT
+          sum(v.qtd) qtd
+          {select_global} -- select_global
+        FROM (
+        WITH vendido AS
+        (
+        SELECT
+          nf.NUM_NOTA_FISCAL NF
+        , nf.DATA_EMISSAO DT
+        , nf.NATOP_NF_NAT_OPER NATOP
+        , nop.COD_NATUREZA COD_NAT
+        , nop.DIVISAO_NATUR DIV_NAT
+        , inf.NIVEL_ESTRUTURA NIVEL
+        , inf.GRUPO_ESTRUTURA REF
+        , TRIM(LEADING '0' FROM
+               (REGEXP_REPLACE(inf.GRUPO_ESTRUTURA,
+                               '^([^a-zA-Z]+)[a-zA-Z]*$', '\\1'
+                               ))) MODELO
+        , inf.SUBGRU_ESTRUTURA TAM
+        , inf.ITEM_ESTRUTURA COR
+        , inf.QTDE_ITEM_FATUR QTD
+        , inf.VALOR_UNITARIO PRECO
+        , r.COLECAO COL
+        , col.DESCR_COLECAO COLECAO
+        , r.CGC_CLIENTE_9 CNPJ9
+        FROM FATU_050 nf -- nota fiscal da Tussor - capa
+        LEFT JOIN OBRF_010 fe -- nota fiscal de entrada/devolução
+          ON fe.NOTA_DEV = nf.NUM_NOTA_FISCAL
+         AND fe.SITUACAO_ENTRADA = 1 -- ativa
+        JOIN PEDI_080 nop -- natureza da operação
+          ON nop.NATUR_OPERACAO = nf.NATOP_NF_NAT_OPER
+         AND nop.ESTADO_NATOPER = nf.NATOP_NF_EST_OPER
+        JOIN fatu_060 inf -- item de nf de saída
+          ON inf.CH_IT_NF_NUM_NFIS = nf.NUM_NOTA_FISCAL
+        LEFT JOIN BASI_030 r -- item (ref+tam+cor)
+          on r.NIVEL_ESTRUTURA = inf.NIVEL_ESTRUTURA
+         AND r.REFERENCIA = inf.GRUPO_ESTRUTURA
+        LEFT JOIN BASI_140 col
+          ON col.COLECAO = r.COLECAO
+        --JOIN BASI_010 rtc -- item (ref+tam+cor)
+        --  on rtc.NIVEL_ESTRUTURA = inf.NIVEL_ESTRUTURA
+        -- AND rtc.GRUPO_ESTRUTURA = inf.GRUPO_ESTRUTURA
+        -- AND rtc.SUBGRU_ESTRUTURA = inf.SUBGRU_ESTRUTURA
+        -- AND rtc.ITEM_ESTRUTURA = inf.ITEM_ESTRUTURA
+        WHERE 1=1
+          AND (nf.NATOP_NF_NAT_OPER IN (1, 2)
+               OR (nop.DIVISAO_NATUR = 8
+                   AND nop.COD_NATUREZA in ('5.11', '6.11')
+                  )
+              )
+          AND nf.SITUACAO_NFISC = 1
+          AND fe.DOCUMENTO IS NULL
+          {pre_filtra_modelo} -- pre_filtra_modelo
+          {pre_filtra_ref} -- pre_filtra_ref
+          {pre_filtra_periodo} -- pre_filtra_periodo
+          {pre_filtra_ultimos_dias} -- pre_filtra_ultimos_dias
+        )
+        SELECT
+          v.qtd
+          {select_por} -- select_por
+        FROM vendido v
+        LEFT JOIN BASI_220 t
+          ON t.TAMANHO_REF = v.TAM
+        WHERE 1=1
+          -- AND v.dt > TO_DATE('2019-01-01', 'yyyy-mm-dd')
+          {filtra_col} -- filtra_col
+          {filtra_cliente} -- filtra_cliente
+          {filtra_modelo} -- filtra_modelo
+          {filtra_ref} -- filtra_ref
+          {filtra_ultimos_dias} -- filtra_ultimos_dias
+        --
+        UNION
+        --
+        SELECT
+          0
+          {select_item} -- select_item
+        FROM BASI_010 v -- item (ref+tam+cor)
+        LEFT JOIN BASI_220 t
+          ON t.TAMANHO_REF = v.SUBGRU_ESTRUTURA
+        WHERE v.NIVEL_ESTRUTURA = 1
+          {filtra_modelo_select_item} -- filtra_modelo_select_item
+        ) v
+        GROUP BY
+          1
+          {group_por} -- group_por
+        ORDER BY
+          {order} -- order
+    """
+    cursor.execute(sql)
+
+    cached_result = rows_to_dict_list_lower(cursor)
+    cache.set(key_cache, cached_result)
+    fo2logger.info('calculated '+key_cache)
+    return cached_result
+
+
+def get_vendas_new(
+        cursor, ref=None, periodo=None, colecao=None, cliente=None, por=None,
         modelo=None, order_qtd=True, ultimos_dias=None, refs_incl=None, mult_incl=None):
     """
     ref e modelo e refs_incl são tratados em conjunto com OR
