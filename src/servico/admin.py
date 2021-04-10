d2d752d3335ecd504d130a8289db5060ae88ba45
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2021-04-09 21:31:34 -0300

    model EquipeAtendimento

--- src/servico/admin.py
+++ src/servico/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from fo2.admin import intr_adm_site
+
+import servico.models
+
+
+class EquipeAtendimentoAdmin(admin.ModelAdmin):
+    list_display = ['nome', 'slug', 'descricao']
+    fields = ['nome', 'slug', 'descricao']
+    readonly_fields = ['slug']
+
+
+intr_adm_site.register(servico.models.EquipeAtendimento, EquipeAtendimentoAdmin)
