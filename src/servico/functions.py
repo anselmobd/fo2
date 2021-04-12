3d7a9fa66bd2148c198aabd593b322862b332f2b
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2021-04-12 16:13:28 -0300

    function get_num_doc

---
+++ src/servico/functions.py
@@ -0,0 +1,8 @@
+import servico.models
+
+
+def get_num_doc(tipo):
+    tipo_obj = servico.models.TipoDocumento.objects.get(slug=tipo)
+    num_doc = servico.models.NumeroDocumento(tipo=tipo_obj)
+    num_doc.save()
+    return num_doc.id
