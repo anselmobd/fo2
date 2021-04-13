294aa126273e3f197f69516d61b2e3c060046e7f
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2021-04-13 13:51:27 -0300

    Iniciadas views da app serviço

--- src/servico/views.py
+++ src/servico/views.py
@@ -1,3 +1,18 @@
+from pprint import pprint
+
 from django.shortcuts import render
 
-# Create your views here.
+from base.views import O2BaseGetView
+
+
+def index(request):
+    return render(request, 'servico/index.html')
+
+
+class Ordens(O2BaseGetView):
+
+    def __init__(self, *args, **kwargs):
+        super(Ordens, self).__init__(*args, **kwargs)
+        self.template_name = 'servico/ordens.html'
+        self.title_name = 'Ordens'
+
