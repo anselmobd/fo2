92782f2c2476d66cdc10ecff757e504eb8f815d5
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2021-04-13 14:04:34 -0300

    view Ordens form

--- src/servico/views.py
+++ src/servico/views.py
@@ -2,17 +2,20 @@ from pprint import pprint
 
 from django.shortcuts import render
 
-from base.views import O2BaseGetView
+from base.views import O2BaseGetPostView
+
+import servico.forms
 
 
 def index(request):
     return render(request, 'servico/index.html')
 
 
-class Ordens(O2BaseGetView):
+class Ordens(O2BaseGetPostView):
 
     def __init__(self, *args, **kwargs):
         super(Ordens, self).__init__(*args, **kwargs)
+        self.Form_class = servico.forms.OrdensForm
         self.template_name = 'servico/ordens.html'
         self.title_name = 'Ordens'
 
