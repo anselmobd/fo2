9e79938e6f76ddd483d9981d30609ea79e633769
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2021-04-14 18:41:53 -0300

    alterei lógica para ter um acesso a menos ao banco

--- src/o2/functions.py
+++ src/o2/functions.py
@@ -1,15 +1,21 @@
 from pprint import pprint
 
+from django.db import IntegrityError
+
 import o2.models
+
+
+def save_csrf(token):
+    try:
+        csrf_token = o2.models.CsrfToken(token=token)
+        csrf_token.save()
+        return True
+    except IntegrityError:
+        return False
 
 
 def csrf_used(request):
     if request.method == "POST":
         token = request.POST['csrfmiddlewaretoken']
-        try:
-            o2.models.CsrfToken.objects.get(token=token)
-            return True
-        except o2.models.CsrfToken.DoesNotExist:
-            csrf_token = o2.models.CsrfToken(token=token)
-            csrf_token.save()
+        return not save_csrf(token)
     return False
