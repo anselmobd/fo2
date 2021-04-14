8b842a352f02ee2e20a8def04ff3ee35e3cdeead
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2021-04-14 17:41:46 -0300

    criado model CsrfToken para ajudar no controle de multiple form submissions

--- src/o2/models.py
+++ src/o2/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class CsrfToken(models.Model):
+    at = models.DateTimeField(
+        null=True, blank=True,
+    )
+    token = models.CharField(
+        max_length=128,
+    )
+
+    class Meta:
+        db_table = 'o2_csrf_token'
+        verbose_name = 'CSRF Token utilizado'
+        verbose_name_plural = 'CSRF Tokens utilizados'
+
+    def save(self, *args, **kwargs):
+        now = timezone.now()
+        if not self.id:
+            self.create_at = now
+        super(CsrfToken, self).save(*args, **kwargs)
