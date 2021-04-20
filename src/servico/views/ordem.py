59a146bb3f95193b3e45aa2ff3794d1da0910964
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2021-04-20 13:33:00 -0300

    muda get variable name

--- src/servico/views/ordem.py
+++ src/servico/views/ordem.py
@@ -14,13 +14,13 @@ class Ordem(O2BaseGetPostView):
         self.Form_class = servico.forms.OrdemForm
         self.template_name = 'servico/ordem.html'
         self.title_name = 'Ordem'
-        self.get_args = ['ordem']
+        self.get_args = ['numero']
 
 
     def mount_context(self):
         try:
-            self.ordem = int(self.ordem)
-            doc = servico.models.NumeroDocumento.objects.get(id=self.ordem)
+            self.numero = int(self.numero)
+            doc = servico.models.NumeroDocumento.objects.get(id=self.numero)
             eventos = servico.models.ServicoEvento.objects.filter(numero=doc).order_by('create_at')
         except servico.models.NumeroDocumento.DoesNotExist:
             self.context.update({
