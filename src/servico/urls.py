440e82a21c44f46a52956221b60ae32fa88fe415
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2021-04-20 10:58:17 -0300

    view Ordem recebe número por GET

--- src/servico/urls.py
+++ src/servico/urls.py
@@ -10,6 +10,8 @@ urlpatterns = [
 
     url(r'^ordem/$',
         views.Ordem.as_view(), name='ordem'),
+    url(r'^ordem/(?P<ordem>\d+)/$',
+        views.Ordem.as_view(), name='ordem__get'),
 
     url(r'^cria_ordem/$',
         views.CriaOrdem.as_view(), name='cria_ordem'),
