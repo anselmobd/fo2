1461ce43f7fac7d679ce585f991d130e4b15462c
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2021-04-29 11:57:54 -0300

    view Lista separando query para servico.queries.lista.lista_documentos

---
+++ src/servico/queries/lista.py
@@ -0,0 +1,61 @@
+from pprint import pprint
+
+from utils.functions.models import rows_to_dict_list_lower
+
+
+def lista_documentos(cursor, ordem):
+    
+    filtra_ordem = ''
+    if ordem != 0:
+        filtra_ordem = f'and il.documento_id = {ordem}'
+
+    sql = f"""
+        with inte_limites as
+        ( select
+            int_lim.documento_id
+        , max(int_lim.create_at) ult_at
+        , min(int_lim.create_at) pri_at
+        , count(int_lim.create_at) conta
+        from fo2_serv_interacao int_lim
+        group by
+            int_lim.documento_id
+        )
+        select
+            inte.documento_id
+        , inte.create_at
+        , s.nome status__nome
+        --, ev.nome evento__nome
+        , u.username user__username
+        , e.nome equipe__nome
+        , na.nome nivel__nome
+        , inte.descricao
+        , last_s.nome last_status__nome
+        , last_inte.create_at last_create_at
+        , il.ult_at - il.pri_at diff_at
+        , il.conta
+        from inte_limites il
+        join fo2_serv_interacao inte
+            on inte.documento_id = il.documento_id
+            and inte.create_at = il.pri_at
+        join auth_user u
+            on u.id = inte.user_id
+        join fo2_serv_equipe e
+            on e.id = inte.equipe_id
+        join fo2_serv_status s
+            on s.id = inte.status_id
+        join fo2_serv_evento ev
+            on ev.id = inte.evento_id
+        join fo2_serv_nivel_atend na
+            on na.id = inte.nivel_id
+        join fo2_serv_interacao last_inte
+            on last_inte.documento_id = il.documento_id
+            and last_inte.create_at = il.ult_at
+        join fo2_serv_status last_s
+            on last_s.id = last_inte.status_id
+        where 1=1
+            {filtra_ordem} -- filtra_ordem
+        order by
+            il.documento_id desc
+    """
+    cursor.execute(sql)
+    return rows_to_dict_list_lower(cursor)
