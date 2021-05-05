e755512cadb3501fbf84e095df30534b24b223d9
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2021-05-05 17:53:07 -0300

    view ordem: spo apresenta botões de eventos que o usuário atual pode executar

--- src/servico/views/ordem.py
+++ src/servico/views/ordem.py
@@ -1,5 +1,7 @@
 from pprint import pprint
 
+from django.db.models import Q
+
 from base.views import O2BaseGetPostView
 from utils.classes import LoggedInUser
 
@@ -49,14 +51,54 @@ class Ordem(O2BaseGetPostView):
             'classificacao__nome',
             'classificacao__horas',
             'equipe__nome',
+            'equipe__id',
             'descricao',
         )
 
         if logged_in.user:
+            try:
+                usuario_funcao = servico.models.UsuarioFuncao.objects.get(
+                    usuario=logged_in.user,
+                    funcao__independente=False,
+                    equipe=interacoes[0]['equipe__id']
+                )
+                nivel_op = usuario_funcao.funcao.nivel_operacional
+            except servico.models.UsuarioFuncao.DoesNotExist:
+                nivel_op = 0
+
+            acesso = logged_in.user == doc.user
+            if not acesso:
+                try:
+                    usuario_funcao = servico.models.UsuarioFuncao.objects.get(
+                        usuario=logged_in.user,
+                        funcao__independente=True,
+                    )
+                    acesso = True
+                except servico.models.UsuarioFuncao.DoesNotExist:
+                    pass
+
             self.tipos_eventos = servico.models.Evento.objects.filter(
                 statusevento__status_pre=interacoes[0]['status_id']
             )
-            
+            if nivel_op > 0 and acesso:
+                self.tipos_eventos = self.tipos_eventos.filter(
+                    Q(nivel_op_minimo__gt=0, nivel_op_minimo__lte=nivel_op)
+                    |
+                    Q(nivel_op_minimo=0)
+                )
+            elif nivel_op > 0:
+                self.tipos_eventos = self.tipos_eventos.filter(
+                    nivel_op_minimo__gt=0, nivel_op_minimo__lte=nivel_op
+                )
+            elif acesso:
+                self.tipos_eventos = self.tipos_eventos.filter(
+                    nivel_op_minimo=0
+                )
+            else:
+                # força não encontrar nada
+                self.tipos_eventos = self.tipos_eventos.filter(
+                    id=-1
+                )
             self.tipos_eventos = self.tipos_eventos.order_by('ordem').values()
         else:
             self.tipos_eventos = []
