98f3c22c3860729bf3094d8533d542fa143ab465
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2021-05-19 11:24:45 -0300

    separando class LotesPermissions

--- src/lotes/models/__init__.py
+++ src/lotes/models/__init__.py
@@ -1,21 +1,5 @@
-from django.db import models
-
 from .impresso import *
 from .op import *
 from .parametro import *
+from .permissions import *
 from .solicitacao import *
-
-
-class LotesPermissions(models.Model):
-
-    class Meta:
-        managed = False
-        permissions = (
-            ("can_edit_estagio_direito", "Can edit direitos a estágios"),
-            ("can_print__solicitacao_parciais",
-             "Pode imprimir etiquetas de solicitações parciais"),
-            ("can_reabrir_solicitacao_completada",
-             "Pode reabrir solicitação marada como completa"),
-            ("libera_coleta_de_solicitacao",
-             "Libera coleta de solicitação"),
-        )
