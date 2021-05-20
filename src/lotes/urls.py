c085c1b6c26f55c4874d027d42aeb9bc0b6a92a0
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2021-05-20 13:56:54 -0300

    preparando rotina para fazer Edit, Add e Delete

--- src/lotes/urls.py
+++ src/lotes/urls.py
@@ -112,7 +112,7 @@ urlpatterns = [
 
     url(r'^regras_lote_caixa/$',
         views.RegrasLoteCaixa.as_view(), name='regras_lote_caixa'),
-    url(r'^regras_lote_caixa/(?P<colecao>[^/]+)?/(?P<referencia>[^/]+)?/$',
+    url(r'^regras_lote_caixa/(?P<colecao>[^/]+)?/(?P<referencia>[^/]+)?/(?P<ead>[^/]+)?/$',
         views.RegrasLoteCaixa.as_view(), name='regras_lote_caixa__get'),
 
     url(r'^meta_giro/$',
