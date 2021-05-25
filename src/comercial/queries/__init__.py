ac93ff7f73d73bd3b0c1770ebca1a0887e5db55d
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2021-05-25 15:32:10 -0300

    seperando query pa_de_modelo

--- src/comercial/queries/__init__.py
+++ src/comercial/queries/__init__.py
@@ -1,5 +1,6 @@
 from .analise_vendas import *
 from .dados_meta_no_ano import *
 from .faturamento_para_meta import *
+from .pa_de_modelo import *
 from .queries import *
 from .tussor import *
--- src/comercial/queries/queries.py
+++ src/comercial/queries/queries.py
@@ -632,38 +632,6 @@ def get_vendas_new(
     return cached_result
 
 
-def pa_de_modelo(cursor, modelo=None):
-
-    key_cache = my_make_key_cache('pa_de_modelo', modelo)
-    cached_result = cache.get(key_cache)
-    if cached_result is not None:
-        fo2logger.info('cached '+key_cache)
-        return cached_result
-
-    sql = f"""
-        SELECT DISTINCT
-          v.REFERENCIA REF
-        FROM BASI_030 v -- ref
-        WHERE 1=1
-          AND v.REFERENCIA LIKE '%{modelo}%' 
-          AND TRIM(
-                LEADING '0' FROM (
-                  REGEXP_REPLACE(
-                    v.REFERENCIA
-                  , '^([^a-zA-Z]+)[a-zA-Z]*$'
-                  , '\\1'
-                  )
-                )
-              ) = '{modelo}'
-    """
-    cursor.execute(sql)
-
-    cached_result = rows_to_dict_list_lower(cursor)
-    cache.set(key_cache, cached_result)
-    fo2logger.info('calculated '+key_cache)
-    return cached_result
-
-
 def devolucao_para_meta(cursor, ano, mes=None, tipo='total', empresa=1, ref=None):
     '''
         tipo:
