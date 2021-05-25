2a0174e6a51ee39c1b4534d38053ce9d303d26e4
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2021-05-25 08:40:06 -0300

    obs já estava nos dados; só não estava no layout

--- src/lotes/views/imprime_caixa_lotes.py
+++ src/lotes/views/imprime_caixa_lotes.py
@@ -54,8 +54,6 @@ class ImprimeCaixaLotes(LoginRequiredMixin, View):
             if n_lote_caixa == 1:
                 l_row = {
                     'lotes_caixa': lotes_caixa,
-                    'obs1': obs1,
-                    'obs2': obs2,
                     'cor': row['cor'],
                     'data_entrada_corte': row['data_entrada_corte'],
                     'descr_cor': row['descr_cor'],
