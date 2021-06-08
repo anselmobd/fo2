e3b6856cdd62a4e93b91fe14e49de20964d58763
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2021-06-08 17:54:43 -0300

    view PedidosParaMeta: fechando primeira versão funcional; simplificada para apresentar apenas os pedidos contados na meta do mês corrente

--- src/comercial/views/pedidos_para_meta.py
+++ src/comercial/views/pedidos_para_meta.py
@@ -1,51 +1,26 @@
 import datetime
 from pprint import pprint
 
-from django.shortcuts import render
-from django.views import View
-
 from fo2.connections import db_cursor_so
 
-from base.views import O2BaseGetPostView
+from base.views import O2BaseGetView
 from utils.functions import dias_mes_data
-from utils.functions.models import queryset_to_dict_list_lower
 from utils.views import totalize_data
 
 import lotes.queries.pedido as l_q_p
 
-import comercial.forms
-import comercial.models
-import comercial.queries
 
-
-class PedidosParaMeta(O2BaseGetPostView):
+class PedidosParaMeta(O2BaseGetView):
 
     def __init__(self, *args, **kwargs):
         super(PedidosParaMeta, self).__init__(*args, **kwargs)
-        self.Form_class = comercial.forms.PedidosParaMetaForm
         self.template_name = 'comercial/pedidos_para_meta.html'
         self.title_name = 'Pedidos no mês'
 
     def mount_context(self):
         cursor = db_cursor_so(self.request)
 
-        ano = self.form.cleaned_data['ano']
-        mes = self.form.cleaned_data['mes']
-
         hoje = datetime.date.today()
-        if ano is None:
-            ano_atual = hoje.year
-        else:
-            ano_atual = ano
-        if mes is None:
-            mes_atual = hoje.month
-        else:
-            mes_atual = mes
-
-        self.context.update({
-            'ano': ano_atual,
-            'mes': mes_atual,
-        })
 
         # ignorando form
         dia_atual = hoje.day
@@ -57,7 +32,8 @@ class PedidosParaMeta(O2BaseGetPostView):
         })
 
         pedidos = l_q_p.pedido_faturavel_modelo(
-            cursor, periodo=f'-{dia_atual}:{dias_mes-dia_atual}', nat_oper=nat_oper)
+            cursor, periodo=f'-{dia_atual}:{dias_mes-dia_atual}',
+            nat_oper=nat_oper, group='p')
 
         if len(pedidos) == 0:
             self.context.update({
@@ -65,11 +41,12 @@ class PedidosParaMeta(O2BaseGetPostView):
             })
             return
 
-        total_pedido = 0
+        n = 1
         for pedido in pedidos:
-            total_pedido += pedido['PRECO']
             pedido['DATA'] = pedido['DATA'].date()
-        # total_pedido = int(round(total_pedido/1000))
+            pedido['valor|DECIMALS'] = 2
+            pedido['N'] = n
+            n += 1
 
         totalize_data(pedidos, {
             'sum': ['QTD', 'PRECO'],
@@ -78,10 +55,10 @@ class PedidosParaMeta(O2BaseGetPostView):
         })
 
         self.context.update({
-            'headers': ['Pedido', 'Data', 'Cliente',
-                        'Referência', 'Quantidade', 'Valor', 'Situação'],
-            'fields': ['PEDIDO', 'DATA', 'CLIENTE',
-                        'REF', 'QTD', 'PRECO', 'FAT'],
+            'headers': ['#', 'Pedido', 'Data', 'Cliente',
+                        'Quantidade', 'Valor', 'Situação'],
+            'fields': ['N', 'PEDIDO', 'DATA', 'CLIENTE',
+                        'QTD', 'PRECO', 'FAT'],
             'data': pedidos,
             'style': {
                 5: 'text-align: right;',
