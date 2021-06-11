2c6027b346d01ed84b218236717944ec39f1b74c
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2021-06-11 09:46:31 -0300

    conexão direta ao banco; sem VPN

--- src/fo2/production_settings.py
+++ src/fo2/production_settings.py
@@ -31,7 +31,8 @@ DATABASES = {
     },
     'sn': {  # Systextil OC (nuvem)
         'ENGINE': 'django.db.backends.oracle',
-        'NAME': '10.0.0.3:1521/db_pdb1.sub02011943440.tussorvcn.oraclevcn.com',
+        # 'NAME': '10.0.0.3:1521/db_pdb1.sub02011943440.tussorvcn.oraclevcn.com',
+        'NAME': '152.67.55.216:1521/db_pdb1.sub02011943440.tussorvcn.oraclevcn.com',
         'USER': 'systextil',
         'PASSWORD': DBPASS_SH,
     },
