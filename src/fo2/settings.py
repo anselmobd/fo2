404e693f0f857c92f8f8d8c9a99f0d5c5f8c4f77
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2021-06-11 19:55:12 -0300

    adicionei os dados de conexão à nuvem no DATABASES_EXTRAS e adicionei teste dessa conexão "extra" na view TestaDB

--- src/fo2/settings.py
+++ src/fo2/settings.py
@@ -229,6 +229,13 @@ DATABASES_EXTRAS = {
         'HOST': 'localhost',
         'PORT': '14521',
     },
+    'sn': {  # Systextil OC (nuvem)
+        'ENGINE': 'django.db.backends.oracle',
+        # 'NAME': 'localhost:14521/db_pdb1.sub02011943440.tussorvcn.oraclevcn.com',
+        'NAME': '152.67.55.216:1521/db_pdb1.sub02011943440.tussorvcn.oraclevcn.com',
+        'USER': 'systextil',
+        'PASSWORD': DBPASS_SH,
+    },
 }
 
 DATABASE_ROUTERS = ['utils.router.Router', ]
