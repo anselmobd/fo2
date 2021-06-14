0d33137d096d955de3e7f3aaf1f733cd004bd7ed
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2021-06-14 14:35:25 -0300

    versão inicial da view EntradaNfLista

--- src/logistica/views/entr_nf_lista.py
+++ src/logistica/views/entr_nf_lista.py
@@ -1,7 +1,34 @@
 from pprint import pprint
 
-from base.views import O2BaseGetPostView
+from base.views import O2BaseGetView
+
+import logistica.models
 
 
-class EntradaNfLista(O2BaseGetPostView):
-    pass
+class EntradaNfLista(O2BaseGetView):
+
+    def __init__(self, *args, **kwargs):
+        super(EntradaNfLista, self).__init__(*args, **kwargs)
+        self.template_name = 'logistica/entrada_nf/lista.html'
+        self.title_name = 'Lista NF de entrada'
+
+    def mount_context(self):
+        dados = logistica.models.NfEntrada.objects.all().values()
+
+        self.context.update({
+            'headers': (
+                'emissor', 'numero', 'descricao', 'qtd',
+                'hora_entrada', 'transportadora', 'motorista', 'placa',
+                'responsavel', 'usuario', 'quando'
+            ),
+            'fields': (
+                'emissor', 'numero', 'descricao', 'qtd',
+                'hora_entrada', 'transportadora', 'motorista', 'placa',
+                'responsavel', 'usuario', 'quando'
+            ),
+            'dados': dados,
+        })
+
+
+
+
