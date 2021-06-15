de356a185989ee3e7e63df48426c5f61881a348c
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2021-06-15 16:27:56 -0300

    iniciada a view EntradaNfSemXml com cópias de outros locais (WIP)

--- src/logistica/forms.py
+++ src/logistica/forms.py
@@ -2,6 +2,7 @@ from datetime import datetime, timedelta
 
 from django import forms
 
+from base.forms import O2BaseForm
 from utils.functions import shift_years
 
 from .models import *
@@ -114,3 +115,95 @@ class NfPosicaoForm(forms.Form):
         label='Posição', required=False,
         queryset=PosicaoCarga.objects.all().order_by('id'),
         initial=2, empty_label='--Todas--')
+
+
+class EntradaNfSemXmlForm(
+        O2BaseForm):
+
+    def data_ini():
+        return (datetime.now().replace(day=1)-timedelta(days=1)).replace(day=1)
+
+    data_de = forms.DateField(
+        label='Data do Faturamento: De',
+        initial=data_ini,
+        widget=forms.DateInput(attrs={'type': 'date',
+                               'autofocus': 'autofocus'}))
+    data_ate = forms.DateField(
+        label='Até', required=False,
+        widget=forms.DateInput(attrs={'type': 'date'}))
+
+    uf = forms.CharField(
+        label='UF', max_length=2, min_length=2, required=False,
+        widget=forms.TextInput(attrs={'size': 2}))
+
+    nf = forms.CharField(
+        label='Número da NF', required=False,
+        widget=forms.TextInput(attrs={'type': 'number'}))
+
+    transportadora = forms.CharField(
+        label='Transportadora', required=False,
+        help_text='Sigla da transportadora.',
+        widget=forms.TextInput())
+
+    cliente = forms.CharField(
+        label='Cliente', required=False,
+        help_text='Parte do nome ou início do CNPJ.',
+        widget=forms.TextInput())
+
+    pedido = forms.CharField(
+        label='Pedido Tussor', required=False,
+        widget=forms.TextInput(attrs={'type': 'number'}))
+
+    ped_cliente = forms.CharField(
+        label='Pedido de cliente', required=False,
+        widget=forms.TextInput(attrs={'type': 'string'}))
+
+    CHOICES = [('N', 'Não filtra'),
+               ('C', 'Com data de saída informada'),
+               ('S', 'Sem data de saída')]
+    data_saida = forms.ChoiceField(
+        label='Quanto a data de saída', choices=CHOICES, initial='S')
+
+    CHOICES = [('T', 'Todos (Sim ou Não)'),
+               ('S', 'Sim'),
+               ('N', 'Não')]
+    entregue = forms.ChoiceField(
+        choices=CHOICES, initial='T')
+
+    CHOICES = [('N', 'Número da nota fiscal'),
+               ('A', 'Atraso (maior primeiro)')]
+    ordem = forms.ChoiceField(
+        label='Ordem de apresentação', choices=CHOICES, initial='A')
+
+    CHOICES = [('V', 'Apenas NF de venda e ativas (não canceladas)'),
+               ('T', 'Totas as notas fiscais')]
+    listadas = forms.ChoiceField(
+        label='Notas listadas', choices=CHOICES, initial='V')
+
+    posicao = forms.ModelChoiceField(
+        label='Posição', required=False,
+        queryset=PosicaoCarga.objects.all().order_by('id'),
+        empty_label='--Todas--')
+
+    por_pagina = forms.IntegerField(
+        label='NF por página', required=False,
+        help_text='Padrão: 100 por página.',
+        widget=forms.TextInput(attrs={'type': 'number'}))
+
+    page = forms.IntegerField(
+        required=False, widget=forms.HiddenInput())
+
+    def clean_uf(self):
+        uf = self.cleaned_data['uf'].upper()
+        data = self.data.copy()
+        data['uf'] = uf
+        self.data = data
+        return uf
+
+    def clean_data_de(self):
+        data_de = self.cleaned_data['data_de']
+        if data_de:
+            if data_de.year < 100:
+                data_de = shift_years(2000, data_de)
+        return data_de
+
--- src/logistica/views/entr_nf_sem_xml.py
+++ src/logistica/views/entr_nf_sem_xml.py
@@ -2,6 +2,37 @@ from pprint import pprint
 
 from base.views import O2BaseGetPostView
 
+import logistica.forms
+import logistica.models
+
 
 class EntradaNfSemXml(O2BaseGetPostView):
-    pass
+
+    def __init__(self, *args, **kwargs):
+        super(EntradaNfSemXml, self).__init__(*args, **kwargs)
+        self.Form_class = logistica.forms.EntradaNfSemXmlForm
+        self.template_name = 'logistica/entrada_nf/sem_xml.html'
+        self.title_name = 'Entrada de NF sem XML'
+
+    def mount_context(self):
+        fields = (
+            'emissor', 'numero', 'descricao', 'qtd',
+            'hora_entrada', 'transportadora', 'motorista', 'placa',
+            'responsavel', 'usuario__username', 'quando'
+        )
+        dados = logistica.models.NfEntrada.objects.all().values(*fields)
+        pprint(dados)
+
+        self.context.update({
+            'headers': (
+                'Emissor', 'NF', 'Descrição', 'Quant.',
+                'Hora de entrada', 'Transportadora', 'Motorista', 'Placa',
+                'Responsável', 'Digitado por', 'Digitado em'
+            ),
+            'fields': fields,
+            'dados': dados,
+        })
+
+
+
+
