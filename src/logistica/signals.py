6cbec1a34d65946dd4e6a804922a926e921156de
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2021-06-16 18:56:28 -0300

    ligado tracking de NfEntrada

--- src/logistica/signals.py
+++ src/logistica/signals.py
@@ -1,9 +1,13 @@
-from django.db.models.signals import post_init, post_save
+from django.db.models.signals import post_init, post_save, post_delete
 
-from geral.signals import post_init_tracking, post_save_tracking
-from .models import NotaFiscal
+from geral.signals import post_init_tracking, post_save_tracking, post_delete_tracking
+from .models import NotaFiscal, NfEntrada
 
 
 post_init.connect(post_init_tracking, sender=NotaFiscal)
+post_init.connect(post_init_tracking, sender=NfEntrada)
 
 post_save.connect(post_save_tracking, sender=NotaFiscal)
+post_save.connect(post_save_tracking, sender=NfEntrada)
+
+post_delete.connect(post_delete_tracking, sender=NfEntrada)
