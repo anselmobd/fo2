e6ef2e664e5cb17b6da8f74312a429847a452ab4
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2021-06-16 12:42:00 -0300

    ertirando tentativa de mensagem de erro melhor para unique_together

--- src/logistica/forms.py
+++ src/logistica/forms.py
@@ -2,10 +2,8 @@ from datetime import datetime, timedelta
 from pprint import pprint
 
 from django import forms
-from django.core.exceptions import NON_FIELD_ERRORS
 from django.forms import ModelForm
 
-from base.forms import O2BaseForm
 from utils.functions import shift_years
 
 from .models import *
@@ -128,8 +126,3 @@ class EntradaNfSemXmlForm(ModelForm):
             'hora_entrada', 'transportadora', 'motorista', 'placa',
             'responsavel'
         ]
-        error_messages = {
-            NON_FIELD_ERRORS: {
-                'unique_together': "O par CNPJ e número não podem ser repetidos",
-            }
-        }
