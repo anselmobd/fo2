cb90ea613cb7f8f50d0c3d0a455116d68c6bc89c
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2021-06-17 21:10:46 -0300

    criado model Empresa

--- src/base/models.py
+++ src/base/models.py
@@ -5,6 +5,20 @@ from django.utils.text import slugify
 from django.contrib.auth.models import User
 
 
+class Empresa(models.Model):
+    numero = models.IntegerField(
+        'número')
+    nome = models.CharField(
+        max_length=20)
+
+    def __str__(self):
+        return f'{self.numero}-{self.nome}'
+
+    class Meta:
+        db_table = "fo2_empresa"
+        verbose_name = "Empresa"
+
+
 class Colaborador(models.Model):
     user = models.OneToOneField(
         User, on_delete=models.CASCADE, verbose_name='usuário')
