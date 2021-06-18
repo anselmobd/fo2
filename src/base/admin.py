98b32a9b158917ab53fde8868c49ae77af5a374f
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2021-06-17 22:16:52 -0300

    ir ao admin da Empresa, sincroniza a tabela com o cadastro de empresa do systextil

--- src/base/admin.py
+++ src/base/admin.py
@@ -1,9 +1,14 @@
+from pprint import pprint
+
 from django.contrib import admin
 
 from fo2.admin import intr_adm_site
 
+from systextil.models import Empresa as ErpEmpresa
+
 from .models import (
     Colaborador,
+    Empresa,
     Imagem,
     Requisicao,
     Tamanho,
@@ -48,8 +53,39 @@ class RequisicaoAdmin(admin.ModelAdmin):
     ]
 
 
+class EmpresaAdmin(admin.ModelAdmin):
+    readonly_fields = ['numero', 'nome']
+
+    def get_queryset(self, request):
+        erp_empr = ErpEmpresa.objects.all().values()
+        dict_erp_empr = {e['codigo_empresa']: e['nome_fantasia'] for e in erp_empr}
+
+        fo2_empr = Empresa.objects.all().values()
+        dict_fo2_empr = {e['numero']: e['nome'] for e in fo2_empr}
+
+        excluir = dict(set(dict_fo2_empr.items())-set(dict_erp_empr.items()))
+        for numero in excluir:
+            exclui = Empresa.objects.get(numero=numero, nome=excluir[numero])
+            exclui.delete()
+
+        incluir = dict(set(dict_erp_empr.items())-set(dict_fo2_empr.items()))
+        for numero in incluir:
+            inclui = Empresa(numero=numero, nome=incluir[numero])
+            inclui.save()
+
+        return super(EmpresaAdmin, self).get_queryset(request)
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    def has_add_permission(self, request, obj=None):
+        return False
+
+
 intr_adm_site.register(TipoImagem, TipoImagemAdmin)
 intr_adm_site.register(Imagem, ImagemAdmin)
 intr_adm_site.register(Tamanho, TamanhoAdmin)
 intr_adm_site.register(Colaborador, ColaboradorAdmin)
 intr_adm_site.register(Requisicao, RequisicaoAdmin)
+intr_adm_site.register(Empresa, EmpresaAdmin)
+
