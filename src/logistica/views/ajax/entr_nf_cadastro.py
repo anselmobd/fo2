05d5b9ef3015edcda7976d129970f1328f09dfb2
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2021-06-21 17:04:18 -0300

    ajax aceita CNPJ formatado e incompleto, como o model

--- src/logistica/views/ajax/entr_nf_cadastro.py
+++ src/logistica/views/ajax/entr_nf_cadastro.py
@@ -3,17 +3,25 @@ from pprint import pprint
 
 from django.http import HttpResponse
 
+from utils.functions.cadastro import CNPJ
+
 from logistica.models import NfEntrada
 
 
 def entr_nf_cadastro(request, *args, **kwargs):
+    result = {}
+
     try:
         cadastro = kwargs['cadastro']
-        result = NfEntrada.objects.filter(
-            cadastro=cadastro
-        ).values(
-            'emissor', 'descricao', 'transportadora'
-        ).order_by('-quando').first()
+        val_cnpj = CNPJ()
+        if val_cnpj.validate(cadastro):
+            cadastro = val_cnpj.cnpj
+            result = NfEntrada.objects.filter(
+                cadastro=cadastro
+            ).values(
+                'emissor', 'descricao', 'transportadora'
+            ).order_by('-quando').first()
     except Exception:
-        result = {}
+        pass
+
     return HttpResponse(json.dumps(result), content_type="application/json")
