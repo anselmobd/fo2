89c98e4048fc9bfeb1cca7d0d2890898817d31c1
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2021-06-22 20:30:17 -0300

    black format

--- src/logistica/views/ajax/entr_nf_cadastro.py
+++ src/logistica/views/ajax/entr_nf_cadastro.py
@@ -14,25 +14,36 @@ def entr_nf_cadastro(request, *args, **kwargs):
     result = {}
 
     try:
-        cadastro = kwargs['cadastro']
+        cadastro = kwargs["cadastro"]
         val_cnpj = CNPJ()
         if val_cnpj.validate(cadastro):
             cadastro = val_cnpj.cnpj
             cadastro_mask = val_cnpj.mask(cadastro)
-            result = NfEntrada.objects.filter(
-                Q(cadastro=cadastro) | Q(cadastro=cadastro_mask)
-            ).values(
-                'emissor', 'descricao', 'transportadora',
-                'motorista', 'placa', 'quando'
-            ).order_by('-quando').first()
-            result.update({
-                'cadastro': cadastro_mask,
-            })
-            result['quando'] = result['quando'].date()
-            if result['quando'] != datetime.date.today():
-                del(result['motorista'])
-                del(result['placa'])
+            result = (
+                NfEntrada.objects.filter(
+                    Q(cadastro=cadastro) | Q(cadastro=cadastro_mask)
+                )
+                .values(
+                    "emissor",
+                    "descricao",
+                    "transportadora",
+                    "motorista",
+                    "placa",
+                    "quando",
+                )
+                .order_by("-quando")
+                .first()
+            )
+            result.update(
+                {"cadastro": cadastro_mask,}
+            )
+            result["quando"] = result["quando"].date()
+            if result["quando"] != datetime.date.today():
+                del result["motorista"]
+                del result["placa"]
     except Exception:
         pass
 
-    return HttpResponse(json.dumps(result, default=str), content_type="application/json")
+    return HttpResponse(
+        json.dumps(result, default=str), content_type="application/json"
+    )
