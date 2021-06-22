725573e0462e7df670bbec9b5f7446e5b5d93967
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2021-06-22 20:33:17 -0300

    black format (BF)

--- src/logistica/urls.py
+++ src/logistica/urls.py
@@ -5,33 +5,50 @@ import logistica.views.ajax
 
 
 urlpatterns = [
-    url(r'^$', logistica.views.index, name='index'),
-
-    url(r'^notafiscal_rel/$',
-        logistica.views.NotafiscalRel.as_view(), name='notafiscal_rel'),
-    url(r'^notafiscal_rel/(?P<dia>\d+)/(?P<mes>\d+)/(?P<ano>\d+)/$',
-        logistica.views.NotafiscalRel.as_view(), name='notafiscal_rel__get'),
-
-    url(r'^notafiscal_chave/(?P<chave>\d+)?/?$',
-        logistica.views.NotafiscalChave.as_view(), name='notafiscal_chave'),
-
-    url(r'^notafiscal_nf/(?P<nf>\d+)?/?$',
-        logistica.views.notafiscal_nf, name='notafiscal_nf'),
-
-    url(r'^notafiscal_embarcando/$',
-        logistica.views.NotafiscalEmbarcando.as_view(), name='notafiscal_embarcando'),
-
-    url(r'^notafiscal_movimentadas/$',
+    url(r"^$", logistica.views.index, name="index"),
+    url(
+        r"^notafiscal_rel/$",
+        logistica.views.NotafiscalRel.as_view(),
+        name="notafiscal_rel",
+    ),
+    url(
+        r"^notafiscal_rel/(?P<dia>\d+)/(?P<mes>\d+)/(?P<ano>\d+)/$",
+        logistica.views.NotafiscalRel.as_view(),
+        name="notafiscal_rel__get",
+    ),
+    url(
+        r"^notafiscal_chave/(?P<chave>\d+)?/?$",
+        logistica.views.NotafiscalChave.as_view(),
+        name="notafiscal_chave",
+    ),
+    url(
+        r"^notafiscal_nf/(?P<nf>\d+)?/?$",
+        logistica.views.notafiscal_nf,
+        name="notafiscal_nf",
+    ),
+    url(
+        r"^notafiscal_embarcando/$",
+        logistica.views.NotafiscalEmbarcando.as_view(),
+        name="notafiscal_embarcando",
+    ),
+    url(
+        r"^notafiscal_movimentadas/$",
         logistica.views.NotafiscalMovimentadas.as_view(),
-        name='notafiscal_movimentadas'),
-
-    url(r'^entrada_nf/_sem_xml/$',
-        logistica.views.EntradaNfSemXml.as_view(), name='entr_nf_sem_xml'),
-
-    url(r'^entrada_nf/lista/$',
-        logistica.views.EntradaNfLista.as_view(), name='entr_nf_lista'),
-
-    url(r'^ajax/entr_nf_cadastro/(?P<cadastro>[^/]+)/$',
-        logistica.views.ajax.entr_nf_cadastro, name='ajax_entr_nf_cadastro'),
-
+        name="notafiscal_movimentadas",
+    ),
+    url(
+        r"^entrada_nf/_sem_xml/$",
+        logistica.views.EntradaNfSemXml.as_view(),
+        name="entr_nf_sem_xml",
+    ),
+    url(
+        r"^entrada_nf/lista/$",
+        logistica.views.EntradaNfLista.as_view(),
+        name="entr_nf_lista",
+    ),
+    url(
+        r"^ajax/entr_nf_cadastro/(?P<cadastro>[^/]+)/$",
+        logistica.views.ajax.entr_nf_cadastro,
+        name="ajax_entr_nf_cadastro",
+    ),
 ]
