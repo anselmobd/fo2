55e27b40233ec206bc9721e978558f7d58df566b
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2021-06-23 16:53:28 -0300

    BF

--- src/logistica/views/entr_nf_sem_xml.py
+++ src/logistica/views/entr_nf_sem_xml.py
@@ -9,18 +9,15 @@ import logistica.models
 
 
 class EntradaNfSemXml(LoginRequiredMixin, O2BaseGetPostView):
-
     def __init__(self, *args, **kwargs):
         super(EntradaNfSemXml, self).__init__(*args, **kwargs)
         self.Form_class = logistica.forms.EntradaNfSemXmlForm
-        self.template_name = 'logistica/entrada_nf/sem_xml.html'
-        self.title_name = 'Entrada de NF sem XML'
+        self.template_name = "logistica/entrada_nf/sem_xml.html"
+        self.title_name = "Entrada de NF sem XML"
 
     def mount_context(self):
         try:
             nf = logistica.models.NfEntrada(**self.form.cleaned_data)
             nf.save()
         except Exception as e:
-            self.context.update({
-                'msg_erro': f"NF não gravada - {e}"
-            })
+            self.context.update({"msg_erro": f"NF não gravada - {e}"})
