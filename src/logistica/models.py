36117fb08dc1fe8af66102a8b667e8e17a88553d
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2021-06-23 16:51:52 -0300

    BF

--- src/logistica/models.py
+++ src/logistica/models.py
@@ -13,9 +13,7 @@ from base.models import Empresa
 
 
 class PosicaoCarga(models.Model):
-    nome = models.CharField(
-        max_length=20,
-        db_index=True, unique=True)
+    nome = models.CharField(max_length=20, db_index=True, unique=True)
 
     def __str__(self):
         return self.nome
@@ -28,66 +26,46 @@ class PosicaoCarga(models.Model):
 
 class NotaFiscal(models.Model):
     # campos importados
-    numero = models.IntegerField(
-        db_index=True, unique=True, verbose_name='número')
-    ativa = models.BooleanField(
-        db_index=True, default=True)
-    faturamento = models.DateTimeField(
-        db_index=True, null=True, blank=True)
-    cod_status = models.IntegerField(
-        null=True, blank=True,
-        verbose_name='status')
+    numero = models.IntegerField(db_index=True, unique=True, verbose_name="número")
+    ativa = models.BooleanField(db_index=True, default=True)
+    faturamento = models.DateTimeField(db_index=True, null=True, blank=True)
+    cod_status = models.IntegerField(null=True, blank=True, verbose_name="status")
     msg_status = models.CharField(
-        max_length=200, null=True, blank=True,
-        verbose_name='status (descr.)')
+        max_length=200, null=True, blank=True, verbose_name="status (descr.)"
+    )
     dest_cnpj = models.CharField(
-        max_length=20, null=True, blank=True,
-        verbose_name='CNPJ')
+        max_length=20, null=True, blank=True, verbose_name="CNPJ"
+    )
     dest_nome = models.CharField(
-        max_length=100, null=True, blank=True,
-        verbose_name='Destinatário')
-    natu_venda = models.BooleanField(
-        db_index=True, default=False, verbose_name='venda')
-    uf = models.CharField(
-        max_length=2, null=True, blank=True,
-        verbose_name='UF')
+        max_length=100, null=True, blank=True, verbose_name="Destinatário"
+    )
+    natu_venda = models.BooleanField(db_index=True, default=False, verbose_name="venda")
+    uf = models.CharField(max_length=2, null=True, blank=True, verbose_name="UF")
     natu_descr = models.CharField(
-        max_length=100, null=True, blank=True,
-        verbose_name='natureza')
+        max_length=100, null=True, blank=True, verbose_name="natureza"
+    )
     transp_nome = models.CharField(
-        max_length=100, null=True, blank=True,
-        verbose_name='transportadora')
-    valor = models.DecimalField(
-        decimal_places=2, max_digits=10,
-        null=True, blank=True)
-    volumes = models.IntegerField(
-        null=True, blank=True)
-    pedido = models.IntegerField(
-        null=True, blank=True,
-        verbose_name='pedido')
+        max_length=100, null=True, blank=True, verbose_name="transportadora"
+    )
+    valor = models.DecimalField(decimal_places=2, max_digits=10, null=True, blank=True)
+    volumes = models.IntegerField(null=True, blank=True)
+    pedido = models.IntegerField(null=True, blank=True, verbose_name="pedido")
     ped_cliente = models.CharField(
-        max_length=30, null=True, blank=True,
-        verbose_name='pedido cliente')
-    nf_devolucao = models.IntegerField(
-        null=True, blank=True, verbose_name='Devolução')
+        max_length=30, null=True, blank=True, verbose_name="pedido cliente"
+    )
+    nf_devolucao = models.IntegerField(null=True, blank=True, verbose_name="Devolução")
     trail = models.CharField(
-        db_index=True, max_length=32, null=True, blank=True, default='')
+        db_index=True, max_length=32, null=True, blank=True, default=""
+    )
     posicao = models.ForeignKey(
-        PosicaoCarga, default=1,
-        verbose_name='Posição',
-        on_delete=models.PROTECT)
+        PosicaoCarga, default=1, verbose_name="Posição", on_delete=models.PROTECT
+    )
 
     # campos editáveis
-    saida = models.DateField(null=True, blank=True, verbose_name='saída')
-    entrega = models.DateField(
-        null=True, blank=True,
-        verbose_name='agendamento')
-    confirmada = models.BooleanField(
-        default=False,
-        verbose_name='entregue')
-    observacao = models.TextField(
-        null=True, blank=True,
-        verbose_name='observação')
+    saida = models.DateField(null=True, blank=True, verbose_name="saída")
+    entrega = models.DateField(null=True, blank=True, verbose_name="agendamento")
+    confirmada = models.BooleanField(default=False, verbose_name="entregue")
+    observacao = models.TextField(null=True, blank=True, verbose_name="observação")
 
     def save(self, *args, **kwargs):
         if self.id:
@@ -113,14 +91,11 @@ class NotaFiscal(models.Model):
         db_table = "fo2_fat_nf"
         verbose_name = "Nota Fiscal"
         verbose_name_plural = "Notas Fiscais"
-        permissions = (("can_beep_shipment", "Can beep shipment"),
-                       )
+        permissions = (("can_beep_shipment", "Can beep shipment"),)
 
 
 class RotinaLogistica(models.Model):
-    nome = models.CharField(
-        max_length=30,
-        db_index=True, unique=True)
+    nome = models.CharField(max_length=30, db_index=True, unique=True)
     slug = models.SlugField()
 
     def __str__(self):
@@ -139,23 +114,20 @@ class RotinaLogistica(models.Model):
 class PosicaoCargaAlteracao(models.Model):
     inicial = models.ForeignKey(
         PosicaoCarga,
-        verbose_name='Estado inicial',
-        related_name='posicao_inicial_set',
-        on_delete=models.PROTECT)
+        verbose_name="Estado inicial",
+        related_name="posicao_inicial_set",
+        on_delete=models.PROTECT,
+    )
     ordem = models.IntegerField(default=0)
-    descricao = models.CharField(
-        'descrição', max_length=200)
-    efeito = models.ForeignKey(
-        RotinaLogistica, default=1,
-        on_delete=models.PROTECT)
+    descricao = models.CharField("descrição", max_length=200)
+    efeito = models.ForeignKey(RotinaLogistica, default=1, on_delete=models.PROTECT)
     final = models.ForeignKey(
         PosicaoCarga,
-        verbose_name='Estado Final',
-        related_name='posicao_final_set',
-        on_delete=models.PROTECT)
-    so_nfs_ativas = models.BooleanField(
-        verbose_name='Só NFs ativas',
-        default=True)
+        verbose_name="Estado Final",
+        related_name="posicao_final_set",
+        on_delete=models.PROTECT,
+    )
+    so_nfs_ativas = models.BooleanField(verbose_name="Só NFs ativas", default=True)
 
     class Meta:
         db_table = "fo2_pos_carga_alt"
@@ -164,25 +136,22 @@ class PosicaoCargaAlteracao(models.Model):
 
 
 class PosicaoCargaAlteracaoLog(models.Model):
-    numero = models.IntegerField(
-        db_index=True, verbose_name='número')
-    time = models.DateTimeField(
-        db_index=True,
-        auto_now_add=True, verbose_name='Hora')
-    user = models.CharField(
-        db_index=True,
-        max_length=64, verbose_name='Usuário')
+    numero = models.IntegerField(db_index=True, verbose_name="número")
+    time = models.DateTimeField(db_index=True, auto_now_add=True, verbose_name="Hora")
+    user = models.CharField(db_index=True, max_length=64, verbose_name="Usuário")
     inicial = models.ForeignKey(
         PosicaoCarga,
-        verbose_name='Estado inicial',
-        related_name='log_posicao_inicial_set',
-        on_delete=models.PROTECT)
+        verbose_name="Estado inicial",
+        related_name="log_posicao_inicial_set",
+        on_delete=models.PROTECT,
+    )
     final = models.ForeignKey(
         PosicaoCarga,
-        verbose_name='Estado Final',
-        related_name='log_posicao_final_set',
-        on_delete=models.PROTECT)
-    saida = models.DateField(null=True, blank=True, verbose_name='saída')
+        verbose_name="Estado Final",
+        related_name="log_posicao_final_set",
+        on_delete=models.PROTECT,
+    )
+    saida = models.DateField(null=True, blank=True, verbose_name="saída")
 
     class Meta:
         db_table = "fo2_pos_carga_alt_log"
@@ -195,39 +164,24 @@ class NfEntrada(models.Model):
     def get_tussor():
         return None
 
-    empresa = models.ForeignKey(
-        Empresa,
-        on_delete=models.PROTECT)
-    cadastro = models.CharField(
-        'CNPJ', max_length=20, default='')
-    emissor = models.CharField(
-        max_length=200)
-    numero = models.IntegerField(
-        'número')
-    descricao = models.CharField(
-        'descrição', max_length=200)
-    qtd = models.DecimalField(
-        'quantidade', max_digits=13, decimal_places=4)
-    hora_entrada = models.TimeField(
-        'hora de entrada', default=timezone.now)
-    transportadora = models.CharField(
-        max_length=100)
-    motorista = models.CharField(
-        max_length=100)
-    placa = models.CharField(
-        max_length=10)
-    responsavel = models.CharField(
-        'responsável', max_length=100)
-    usuario = models.ForeignKey(
-        User, models.PROTECT,
-        verbose_name='usuário')
-    quando = models.DateTimeField(
-        null=True, editable=False)
+    empresa = models.ForeignKey(Empresa, on_delete=models.PROTECT)
+    cadastro = models.CharField("CNPJ", max_length=20, default="")
+    emissor = models.CharField(max_length=200)
+    numero = models.IntegerField("número")
+    descricao = models.CharField("descrição", max_length=200)
+    qtd = models.DecimalField("quantidade", max_digits=13, decimal_places=4)
+    hora_entrada = models.TimeField("hora de entrada", default=timezone.now)
+    transportadora = models.CharField(max_length=100)
+    motorista = models.CharField(max_length=100)
+    placa = models.CharField(max_length=10)
+    responsavel = models.CharField("responsável", max_length=100)
+    usuario = models.ForeignKey(User, models.PROTECT, verbose_name="usuário")
+    quando = models.DateTimeField(null=True, editable=False)
 
     def __str__(self):
         cnpj = CNPJ()
         cnpj.validate(self.cadastro)
-        return f'{cnpj.mask(cnpj.cnpj)} NF {self.numero}'
+        return f"{cnpj.mask(cnpj.cnpj)} NF {self.numero}"
 
     def clean_cadastro(self):
         val_cnpj = CNPJ()
@@ -257,7 +211,6 @@ class NfEntrada(models.Model):
 
 
 class NfEntradaXManager(models.Manager):
-
     def __init__(self, nome):
         self.nome = nome
         super().__init__()
@@ -267,8 +220,9 @@ class NfEntradaXManager(models.Manager):
             numero = Empresa.objects.get(nome=self.nome).numero
         except Exception:
             numero = 0
-        return super(NfEntradaXManager, self).get_queryset().filter(
-            empresa__numero=numero)
+        return (
+            super(NfEntradaXManager, self).get_queryset().filter(empresa__numero=numero)
+        )
 
 
 class NfEntradaAgator(NfEntrada):
