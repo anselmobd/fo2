ec8a3fd03bf698a94b67df0e89325d564a7eee15
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2021-06-24 10:54:51 -0300

    BF

--- src/logistica/views/entr_nf_historico.py
+++ src/logistica/views/entr_nf_historico.py
@@ -7,32 +7,51 @@ import logistica.models
 
 
 class EntradaNfHistorico(O2BaseGetView):
-
     def __init__(self, *args, **kwargs):
         super(EntradaNfHistorico, self).__init__(*args, **kwargs)
-        self.template_name = 'logistica/entrada_nf/historico.html'
-        self.title_name = 'Histórico de NF de entrada'
-        self.get_args = ['id']
+        self.template_name = "logistica/entrada_nf/historico.html"
+        self.title_name = "Histórico de NF de entrada"
+        self.get_args = ["id"]
 
     def mount_context(self):
-        print(self.context['id'])
+        self.id = self.get_arg("id")
         fields = (
-            'cadastro', 'emissor', 'numero', 'descricao', 'volumes',
-            'chegada', 'transportadora', 'motorista', 'placa',
-            'responsavel', 'usuario__username', 'quando'
+            "cadastro",
+            "emissor",
+            "numero",
+            "descricao",
+            "volumes",
+            "chegada",
+            "transportadora",
+            "motorista",
+            "placa",
+            "responsavel",
+            "usuario__username",
+            "quando",
         )
-        dados = logistica.models.NfEntrada.objects.all().values(*fields)
+        dados = logistica.models.NfEntrada.objects.filter(id=self.id).values(*fields)
 
         cnpj = CNPJ()
         for row in dados:
-            row['cadastro'] = cnpj.mask(row['cadastro'])
+            row["cadastro"] = cnpj.mask(row["cadastro"])
 
-        self.context.update({
-            'headers': (
-                'CNPJ', 'Emissor', 'NF', 'Descrição', 'Volumes',
-                'Chegada', 'Transportadora', 'Motorista', 'Placa',
-                'Responsável', 'Digitado por', 'Digitado em'
-            ),
-            'fields': fields,
-            'dados': dados,
-        })
+        self.context.update(
+            {
+                "headers": (
+                    "CNPJ",
+                    "Emissor",
+                    "NF",
+                    "Descrição",
+                    "Volumes",
+                    "Chegada",
+                    "Transportadora",
+                    "Motorista",
+                    "Placa",
+                    "Responsável",
+                    "Digitado por",
+                    "Digitado em",
+                ),
+                "fields": fields,
+                "dados": dados,
+            }
+        )
