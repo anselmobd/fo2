fa67eb760965ffcc49e2d09fa9fc06fa54cb9e25
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2021-07-15 16:38:50 -0300

    criada query op_lotes2, com novo sql, substituindo op_lotes para a view de OP

--- src/lotes/queries/lote/get_lotes.py
+++ src/lotes/queries/lote/get_lotes.py
@@ -1,6 +1,89 @@
 from utils.functions.models import rows_to_dict_list, dict_list_to_lower
 
 
+def op_lotes2(cursor, op):
+    sql = f'''
+        WITH
+        filtro AS (
+        SELECT 
+          {op} OP
+        FROM dual
+        ),
+        ocs AS (
+          SELECT DISTINCT 
+            l.PERIODO_PRODUCAO PER
+          , l.ORDEM_CONFECCAO OC
+          , min(
+              CASE
+              WHEN l.QTDE_DISPONIVEL_BAIXA > 0
+                OR l.QTDE_CONSERTO > 0
+              THEN l.CODIGO_ESTAGIO
+              ELSE 999
+              END
+            ) EST
+          , min(
+              CASE
+              WHEN l.NUMERO_ORDEM > 0
+              THEN l.CODIGO_ESTAGIO
+              ELSE 999
+              END
+            ) EST_OS
+          , max(l.NUMERO_ORDEM) OS
+          FROM filtro f
+          JOIN PCPC_040 l
+            ON l.ORDEM_PRODUCAO = f.OP
+          GROUP BY 
+            l.PERIODO_PRODUCAO
+          , l.ORDEM_CONFECCAO
+        ),
+        lotes AS (
+          SELECT 
+            ocs.EST
+          , ed.DESCRICAO EST_DESCR
+          , ocs.OS
+          , ocs.EST_OS
+          , eod.DESCRICAO EST_OS_DESCR
+          , l.PROCONF_GRUPO REF
+          , l.PROCONF_ITEM COR
+          , l.PROCONF_SUBGRUPO TAM
+          , t.ORDEM_TAMANHO TAM_ORD
+          , ocs.PER
+          , ocs.OC
+          , l.QTDE_PECAS_PROG QTD
+          FROM ocs
+          JOIN PCPC_040 l
+            ON l.PERIODO_PRODUCAO = ocs.PER 
+          AND l.ORDEM_CONFECCAO = ocs.OC
+          AND l.CODIGO_ESTAGIO = ocs.EST
+          LEFT JOIN BASI_220 t
+            ON t.TAMANHO_REF = l.PROCONF_SUBGRUPO
+          JOIN MQOP_005 ed
+            ON ed.CODIGO_ESTAGIO = ocs.EST
+          LEFT JOIN MQOP_005 eod
+            ON eod.CODIGO_ESTAGIO = ocs.EST_OS
+        )
+        SELECT
+          l.EST || ' - ' || l.EST_DESCR EST
+        , l.OS || ' (' || l.EST_OS_DESCR || ')' OS
+        , l.REF
+        , l.COR
+        , l.TAM
+        , l.PER PERIODO
+        , l.OC
+        , l.QTD
+        FROM lotes l
+        ORDER BY 
+          l.EST
+        , l.OS
+        , l.REF
+        , l.COR
+        , l.TAM_ORD
+        , l.PER
+        , l.OC
+    '''
+    cursor.execute(sql)
+    return rows_to_dict_list(cursor)
+
 def op_lotes(cursor, op):
     # Lotes ordenados por OS + referência + estágio
     return get_lotes(cursor, op=op, order='e')
