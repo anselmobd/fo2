4a37fb0344ff8f5fd5344c47addca6fd03490c8d
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2021-07-16 13:23:20 -0300

    ajax entr_nf_cadastro prevendo CPF

--- src/logistica/views/ajax/entr_nf_cadastro.py
+++ src/logistica/views/ajax/entr_nf_cadastro.py
@@ -5,7 +5,7 @@ from pprint import pprint
 from django.db.models import Q
 from django.http import HttpResponse
 
-from utils.functions.cadastro import CNPJ
+from utils.functions.cadastro import CNPJ, CPF
 
 from logistica.models import NfEntrada
 
@@ -15,10 +15,19 @@ def entr_nf_cadastro(request, *args, **kwargs):
 
     try:
         cadastro = kwargs["cadastro"]
+        valido = False
         val_cnpj = CNPJ()
         if val_cnpj.validate(cadastro):
+            valido = True
             cadastro = val_cnpj.cnpj
             cadastro_mask = val_cnpj.mask(cadastro)
+        if not valido:
+            val_cpf = CPF()
+            if val_cpf.validate(cadastro):
+                valido = True
+                cadastro = val_cpf.cpf
+                cadastro_mask = val_cpf.mask(cadastro)
+        if valido:
             result = (
                 NfEntrada.objects.filter(
                     Q(cadastro=cadastro) | Q(cadastro=cadastro_mask)
