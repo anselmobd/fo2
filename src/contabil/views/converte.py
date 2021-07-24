b57a80e6b27720a1045e508bd326342870405385
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2021-07-23 22:52:46 -0300

    versão apresentando resultado apenas na página

--- src/contabil/views/converte.py
+++ src/contabil/views/converte.py
@@ -26,11 +26,73 @@ class Converte(View):
         form = self.Form_class(request.POST, request.FILES)
         if form.is_valid():
             lines = []
-            for line in request.FILES['arquivo']:
+            i_contas = -1
+            for i, line in enumerate(request.FILES['arquivo']):
                 print(line)
-                line = line.decode(errors='ignore').strip("\n")
+                line = line.decode('utf-8', errors='ignore').strip("\n").strip()
                 lines.append(line)
+                if line == '[Contas]':
+                    i_contas = i
             context['original'] = "\n".join(lines)
+
+            if i_contas == -1:
+                context['erro'] = "Arquivo sem '[Contas]'"
+            else:
+
+                codigos = {}
+                for idx in range(i_contas, len(lines)):
+                    linha = lines[idx]
+                    if "=" not in linha:
+                        continue
+                    codigo, linha = tuple(linha.split("="))
+                    colunas = linha.split(",")
+                    codigos[codigo] = colunas[2].strip('"')
+                pprint(codigos)
+
+                registros = []
+                for idx in range(i_contas):
+                    linha = lines[idx]
+                    if len(linha) == 0:
+                        continue
+                    print(linha)
+                    colunas = linha[1:-1].split('","')
+                    pprint(colunas)
+                    conta_d = colunas[1]
+                    conta_c = colunas[2]
+                    data = colunas[3]
+                    if data == "D":
+                        continue
+
+                    valor = float(colunas[4].replace(",", "."))
+                    descricao = "/".join([
+                        codigos[conta_d],
+                        codigos[conta_c],
+                    ])
+
+                    registros.append(" ".join([
+                        "002",
+                        data,
+                        f"{int(conta_d):020}",
+                        "D",
+                        "700001",
+                        "0502",
+                        f"{descricao:100}",
+                        f"{valor:015.2f}"
+                    ]))
+
+                    registros.append(" ".join([
+                        "002",
+                        data,
+                        f"{int(conta_c):020}",
+                        "C",
+                        "700001",
+                        "0502",
+                        f"{descricao:100}",
+                        f"{valor:015.2f}"
+                    ]))
+
+            context['systextil'] = "\n".join(registros)
+
         else:
             context['erro'] = 'Erro inexperado!'
         context['form'] = form
