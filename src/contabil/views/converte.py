7f7c67ef0aafdb184d42e92ea4f6def65e6e85d9
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2021-07-23 21:19:01 -0300

    view class Converte apenas visualizando

--- src/contabil/views/converte.py
+++ src/contabil/views/converte.py
@@ -1,5 +1,37 @@
+from pprint import pprint
+
+from django.shortcuts import render
 from django.views import View
+
+import contabil.forms
 
 
 class Converte(View):
-    pass
+
+    def __init__(self, *args, **kwargs):
+        super(Converte, self).__init__(*args, **kwargs)
+        self.Form_class = contabil.forms.UploadArquivoForm
+        self.template_name = 'contabil/converte.html'
+        self.title_name = 'Converte para importação'
+
+    def get(self, request, *args, **kwargs):
+        context = {'titulo': self.title_name}
+        form = self.Form_class()
+        context['form'] = form
+        return render(request, self.template_name, context)
+
+    def post(self, request, *args, **kwargs):
+        self.request = request
+        context = {'titulo': self.title_name}
+        form = self.Form_class(request.POST, request.FILES)
+        if form.is_valid():
+            lines = []
+            for line in request.FILES['arquivo']:
+                print(line)
+                line = line.decode(errors='ignore').strip("\n")
+                lines.append(line)
+            context['original'] = "\n".join(lines)
+        else:
+            context['erro'] = 'Erro inexperado!'
+        context['form'] = form
+        return render(request, self.template_name, context)
