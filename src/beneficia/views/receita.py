c0a525a3f9e319d694546d23f5e22e48bc0f397a
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2021-08-05 11:29:33 -0300

    primeira versão apresentando algum dado

--- src/beneficia/views/receita.py
+++ src/beneficia/views/receita.py
@@ -5,6 +5,7 @@ from fo2.connections import db_cursor_so
 from base.views import O2BaseGetPostView
 
 import beneficia.forms
+import beneficia.queries
 
 
 class Receita(O2BaseGetPostView):
@@ -15,6 +16,15 @@ class Receita(O2BaseGetPostView):
         self.template_name = 'beneficia/receita.html'
         self.title_name = 'Receita'
         self.get_args = ['receita']
+        self.cleaned_data2self = True
 
     def mount_context(self):
         self.cursor = db_cursor_so(self.request)
+
+        dados = beneficia.queries.receita_inform(self.cursor, self.receita)
+
+        self.context.update({
+            'headers': ['Receita', 'Descrição'],
+            'fields': ['ref', 'descr'],
+            'dados': dados,
+        })
