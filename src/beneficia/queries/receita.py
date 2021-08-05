22d1d471a388b398c9a7ad4dd7e62715b5a5ceb2
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2021-08-05 12:03:05 -0300

    view Receita Cores

--- src/beneficia/queries/receita.py
+++ src/beneficia/queries/receita.py
@@ -25,13 +25,30 @@ def receita_inform(cursor, receita):
 def receita_subgrupo(cursor, receita):
     sql = f"""
         SELECT DISTINCT
-          t.TAMANHO_REF COD
+          t.TAMANHO_REF SUBGRUPO
         , t.DESCR_TAM_REFER DESCR
         FROM basi_020 t
         WHERE t.BASI030_NIVEL030 = 5
           AND t.BASI030_REFERENC = '{receita}'
         ORDER BY
           t.TAMANHO_REF
+    """
+    cursor.execute(sql)
+    return rows_to_dict_list_lower(cursor)
+
+
+def receita_cores(cursor, receita):
+    sql = f"""
+        SELECT 
+          c.SUBGRU_ESTRUTURA SUBGRUPO
+        , c.ITEM_ESTRUTURA COR
+        , c.DESCRICAO_15 DESCR
+        FROM basi_010 c
+        WHERE c.NIVEL_ESTRUTURA = 5
+          AND c.GRUPO_ESTRUTURA = '{receita}'
+        ORDER BY
+          c.SUBGRU_ESTRUTURA
+        , c.ITEM_ESTRUTURA
     """
     cursor.execute(sql)
     return rows_to_dict_list_lower(cursor)
