9c22210ba579b23ec77076cb4cc9bf7efaaa7aa1
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2021-08-05 13:18:39 -0300

    view ReceitaEstrutura primeira versão funcional

--- src/beneficia/views/receita_estrutura.py
+++ src/beneficia/views/receita_estrutura.py
@@ -12,11 +12,45 @@ class ReceitaEstrutura(O2BaseGetPostView):
 
     def __init__(self, *args, **kwargs):
         super(ReceitaEstrutura, self).__init__(*args, **kwargs)
-        self.Form_class = beneficia.forms.ReceitaForm
+        self.Form_class = beneficia.forms.ReceitaItemForm
         self.template_name = 'beneficia/receita_estrutura.html'
-        self.title_name = 'Receita'
-        self.get_args = ['receita']
+        self.title_name = 'Estrutura de receita'
+        self.get_args = ['item']
         self.cleaned_data2self = True
 
     def mount_context(self):
         self.cursor = db_cursor_so(self.request)
+
+        niv, grup, sub, item = tuple(
+            self.item.split('.')
+        )
+        dados = beneficia.queries.receita_estrutura(
+            self.cursor, niv, grup, sub, item
+        )
+
+        if not dados:
+            return
+
+        for row in dados:
+            row['comp'] = '.'.join([
+                row['niv'],
+                row['grup'],
+                row['sub'],
+                row['item'],
+            ])
+            row['calculo'] = ':'.join([
+                str(row['tipo_calc']),
+                row['calc_descr'],
+            ])
+
+        self.context.update({
+            'headers': [
+                'Seq.', 'Componente', 'Narrativa', 'Alt.',
+                'Consumo/Unid.', 'Cálculo', 'Consumo', 'Letra'
+            ],
+            'fields': [
+                'seq', 'comp', 'descr', 'alt',
+                'consumo_unidade', 'calculo', 'consumo', 'letra'
+            ],
+            'dados': dados,
+        })
