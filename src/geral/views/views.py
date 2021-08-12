814cbe7eef0ec90af07cf84ef3bce2a68f3c230c
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2021-08-12 17:12:25 -0300

    separando query unidades

--- src/geral/views/views.py
+++ src/geral/views/views.py
@@ -383,31 +383,7 @@ def roteiros_de_fluxo(request, id):
 
 def unidade(request):
     cursor = db_cursor_so(request)
-    sql = '''
-        SELECT
-          di.DIVISAO_PRODUCAO DIV
-        , di.DESCRICAO DESCR
-        , ci.ESTADO UF
-        , ci.CIDADE
-        , ' (' || lpad(fo.FORNECEDOR9, 8, '0')
-          || '/' || lpad(fo.FORNECEDOR4, 4, '0')
-          || '-' || lpad(fo.FORNECEDOR2, 2, '0')
-          || ') '
-          || fo.NOME_FORNECEDOR NOME
-        FROM BASI_180 di -- divisão / unidade
-        JOIN SUPR_010 fo -- fornacedor
-          ON fo.FORNECEDOR9 = di.FACCIONISTA9
-         AND fo.FORNECEDOR4 = di.FACCIONISTA4
-         AND fo.FORNECEDOR2 = di.FACCIONISTA2
-        JOIN BASI_160 ci -- cidade
-          ON ci.COD_CIDADE = fo.COD_CIDADE
-        WHERE di.DIVISAO_PRODUCAO > 1
-          AND di.DIVISAO_PRODUCAO < 1000
-        ORDER BY
-          di.DIVISAO_PRODUCAO
-    '''
-    cursor.execute(sql)
-    data = rows_to_dict_list(cursor)
+    data = queries.unidades(cursor)
     context = {
         'titulo': 'Unidades / Divisões',
         'headers': ('Código ', 'Descrição', 'UF', 'Cidade',
