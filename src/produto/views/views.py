8d3a22c71110bbd0ec85afd779ee0c64074537a3
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2021-08-12 16:54:37 -0300

    separando dict_colecao_fluxos

--- src/produto/views/views.py
+++ src/produto/views/views.py
@@ -13,7 +13,8 @@ from fo2.connections import db_cursor_so
 
 from base.views import O2BaseGetPostView
 from geral.functions import has_permission
-from geral.views import dict_colecao_fluxos, get_roteiros_de_fluxo
+from geral.dados.colecao_fluxos import dict_colecao_fluxos
+from geral.views import get_roteiros_de_fluxo
 from utils.forms import FiltroForm
 from utils.functions.models import rows_to_dict_list
 from utils.views import group_rowspan
