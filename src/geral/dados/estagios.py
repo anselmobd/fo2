e3aeea44a68ac8559c71118a158be0fa84205670
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2021-08-12 16:33:32 -0300

    separando função de definição de estrutura de dados dict_estágios

---
+++ src/geral/dados/estagios.py
@@ -0,0 +1,127 @@
+from pprint import pprint
+
+
+def dict_estagios():
+    return {
+        3: {
+            'descr': 'PCP (Liberação)',
+            'deposito': '-',
+        },
+        6: {
+            'descr': 'Risco',
+            'deposito': '-',
+        },
+        9: {
+            'descr': 'Abastece Fio',
+            'deposito': '212',
+        },
+        12: {
+            'descr': 'Etiquetas',
+            'deposito': '231',
+        },
+        15: {
+            'descr': 'Corte',
+            'deposito': '-',
+        },
+        18: {
+            'descr': 'Separação insumo',
+            'deposito': '231',
+        },
+        19: {
+            'descr': 'Separação insumo',
+            'deposito': '-',
+        },
+        21: {
+            'descr': 'Distribuição',
+            'deposito': '-',
+        },
+        22: {
+            'descr': 'Distribuição Tecelagem',
+            'deposito': '-',
+        },
+        'os': {
+            'codigo': 'OS',
+            'descr': 'OS/NF',
+            'deposito': '',
+        },
+        24: {
+            'descr': 'Recepção',
+            'deposito': '-',
+        },
+        27: {
+            'descr': 'Tecelagem saia / meia',
+            'deposito': '212',
+        },
+        28: {
+            'descr': 'Passadoria',
+            'deposito': '-',
+        },
+        30: {
+            'descr': 'Tecelagem fundo',
+            'deposito': '212',
+        },
+        31: {
+            'descr': 'Prepara parte',
+            'deposito': '-',
+        },
+        32: {
+            'descr': 'Prepara peça',
+            'deposito': '-',
+        },
+        33: {
+            'descr': 'Costura Costurado',
+            'deposito': '231',
+        },
+        36: {
+            'descr': 'Costura Tecelagem',
+            'deposito': '231',
+        },
+        39: {
+            'descr': 'Tinturaria',
+            'deposito': '-',
+        },
+        42: {
+            'descr': 'Estamparia',
+            'deposito': '-',
+        },
+        43: {
+            'descr': 'Sublimação',
+            'deposito': '-',
+        },
+        45: {
+            'descr': 'Transfer / TAG',
+            'deposito': '231',
+        },
+        48: {
+            'descr': 'Revisão',
+            'deposito': '-',
+        },
+        51: {
+            'descr': 'CD MD',
+            'deposito': '-',
+        },
+        54: {
+            'descr': 'Terceiro MG',
+            'deposito': '231',
+        },
+        55: {
+            'descr': 'Terceiro RJ',
+            'deposito': '231',
+        },
+        57: {
+            'descr': 'Armazena',
+            'deposito': '-',
+        },
+        60: {
+            'descr': 'Embalagem',
+            'deposito': '231',
+        },
+        63: {
+            'descr': 'CD',
+            'deposito': '-',
+        },
+        66: {
+            'descr': 'Expedição',
+            'deposito': '231',
+        },
+    }
