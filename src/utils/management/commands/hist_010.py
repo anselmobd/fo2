12aaeecedc07513de60030c380a36d4e9258d69d
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2021-08-18 18:07:00 -0300

    __MAX_TASKS

--- src/utils/management/commands/hist_010.py
+++ src/utils/management/commands/hist_010.py
@@ -14,7 +14,7 @@ import lotes.models as models
 
 class Command(BaseCommand):
     help = 'Move HIST_010 do banco do systextil para o banco da intranet'
-    __MAX_TASKS = 10000
+    __MAX_TASKS = 20000
 
     def my_println(self, text=''):
         self.my_print(text, ending='\n')
