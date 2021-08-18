37d933a989c3968f8f1ce43e160bec7700047aab
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2021-08-18 15:54:53 -0300

    iniciando view sessions

--- src/systextil/urls.py
+++ src/systextil/urls.py
@@ -6,4 +6,6 @@ import systextil.views
 app_name = 'systextil'
 urlpatterns = [
     re_path(r'^$', systextil.views.index, name='index'),
+
+    re_path(r'^sessions/$', systextil.views.sessions, name='sessions'),
 ]
