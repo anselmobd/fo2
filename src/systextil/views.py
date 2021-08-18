ed42278f60871d896eb3649bc67481d06a4102ee
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2021-08-18 15:54:53 -0300

    iniciando view sessions

--- src/systextil/views.py
+++ src/systextil/views.py
@@ -1,5 +1,16 @@
+from pprint import pprint, pformat
+
 from django.shortcuts import render
 
 
 def index(request):
     return render(request, 'systextil/index.html')
+
+
+def sessions(request):
+    json = {'teste': "Teste!"}
+    context = {
+        "titulo": "Sessions",
+        "json": pformat(json),
+    }
+    return render(request, 'systextil/sessions.html', context)
