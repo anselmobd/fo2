04d5404c5c04332c4534d8c60bf0674ef4369ce1
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2021-08-23 16:22:12 -0300

    query historico_op filtro por OC

--- src/lotes/queries/op/historico_op.py
+++ src/lotes/queries/op/historico_op.py
@@ -3,13 +3,18 @@ from pprint import pprint
 from utils.functions.models import rows_to_dict_list
 
 
-def historico_op(cursor, op):
+def historico_op(cursor, op, oc=None):
+    filter_oc = ""
+    if oc is not None and oc != "":
+      filter_oc = f"AND h.ordem_confeccao = {oc}"
+
     sql = f"""
       select 
         h.*
       from systextil_logs.hist_010 h
       where 1=1
         and h.ordem_producao = '{op}'
+        {filter_oc} -- filter_oc
       order by
         h.periodo_producao
       , h.ordem_confeccao
