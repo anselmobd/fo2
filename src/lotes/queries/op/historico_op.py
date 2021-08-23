9ea8c46f25e00a38cc8bba3b01afdeec8e6d959c
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2021-08-23 17:43:46 -0300

    query historico_op filter usuário

--- src/lotes/queries/op/historico_op.py
+++ src/lotes/queries/op/historico_op.py
@@ -3,18 +3,22 @@ from pprint import pprint
 from utils.functions.models import rows_to_dict_list
 
 
-def historico_op(cursor, op, oc=None):
+def historico_op(cursor, op, oc=None, usuario=None):
     filter_oc = ""
     if oc is not None and oc != "":
       filter_oc = f"AND h.ordem_confeccao = {oc}"
+
+    filter_usuario = ""
+    if usuario is not None and usuario != "":
+      filter_usuario = f"AND h.usuario_sistema = {usuario}"
 
     sql = f"""
       select 
         h.*
       from systextil_logs.hist_010 h
-      where 1=1
-        and h.ordem_producao = '{op}'
+      where h.ordem_producao = '{op}'
         {filter_oc} -- filter_oc
+        {filter_usuario} -- filter_usuario
       order by
         h.periodo_producao
       , h.ordem_confeccao
