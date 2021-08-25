5c690c30873a5dccbef6a1541fef5f04dc774b31
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2021-08-25 13:58:17 -0300

    histórico de OP passar a filtrar por usuário rede ao invés de usuário sistema

--- src/lotes/queries/op/historico_op.py
+++ src/lotes/queries/op/historico_op.py
@@ -14,7 +14,7 @@ def historico_op(cursor, op, oc=None, dia=None, usuario=None):
 
     filter_usuario = ""
     if usuario is not None and usuario != "":
-      filter_usuario = f"AND h.usuario_sistema LIKE '%{usuario}%'"
+      filter_usuario = f"AND h.usuario_rede LIKE '%{usuario}%'"
 
     sql = f"""
       select 
