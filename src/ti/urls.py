fe9fb450c76f77e23514cef041312fe2c4316400
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2021-09-28 13:57:04 -0300

    view Lista equipamentos iniciada

--- src/ti/urls.py
+++ src/ti/urls.py
@@ -5,5 +5,9 @@ import ti.views
 
 app_name = 'ti'
 urlpatterns = [
-    re_path(r'^$', ti.views.index, name='index'),
+    re_path(r'^$', ti.views.views.index, name='index'),
+    re_path(
+        r'^equipamento_lista$',
+        ti.views.lista.EquipamentoLista.as_view(),
+        name='equipamento_lista'),
 ]
