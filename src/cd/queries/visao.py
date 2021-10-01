47e2bebe06c3a6c3aa2446fe2f2883976987eedc
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2021-10-01 14:27:12 -0300

    query get_solic_dict passa a devolver HTML do link

--- src/cd/queries/visao.py
+++ src/cd/queries/visao.py
@@ -1,5 +1,7 @@
 from pprint import pprint
 
+from django.urls import reverse
+
 from utils.functions.digits import fo2_digit_with
 
 import lotes.models
@@ -29,12 +31,19 @@ def get_solic_dict(rua):
         else:
             solic_dt_entrega = ''
 
-        solic_numero = fo2_digit_with(solic['solicitacao__id'])
-
+        solic_id = solic['solicitacao__id']
+        solic_numero = fo2_digit_with(solic_id)
         solic_txt = f"#{solic_numero}{solic_dt_entrega}"
+        solicit_num_link = reverse(
+            'cd:solicitacoes', args=[solic_id])
+
+        solicit_html = (
+            f'<a href="{solicit_num_link}" '
+            f'target="_BLANK">{solic_txt}</a>'
+        )
 
         if solic_lote not in solic_dict:
             solic_dict[solic_lote] = []
         
-        solic_dict[solic_lote].append(solic_txt)
+        solic_dict[solic_lote].append(solicit_html)
     return solic_dict
