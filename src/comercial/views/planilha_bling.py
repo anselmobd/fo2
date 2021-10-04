97653338f7b6deb9002605223d925312fce4ca27
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2021-10-04 19:18:28 -0300

    view PlanilhaBling: primeira versão funcional

--- src/comercial/views/planilha_bling.py
+++ src/comercial/views/planilha_bling.py
@@ -19,73 +19,14 @@ class PlanilhaBling(O2BaseGetPostView):
         self.title_name = 'Cria planilha Bling'
         self.Form_class = forms.TabelaDePrecoForm
         self.cleaned_data2self = True
-        self.colunas = {
-            "ID": "",
-            "Codigo": "",
-            "Descricao": "",
-            "Unidade": "UN",
-            "Classificacao_fiscal": "",
-            "Origem": "",
-            "Preco": "",
-            "Valor_IPI_fixo": "",
-            "Observacoes": "",
-            "Situacao": "",
-            "Estoque": "",
-            "Preco_de_custo": "",
-            "Cod_no_fornecedor": "",
-            "Fornecedor": "",
-            "Localizacao": "",
-            "Estoque_maximo": "",
-            "Estoque_minimo": "",
-            "Peso_liquido_kg": "",
-            "Peso_bruto_kg": "",
-            "GTIN_EAN": "",
-            "GTIN_EAN_da_ embalagem": "",
-            "Largura_do_ Produto": "",
-            "Altura_do_Produto": "",
-            "Profundidade_do_produto": "",
-            "Data_Validade": "",
-            "Descricao_do_Produto_no_Fornecedor": "",
-            "Descricao_Complementar": "",
-            "Unidade_por_Caixa": "",
-            "Produto_Variacao": "",
-            "Tipo_Producao": "",
-            "Classe_de_enquadramento_do_IPI": "",
-            "Codigo_da_lista_de_servicos": "",
-            "Tipo_do_item": "",
-            "Grupo de Tags/Tags": "",
-            "Tributos": "",
-            "Código Pai": "",
-            "Código Integração": "",
-            "Grupo de Produtos": "",
-            "Marca": "",
-            "CEST": "",
-            "Volumes": "",
-            "Descriçãoo curta": "",
-            "Cross-Docking": "",
-            "URL Imagens Externas": "",
-            "Link Externo": "",
-            "Meses Garantia": "",
-            "Clonar dados do pai": "",
-            "Condição do produto": "",
-            "Frete Grátis": "",
-            "Número FCI": "",
-            "Vídeo": "",
-            "Departamento": "",
-            "Unidade de medida": "",
-            "Preço de compra": "",
-            "Valor base ICMS ST para retenÃ§Ã£o": "",
-            "Valor ICMS ST para retenção": "",
-            "Valor ICMS próprio do substituto": "",
-            "Categoria do produto": ""
-        }
+        self.colunas = self.init_colunas()
 
     def gera_linha(self, registro, header=False):
         if header:
             valores = registro.keys()
         else:
             valores = registro.values()
-        return ','.join(valores)
+        return ','.join(map(str, valores))
 
     def mount_context(self):
         self.context.update({
@@ -148,7 +89,8 @@ class PlanilhaBling(O2BaseGetPostView):
             'data': data,
         })
 
-        i_data = queries.itens_tabela_preco(cursor, *codigo_tabela_ints)
+        i_data = queries.itens_tabela_preco_cor_tam(
+            cursor, *codigo_tabela_ints)
         if len(i_data) == 0:
             self.context.update({
                 'erro': 'Tabela vazia'
@@ -156,66 +98,22 @@ class PlanilhaBling(O2BaseGetPostView):
             return
 
         registros = [self.gera_linha(self.colunas, header=True)]
-        for row in i_data[:1]:
-            pprint(row)
+        for row in i_data:
+            cor = row["cor"].strip("0")
+            codigo = ".".join((row["ref"], row["tam"], cor))
+            descr = " - ".join((row["xref"], row["xtam"], row["xcor"]))
+            class_fiscal = "9999.99.99".replace("9", "{}").format(*row["cfiscal"])
+
             registro = self.colunas.copy()
             registro.update({
-                "Codigo": row["grupo_estrutura"],
-                "Descricao": row["descr_referencia"],
-                "Classificacao_fiscal": "",
-                "Origem": "",
-                "Preco": "",
-                "Valor_IPI_fixo": "",
-                "Observacoes": "",
-                "Situacao": "",
-                "Estoque": "",
-                "Preco_de_custo": "",
-                "Cod_no_fornecedor": "",
-                "Fornecedor": "",
-                "Localizacao": "",
-                "Estoque_maximo": "",
-                "Estoque_minimo": "",
-                "Peso_liquido_kg": "",
-                "Peso_bruto_kg": "",
-                "GTIN_EAN": "",
-                "GTIN_EAN_da_ embalagem": "",
-                "Largura_do_ Produto": "",
-                "Altura_do_Produto": "",
-                "Profundidade_do_produto": "",
-                "Data_Validade": "",
-                "Descricao_do_Produto_no_Fornecedor": "",
-                "Descricao_Complementar": "",
-                "Unidade_por_Caixa": "",
-                "Produto_Variacao": "",
-                "Tipo_Producao": "",
-                "Classe_de_enquadramento_do_IPI": "",
-                "Codigo_da_lista_de_servicos": "",
-                "Tipo_do_item": "",
-                "Grupo de Tags/Tags": "",
-                "Tributos": "",
-                "Código Pai": "",
-                "Código Integração": "",
-                "Grupo de Produtos": "",
-                "Marca": "",
-                "CEST": "",
-                "Volumes": "",
-                "Descriçãoo curta": "",
-                "Cross-Docking": "",
-                "URL Imagens Externas": "",
-                "Link Externo": "",
-                "Meses Garantia": "",
-                "Clonar dados do pai": "",
-                "Condição do produto": "",
-                "Frete Grátis": "",
-                "Número FCI": "",
-                "Vídeo": "",
-                "Departamento": "",
-                "Unidade de medida": "",
-                "Preço de compra": "",
-                "Valor base ICMS ST para retenÃ§Ã£o": "",
-                "Valor ICMS ST para retenção": "",
-                "Valor ICMS próprio do substituto": "",
-                "Categoria do produto": ""
+                "Codigo": codigo,
+                "Descricao": descr,
+                "Classificacao_fiscal": class_fiscal,
+                "Preco": row["preco"],
+                "Peso_liquido_kg": row["pesol"],
+                "Peso_bruto_kg": row["pesol"],
+                "GTIN_EAN": row["gtin"],
+                "Código Pai": "",  # Sem código pai
             })
 
             registros.append(
@@ -227,3 +125,65 @@ class PlanilhaBling(O2BaseGetPostView):
             [r.replace(" ", "%20") for r in registros]
         )
         self.context['file_name'] = f"produtos_{tabela['tabela']}.csv"
+
+    def init_colunas(self):
+        return {
+            "ID": "",
+            "Codigo": "",
+            "Descricao": "",
+            "Unidade": "UN",
+            "Classificacao_fiscal": "",
+            "Origem": 0,
+            "Preco": "",
+            "Valor_IPI_fixo": 0,
+            "Observacoes": "",
+            "Situacao": "Ativo",
+            "Estoque": 100000,
+            "Preco_de_custo": 0,
+            "Cod_no_fornecedor": "",
+            "Fornecedor": "",
+            "Localizacao": "",
+            "Estoque_maximo": 0,
+            "Estoque_minimo": 0,
+            "Peso_liquido_kg": "",
+            "Peso_bruto_kg": "",
+            "GTIN_EAN": "",
+            "GTIN_EAN_da_ embalagem": "",
+            "Largura_do_ Produto": 35.5,
+            "Altura_do_Produto": 25.5,
+            "Profundidade_do_produto": 4.5,
+            "Data_Validade": "",
+            "Descricao_do_Produto_no_Fornecedor": "",
+            "Descricao_Complementar": "",
+            "Unidade_por_Caixa": 1,
+            "Produto_Variacao": "Produto",
+            "Tipo_Producao": "Própria",
+            "Classe_de_enquadramento_do_IPI": "",
+            "Codigo_da_lista_de_servicos": "",
+            "Tipo_do_item": "",
+            "Grupo de Tags/Tags": "",
+            "Tributos": 0,
+            "Código Pai": "",
+            "Código Integração": 0,
+            "Grupo de Produtos": "",
+            "Marca": "DUOMO",
+            "CEST": "",
+            "Volumes": 0,
+            "Descriçãoo curta": "",
+            "Cross-Docking": 0,
+            "URL Imagens Externas": "",
+            "Link Externo": "",
+            "Meses Garantia": "",
+            "Clonar dados do pai": "NÃO",
+            "Condição do produto": "NOVO",
+            "Frete Grátis": "SIM",
+            "Número FCI": "",
+            "Vídeo": "",
+            "Departamento": "",
+            "Unidade de medida": "Centímetro",
+            "Preço de compra": 0,
+            "Valor base ICMS ST para retenÃ§Ã£o": 0,
+            "Valor ICMS ST para retenção": 0,
+            "Valor ICMS próprio do substituto": 0,
+            "Categoria do produto": "ROUPA INTIMA"
+        }
