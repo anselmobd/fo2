879d3bff677a67c6d319ece9e011fbedebf9d7c9
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2021-10-04 17:08:37 -0300

    view PlanilhaBling apenas validando tabea pedida

--- src/comercial/views/planilha_bling.py
+++ src/comercial/views/planilha_bling.py
@@ -2,7 +2,8 @@ from pprint import pprint
 
 from django.shortcuts import render
 from django.urls import reverse
-from django.views import View
+
+from base.views import O2BaseGetPostView
 
 from fo2.connections import db_cursor_so
 
@@ -10,75 +11,55 @@ import comercial.forms as forms
 import comercial.queries as queries
 
 
-class PlanilhaBling(View):
-    Form_class = forms.TabelaDePrecoForm
-    template_name = 'comercial/planilha_bling.html'
-    title_name = 'Tabela de preços'
+class PlanilhaBling(O2BaseGetPostView):
 
-    def mount_context(self, cursor, tabela):
-        context = {'tabela': tabela}
-        codigo_tabela_chunks = tabela.split('.')
+    def __init__(self, *args, **kwargs):
+        super(PlanilhaBling, self).__init__(*args, **kwargs)
+        self.template_name = 'comercial/planilha_bling.html'
+        self.title_name = 'Cria planilha Bling'
+        self.Form_class = forms.TabelaDePrecoForm
+        self.cleaned_data2self = True
+
+    def mount_context(self):
+        self.context.update({
+            'tabela': self.tabela
+        })
+
+        codigo_tabela_chunks = self.tabela.split('.')
         if len(codigo_tabela_chunks) != 3:
-            context.update({
+            self.context.update({
                 'erro': 'Código inválido. '
                         '3 números inteiros separados por ".".'
             })
-
-            data = queries.get_tabela_preco(cursor)
-            if len(data) == 0:
-                context.update({'erro': 'Sem tabelas de preço'})
-                return context
-
-            for row in data:
-                row['tabela'] = "{:02d}.{:02d}.{:02d}".format(
-                    row['col_tabela_preco'],
-                    row['mes_tabela_preco'],
-                    row['seq_tabela_preco'],
-                )
-                row['tabela|LINK'] = reverse(
-                    'comercial:tabela_de_preco__get',
-                    args=[row['tabela']]
-                )
-                row['data_ini_tabela'] = row['data_ini_tabela'].date()
-                row['data_fim_tabela'] = row['data_fim_tabela'].date()
-
-            context.update({
-                'headers': [
-                    'Tabela',
-                    'Descrição',
-                    'Início',
-                    'Fim',
-                ],
-                'fields': [
-                    'tabela',
-                    'descricao',
-                    'data_ini_tabela',
-                    'data_fim_tabela',
-                ],
-                'data': data,
-            })
-            return context
+            return
 
         for subcodigo_tabela in codigo_tabela_chunks:
             if not subcodigo_tabela.isdigit():
-                context.update({
+                self.context.update({
                     'erro': 'Cada subcódigo deve ser um número inteiro.'
                 })
-                return context
+                return
+
         codigo_tabela_ints = list(map(int, codigo_tabela_chunks))
-        tabela = "{:02d}.{:02d}.{:02d}".format(*codigo_tabela_ints)
-        context = {'tabela': tabela}
+        self.tabela = "{:02d}.{:02d}.{:02d}".format(*codigo_tabela_ints)
+        self.context.update({
+            'tabela': self.tabela
+        })
+
+        cursor = db_cursor_so(self.request)
 
         data = queries.get_tabela_preco(cursor, *codigo_tabela_ints)
         if len(data) == 0:
-            context.update({'erro': 'Tabela não encontrada'})
-            return context
+            self.context.update({
+                'erro': 'Tabela não encontrada'
+            })
+            return
 
         for row in data:
             row['data_ini_tabela'] = row['data_ini_tabela'].date()
             row['data_fim_tabela'] = row['data_fim_tabela'].date()
 
-        context.update({
+        self.context.update({
             'headers': [
                 'Descrição',
                 'Início',
@@ -91,46 +72,3 @@ class PlanilhaBling(View):
             ],
             'data': data,
         })
-
-        i_data = queries.itens_tabela_preco(cursor, *codigo_tabela_ints)
-        if len(i_data) == 0:
-            context.update({'erro': 'Tabela vazia'})
-            return context
-
-        context.update({
-            'i_headers': [
-                'Referência',
-                'Descrição',
-                'Valor',
-            ],
-            'i_fields': [
-                'grupo_estrutura',
-                'descr_referencia',
-                'val_tabela_preco'
-            ],
-            'i_data': i_data,
-        })
-
-        return context
-
-    def get(self, request, *args, **kwargs):
-        if 'tabela' in kwargs:
-            return self.post(request, *args, **kwargs)
-        else:
-            context = {'titulo': self.title_name}
-            form = self.Form_class()
-            context['form'] = form
-            return render(request, self.template_name, context)
-
-    def post(self, request, *args, **kwargs):
-        context = {'titulo': self.title_name}
-        form = self.Form_class(request.POST)
-        form.data = form.data.copy()
-        if 'tabela' in kwargs:
-            form.data['tabela'] = kwargs['tabela']
-        if form.is_valid():
-            tabela = form.cleaned_data['tabela']
-            cursor = db_cursor_so(request)
-            context.update(self.mount_context(cursor, tabela))
-        context['form'] = form
-        return render(request, self.template_name, context)
