87a472ee4484e357745066259ce067c3dca6d54a
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2021-10-20 16:48:58 -0300

    admin de Account

--- src/email_signature/admin.py
+++ src/email_signature/admin.py
@@ -2,15 +2,18 @@ from django.contrib import admin
 
 from fo2.admin import intr_adm_site
 
-from .models import Layout
+import email_signature.models
 
 
+@admin.register(email_signature.models.Account, site=intr_adm_site)
+class AccountAdmin(admin.ModelAdmin):
+    readonly_fields = ['create_at', 'update_at']
+
+
+@admin.register(email_signature.models.Layout, site=intr_adm_site)
 class LayoutAdmin(admin.ModelAdmin):
     list_display = ['nome', 'slug', 'template', 'habilitado']
     search_fields = ['nome']
     ordering = ['nome']
     fields = ['nome', 'slug', 'template', 'habilitado']
     readonly_fields = ['slug']
-
-
-intr_adm_site.register(Layout, LayoutAdmin)
