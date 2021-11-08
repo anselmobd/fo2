50a747c4e0db1ce3112698b060d018235209e34d
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2021-11-08 13:05:59 -0300

    add filter
    AccountListView agora tem filter utilizando form GET

--- src/email_signature/views/account.py
+++ src/email_signature/views/account.py
@@ -2,6 +2,7 @@ from django.contrib.auth.mixins import (
     PermissionRequiredMixin,
     LoginRequiredMixin
     )
+from django.db.models import Q
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 
@@ -16,6 +17,24 @@ class AccountListView(PermissionRequiredMixin, ListView):
     ordering = ['tipo', 'email']
     paginate_by = 100
 
+    def get_queryset(self):
+        object_list = self.model.objects
+
+        query = self.request.GET.get('q')
+        queries = query.split(' ') if query else []
+
+        for q in queries:
+            if q:
+                object_list = object_list.filter(
+                    Q(tipo__icontains=q) |
+                    Q(email__icontains=q) |
+                    Q(nome__icontains=q) |
+                    Q(setor__icontains=q)
+                )
+        else:
+            object_list = object_list.all()
+
+        return object_list.order_by(*self.ordering)
 
 class AccountCreateView(PermissionRequiredMixin, CreateView):
     permission_required = 'email_signature.can_edit_mail_signature'
