117417aaa443bbd40c4e1816769150f236a5892e
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2021-11-22 16:48:21 -0300

    view Demorada permission

--- src/systextil/views/dba.py
+++ src/systextil/views/dba.py
@@ -1,13 +1,18 @@
 from pprint import pprint
 
+from django.contrib.auth.mixins import (
+    LoginRequiredMixin,
+    PermissionRequiredMixin,
+)
 from django.shortcuts import render
 
 from base.views import O2BaseGetView
 
 
-class Demorada(O2BaseGetView):
+class Demorada(LoginRequiredMixin, PermissionRequiredMixin, O2BaseGetView):
 
     def __init__(self, *args, **kwargs):
         super(Demorada, self).__init__(*args, **kwargs)
+        self.permission_required = 'systextil.can_be_dba'
         self.template_name = 'systextil/dba/demorada.html'
         self.title_name = 'Queries Demoradas'
