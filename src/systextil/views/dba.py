051e7fc4c353a19bdfa3cfde37499895dfc6473a
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2021-11-22 18:01:28 -0300

    view Demorada vira O2BaseGetPostView

--- src/systextil/views/dba.py
+++ src/systextil/views/dba.py
@@ -4,15 +4,18 @@ from django.contrib.auth.mixins import (
     LoginRequiredMixin,
     PermissionRequiredMixin,
 )
-from django.shortcuts import render
 
-from base.views import O2BaseGetView
+from base.views import O2BaseGetPostView
+
+from systextil.forms import MinutosForm
 
 
-class Demorada(LoginRequiredMixin, PermissionRequiredMixin, O2BaseGetView):
+class Demorada(LoginRequiredMixin, PermissionRequiredMixin, O2BaseGetPostView):
 
     def __init__(self, *args, **kwargs):
         super(Demorada, self).__init__(*args, **kwargs)
         self.permission_required = 'systextil.can_be_dba'
         self.template_name = 'systextil/dba/demorada.html'
         self.title_name = 'Queries Demoradas'
+        self.Form_class = MinutosForm
+        self.form_class_initial = True
