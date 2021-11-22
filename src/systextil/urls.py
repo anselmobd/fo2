27c5a9a95ed31b9ebe59e7645131a9f0dfd0a4da
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2021-11-22 14:38:39 -0300

    app systextil package view

--- src/systextil/urls.py
+++ src/systextil/urls.py
@@ -1,11 +1,11 @@
 from django.urls import re_path
 
-import systextil.views
+import systextil.views.views
 
 
 app_name = 'systextil'
 urlpatterns = [
-    re_path(r'^$', systextil.views.index, name='index'),
+    re_path(r'^$', systextil.views.views.index, name='index'),
 
-    re_path(r'^sessions/$', systextil.views.sessions, name='sessions'),
+    re_path(r'^sessions/$', systextil.views.views.sessions, name='sessions'),
 ]
