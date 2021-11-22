d76053541ce75f51c90cbe45eb2f3a280efd1a0f
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2021-11-22 16:40:53 -0300

    view demorada vira class e urls tem ajustes

--- src/systextil/views/dba.py
+++ src/systextil/views/dba.py
@@ -2,6 +2,12 @@ from pprint import pprint
 
 from django.shortcuts import render
 
+from base.views import O2BaseGetView
 
-def demorada(request):
-    return render(request, 'systextil/dba/demorada.html')
+
+class Demorada(O2BaseGetView):
+
+    def __init__(self, *args, **kwargs):
+        super(Demorada, self).__init__(*args, **kwargs)
+        self.template_name = 'systextil/dba/demorada.html'
+        self.title_name = 'Queries Demoradas'
