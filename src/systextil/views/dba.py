50567185a73586ce1f69884aae237ee1480a1e15
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2021-11-22 18:19:19 -0300

    view Demorada primeira versão funcional

--- src/systextil/views/dba.py
+++ src/systextil/views/dba.py
@@ -6,9 +6,10 @@ from django.contrib.auth.mixins import (
 )
 
 from base.views import O2BaseGetPostView
+from fo2.connections import db_cursor_so
 
 from systextil.forms import MinutosForm
-
+from systextil.queries.dba.main import rodando_a_segundos
 
 class Demorada(LoginRequiredMixin, PermissionRequiredMixin, O2BaseGetPostView):
 
@@ -19,3 +20,21 @@ class Demorada(LoginRequiredMixin, PermissionRequiredMixin, O2BaseGetPostView):
         self.title_name = 'Queries Demoradas'
         self.Form_class = MinutosForm
         self.form_class_initial = True
+        self.cleaned_data2self = True
+
+    def mount_context(self):
+
+        cursor = db_cursor_so(self.request)
+
+        data = rodando_a_segundos(cursor,self.minutos)
+        if len(data) == 0:
+            self.context.update({
+                'msg': 'Nenhuma query demorada encontrada'
+            })
+            return
+
+        self.context.update({
+            'headers': ['Username', 'SID', 'Serial#', 'Minutos rodando', 'SQL'],
+            'fields': ['username', 'sid', 'serial#', 'mins_running', 'sql_text'],
+            'data': data,
+        })
