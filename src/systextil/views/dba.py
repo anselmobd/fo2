aa8a8d997030dee9e9fc0abd67fe0d8870781a81
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2021-11-22 21:26:38 -0300

    view Demorada: apresentando o SQL em um campo <pre> e com tratamento de identação

--- src/systextil/views/dba.py
+++ src/systextil/views/dba.py
@@ -1,3 +1,4 @@
+from itertools import takewhile
 from pprint import pprint
 
 from django.contrib.auth.mixins import (
@@ -25,15 +26,92 @@ class Demorada(LoginRequiredMixin, PermissionRequiredMixin, O2BaseGetPostView):
     def mount_context(self):
         cursor = db_cursor_so(self.request)
 
-        data = rodando_a_segundos(cursor,self.segundos)
+        raw_data = rodando_a_segundos(cursor,self.segundos)
+
+        # print('raw_data')
+        # pprint(raw_data)
+
+        data = []
+        last_sid = -1
+        last_serial = -1
+        for row in raw_data:
+            if row['sid'] == last_sid and row['serial#'] == last_serial:
+                data[-1]['sql_text'] += row['sql_text']
+            else:
+                data.append(row)                
+                last_sid = row['sid']
+                last_serial = row['serial#']
+
+        # print('data')
+        # pprint(data)
 
         for row in data:
             mins = row['secs'] // 60
             secs = row['secs'] % 60
             row['secs'] = f"{mins}:{secs:02d}"
 
+            linhas = row['sql_text'].split('\n')
+            min_spaces = 1000
+            state = 'inicio'
+            linhas_vazias_inicio = 0
+            iguais = 0
+            ult_tem_algo = True
+            for linha in linhas:
+                linhas_strip = linha.strip()
+                tem_algo = linhas_strip and linhas_strip[:2] != '--'
+
+                if state == 'inicio':
+                    if not tem_algo:
+                        linhas_vazias_inicio += 1
+                    else:
+                        state = 'final'
+
+                if state == 'final':
+                    if tem_algo == ult_tem_algo:
+                        iguais += 1
+                    else:
+                        ult_tem_algo = tem_algo
+                        iguais = 1
+
+                if tem_algo:
+                    spaces = sum(1 for _ in takewhile(lambda c: c == ' ', linha))
+                    min_spaces = min(spaces, min_spaces)
+
+            if ult_tem_algo:
+                linhas_vazias_final = 0
+            else:
+                linhas_vazias_final = iguais
+
+            sql_text_list = []
+            for linha in linhas[linhas_vazias_inicio:-linhas_vazias_final]:
+                linhas_strip = linha.strip()
+                if linhas_strip and linhas_strip[:2] != '--':
+                    sql_text_list.append(linha[min_spaces:])
+                else:
+                    sql_text_list.append(linhas_strip)
+
+            row['sql_text'] = '\n'.join(sql_text_list)
+                   
+            # quebras = row['sql_text'].count('\n')
+            # print('quebras', quebras)
+            # i = 0
+            # while True:
+            #     n_chars = len(row['sql_text'])
+            #     print('n_chars', n_chars)
+            #     sql_temp = row['sql_text'].replace('\n ', '\n')
+            #     new_n_chars = len(sql_temp)
+            #     print('new_n_chars', new_n_chars)
+            #     print('n_chars - new_n_chars', n_chars - new_n_chars)
+            #     if (n_chars - new_n_chars) != quebras:
+            #         break
+            #     row['sql_text'] = sql_temp
+            #     i += 1
+            #     if i == 30:
+            #         break
+
         self.context.update({
             'headers': ['Username', 'SID', 'Serial#', 'Rodando a', 'SQL'],
             'fields': ['username', 'sid', 'serial#', 'secs', 'sql_text'],
+            'pre': ['sql_text'],
             'data': data,
         })
