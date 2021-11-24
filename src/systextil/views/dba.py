60d9b12e8574053d18a550b29382b405744ccb0d
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2021-11-24 15:42:40 -0300

    utilizando sql_formato_fo2

--- src/systextil/views/dba.py
+++ src/systextil/views/dba.py
@@ -1,5 +1,4 @@
 import sqlparse
-from itertools import takewhile
 from pprint import pprint
 
 from django.contrib.auth.mixins import (
@@ -9,6 +8,7 @@ from django.contrib.auth.mixins import (
 
 from base.views import O2BaseGetPostView
 from fo2.connections import db_cursor_so
+from utils.functions.sql import sql_formato_fo2
 
 from systextil.forms import SegundosForm
 from systextil.queries.dba.main import rodando_a_segundos
@@ -51,37 +51,7 @@ class Demorada(LoginRequiredMixin, PermissionRequiredMixin, O2BaseGetPostView):
                 indent_width=2,
                 keyword_case='upper',
             )
-
-            linhas = row['sql_text'].split('\n')
-
-            self.min_spaces = 1000
-            def strip_min_spaces(linha):
-                linha_strip = linha.strip()
-                if linha_strip and linha_strip[:2] != '--':
-                    self.min_spaces = min(
-                        self.min_spaces,
-                        sum(1 for _ in takewhile(lambda c: c == ' ', linha)),
-                    )
-                return linha_strip
-
-            linhas_strip = list(map(strip_min_spaces, linhas))
-            linhas_vazias_inicio = sum(1 for _ in takewhile(lambda l: len(l) == 0, linhas_strip))
-            linhas_vazias_final = sum(1 for _ in takewhile(lambda l: len(l) == 0, reversed(linhas_strip)))
-
-            del linhas[:linhas_vazias_inicio]
-            if linhas_vazias_final:
-                del linhas[-linhas_vazias_final:]
-
-            def put_min_spaces(linha):
-                linha_strip = linha.strip()
-                if linha_strip and linha_strip[:2] != '--':
-                    linha = linha[self.min_spaces:]
-                else:
-                    linha = linha_strip
-                return linha
-
-            linhas = list(map(put_min_spaces, linhas))
-            row['sql_text_fo2'] = '\n'.join(linhas)
+            row['sql_text_fo2'] = sql_formato_fo2(row['sql_text'])
 
         self.context.update({
             'headers': ['Username', 'SID', 'Serial', 'Rodando a', 'SQL'],
