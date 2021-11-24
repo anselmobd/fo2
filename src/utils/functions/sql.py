4bbf8ee5deb389b7938400f97eeeb76e1d64e96c
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2021-11-24 16:50:03 -0300

    using count_at_start and count_at_end

--- src/utils/functions/sql.py
+++ src/utils/functions/sql.py
@@ -1,5 +1,6 @@
-from itertools import takewhile
 from pprint import pprint
+
+from utils.functions.list import empty, count_at_start, count_at_end
 
 
 __all__ = [
@@ -22,13 +23,14 @@ def sql_formato_fo2(sql):
         if len(linha_strip) >= 2 and linha_strip[:2] != '--':
             min_spaces = min(
                 min_spaces,
-                sum(1 for _ in takewhile(lambda c: c == ' ', linha)),
+                count_at_start(linha, ' '),
             )
         return linha_strip
 
     linhas_strip = list(map(strip_min_spaces, linhas))
-    linhas_vazias_inicio = sum(1 for _ in takewhile(lambda l: len(l) == 0, linhas_strip))
-    linhas_vazias_final = sum(1 for _ in takewhile(lambda l: len(l) == 0, reversed(linhas_strip)))
+
+    linhas_vazias_inicio = count_at_start(linhas_strip, empty)
+    linhas_vazias_final = count_at_end(linhas_strip, empty)
 
     del linhas[:linhas_vazias_inicio]
     if linhas_vazias_final:
@@ -43,7 +45,7 @@ def sql_formato_fo2(sql):
             pprint(linha)
             if len(linha_strip) >= 2:
                 print('maior')
-                spaces = sum(1 for _ in takewhile(lambda c: c == ' ', linha))
+                spaces = count_at_start(linha, ' ')
                 print('spaces', spaces)
                 linha = ' '*max(0, spaces-min_spaces)+linha_strip
             else:
