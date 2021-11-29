97c5c088309a5a2a16fddbeb070b0844c84d2ab0
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2021-11-29 16:57:11 -0300

    criada view ReativaPedido

---
+++ src/lotes/views/pedido/reativa_pedido.py
@@ -0,0 +1,21 @@
+from pprint import pprint
+
+from django.contrib.auth.mixins import PermissionRequiredMixin
+from django.shortcuts import redirect
+from django.views import View
+
+from fo2.connections import db_cursor_so
+
+from lotes.queries.pedido.reativa_pedido import exec_reativa_pedido
+
+
+class ReativaPedido(PermissionRequiredMixin, View):
+
+    def __init__(self):
+        self.permission_required = 'lotes.can_repair_seq_op'
+
+    def get(self, request, *args, **kwargs):
+        cursor = db_cursor_so(request)
+        pedido = kwargs['pedido']
+        exec_reativa_pedido(cursor, pedido)
+        return redirect('producao:pedido__get', pedido)
