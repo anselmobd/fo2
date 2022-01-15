8d458fc56e070021029fdbdbb0d9c41173f010ec
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2022-01-15 11:46:29 -0300

    get_sessions aumentando a velocidade quando acesso externo

--- src/systextil/functions.py
+++ src/systextil/functions.py
@@ -1,12 +1,16 @@
 import requests
 from pprint import pprint
 
+from utils.functions import acesso_externo
+
 
 def get_sessions():
     urls = [
         "http://oc.tussor.com.br/systextil/sessions",
         "http://tussor.systextil.com.br/systextil/sessions",
     ]
+    if acesso_externo():
+        urls = urls.reverse()
     for u in urls:
         try:
             req = requests.get(u, timeout=10)
