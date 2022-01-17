c8bbc190f5e3a458c81748878e2df20e569acad6
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2022-01-17 11:43:38 -0300

    melhoria de código

--- src/comercial/queries/faturamento_para_meta.py
+++ src/comercial/queries/faturamento_para_meta.py
@@ -3,17 +3,187 @@ from pprint import pprint
 from utils.functions.models import rows_to_dict_list_lower
 
 
+SQL_TIPO_FAT_META = {
+    'mes': {
+        'fields': """--
+            , to_char(f.DATA_EMISSAO, 'MM/YYYY') MES
+        """,
+        'group': """--
+            GROUP BY
+              to_char(f.DATA_EMISSAO, 'MM/YYYY')
+        """,
+        'order': """--
+            ORDER BY
+              to_char(f.DATA_EMISSAO, 'MM/YYYY')
+        """
+    },
+    'cliente': {
+        'fields': """--
+            , max(c.NOME_CLIENTE)
+              || ' (' || lpad(c.CGC_9, 8, '0')
+              || '/....-..)' CLIENTE
+        """,
+        'group': """--
+            GROUP BY
+              c.CGC_9
+        """,
+        'order': """--
+            ORDER BY
+              max(c.NOME_CLIENTE)
+              || ' (' || lpad(c.CGC_9, 8, '0')
+              || '/....-..)'
+        """
+    },
+    'nota': {
+        'fields': """--
+            , f.NUM_NOTA_FISCAL NF
+            , f.DATA_EMISSAO DATA
+            , c.NOME_CLIENTE
+              || ' (' || lpad(c.CGC_9, 8, '0')
+              || '/' || lpad(c.CGC_4, 4, '0')
+              || '-' || lpad(c.CGC_2, 2, '0')
+              || ')' CLIENTE
+            , n.COD_NATUREZA NAT
+            , n.DIVISAO_NATUR DIV
+            , ped.PEDIDO_VENDA PEDIDO
+            , ped.COD_PED_CLIENTE PEDIDO_CLIENTE
+        """,
+        'group': """--
+            GROUP BY
+              f.NUM_NOTA_FISCAL
+            , f.DATA_EMISSAO
+            , c.NOME_CLIENTE
+            , c.CGC_9
+            , c.CGC_4
+            , c.CGC_2
+            , n.COD_NATUREZA
+            , n.DIVISAO_NATUR
+            , ped.PEDIDO_VENDA
+            , ped.COD_PED_CLIENTE
+        """,
+        'order': """--
+            ORDER BY
+              f.NUM_NOTA_FISCAL
+        """
+    },
+    'nota_referencia': {
+        'fields': """--
+            , f.NUM_NOTA_FISCAL NF
+            , f.DATA_EMISSAO DATA
+            , c.NOME_CLIENTE
+              || ' (' || lpad(c.CGC_9, 8, '0')
+              || '/' || lpad(c.CGC_4, 4, '0')
+              || '-' || lpad(c.CGC_2, 2, '0')
+              || ')' CLIENTE
+            , n.COD_NATUREZA NAT
+            , n.DIVISAO_NATUR DIV
+            , ped.PEDIDO_VENDA PEDIDO
+            , ped.COD_PED_CLIENTE PEDIDO_CLIENTE
+            , fi.NIVEL_ESTRUTURA NIVEL
+            , fi.GRUPO_ESTRUTURA REF
+        """,
+        'group': """--
+            GROUP BY
+              f.NUM_NOTA_FISCAL
+            , f.DATA_EMISSAO
+            , c.NOME_CLIENTE
+            , c.CGC_9
+            , c.CGC_4
+            , c.CGC_2
+            , n.COD_NATUREZA
+            , n.DIVISAO_NATUR
+            , ped.PEDIDO_VENDA
+            , ped.COD_PED_CLIENTE
+            , fi.NIVEL_ESTRUTURA
+            , fi.GRUPO_ESTRUTURA
+        """,
+        'order': """--
+            ORDER BY
+              f.NUM_NOTA_FISCAL
+            , fi.GRUPO_ESTRUTURA
+        """
+    },
+    'referencia': {
+        'fields': """--
+            , fi.NIVEL_ESTRUTURA NIVEL
+            , fi.GRUPO_ESTRUTURA REF
+            , co.COLECAO || '-' || co.DESCR_COLECAO COLECAO
+        """,
+        'group': """--
+            GROUP BY
+              fi.NIVEL_ESTRUTURA
+            , fi.GRUPO_ESTRUTURA
+            , co.COLECAO
+            , co.DESCR_COLECAO
+        """,
+        'order': """--
+            ORDER BY
+              fi.NIVEL_ESTRUTURA
+            , fi.GRUPO_ESTRUTURA
+        """
+    },
+    'modelo': {
+        'fields': """--
+            , CASE WHEN fi.NIVEL_ESTRUTURA = 1 THEN
+                TO_NUMBER(
+                  REGEXP_REPLACE(fi.GRUPO_ESTRUTURA, '[^0-9]', '')
+                )
+              ELSE 0
+              END
+              MODELO
+            , co.COLECAO || '-' || co.DESCR_COLECAO COLECAO
+        """,
+        'group': """--
+            GROUP BY
+              CASE WHEN fi.NIVEL_ESTRUTURA = 1 THEN
+                TO_NUMBER(
+                  REGEXP_REPLACE(fi.GRUPO_ESTRUTURA, '[^0-9]', '')
+                )
+              ELSE 0
+              END
+            , co.COLECAO
+            , co.DESCR_COLECAO
+        """,
+        'order': """--
+            ORDER BY
+              CASE WHEN fi.NIVEL_ESTRUTURA = 1 THEN
+                TO_NUMBER(
+                  REGEXP_REPLACE(fi.GRUPO_ESTRUTURA, '[^0-9]', '')
+                )
+              ELSE 0
+              END
+            , co.COLECAO
+        """
+    },
+    'colecao': {
+        'fields': """--
+            , co.COLECAO || '-' || co.DESCR_COLECAO COLECAO
+        """,
+        'group': """--
+            GROUP BY
+              co.COLECAO
+            , co.DESCR_COLECAO
+        """,
+        'order': """--
+            ORDER BY
+              co.COLECAO
+        """
+    },
+}
+
+
 def faturamento_para_meta(
         cursor, ano, mes=None, tipo='mes', empresa=1,
         ref=None, ordem='apresentacao', cliente=None, colecao=None):
     '''
-        tipo:
+        tipo: 
             mes - totaliza por mês
             cliente - totaliza por cliente
             nota - mostra por nota
             nota_referencia - mostra por nota e referência
             referencia - totaliza por referência
             modelo - totaliza por modelo
+            colecao - totaliza por colecao
     '''
     ano = str(ano)
     if mes is None:
@@ -30,6 +200,13 @@ def faturamento_para_meta(
             prox_ano = ano
         mes = f"{mes:02}"
         prox_mes = f"{prox_mes:02}"
+
+    filtra_emissao = """
+        AND f.DATA_EMISSAO >=
+            TIMESTAMP '{ano}-{mes}-01 00:00:00.000'
+        AND f.DATA_EMISSAO <
+            TIMESTAMP '{prox_ano}-{prox_mes}-01 00:00:00.000'
+    """
 
     filtra_ref = ""
     if ref:
@@ -48,176 +225,10 @@ def faturamento_para_meta(
               || '-' || lpad(c.CGC_2, 2, '0')
               || ')' like '%{cliente}%' '''
 
-    sql_tipo = {
-        'mes': {
-            'fields': """--
-                , to_char(f.DATA_EMISSAO, 'MM/YYYY') MES
-            """,
-            'group': """--
-                GROUP BY
-                  to_char(f.DATA_EMISSAO, 'MM/YYYY')
-            """,
-            'order': """--
-                ORDER BY
-                  to_char(f.DATA_EMISSAO, 'MM/YYYY')
-            """
-        },
-        'cliente': {
-            'fields': """--
-                , max(c.NOME_CLIENTE)
-                  || ' (' || lpad(c.CGC_9, 8, '0')
-                  || '/....-..)' CLIENTE
-            """,
-            'group': """--
-                GROUP BY
-                  c.CGC_9
-            """,
-            'order': """--
-                ORDER BY
-                  max(c.NOME_CLIENTE)
-                  || ' (' || lpad(c.CGC_9, 8, '0')
-                  || '/....-..)'
-            """
-        },
-        'nota': {
-            'fields': """--
-                , f.NUM_NOTA_FISCAL NF
-                , f.DATA_EMISSAO DATA
-                , c.NOME_CLIENTE
-                  || ' (' || lpad(c.CGC_9, 8, '0')
-                  || '/' || lpad(c.CGC_4, 4, '0')
-                  || '-' || lpad(c.CGC_2, 2, '0')
-                  || ')' CLIENTE
-                , n.COD_NATUREZA NAT
-                , n.DIVISAO_NATUR DIV
-                , ped.PEDIDO_VENDA PEDIDO
-                , ped.COD_PED_CLIENTE PEDIDO_CLIENTE
-            """,
-            'group': """--
-                GROUP BY
-                  f.NUM_NOTA_FISCAL
-                , f.DATA_EMISSAO
-                , c.NOME_CLIENTE
-                , c.CGC_9
-                , c.CGC_4
-                , c.CGC_2
-                , n.COD_NATUREZA
-                , n.DIVISAO_NATUR
-                , ped.PEDIDO_VENDA
-                , ped.COD_PED_CLIENTE
-            """,
-            'order': """--
-                ORDER BY
-                  f.NUM_NOTA_FISCAL
-            """
-        },
-        'nota_referencia': {
-            'fields': """--
-                , f.NUM_NOTA_FISCAL NF
-                , f.DATA_EMISSAO DATA
-                , c.NOME_CLIENTE
-                  || ' (' || lpad(c.CGC_9, 8, '0')
-                  || '/' || lpad(c.CGC_4, 4, '0')
-                  || '-' || lpad(c.CGC_2, 2, '0')
-                  || ')' CLIENTE
-                , n.COD_NATUREZA NAT
-                , n.DIVISAO_NATUR DIV
-                , ped.PEDIDO_VENDA PEDIDO
-                , ped.COD_PED_CLIENTE PEDIDO_CLIENTE
-                , fi.NIVEL_ESTRUTURA NIVEL
-                , fi.GRUPO_ESTRUTURA REF
-            """,
-            'group': """--
-                GROUP BY
-                  f.NUM_NOTA_FISCAL
-                , f.DATA_EMISSAO
-                , c.NOME_CLIENTE
-                , c.CGC_9
-                , c.CGC_4
-                , c.CGC_2
-                , n.COD_NATUREZA
-                , n.DIVISAO_NATUR
-                , ped.PEDIDO_VENDA
-                , ped.COD_PED_CLIENTE
-                , fi.NIVEL_ESTRUTURA
-                , fi.GRUPO_ESTRUTURA
-            """,
-            'order': """--
-                ORDER BY
-                  f.NUM_NOTA_FISCAL
-                , fi.GRUPO_ESTRUTURA
-            """
-        },
-        'referencia': {
-            'fields': """--
-                , fi.NIVEL_ESTRUTURA NIVEL
-                , fi.GRUPO_ESTRUTURA REF
-                , co.COLECAO || '-' || co.DESCR_COLECAO COLECAO
-            """,
-            'group': """--
-                GROUP BY
-                  fi.NIVEL_ESTRUTURA
-                , fi.GRUPO_ESTRUTURA
-                , co.COLECAO
-                , co.DESCR_COLECAO
-            """,
-            'order': """--
-                ORDER BY
-                  fi.NIVEL_ESTRUTURA
-                , fi.GRUPO_ESTRUTURA
-            """
-        },
-        'modelo': {
-            'fields': """--
-                , CASE WHEN fi.NIVEL_ESTRUTURA = 1 THEN
-                    TO_NUMBER(
-                      REGEXP_REPLACE(fi.GRUPO_ESTRUTURA, '[^0-9]', '')
-                    )
-                  ELSE 0
-                  END
-                  MODELO
-                , co.COLECAO || '-' || co.DESCR_COLECAO COLECAO
-            """,
-            'group': """--
-                GROUP BY
-                  CASE WHEN fi.NIVEL_ESTRUTURA = 1 THEN
-                    TO_NUMBER(
-                      REGEXP_REPLACE(fi.GRUPO_ESTRUTURA, '[^0-9]', '')
-                    )
-                  ELSE 0
-                  END
-                , co.COLECAO
-                , co.DESCR_COLECAO
-            """,
-            'order': """--
-                ORDER BY
-                  CASE WHEN fi.NIVEL_ESTRUTURA = 1 THEN
-                    TO_NUMBER(
-                      REGEXP_REPLACE(fi.GRUPO_ESTRUTURA, '[^0-9]', '')
-                    )
-                  ELSE 0
-                  END
-                , co.COLECAO
-            """
-        },
-        'colecao': {
-            'fields': """--
-                , co.COLECAO || '-' || co.DESCR_COLECAO COLECAO
-            """,
-            'group': """--
-                GROUP BY
-                  co.COLECAO
-                , co.DESCR_COLECAO
-            """,
-            'order': """--
-                ORDER BY
-                  co.COLECAO
-            """
-        },
-    }
-
+    sql_fields = SQL_TIPO_FAT_META[tipo]['fields']
+    sql_group = SQL_TIPO_FAT_META[tipo]['group']
     if ordem == 'apresentacao':
-        sql_order = sql_tipo[tipo]['order']
+        sql_order = SQL_TIPO_FAT_META[tipo]['order']
     else:
         sql_order = """
             ORDER BY
@@ -228,7 +239,7 @@ def faturamento_para_meta(
         SELECT
           sum(fi.VALOR_FATURADO +  fi.RATEIO_DESPESA) VALOR
         , sum(fi.QTDE_ITEM_FATUR) QTD
-        {sql_tipo[tipo]['fields']} -- fields
+        {sql_fields} -- fields
         FROM FATU_050 f
         JOIN fatu_060 fi
           ON fi.ch_it_nf_cd_empr = f.codigo_empresa
@@ -267,12 +278,8 @@ def faturamento_para_meta(
           AND f.COD_CANC_NFISC = 0
           -- utilizou natureza configurada como faturamento
           AND n.faturamento = 1
-          -- filtra data
-          AND f.DATA_EMISSAO >=
-              TIMESTAMP '{ano}-{mes}-01 00:00:00.000'
-          AND f.DATA_EMISSAO <
-              TIMESTAMP '{prox_ano}-{prox_mes}-01 00:00:00.000'
-        {sql_tipo[tipo]['group']} -- group
+          {filtra_emissao} -- filtra_emissao
+        {sql_group} -- group
         {sql_order} -- order
     """
     cursor.execute(sql)
