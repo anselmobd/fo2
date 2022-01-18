b09a5baeb982bced1aea846ce81de20ec80911ae
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2022-01-18 15:49:29 -0300

    view de coleção: mostrando contagens

--- src/systextil/views/table/colecao.py
+++ src/systextil/views/table/colecao.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 
 from fo2.connections import db_cursor_so
 
-from utils.functions.format import format_cnpj
+from utils.views import totalize_data
 
 from systextil.queries.tabela.colecao import query_colecao
 
@@ -12,10 +12,65 @@ from systextil.queries.tabela.colecao import query_colecao
 def view(request):
     cursor = db_cursor_so(request)
     data = query_colecao(cursor)
+
+    totalize_data(
+        data,
+        {
+            'sum': [
+                'produto',
+                'pa',
+                'pg',
+                'pb',
+                'parte',
+                'md',
+                'mp',
+                'insumo',
+                'total',
+            ],
+            'descr': {'descr_colecao': 'Totais:'},
+            'row_style': 'font-weight: bold;',
+        }
+    )
+
     context = {
         'titulo': 'Coleções',
-        'headers': ('Código', 'Descrição'),
-        'fields': ('colecao', 'descr_colecao'),
+        'headers': (
+            'Código',
+            'Descrição',
+            'Produto',
+            'PA',
+            'PG',
+            'PB',
+            'Parte',
+            'MD',
+            'MP',
+            'Insumo',
+            'Total',
+        ),
+        'fields': (
+            'colecao',
+            'descr_colecao',
+            'produto',
+            'pa',
+            'pg',
+            'pb',
+            'parte',
+            'md',
+            'mp',
+            'insumo',
+            'total',
+        ),
+        'style': {
+            3: 'text-align: right;',
+            4: 'text-align: right;',
+            5: 'text-align: right;',
+            6: 'text-align: right;',
+            7: 'text-align: right;',
+            8: 'text-align: right;',
+            9: 'text-align: right;',
+            10: 'text-align: right;',
+            11: 'text-align: right;',
+        },
         'data': data,
     }
     return render(request, 'systextil/tabela.html', context)
