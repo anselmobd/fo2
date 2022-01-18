52c8f2f86770b01d265155a16f49bcb34dec6970
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2022-01-18 14:12:10 -0300

    app systextil index menu também para o Apoio

--- src/systextil/urls/main.py
+++ src/systextil/urls/main.py
@@ -1,6 +1,7 @@
 from django.urls import include, re_path
 
 from systextil.views import (
+    apoio_index,
     index,
     sessions,
 )
@@ -10,7 +11,9 @@ app_name = 'systextil'
 urlpatterns = [
     re_path(r'^$', index.view, name='index'),
 
-    re_path(r'^sessions/$', sessions.view, name='sessions'),
+    re_path(r'^apoio/$', apoio_index.view, name='apoio_index'),
 
     re_path(r'^dba/', include('systextil.urls.dba'), name='dba'),
+
+    re_path(r'^sessions/$', sessions.view, name='sessions'),
 ]
