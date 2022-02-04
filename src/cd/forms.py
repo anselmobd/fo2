555f62dc93299a925090c2fa56a3dc63807dc5c9
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2022-02-04 16:14:27 -0300

    form RetirarParcialForm: clean_lote praticamente igual ao do RetirarForm; um flag cuida da diferença

--- src/cd/forms.py
+++ src/cd/forms.py
@@ -146,6 +146,8 @@ class EnderecarForm(forms.Form):
 
 
 class RetirarForm(forms.Form):
+    _tipo_retirada = 'total'
+
     lote = forms.CharField(
         label='Lote', max_length=9, min_length=9,
         widget=forms.TextInput(attrs={'type': 'number', 'size': 9,
@@ -206,9 +208,10 @@ class RetirarForm(forms.Form):
                 "Lote não consta em nenhuma solicitação")
 
         slq = slqs[0]
-        if slq['qtdsum'] != slq['lote__qtd']:
-            raise forms.ValidationError(
-                "Solicitação não é de total disponível")
+        if self._tipo_retirada == 'total':
+            if slq['qtdsum'] != slq['lote__qtd']:
+                raise forms.ValidationError(
+                    "Solicitação não é de total disponível")
 
         if not slq['solicitacao__coleta']:
             raise forms.ValidationError(
@@ -222,6 +225,8 @@ class RetirarForm(forms.Form):
 
 
 class RetirarParcialForm(forms.Form):
+    _tipo_retirada = 'parcial'
+
     lote = forms.CharField(
         label='Lote', max_length=9, min_length=9,
         widget=forms.TextInput(attrs={'type': 'number', 'size': 9,
@@ -235,15 +240,7 @@ class RetirarParcialForm(forms.Form):
         required=False,
         widget=forms.HiddenInput())
 
-    def clean_lote(self):
-        lote = self.cleaned_data.get('lote', '')
-
-        try:
-            self.lote_record = lotes.models.Lote.objects.get(lote=lote)
-        except lotes.models.Lote.DoesNotExist:
-            raise forms.ValidationError("Lote não encontrado")
-
-        return lote
+    clean_lote = RetirarForm.clean_lote
 
     def clean(self):
         if not self.errors:
