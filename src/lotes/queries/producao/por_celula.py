d0f7659cf1bcc85eb9e664d26f552c5b033008f0
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2022-02-15 19:08:35 -0300

    view PorCelula versão parcialmente funcional

---
+++ src/lotes/queries/producao/por_celula.py
@@ -0,0 +1,62 @@
+from pprint import pprint
+
+from utils.functions.models import rows_to_dict_list_lower
+from utils.functions.queries import debug_cursor_execute
+
+
+def query(cursor, dada_de, dada_ate):
+    filtro_dada_de = (
+        f"AND ml.DATA_PRODUCAO >= '{dada_de}'"
+    ) if dada_de else ''
+
+    filtro_dada_ate = (
+        f"AND ml.DATA_PRODUCAO <= '{dada_ate}'"
+    ) if dada_ate else ''
+
+    sql = f'''
+        WITH mlseq AS
+        (
+        SELECT 
+          ml.PCPC040_PERCONF per
+        , ml.PCPC040_ORDCONF ord
+        , ml.PCPC040_ESTCONF est
+        , max(ml.SEQUENCIA) seq
+        FROM  PCPC_045 ml
+        GROUP BY
+          ml.PCPC040_PERCONF
+        , ml.PCPC040_ORDCONF
+        , ml.PCPC040_ESTCONF
+        ) 
+        SELECT
+          TO_DATE(ml.DATA_PRODUCAO) data
+        , l.PROCONF_GRUPO ref
+        , count(l.ORDEM_CONFECCAO) lotes
+        , sum(l.QTDE_PECAS_PROD) qtd
+        FROM PCPC_040 l
+        JOIN mlseq mls
+          ON mls.per = l.PERIODO_PRODUCAO
+        AND mls.ord = l.ORDEM_CONFECCAO
+        AND mls.est = l.CODIGO_ESTAGIO
+        JOIN PCPC_045 ml
+          ON ml.PCPC040_PERCONF = l.PERIODO_PRODUCAO
+        AND ml.PCPC040_ORDCONF = l.ORDEM_CONFECCAO 
+        AND ml.PCPC040_ESTCONF = l.CODIGO_ESTAGIO
+        AND ml.SEQUENCIA = mls.seq
+        WHERE 1=1
+          -- AND ml.DATA_PRODUCAO = DATE '2022-02-15'
+          {filtro_dada_de} -- filtro_dada_de
+          {filtro_dada_ate} -- filtro_dada_ate
+          AND l.CODIGO_ESTAGIO = 33
+          AND l.CODIGO_FAMILIA = 2836
+          AND l.QTDE_PECAS_PROD <> 0 
+        GROUP BY 
+          ml.DATA_PRODUCAO
+        , l.PROCONF_GRUPO
+        ORDER BY 
+          ml.DATA_PRODUCAO
+    '''
+    debug_cursor_execute(cursor, sql)
+    dados = rows_to_dict_list_lower(cursor)
+    for row in dados:
+        row['data'] = row['data'].date()
+    return dados
--- src/lotes/views/por_celula.py
+++ src/lotes/views/por_celula.py
@@ -7,6 +7,7 @@ from fo2.connections import db_cursor_so
 from base.views import O2BaseGetPostView
 
 from lotes.forms.por_celula import PorCelulaForm
+from lotes.queries.producao.por_celula import query as query_por_celula
 
 
 class PorCelula(O2BaseGetPostView):
@@ -17,17 +18,27 @@ class PorCelula(O2BaseGetPostView):
         self.template_name = 'lotes/por_celula.html'
         self.title_name = 'Produção por célula'
         self.cleaned_data2self = True
+        self.get_args2context = True
 
     def mount_context(self):
         self.cursor = db_cursor_so(self.request)
 
-        dados = []
+        if not self.data_ate:
+            self.data_ate = self.data_de
+
+        dados = query_por_celula(
+            self.cursor,
+            self.data_de,
+            self.data_ate,
+        )
+        # pprint(dados)
+        # pprint(self.context)
 
         if not dados:
             return
 
         self.context.update({
-            'headers': ['Data', 'Referência', 'Quantidade'],
-            'fields': ['data', 'ref', 'qtd'],
+            'headers': ['Data', 'Referência', 'Lotes', 'Peças'],
+            'fields': ['data', 'ref', 'lotes', 'qtd'],
             'dados': dados,
         })
