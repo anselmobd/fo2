652002098a2638dd424ebbd9942e0e3d02b67fa3
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2022-02-23 16:48:35 -0300

    separando queries insumos_cor_tamanho_usados e insumos_cor_tamanho

--- src/insumo/queries/__init__.py
+++ src/insumo/queries/__init__.py
@@ -1,3 +1,5 @@
+from .insumos_cor_tamanho import *
+from .insumos_cor_tamanho_usados import *
 from .insumo_descr import *
 from .insumo_necessidade_semana import *
 from .insumo_recebimento_semana import *
