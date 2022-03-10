d8861390e1d96047859fa768c0503ff462d5e8c0
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2022-03-10 14:09:56 -0300

    view RoteirosSemSequ: utilizando O2BaseGetView

--- src/produto/views/roteiro_sem_sequencia.py
+++ src/produto/views/roteiro_sem_sequencia.py
@@ -1,35 +1,32 @@
 from pprint import pprint
 
-from django.shortcuts import render
 from django.urls import reverse
-from django.views import View
 
+from base.views import O2BaseGetView
 from utils.forms import FiltroForm
 
-import produto.queries as queries
+import produto.queries
 
 
-class RoteirosSemSequ(View):
-    Form_class = FiltroForm
-    template_name = 'produto/roteiro_sem_sequencia.html'
-    title_name = 'Roteiros sem sequência'
+class RoteirosSemSequ(O2BaseGetView):
+
+    def __init__(self, *args, **kwargs):
+        super(RoteirosSemSequ, self).__init__(*args, **kwargs)
+        self.Form_class = FiltroForm
+        self.template_name = 'produto/roteiro_sem_sequencia.html'
+        self.title_name = 'Roteiros sem sequência'
 
     def mount_context(self):
-        data = queries.roteiro_sem_sequencia()
+        data = produto.queries.roteiro_sem_sequencia()
 
         for row in data:
             row['ref|LINK'] = reverse('produto:ref__get', args=[row['ref']])
             row['ref|TARGET'] = '_blank'
 
-        return {
+        self.context.update({
             'headers': ('Referência', 'Tamanho', 'Cor', 
                         'Alternativa', 'Roteiro'),
             'fields': ('ref', 'tam', 'cor',
                         'alt', 'rot'),
             'data': data,
-        }
-
-    def get(self, request, *args, **kwargs):
-        context = {'titulo': self.title_name}
-        context.update(self.mount_context())
-        return render(request, self.template_name, context)
+        })
