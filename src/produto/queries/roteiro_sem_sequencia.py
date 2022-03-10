4bb10acdf670ba1b2ec3c6235ae09f14c07e6f0b
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2022-03-10 15:48:41 -0300

    tirando montagem de query de dentro de SQuery e criando MountQuery; testado em roteiro_sem_sequencia

--- src/produto/queries/roteiro_sem_sequencia.py
+++ src/produto/queries/roteiro_sem_sequencia.py
@@ -1,25 +1,25 @@
 from pprint import pprint
 
-from systextil.queries.base import SQuery
+from systextil.queries.base import SQuery, MountQuery
 
 
 def roteiro_sem_sequencia():
-    return SQuery(
-      fields=[
-        'r.GRUPO_ESTRUTURA REF',
-        'r.SUBGRU_ESTRUTURA TAM',
-        'r.ITEM_ESTRUTURA COR',
-        'r.NUMERO_ALTERNATI ALT',
-        'r.NUMERO_ROTEIRO ROT',
-      ],
-      table='MQOP_050 r',
-      where=[
-          'r.NIVEL_ESTRUTURA = 1',
-          'r.SEQUENCIA_ESTAGIO = 0',
-      ],
-      group_all_fields=True,
-      order_all_fields=True,
-    ).debug_execute()
+    return SQuery(MountQuery(
+        fields=[
+          'r.GRUPO_ESTRUTURA REF',
+          'r.SUBGRU_ESTRUTURA TAM',
+          'r.ITEM_ESTRUTURA COR',
+          'r.NUMERO_ALTERNATI ALT',
+          'r.NUMERO_ROTEIRO ROT',
+        ],
+        table='MQOP_050 r',
+        where=[
+            'r.NIVEL_ESTRUTURA = 1',
+            'r.SEQUENCIA_ESTAGIO = 0',
+        ],
+        group_all_fields=True,
+        order_all_fields=True,
+    )).debug_execute()
     return SQuery("""
         SELECT
           r.GRUPO_ESTRUTURA REF
--- src/systextil/queries/base.py
+++ src/systextil/queries/base.py
@@ -6,11 +6,10 @@ from utils.functions.models import dictlist
 from utils.functions.queries import debug_cursor_execute
 
 
-class SQuery():
+class MountQuery():
 
     def __init__(
             self,
-            sql=None,
             fields=None,
             table=None,
             where=None,
@@ -19,21 +18,26 @@ class SQuery():
             group_all_fields=False,
             order_all_fields=False,
         ) -> None:
-        self.cursor = db_cursor_so()
-        if sql:
-            self.sql = sql
-        else:
-            if group_all_fields:
-                group = self.off_alias(fields)
-            if order_all_fields:
-                order = self.off_alias(fields)
-            self.sql = self.query(
-                fields,
-                table,
-                where,
-                group,
-                order,
-            )
+        self.fields = fields
+        self.table = table
+        self.where = where
+        self.group = group
+        self.order = order
+        self.group_all_fields = group_all_fields
+        self.order_all_fields = order_all_fields
+
+    def sql(self):
+        if self.group_all_fields:
+            self.group = self.off_alias(self.fields)
+        if self.order_all_fields:
+            self.order = self.off_alias(self.fields)
+        return self.query(
+            self.fields,
+            self.table,
+            self.where,
+            self.group,
+            self.order,
+        )
 
     def off_alias(self, fields):
         new_fields = []
@@ -41,9 +45,8 @@ class SQuery():
             last_space = field.rfind(' ')
             if last_space + 1:  # se last_space >= 0
                 alias_candidate = field[last_space+1:]
-                print(alias_candidate)
                 if alias_candidate.find('.') == -1:
-                    new_fields.append(field[:last_space])
+                    new_fields.append(field[:last_space].strip())
                     continue
             new_fields.append(field)
         return new_fields
@@ -60,7 +63,7 @@ class SQuery():
             {", ".join(fields)}"""
         qfrom = f"""FROM {table}"""
         qwhere = f"""WHERE
-            {"AND ".join(where)}""" if where else "--"
+            {" AND ".join(where)}""" if where else "--"
         qgroup = f"""GROUP BY
             {", ".join(group)}""" if group else "--"
         qorder = f"""ORDER BY
@@ -72,6 +75,16 @@ class SQuery():
             {qgroup}
             {qorder}
         """
+
+
+class SQuery():
+
+    def __init__(self, sql) -> None:
+        self.cursor = db_cursor_so()
+        if isinstance(sql, MountQuery):
+            self.sql = sql.sql()
+        else:
+            self.sql = sql
 
     def debug_execute(self):
         debug_cursor_execute(self.cursor, self.sql)
