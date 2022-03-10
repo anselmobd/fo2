e7554134e1325407a164cdc543bf358b9d2692d3
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2022-03-10 15:35:43 -0300

    SQuery com recurso básico de montagem de query; testado em roteiro_sem_sequencia

--- src/produto/queries/roteiro_sem_sequencia.py
+++ src/produto/queries/roteiro_sem_sequencia.py
@@ -4,6 +4,22 @@ from systextil.queries.base import SQuery
 
 
 def roteiro_sem_sequencia():
+    return SQuery(
+      fields=[
+        'r.GRUPO_ESTRUTURA REF',
+        'r.SUBGRU_ESTRUTURA TAM',
+        'r.ITEM_ESTRUTURA COR',
+        'r.NUMERO_ALTERNATI ALT',
+        'r.NUMERO_ROTEIRO ROT',
+      ],
+      table='MQOP_050 r',
+      where=[
+          'r.NIVEL_ESTRUTURA = 1',
+          'r.SEQUENCIA_ESTAGIO = 0',
+      ],
+      group_all_fields=True,
+      order_all_fields=True,
+    ).debug_execute()
     return SQuery("""
         SELECT
           r.GRUPO_ESTRUTURA REF
--- src/systextil/queries/base.py
+++ src/systextil/queries/base.py
@@ -8,9 +8,70 @@ from utils.functions.queries import debug_cursor_execute
 
 class SQuery():
 
-    def __init__(self, sql) -> None:
+    def __init__(
+            self,
+            sql=None,
+            fields=None,
+            table=None,
+            where=None,
+            group=None,
+            order=None,
+            group_all_fields=False,
+            order_all_fields=False,
+        ) -> None:
         self.cursor = db_cursor_so()
-        self.sql = sql
+        if sql:
+            self.sql = sql
+        else:
+            if group_all_fields:
+                group = self.off_alias(fields)
+            if order_all_fields:
+                order = self.off_alias(fields)
+            self.sql = self.query(
+                fields,
+                table,
+                where,
+                group,
+                order,
+            )
+
+    def off_alias(self, fields):
+        new_fields = []
+        for field in map(str.strip, fields):
+            last_space = field.rfind(' ')
+            if last_space + 1:  # se last_space >= 0
+                alias_candidate = field[last_space+1:]
+                print(alias_candidate)
+                if alias_candidate.find('.') == -1:
+                    new_fields.append(field[:last_space])
+                    continue
+            new_fields.append(field)
+        return new_fields
+
+    def query(
+            self,
+            fields,
+            table,
+            where,
+            group,
+            order,
+        ):
+        qselect = f"""SELECT
+            {", ".join(fields)}"""
+        qfrom = f"""FROM {table}"""
+        qwhere = f"""WHERE
+            {"AND ".join(where)}""" if where else "--"
+        qgroup = f"""GROUP BY
+            {", ".join(group)}""" if group else "--"
+        qorder = f"""ORDER BY
+            {", ".join(order)}""" if order else "--"
+        return f"""
+            {qselect}
+            {qfrom}
+            {qwhere}
+            {qgroup}
+            {qorder}
+        """
 
     def debug_execute(self):
         debug_cursor_execute(self.cursor, self.sql)
