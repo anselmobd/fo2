44bd849712e022f9d43f1c5faa2f2bcf615749f2
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2022-03-10 17:19:45 -0300

    criadas MountQuery e OQuery vontadas para o connection.cursor; vários testes na query hist_narrativa

---
+++ src/o2/queries.py
@@ -0,0 +1,114 @@
+from pprint import pprint
+
+from django.db import connection
+
+from utils.functions.models import dictlist
+from utils.functions.queries import debug_cursor_execute
+
+
+class MountQuery():
+
+    def __init__(
+            self,
+            fields=None,
+            table=None,
+            where=None,
+            group=None,
+            order=None,
+            group_all_fields=False,
+            order_all_fields=False,
+        ) -> None:
+        self.fields = fields
+        self.table = table
+        self.where = where
+        self.group_arg = group if group else []
+        self.order_arg = order if order else []
+        self.group_all_fields = group_all_fields
+        self.order_all_fields = order_all_fields
+
+        self._group = None
+        self._order = None
+        self._OQuery = None
+
+    @property
+    def group(self):
+        if not self._group:
+            group_form_fields = (
+                self.off_alias(self.fields)
+                if self.group_all_fields
+                else []
+            )
+            self._group = group_form_fields + self.group_arg
+        return self._group
+
+    @property
+    def order(self):
+        if not self._order:
+            order_form_fields = (
+                self.off_alias(self.fields)
+                if self.order_all_fields
+                else []
+            )
+            self._order = order_form_fields + self.order_arg
+        return self._order
+
+    @property
+    def query(self):
+        qselect = f"""SELECT
+            {", ".join(self.fields)}"""
+        qfrom = f"""FROM {self.table}"""
+        qwhere = f"""WHERE
+            {" AND ".join(self.where)}""" if self.where else "--"
+        qgroup = f"""GROUP BY
+            {", ".join(self.group)}""" if self.group else "--"
+        qorder = f"""ORDER BY
+            {", ".join(self.order)}""" if self.order else "--"
+        return f"""
+            {qselect}
+            {qfrom}
+            {qwhere}
+            {qgroup}
+            {qorder}
+        """
+
+    @property
+    def OQuery(self):
+        if not self._OQuery:
+            self._OQuery = OQuery
+        return self._OQuery
+
+    @property
+    def oquery(self):
+        return self.OQuery(self.query)
+
+    def off_alias(self, fields):
+        new_fields = []
+        for field in map(str.strip, fields):
+            last_space = field.rfind(' ')
+            if last_space + 1:  # se last_space >= 0
+                alias_candidate = field[last_space+1:]
+                if alias_candidate.find('.') == -1:
+                    new_fields.append(field[:last_space].strip())
+                    continue
+            new_fields.append(field)
+        return new_fields
+
+
+class OQuery():
+
+    def __init__(self, sql) -> None:
+        self._cursor = None
+        if isinstance(sql, MountQuery):
+            self.sql = sql.query
+        else:
+            self.sql = sql
+
+    @property
+    def cursor(self):
+        if not self._cursor:
+            self._cursor = connection.cursor()
+        return self._cursor
+
+    def debug_execute(self):
+        debug_cursor_execute(self.cursor, self.sql)
+        return dictlist(self.cursor)
--- src/produto/queries/historico.py
+++ src/produto/queries/historico.py
@@ -3,11 +3,75 @@ from pprint import pprint
 from django.conf import settings
 from django.db import connection
 
+from o2.queries import MountQuery, OQuery
+
 from utils.functions.models import rows_to_dict_list
 from utils.functions.queries import debug_cursor_execute
 
 
 def hist_narrativa(referencia):
+    MQ = MountQuery(
+        fields=[
+          "h.data_ocorr",
+          "h.usuario",
+          "h.str03 tam",
+          "h.str04 cor",
+          "h.str05 narrativa",
+        ],
+        table="systextil_logs.hist_100 h",
+        where=[
+            "h.tabela = 'basi_010'",
+            "h.programa = 'basi_f325'",
+            "h.str01 = '1'",
+            f"h.str02 = '{referencia}'",
+        ],
+        order=[
+            "h.sequencia desc",
+        ],
+    )
+    return OQuery(MQ).debug_execute()
+
+    sql = MountQuery(
+        fields=[
+          "h.data_ocorr",
+          "h.usuario",
+          "h.str03 tam",
+          "h.str04 cor",
+          "h.str05 narrativa",
+        ],
+        table="systextil_logs.hist_100 h",
+        where=[
+            "h.tabela = 'basi_010'",
+            "h.programa = 'basi_f325'",
+            "h.str01 = '1'",
+            f"h.str02 = '{referencia}'",
+        ],
+        order=[
+            "h.sequencia desc",
+        ],
+    ).query
+    return OQuery(sql).debug_execute()
+
+    return MountQuery(
+        fields=[
+          "h.data_ocorr",
+          "h.usuario",
+          "h.str03 tam",
+          "h.str04 cor",
+          "h.str05 narrativa",
+        ],
+        table="systextil_logs.hist_100 h",
+        where=[
+            "h.tabela = 'basi_010'",
+            "h.programa = 'basi_f325'",
+            "h.str01 = '1'",
+            f"h.str02 = '{referencia}'",
+        ],
+        order=[
+            "h.sequencia desc",
+        ],
+    ).oquery.debug_execute()
+
     cursor = connection.cursor()
     sql = f"""
       select 
