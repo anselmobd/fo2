de7bd808a704ad855a976e7d697a8c3d6d55a022
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2022-03-21 11:57:25 -0300

    strnum_mod1110_digit alterando lógica de multiplicadores

--- src/o2/functions/check_digit.py
+++ src/o2/functions/check_digit.py
@@ -11,13 +11,25 @@ def strnum_mod1110_digits(strnum, ndigits=2):
 
 
 def strnum_mod1110_digit(strnum):
+    """Calcula dígito verificador de string de representação numérica.
+    Recebe: String representando um valor inteiro, com ou sem zeros à 
+            esquerda.
+            Obs.: Zeros à esquerda são significativos
+    Retorna: String representando um dígito verificador
+    """
     sum = 0
     for idx, strdigit in enumerate(strnum[::-1]):
-        mult = idx%8+2
+        # Multiplicadores:
+        #   Da direita para a esquerda, 9 a 2, ciclicamente
+        #   Exemplo para tamanho 10: 8, 9, 2, 3, 4, 5, 6, 7, 8, 9
+        mult = (-idx-1)%8+2
+        # Antes de multiplicar os dígitos são somados de 1
         digit_add1 = int(strdigit) + 1
         calc = mult * digit_add1
+        # Acumula-se o módulo 11 da multiplicação
         calc_mod11 = calc % 11
         sum += calc_mod11
+    # Dígito verificador é o módulo 10 do acumulado
     sum_mod10 = sum % 10
     return str(sum_mod10)
 
