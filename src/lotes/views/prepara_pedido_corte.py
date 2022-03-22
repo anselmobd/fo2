28d48b9404e59d63b2320c53c7ce36169e988ced
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2022-03-22 19:52:42 -0300

    view PreparaPedidoCorte critica pedido e altera observação

--- src/lotes/views/prepara_pedido_corte.py
+++ src/lotes/views/prepara_pedido_corte.py
@@ -3,18 +3,45 @@ from pprint import pprint
 from django.http import JsonResponse
 from django.views import View
 
+from fo2.connections import db_cursor_so
+
+import lotes.queries
+from lotes.queries.pedido.ped_alter import altera_pedido
+from lotes.queries.producao import romaneio_corte
+
 
 class PreparaPedidoCorte(View):
 
-    def result(self, status, message):
-        return JsonResponse({
+    def process(self, request, kwargs):
+        cursor = db_cursor_so(request)
+
+        data = kwargs['data']
+        cliente = kwargs['cliente']
+        pedido = kwargs['pedido']
+
+        dados = lotes.queries.pedido.ped_inform(cursor, pedido, empresa=3)
+        pprint(dados)
+        if not dados:
+            self.result = ('ERRO', "Pedido não encontrado!")
+            return
+
+        dados, clientes = romaneio_corte.query_completa(cursor, data, nf=True, cliente_slug=cliente)
+        pprint(dados)
+        pprint(clientes)
+
+        observacao = dados[0]['obs']
+        altera_pedido(cursor, pedido, 3, observacao)
+
+        self.result = ('OK', "OK!")
+
+    def response(self):
+        status, message = self.result
+        return {
             'status': status,
             'message': message,
-        }, safe=False)
+        }
 
     def get(self, request, *args, **kwargs):
-        return self.result(
-            'OK',
-            "OK!",
-        )
+        self.process(request, kwargs)
+        return JsonResponse(self.response(), safe=False)
 
