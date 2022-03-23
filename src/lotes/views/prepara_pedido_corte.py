669ea62c8c6d4e19c5b207051275c26c8e362de2
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2022-03-23 17:43:33 -0300

    view PreparaPedidoCorte padronizando mensagem de observação

--- src/lotes/views/prepara_pedido_corte.py
+++ src/lotes/views/prepara_pedido_corte.py
@@ -22,26 +22,39 @@ class PreparaPedidoCorte(View):
         dados = lotes.queries.pedido.ped_inform(cursor, pedido, empresa=3)
         pprint(dados)
         if not dados:
-            self.result = ('ERRO', "Pedido não encontrado!")
-            return
+            return ('ERRO', "Pedido não encontrado!")
 
         dados, clientes = romaneio_corte.query_completa(cursor, data, nf=True, cliente_slug=cliente)
         pprint(dados)
         pprint(clientes)
 
-        observacao = dados[0]['obs']
+        # MPCFM - Movimentação de Peças Cortadas da Filial p/ Matriz; Data: 2022-03-16
+        # Produção para o cliente Renner. Pedido(5214524)-OP(34023), Pedido(5214547)-OP(34027)
+        # Produção para estoque. OP(34082, 34307, 34339, 34262, 34297)
+
+        if cliente == 'estoque':
+            observacao = (
+                "[MPCFM] Movimentacao de Pecas Cortadas da Filial p/ Matriz; Data: 2022-03-16\n"
+                f"Producao para estoque. {dados[0]['obs']}\n"
+            )
+        else:
+            observacao = (
+                "[MPCFM] Movimentacao de Pecas Cortadas da Filial p/ Matriz; Data: 2022-03-16\n"
+                f"Producao para o cliente {cliente.capitalize()}. {dados[0]['obs']}\n"
+            )
+
         altera_pedido(cursor, pedido, 3, observacao)
 
-        self.result = ('OK', "OK!")
+        return ('OK', "OK!")
 
-    def response(self):
-        status, message = self.result
+    def response(self, result):
+        status, message = result
         return {
             'status': status,
             'message': message,
         }
 
     def get(self, request, *args, **kwargs):
-        self.process(request, kwargs)
-        return JsonResponse(self.response(), safe=False)
+        result = self.process(request, kwargs)
+        return JsonResponse(self.response(result), safe=False)
 
