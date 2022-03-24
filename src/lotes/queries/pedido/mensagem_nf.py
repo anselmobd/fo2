36d226ba2b96c18f72dfdd9c72c1c6380cd0e104
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2022-03-23 21:33:21 -0300

    cria_mens_nf utilizando o split_size_by_char para criar as 10 linhas

--- src/lotes/queries/pedido/mensagem_nf.py
+++ src/lotes/queries/pedido/mensagem_nf.py
@@ -1,21 +1,34 @@
 from pprint import pprint
 
-from utils.functions.models import rows_to_dict_list
+from o2.functions.text import split_size_by_char
+
+__all__=['cria_mens_nf']
 
 
 def cria_mens_nf(cursor, codigo, mensagem):
+    if isinstance(mensagem, str):
+        mensagem = (mensagem, )
+    linhas = []
+    for linha in map(str.strip, mensagem):
+        while linha:
+            before, linha = split_size_by_char(linha, 100)
+            if before:
+                linhas.append(before)
+    linhas += [' ']*10
+    
+    apaga_mens_nf(cursor, codigo)
+
     sql = f"""
         INSERT INTO SYSTEXTIL.OBRF_874
         (COD_MENSAGEM, TIP_MENSAGEM, IND_LOCAL, MSG_INFORMAR, DES_MENSAGEM1, DES_MENSAGEM2, DES_MENSAGEM3, DES_MENSAGEM4, DES_MENSAGEM5, DES_MENSAGEM6, DES_MENSAGEM7, DES_MENSAGEM8, DES_MENSAGEM9, DES_MENSAGEM10, MSG_DEFAULT, PERTENCE_REG_1921)
-        VALUES({codigo}, 'C', 'D', 'S', 'TEEEEEEEESTEEEEEEE - PED. TUSSOR:', ' - PEDIDO DO CLIENTE:', ' - CONTAR VENCIMENTO A PARTIR DE 25/03/2022.', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'N', 'N');
+        VALUES({codigo}, 'C', 'D', 'S', '{linhas[0]}', '{linhas[1]}', '{linhas[2]}', '{linhas[3]}', '{linhas[4]}', '{linhas[5]}', '{linhas[6]}', '{linhas[7]}', '{linhas[8]}', '{linhas[9]}', 'N', 'N')
     """
     cursor.execute(sql)
 
 
-def apaga_mens_nf(cursor, codigo, mensagem):
+def apaga_mens_nf(cursor, codigo):
     sql = f"""
         DELETE FROM SYSTEXTIL.OBRF_874
-        (COD_MENSAGEM, TIP_MENSAGEM, IND_LOCAL, MSG_INFORMAR, DES_MENSAGEM1, DES_MENSAGEM2, DES_MENSAGEM3, DES_MENSAGEM4, DES_MENSAGEM5, DES_MENSAGEM6, DES_MENSAGEM7, DES_MENSAGEM8, DES_MENSAGEM9, DES_MENSAGEM10, MSG_DEFAULT, PERTENCE_REG_1921)
-        VALUES({codigo}, 'C', 'D', 'S', ' - PED. TUSSOR:', ' - PEDIDO DO CLIENTE:', ' - CONTAR VENCIMENTO A PARTIR DE 25/03/2022.', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'N', 'N');
+        WHERE COD_MENSAGEM = {codigo}
     """
     cursor.execute(sql)
