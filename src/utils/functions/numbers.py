b0d166560ed0f0e2c8502de7ab38416abedb6fb0
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2022-04-08 15:33:13 -0300

    criados number_to_base, digits_to_str e inversos

--- src/utils/functions/numbers.py
+++ src/utils/functions/numbers.py
@@ -8,3 +8,28 @@ def num_digits(number):
         0,
         str(number)[::-1].find('.')
     )
+
+
+def digits_to_str(digs, b='0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'):
+    return ''.join([b[i] for i in digs])
+
+
+def str_to_digits(str, b='0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'):
+    return [b.index(char) for char in str]
+
+
+def number_to_base(n, b):
+    if n == 0:
+        return [0]
+    digits = []
+    while n:
+        digits.append(int(n % b))
+        n //= b
+    return digits[::-1]
+
+
+def base_to_number(digits, b):
+    n = 0
+    for i in digits:
+        n = n*b + i
+    return n
