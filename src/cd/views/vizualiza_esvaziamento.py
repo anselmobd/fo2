293d4dae378e639acd9e4200917120fd03ed4775
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2022-04-11 19:36:44 -0300

    view VisualizaEsvaziamento compara esvaziamento com versão atual

--- src/cd/views/vizualiza_esvaziamento.py
+++ src/cd/views/vizualiza_esvaziamento.py
@@ -7,9 +7,12 @@ from django.views import View
 
 from fo2.connections import db_cursor_so
 
+from utils.functions import coalesce
+
 import cd.forms
 import cd.views.gerais
 from cd.queries.endereco import (
+    lotes_em_local,
     lotes_em_versao_palete,
 )
 
@@ -32,11 +35,42 @@ class VisualizaEsvaziamento(View):
         })
 
         lotes_versao = lotes_em_versao_palete(self.cursor, palete, data_versao)
+        lotes_versao_dict = {
+            row['lote']: row['data'].strftime('%d/%m/%y %H:%M:%S')
+            for row in lotes_versao
+        }
+
+        lotes_end = lotes_em_local(self.cursor, palete)
+        lotes_end_dict = {
+            row['lote']: row['data'].strftime('%d/%m/%y %H:%M:%S')
+            for row in lotes_end
+        }
+
+        lotes = set(lotes_versao_dict.keys())
+        lotes = lotes.union(set(lotes_end_dict.keys()))
+
+        dados = []
+        for lote in sorted(lotes):
+            data_antes = lotes_versao_dict[lote] if lote in lotes_versao_dict else None
+            data_agora = lotes_end_dict[lote] if lote in lotes_end_dict else None
+            if data_antes and not data_agora:
+                style = 'color: red;'
+            elif data_agora and not data_antes:
+                style = 'color: darkorange;'
+            else:
+                style = 'color: darkgreen;'
+            style += 'text-align: center;'
+            dados.append({
+                'lote': lote,
+                'data_antes': coalesce(data_antes, 'inserido'),
+                'data_agora': coalesce(data_agora, 'retirado'),
+                '|STYLE': style,
+            })
 
         self.context.update({
-            'headers': ['Bipado em', 'Lote'],
-            'fields': ['data', 'lote'],
-            'data': lotes_versao,
+            'headers': ['Bipado antes', 'Lote', 'Bipado agora'],
+            'fields': ['data_antes', 'lote', 'data_agora'],
+            'data': dados,
         })
 
     def get(self, request, *args, **kwargs):
