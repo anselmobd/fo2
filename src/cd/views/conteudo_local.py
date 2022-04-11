c63c94a024b7726e56979fd6d47a115780969e57
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2022-04-11 17:38:45 -0300

    view ConteudoLocal passando para self.cursor e self.context

--- src/cd/views/conteudo_local.py
+++ src/cd/views/conteudo_local.py
@@ -20,34 +20,36 @@ class ConteudoLocal(View):
     def __init__(self):
         self.Form_class = cd.forms.ConteudoLocalForm
         self.template_name = 'cd/conteudo_local.html'
-        self.title_name = 'Conteúdo'
+        self.context = {'titulo': 'Conteúdo'}
 
-    def add_esvaziamento(self, cursor, codigo, context):
-        dh_esvaziamento = get_esvaziamentos_de_palete(cursor, codigo)
+    def add_esvaziamento(self, codigo):
+        dh_esvaziamento = get_esvaziamentos_de_palete(self.cursor, codigo)
 
         if dh_esvaziamento:
-            context.update({
+            self.context.update({
                 'e_headers': ['Data/hora'],
                 'e_fields': ['dh'],
                 'e_data': dh_esvaziamento,
             })
 
     def mount_context(self, request, form):
-        cursor = db_cursor_so(request)
+        self.cursor = db_cursor_so(request)
 
         codigo = form.cleaned_data['codigo'].upper()
-        context = {'endereco': codigo}
+        self.context.update({
+            'endereco': codigo
+        })
 
-        lotes_end = lotes_em_endereco(cursor, codigo)
+        lotes_end = lotes_em_endereco(self.cursor, codigo)
 
         eh_palete = len(codigo) == 8
 
         if (not lotes_end) or (not lotes_end[0]['lote']):
-            context.update({
+            self.context.update({
                 'erro': 'Nenhum lote no endereço.'})
             if eh_palete:
-                self.add_esvaziamento(cursor, codigo, context)
-            return context
+                self.add_esvaziamento(codigo)
+            return
 
         headers = ["Bipado em", "Lote", "OP"]
         fields = ['data', 'lote', 'op']
@@ -57,7 +59,7 @@ class ConteudoLocal(View):
             for row in lotes_end:
                 if row['endereco']:
                     enderecos.add(row['endereco'])
-            context.update({
+            self.context.update({
                 'endereco_lotes': ', '.join(enderecos),
             })
             if len(enderecos) > 1:
@@ -68,7 +70,7 @@ class ConteudoLocal(View):
             for row in lotes_end:
                 if row['palete']:
                     paletes.add(row['palete'])
-            context.update({
+            self.context.update({
                 'palete_lotes': ', '.join(paletes),
             })
             if len(paletes) > 1:
@@ -97,7 +99,7 @@ class ConteudoLocal(View):
             if not row['endereco']:
                 row['endereco'] = '-'
 
-        context.update({
+        self.context.update({
             'eh_palete': eh_palete,
             'headers': headers,
             'fields': fields,
@@ -105,26 +107,23 @@ class ConteudoLocal(View):
         })
 
         if eh_palete:
-            self.add_esvaziamento(cursor, codigo, context)
+            self.add_esvaziamento(codigo)
 
-        return context
+        return
 
     def get(self, request, *args, **kwargs):
         if 'codigo' in kwargs and kwargs['codigo']:
             return self.post(request, *args, **kwargs)
-        context = {'titulo': self.title_name}
         form = self.Form_class()
-        context['form'] = form
-        return render(request, self.template_name, context)
+        self.context['form'] = form
+        return render(request, self.template_name, self.context)
 
     def post(self, request, *args, **kwargs):
-        context = {'titulo': self.title_name}
         if 'codigo' in kwargs and kwargs['codigo']:
             form = self.Form_class(kwargs)
         else:
             form = self.Form_class(request.POST)
         if form.is_valid():
-            data = self.mount_context(request, form)
-            context.update(data)
-        context['form'] = form
-        return render(request, self.template_name, context)
+            self.mount_context(request, form)
+        self.context['form'] = form
+        return render(request, self.template_name, self.context)
