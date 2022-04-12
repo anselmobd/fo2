b46ac8945ef8c4a5099a959892da840866649a75
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2022-04-11 22:19:26 -0300

    criada lotes_em_estoque que busca lotes em estoque e pode devolver referências em estoque

---
+++ src/cd/queries/grade_cd.py
@@ -0,0 +1,38 @@
+from pprint import pprint
+
+from utils.functions.models import dictlist
+from utils.functions.queries import debug_cursor_execute
+
+
+def lotes_em_estoque(cursor, get='ref'):
+
+    if get == 'ref':
+        distinct = True
+        fields = """
+              l.PROCONF_GRUPO ref
+        """
+    else:
+        distinct = False
+        fields = """
+              l.PERIODO_PRODUCAO per
+            , l.ORDEM_CONFECCAO oc
+            , l.PROCONF_GRUPO ref
+            , l.PROCONF_SUBGRUPO tam
+            , l.PROCONF_ITEM cor
+            , l.ORDEM_PRODUCAO op
+            , l.QTDE_PECAS_PROG prog
+            , l.QTDE_DISPONIVEL_BAIXA disp
+        """
+
+    sql = f"""
+        SELECT {"DISTINCT" if distinct else ""}
+        {fields}
+        FROM ENDR_014 lp
+        JOIN PCPC_040 l
+          ON l.ORDEM_PRODUCAO = lp.ORDEM_PRODUCAO 
+         AND l.ORDEM_CONFECCAO = MOD(lp.ORDEM_CONFECCAO, 100000)
+        WHERE l.CODIGO_ESTAGIO = 63
+          AND l.QTDE_DISPONIVEL_BAIXA > 0
+    """
+    debug_cursor_execute(cursor, sql)
+    return dictlist(cursor)
