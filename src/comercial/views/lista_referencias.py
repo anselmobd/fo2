3c9f381c854277f3a23801c888798d34ac96ea9d
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2022-04-13 11:01:52 -0300

    lista referências com códigos de barras no sistema antigo

--- src/comercial/views/lista_referencias.py
+++ src/comercial/views/lista_referencias.py
@@ -1,12 +1,13 @@
+import operator
+import tempfile
+from dbfread import DBF
+from smb.SMBConnection import SMBConnection
 from pprint import pprint
 
 from django.shortcuts import render
-from django.urls import reverse
 from django.views import View
 
-
-import comercial.forms as forms
-import comercial.queries as queries
+from utils.functions.strings import only_digits
 
 
 class ListaReferencias(View):
@@ -17,7 +18,44 @@ class ListaReferencias(View):
         self.context = {'titulo': 'Lista referências'}
 
     def mount_context(self):
-        pass
+
+        # There will be some mechanism to capture userID, password, client_machine_name, server_name and server_ip
+        # client_machine_name can be an arbitary ASCII string
+        # server_name should match the remote machine name, or else the connection will be rejected
+        conn = SMBConnection('', '', 'localhost', 'arquivos.t', use_ntlm_v2 = True)
+        assert conn.connect('arquivos.t', 139)
+
+        file_obj = tempfile.NamedTemporaryFile('wb', delete=False)
+        file_attributes, filesize = conn.retrieveFile('teg', '/Tussor/bd/DIS_BAR.DBF', file_obj)
+        # Retrieved file contents are inside file_obj
+        # Do what you need with the file_obj and then close it
+        # Note that the file obj is positioned at the end-of-file,
+        # so you might need to perform a file_obj.seek() if you need
+        # to read from the beginning
+        file_obj.close()
+
+        refs = set()
+        with DBF(file_obj.name) as bars:
+            print('DBF')
+            for bar in bars:
+                if bar['B_DESLIG'] is None:
+                    refs.add(bar['B_PROD'])
+
+        dados = []
+        for ref in refs:
+            dados.append({
+                'modelo': int(only_digits(ref)),
+                'ref': ref,
+            })
+        
+        dados.sort(key=operator.itemgetter('modelo', 'ref'))
+
+        self.context.update({
+            'headers': ['Modelo', 'Referência sistema antigo'],
+            'fields': ['modelo', 'ref'],
+            'data': dados,
+        })
 
     def get(self, request, *args, **kwargs):
+        self.mount_context()
         return render(request, self.template_name, self.context)
