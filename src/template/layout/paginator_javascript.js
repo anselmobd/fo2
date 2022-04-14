5fbeb596f8ec7d74b492d7610c7ab47420478522
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2022-04-14 10:47:15 -0300

    passando o nome do campo input que controla a página

--- src/template/layout/paginator_javascript.js
+++ src/template/layout/paginator_javascript.js
@@ -1,9 +1,9 @@
 function o2SetPageAndSubmit(page) {
-  document.getElementById('id_page').value = page;
+  document.getElementById('id_{{field}}').value = page;
   document.{{form}}.submit();
 }
 
 function o2SetPage1AndSubmit() {
-  document.getElementById('id_page').value = '1';
+  document.getElementById('id_{{field}}').value = '1';
   document.{{form}}.submit();
 }
