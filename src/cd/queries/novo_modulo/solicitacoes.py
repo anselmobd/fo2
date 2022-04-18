aa124e8f93f30619a500fe9e262ed51c5867d66c
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2022-04-18 17:17:53 -0300

    view Solicitacoes evoluindo

--- src/cd/queries/novo_modulo/solicitacoes.py
+++ src/cd/queries/novo_modulo/solicitacoes.py
@@ -8,8 +8,20 @@ def get_solicitacoes(cursor):
     sql = f"""
         SELECT DISTINCT
           sl.SOLICITACAO 
+        , sum(CASE WHEN sl.SITUACAO = 1 THEN 1 ELSE 0 END) lotes_1
+        , sum(CASE WHEN sl.SITUACAO = 1 THEN sl.QTDE ELSE 0 END) qtde_1
+        , sum(CASE WHEN sl.SITUACAO = 2 THEN 1 ELSE 0 END) lotes_2
+        , sum(CASE WHEN sl.SITUACAO = 2 THEN sl.QTDE ELSE 0 END) qtde_2
+        , sum(CASE WHEN sl.SITUACAO = 3 THEN 1 ELSE 0 END) lotes_3
+        , sum(CASE WHEN sl.SITUACAO = 3 THEN sl.QTDE ELSE 0 END) qtde_3
+        , sum(CASE WHEN sl.SITUACAO = 4 THEN 1 ELSE 0 END) lotes_4
+        , sum(CASE WHEN sl.SITUACAO = 4 THEN sl.QTDE ELSE 0 END) qtde_4
+        , sum(CASE WHEN sl.SITUACAO = 5 THEN 1 ELSE 0 END) lotes_5
+        , sum(CASE WHEN sl.SITUACAO = 5 THEN sl.QTDE ELSE 0 END) qtde_5
         FROM pcpc_044 sl -- solicitação / lote 
         WHERE sl.SOLICITACAO IS NOT NULL 
+        GROUP BY 
+          sl.SOLICITACAO
         ORDER BY 
           sl.SOLICITACAO 
     """
--- src/cd/views/novo_modulo/solicitacoes.py
+++ src/cd/views/novo_modulo/solicitacoes.py
@@ -1,9 +1,11 @@
 from pprint import pprint
 
+from fo2.connections import db_cursor_so
+
 from base.paginator import paginator_basic
 from base.views import O2BaseGetView
-
-from fo2.connections import db_cursor_so
+from utils.functions import untuple_keys_concat
+from utils.views import totalize_data
 
 from cd.queries.novo_modulo.solicitacoes import get_solicitacoes
 
@@ -21,14 +23,55 @@ class Solicitacoes(O2BaseGetView):
 
         data = get_solicitacoes(cursor)
 
+        totalize_data(data, {
+            'sum': [
+                'lotes_1',
+                'qtde_1',
+                'lotes_2',
+                'qtde_2',
+                'lotes_3',
+                'qtde_3',
+                'lotes_4',
+                'qtde_4',
+                'lotes_5',
+                'qtde_5',
+            ],
+            'count': [],
+            'descr': {'solicitacao': 'Totais:'},
+            'row_style': 'font-weight: bold;',
+        })
+
         data = paginator_basic(data, 100, page)
 
         self.context.update({
             'headers': [
                 'Solicitação',
+                'Sit.1 Lotes',
+                'Qtd.',
+                'Sit.2 Lotes',
+                'Qtd.',
+                'Sit.3 Lotes',
+                'Qtd.',
+                'Sit.4 Lotes',
+                'Qtd.',
+                'Sit.5 Lotes',
+                'Qtd.',
             ],
             'fields': [
                 'solicitacao',
+                'lotes_1',
+                'qtde_1',
+                'lotes_2',
+                'qtde_2',
+                'lotes_3',
+                'qtde_3',
+                'lotes_4',
+                'qtde_4',
+                'lotes_5',
+                'qtde_5',
             ],
+            'style': untuple_keys_concat({
+                tuple(range(2, 12)): 'text-align: right;',
+            }),
             'data': data,
         })
