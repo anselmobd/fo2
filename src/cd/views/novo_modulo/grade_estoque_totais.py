fa39736d877841b463fbb22a2132ebb3b630ffc2
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2022-04-20 17:52:06 -0300

    view GradeEstoqueTotais começando a apresentar grade de inventario

--- src/cd/views/novo_modulo/grade_estoque_totais.py
+++ src/cd/views/novo_modulo/grade_estoque_totais.py
@@ -25,36 +25,58 @@ class GradeEstoqueTotais(PermissionRequiredMixin, View):
         self.template_name = 'cd/novo_modulo/grade_estoque_totais.html'
 
     def mount_context(self):
-        referencias = lotes_em_estoque(self.cursor, get='ref')
+        referencias = lotes_em_estoque(self.cursor, get='ref', ref=['B0001', 'B0002'])
 
         referencias = sorted(referencias, key=itemgetter('modelo', 'ref'))
 
-        referencias = paginator_basic(referencias, 1, self.page)
+        referencias = paginator_basic(referencias, 2, self.page)
 
-        inventario = lotes_em_estoque(self.cursor, tipo='i')
-        pedido = lotes_em_estoque(self.cursor, tipo='p')
-        solicitado = lotes_em_estoque(self.cursor, tipo='s')
+        inventario = lotes_em_estoque(self.cursor, tipo='i', ref=['B0001', 'B0002'])
+        pedido = lotes_em_estoque(self.cursor, tipo='p', ref=['B0001', 'B0002'])
+        solicitado = lotes_em_estoque(self.cursor, tipo='s', ref=['B0001', 'B0002'])
 
         grades = []
         for row_ref in referencias.object_list:
-            referencia = row_ref['modelo']
+            referencia = row_ref['ref']
 
-            inventario_ref = filter(lambda x: x['ref'] == referencia, inventario)
-            pprint(inventario_ref)
+            inventario_ref = list(
+                filter(
+                    lambda x: x['ref'] == referencia,
+                    inventario,
+                )
+            )
+            # pprint(inventario_ref)
+
+            grade_invent_ref = dados_to_grade(
+                inventario_ref,
+                field_linha='cor',
+                field_coluna='tam',
+                facade_coluna='Tamanho',
+                field_ordem_coluna='ordem_tam',
+                field_quantidade='qtd',
+            )
+
+            grade_invent_ref.update({
+                'referencia': referencia,
+            })
+            # pprint(grade_invent_ref)
+
             grade_ref = {
-                'ref': row_ref['ref'],
-                'inventario': inventario_ref,
-                }
+                'inventario': grade_invent_ref,
+                'ref': referencia,
+                'refnum': row_ref['modelo'],
+            }
             grades.append(grade_ref)
 
         headers = ["Referência", "Modelo"]
         fields = ['ref', 'modelo']
         self.context.update({
-            'headers': headers,
-            'fields': fields,
-            'data': referencias,
+            # 'headers': headers,
+            # 'fields': fields,
+            # 'data': referencias,
             'grades': grades,
         })
+        pprint(self.context)
 
     def get(self, request, *args, **kwargs):
         self.cursor = db_cursor_so(request)
