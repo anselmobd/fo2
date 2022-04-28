8aab350bf747e2b3ace8ef7fd30d6f0c64ba2bfc
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2022-04-28 15:02:57 -0300

    view PreparaPedidoCompraMatriz; tirando rotina de criaçao de pedido de compra do método process para uma função externa; Adicionando tratamento de erro devolvendo boolean de sucesso

--- src/lotes/views/prepara_pedido_compra_matriz.py
+++ src/lotes/views/prepara_pedido_compra_matriz.py
@@ -12,30 +12,15 @@ from lotes.queries.pedido.ped_alter import (
     inclui_pedido_compra_matriz_capa,
     emite_pedido_compra_matriz,
     inclui_pedido_compra_matriz_itens,
+    exclui_pedido_compra_matriz_itens,
     get_pedido_compra_matriz_itens,
     pedido_matriz_de_pedido_filial,
 )
 
 
-class PreparaPedidoCompraMatriz(View):
-
-    def process(self, request, kwargs):
-        cursor = db_cursor_so(request)
-
-        pedido_filial = kwargs['pedido_filial']
-
-        dados = lotes.queries.pedido.ped_inform(cursor, pedido_filial, empresa=3)
-        if not dados:
-            return ('ERRO', "Pedido não encontrado!")
-        if dados[0]['COD_CANCELAMENTO'] != 0:
-            return ('ERRO', "Pedido cancelado!")
-        if (
-            (not dados[0]['OBSERVACAO'])
-            or (not re.search('^\[MPCFM\] ', dados[0]['OBSERVACAO']))
-        ):
-            return ('ERRO', "Não é pedido preparado para faturar filial!")
-
-
+def cria_pedido_compra_matriz(cursor, pedido_filial):
+    pedido_compra = None
+    try:
         pedido_compra_matriz = pedido_matriz_de_pedido_filial(cursor, pedido_filial)
         if pedido_compra_matriz:
             pedido_compra = pedido_compra_matriz[0]['pedido_compra']
@@ -56,7 +41,50 @@ class PreparaPedidoCompraMatriz(View):
 
             emite_pedido_compra_matriz(cursor, pedido_compra)
 
-        return ('OK', "OK!")
+        return True
+
+    except Exception:
+        if pedido_compra:
+            try:
+                emite_pedido_compra_matriz(cursor, pedido_compra, False)
+            except Exception:
+                pass
+
+            try:
+                exclui_pedido_compra_matriz_itens(cursor, pedido_compra)
+            except Exception:
+                pass
+
+            try:
+                exclui_pedido_compra_matriz_capa(cursor, pedido_compra)
+            except Exception:
+                pass
+
+        return False
+
+class PreparaPedidoCompraMatriz(View):
+
+    def process(self, request, kwargs):
+        cursor = db_cursor_so(request)
+
+        pedido_filial = kwargs['pedido_filial']
+
+        dados = lotes.queries.pedido.ped_inform(cursor, pedido_filial, empresa=3)
+        if not dados:
+            return ('ERRO', "Pedido não encontrado!")
+        if dados[0]['COD_CANCELAMENTO'] != 0:
+            return ('ERRO', "Pedido cancelado!")
+        if (
+            (not dados[0]['OBSERVACAO'])
+            or (not re.search('^\[MPCFM\] ', dados[0]['OBSERVACAO']))
+        ):
+            return ('ERRO', "Não é pedido preparado para faturar filial!")
+
+        if cria_pedido_compra_matriz(
+                cursor, pedido_filial):
+            return ('OK', "OK!")
+        else:
+            return ('ERRO', "Algum erro ocorreu durante a criação do pedido de compra!")
 
     def response(self, result):
         status, message = result
