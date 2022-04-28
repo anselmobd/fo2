1189d40d8bbae8a185292ec802a5ee8de77fb9ee
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2022-04-28 15:28:50 -0300

    view RomaneioCorte: quanto tem pedido filial e não tem matriz, mostra link

--- src/lotes/views/romaneio_corte.py
+++ src/lotes/views/romaneio_corte.py
@@ -94,6 +94,14 @@ class RomaneioCorte(O2BaseGetPostView):
                         'producao:pedido__get',
                         args=[row['pedido_filial']],
                     )
+                if row['pedido_matriz'] == '+':
+                    row['pedido_matriz'] = ""
+                    row['pedido_matriz|GLYPHICON'] = 'glyphicon-plus-sign'
+                    row['pedido_matriz|TARGET'] = '_blank'
+                    row['pedido_matriz|LINK'] = reverse(
+                        'producao:prepara_pedido_compra_matriz',
+                        args=[row['pedido_filial']],
+                    )
 
             group = ['cliente', 'pedido_filial', 'pedido_matriz', 'obs']
             sum_fields = ['mov_qtd']
