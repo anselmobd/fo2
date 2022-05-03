b3a952ee96dc0df7fcca99d4ee5bca9146a5d889
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2022-05-03 16:41:49 -0300

    pedidos_gerados query começando a funcionar

--- src/lotes/queries/producao/romaneio_corte/pedidos_gerados.py
+++ src/lotes/queries/producao/romaneio_corte/pedidos_gerados.py
@@ -3,23 +3,43 @@ from pprint import pprint
 from utils.functions.models import rows_to_dict_list_lower
 from utils.functions.queries import debug_cursor_execute
 
+from lotes.queries.pedido.ped_alter import pedidos_filial_na_data
+
 
 def query(cursor, data):
-    sql = f'''
-    '''
-    debug_cursor_execute(cursor, sql)
-    dados = rows_to_dict_list_lower(cursor)
-    clientes = {}
-    for row in dados:
-        if row['cliente'] not in clientes:
-            clientes[row['cliente_slug']] = {
-                'cliente': row['cliente'],
-                'pedidos': {}
-            }
-            cliaux = clientes[row['cliente_slug']]['pedidos']
-            if row['ped_cli'] not in cliaux:
-                cliaux[row['ped_cli']] = {row['op']}
-            else:
-                cliaux[row['ped_cli']].add(row['op'])
+    clientes = pedidos_filial_na_data(cursor, data)
 
-    return dados, clientes
+    dados = []
+    for cliente in clientes:
+        for nf_ped in clientes[cliente]:
+            sql = f'''
+                select
+                  '{cliente}' cliente
+                , '*{nf_ped['ped']}' pedido_filial
+                , '-' pedido_matriz
+                , pv.OBSERVACAO obs
+                , pvi.CD_IT_PE_NIVEL99 ||'.'|| pvi.CD_IT_PE_GRUPO ||'.'|| pvi.CD_IT_PE_SUBGRUPO  ||'.'|| pvi.CD_IT_PE_ITEM item
+                , 1 mov_qt
+                , 1 op
+                , pvi.QTDE_PEDIDA mov_qtd
+                from PEDI_100 pv
+                join PEDI_110 pvi
+                  on pvi.PEDIDO_VENDA = pv.PEDIDO_VENDA
+                where 1=1
+                  and pv.PEDIDO_VENDA = {nf_ped['ped']}
+                order by
+                  5
+            '''
+            debug_cursor_execute(cursor, sql)
+            dados_cliente = rows_to_dict_list_lower(cursor)
+            for row in dados_cliente:
+                row['cliente'] = row['cliente'].upper()
+                row['mov_qtd'] = round(row['mov_qtd'])
+                obs_parts = row['obs'].split(':')
+                if len(obs_parts) > 2:
+                    row['obs'] = obs_parts[2].strip()
+                else:
+                    row['obs'] = '-'
+                dados.append(row)
+
+    return dados
