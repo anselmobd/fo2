425cfafe5a4c4881aa6ab90abd2e3d688f2fa8b0
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2022-05-06 16:57:38 -0300

    query grade_estoque trocando sql_em_estoque por SqlEmEstoque

--- src/cd/queries/novo_modulo/grade_cd.py
+++ src/cd/queries/novo_modulo/grade_cd.py
@@ -2,17 +2,24 @@ from pprint import pprint
 
 from utils.functions.models import GradeQtd
 
-from cd.queries.novo_modulo.lotes import sql_em_estoque
+from cd.queries.novo_modulo.lotes import SqlEmEstoque
 
 
 def grade_estoque(cursor, tipo=None, ref=None):
     grade = GradeQtd(cursor, case='lower')
 
     if tipo in ['i', 's', 'p']:
-        sql_base = sql_em_estoque(tipo=tipo, ref=ref, get='lote_qtd')
+        # sql_base = sql_em_estoque(tipo=tipo, ref=ref, get='lote_qtd')
+        sql_em_stq = SqlEmEstoque(tipo=tipo, ref=ref, get='lote_qtd')
+        sql_base = sql_em_stq.sql()
     if tipo == 'sp':
-        sql_base_s = sql_em_estoque(tipo='s', ref=ref, get='lote_qtd')
-        sql_base_p = sql_em_estoque(tipo='p', ref=ref, get='lote_qtd')
+        # sql_base_s = sql_em_estoque(tipo='s', ref=ref, get='lote_qtd')
+        sql_em_stq = SqlEmEstoque(tipo='s', ref=ref, get='lote_qtd')
+        sql_base_s = sql_em_stq.sql()
+
+        # sql_base_p = sql_em_estoque(tipo='p', ref=ref, get='lote_qtd')
+        sql_em_stq = SqlEmEstoque(tipo='p', ref=ref, get='lote_qtd')
+        sql_base_p = sql_em_stq.sql()
         sql_base = f"""--
             (
                 {sql_base_s}
@@ -21,9 +28,18 @@ def grade_estoque(cursor, tipo=None, ref=None):
             )
         """
     if tipo == 'i-sp':
-        sql_base_i = sql_em_estoque(tipo='i', ref=ref, get='lote_qtd')
-        sql_base_s = sql_em_estoque(tipo='s', ref=ref, sinal="-", get='lote_qtd')
-        sql_base_p = sql_em_estoque(tipo='p', ref=ref, sinal="-", get='lote_qtd')
+        # sql_base_i = sql_em_estoque(tipo='i', ref=ref, get='lote_qtd')
+        sql_em_stq = SqlEmEstoque(tipo='i', ref=ref, get='lote_qtd')
+        sql_base_i = sql_em_stq.sql()
+
+        # sql_base_s = sql_em_estoque(tipo='s', ref=ref, sinal="-", get='lote_qtd')
+        sql_em_stq = SqlEmEstoque(tipo='s', ref=ref, sinal="-", get='lote_qtd')
+        sql_base_s = sql_em_stq.sql()
+
+        # sql_base_p = sql_em_estoque(tipo='p', ref=ref, sinal="-", get='lote_qtd')
+        sql_em_stq = SqlEmEstoque(tipo='p', ref=ref, sinal="-", get='lote_qtd')
+        sql_base_p = sql_em_stq.sql()
+
         sql_base = f"""--
             (
                 {sql_base_i}
