351e9e6081c4025f5d7bccd7a7af06c839480e31
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2022-05-06 15:25:20 -0300

    criadas classes LotesEmEstoque e SqlEmEstoque

--- src/cd/queries/novo_modulo/lotes.py
+++ src/cd/queries/novo_modulo/lotes.py
@@ -5,6 +5,224 @@ from utils.functions.queries import debug_cursor_execute
 from utils.functions.strings import only_digits
 
 
+class LotesEmEstoque():
+
+    def __init__(self, cursor, tipo=None, ref=None, get=None, colecao=None, modelo=None, fields_tuple=None):
+        self.cursor = cursor
+        self.tipo = tipo
+        self.ref = ref
+        self.get = get
+        self.colecao = colecao
+        self.modelo = modelo
+        self.fields_tuple = fields_tuple
+
+        self.sql_em_stq = SqlEmEstoque(
+            tipo=self.tipo,
+            ref=self.ref,
+            get=self.get,
+            colecao=self.colecao,
+            fields_tuple=self.fields_tuple,
+        )
+
+    def dados(self):
+        cursor = self.cursor
+        modelo = self.modelo
+
+        sql = self.sql_em_stq.sql()
+
+        debug_cursor_execute(cursor, sql)
+        dados = dictlist(cursor)
+
+        if modelo:
+            dados_modelo = []
+        for row in dados:
+            try:
+                ref_modelo = int(only_digits(row['ref']))
+            except ValueError:
+                ref_modelo = 0
+            row['modelo'] = ref_modelo
+            if ref_modelo == modelo:
+                dados_modelo.append(row)
+        if modelo:
+            return dados_modelo
+        else:
+            return dados
+
+
+
+class SqlEmEstoque():
+    """Monta SQL base de selecão de lotes
+    - endereçados; e
+    - no estágio 63
+    Recebe:
+    tipo
+        i = inventário: todos os lotes endereçados e com quantidade no estágio 63
+        p = lotes que aparece no inventário (opção acima) e são de OPs de Pedidos
+        s = solicitado (situação 2, 3 ou 4) de OP com estágio 63
+        iq = inventário: todos os lotes endereçados e com quantidade em qq estágio
+        None = todos os lotes
+    ref: filtro de referências
+        None = não filtra
+        {condição}string = uma referência
+        condição: altera o funcionamento do filtro
+            '' = igualdade
+            '<' = menor que
+            '>' = maior que
+        list = uma lista de referências
+    get
+        ref = busca apenas o campo referência (com distinct)
+        None = busca apenas o campo referência (sem distinct)
+        lote_qtd = busca op, lote, item e quantidades
+        # loc = busca op, lote, item, quantidades e informação de endereçamento
+    sinal: positivo ou negativo
+        '+' = quantidades como estão no banco de dados
+        '-' = inverte o sinal das quantidades       
+    fields_tuple
+        Lista de fields que devem ser retornados
+        Field pode ter alias a ser utilizado. Ex.: "qtd quanti"
+        obs.: ref sempre é retornado
+    """
+
+    def __init__(self, tipo=None, ref=None, get=None, colecao=None, sinal='+', fields_tuple=None):
+        self.tipo = tipo
+        self.ref = ref
+        self.get = get
+        self.colecao = colecao
+        self.sinal = sinal
+        self.fields_tuple = fields_tuple
+
+    def sql(self):
+        tipo = self.tipo
+        ref = self.ref
+        get = self.get
+        colecao = self.colecao
+        sinal = self.sinal
+        fields_tuple = self.fields_tuple
+
+        if ref is None:
+            filter_ref = ''
+        elif isinstance(ref, str):
+            if ref[0] in ['<', '>']:
+                condicao = ref[0]
+                ref = ref[1:]
+            else:
+                condicao = '='
+            filter_ref = f"and l.PROCONF_GRUPO {condicao} '{ref}'"
+        else:
+            refs = ', '.join([f"'{r}'" for r in ref])
+            filter_ref = f"and l.PROCONF_GRUPO in ({refs})"
+
+        join_para_colecao = ""
+        filter_colecao = ""
+        if colecao is not None:
+            join_para_colecao = """
+                JOIN BASI_030 r
+                ON r.NIVEL_ESTRUTURA = 1
+                AND r.REFERENCIA = l.PROCONF_GRUPO 
+            """
+            filter_colecao = f"AND r.COLECAO = '{colecao}'"
+
+        fields_tuple = fields_tuple if fields_tuple else []
+        fields_set = {'ref'}.union(fields_tuple)
+
+        available_fields = {
+            'ref': "l.PROCONF_GRUPO",
+            'per': "l.PERIODO_PRODUCAO",
+            'oc': "l.ORDEM_CONFECCAO",
+            'tam': "l.PROCONF_SUBGRUPO",
+            'ordem_tam': "tam.ORDEM_TAMANHO",
+            'cor': "l.PROCONF_ITEM",
+            'op': "l.ORDEM_PRODUCAO",
+            'qtd_prog': "l.QTDE_PECAS_PROG",
+            'qtd_dbaixa': "l.QTDE_DISPONIVEL_BAIXA",
+        }
+
+        distinct = False
+        if get == 'ref':
+            distinct = True
+        elif get == 'lote_qtd':
+            fields_set = fields_set.union([
+                'per',
+                'oc',
+                'tam',
+                'ordem_tam',
+                'cor',
+                'op',
+                'qtd_prog',
+                'qtd_dbaixa',
+            ])
+
+        filtra_estagio = "AND l.CODIGO_ESTAGIO = 63"
+        if tipo == 'p':
+            field_qtd = f"""--
+                , {sinal}l.QTDE_DISPONIVEL_BAIXA qtd
+            """
+            tipo_join = """--
+                JOIN PCPC_020 op
+                ON op.ORDEM_PRODUCAO = lp.ORDEM_PRODUCAO
+            """
+            tipo_filter = """--
+                AND op.PEDIDO_VENDA <> 0
+                AND l.QTDE_DISPONIVEL_BAIXA > 0
+            """
+        elif tipo == 's':
+            field_qtd = f"""--
+                , {sinal}sl.QTDE qtd
+            """
+            tipo_join = """--
+                JOIN PCPC_044 sl
+                ON sl.ORDEM_PRODUCAO = lp.ORDEM_PRODUCAO
+                AND sl.ORDEM_CONFECCAO = MOD(lp.ORDEM_CONFECCAO, 100000) 
+            """
+            tipo_filter = """--
+                AND sl.SITUACAO IN (2, 3, 4)
+            """
+        elif tipo and tipo.startswith('i'):
+            field_qtd = f"""--
+                , {sinal}l.QTDE_DISPONIVEL_BAIXA qtd
+            """
+            tipo_join = ""
+            tipo_filter = """--
+                AND l.QTDE_DISPONIVEL_BAIXA > 0
+            """
+            if 'q' in tipo:
+                filtra_estagio = ''
+        else:
+            field_qtd = ""
+            tipo_join = ""
+            tipo_filter = """--
+                AND l.QTDE_DISPONIVEL_BAIXA > 0
+            """
+
+        fields = "\n, ".join(
+            [
+                f"{available_fields[field.split()[0]]} {field.split()[-1]}"
+                for field in fields_set
+            ]
+        )
+
+        sql = f"""
+            SELECT {"DISTINCT" if distinct else ""}
+            {fields} -- fields
+            {field_qtd} -- field_qtd
+            FROM ENDR_014 lp
+            JOIN PCPC_040 l
+            ON l.ORDEM_PRODUCAO = lp.ORDEM_PRODUCAO 
+            AND l.ORDEM_CONFECCAO = MOD(lp.ORDEM_CONFECCAO, 100000)
+            {tipo_join} -- tipo_join
+            {join_para_colecao} -- join_para_colecao
+            LEFT JOIN BASI_220 tam -- cadastro de tamanhos
+            ON tam.TAMANHO_REF = l.PROCONF_SUBGRUPO
+            WHERE 1=1
+            {filtra_estagio} -- filtra_estagio
+            {tipo_filter} -- tipo_filter
+            {filter_ref} -- filter_ref
+            {filter_colecao} -- filter_colecao
+        """
+        return sql
+
+
+
 def sql_em_estoque(tipo=None, ref=None, get=None, colecao=None, sinal='+', field_list=None):
     """Monta SQL base de selecão de lotes
     - endereçados; e
