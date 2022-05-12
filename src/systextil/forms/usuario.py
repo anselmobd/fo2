698240cbfa2c7b5f769c43d7267842b47b8fd89a
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2022-05-12 15:16:33 -0300

    view ZeraSenha pedindo empresa

--- src/systextil/forms/usuario.py
+++ src/systextil/forms/usuario.py
@@ -1,11 +1,29 @@
 from django import forms
 
+from systextil.models.base import Empresa
+
 __all__=['ZeraSenhaForm']
 
 
 class ZeraSenhaForm(forms.Form):
+    empresa = forms.ChoiceField(
+        required=True, initial=None)
     login = forms.CharField(
+        label="Usuário (login)",
         min_length=1,
         max_length=20,
         widget=forms.TextInput()
     )
+
+    def __init__(self, *args, **kwargs):
+        self.request = kwargs.get('request', None)
+        super(ZeraSenhaForm, self).__init__(*args, **kwargs)
+
+        CHOICES = []
+        empresas = Empresa.objects.all().order_by('codigo_empresa')
+        for empresa in empresas:
+            CHOICES.append((
+                empresa.codigo_empresa,
+                f"{empresa.codigo_empresa}-{empresa.nome_fantasia}",
+            ))
+        self.fields['empresa'].choices = CHOICES
--- src/systextil/views/usuario/zera_senha.py
+++ src/systextil/views/usuario/zera_senha.py
@@ -1,13 +1,14 @@
 from pprint import pprint
 
 from django.contrib.auth.mixins import PermissionRequiredMixin
-from django.shortcuts import render
 
 from fo2.connections import db_cursor_so
 
 from base.views import O2BaseGetPostView
 
+from systextil.models.base import Usuario
 from systextil.forms.usuario import ZeraSenhaForm
+
 
 
 class ZeraSenha(PermissionRequiredMixin, O2BaseGetPostView):
@@ -22,3 +23,16 @@ class ZeraSenha(PermissionRequiredMixin, O2BaseGetPostView):
 
     def mount_context(self):
         cursor = db_cursor_so(self.request)
+
+        try:
+            usuario = Usuario.objects_all.get(
+                usuario=self.login, empresa=self.empresa)
+        except Usuario.DoesNotExist:
+            self.context.update({
+                'msg': 'Usuário inexistente',
+            })
+            return
+
+        self.context.update({
+            'msg': f"Confirma zerar senha de {usuario}",
+        })
