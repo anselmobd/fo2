b8d0c85828dde12fcf778efb2a356f3f57cfca87
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2022-05-12 18:00:08 -0300

    view CorrigeSequenciamento convertida para O2BaseGetPostView

--- src/lotes/views/ops/corrige_seq.py
+++ src/lotes/views/ops/corrige_seq.py
@@ -1,31 +1,32 @@
 from pprint import pprint
 
 from django.contrib.auth.mixins import PermissionRequiredMixin
-from django.shortcuts import render
-from django.views import View
 
 from fo2.connections import db_cursor_so
+
+from base.views import O2BaseGetPostView
 
 import lotes.forms as forms
 import lotes.queries.lote
 import lotes.queries.op
 
 
-class CorrigeSequenciamento(PermissionRequiredMixin, View):
+class CorrigeSequenciamento(PermissionRequiredMixin, O2BaseGetPostView):
 
     def __init__(self):
+        super(CorrigeSequenciamento, self).__init__()
         self.permission_required = 'lotes.can_repair_seq_op'
         self.Form_class = forms.OpForm
         self.template_name = 'lotes/corrige_sequenciamento.html'
         self.title_name = 'Corrige sequenciamento de lotes de OP'
 
-    def mount_context(self, request):
+    def mount_context(self):
         op = self.form.cleaned_data['op']
         self.context.update({
             'op': op
         })
 
-        cursor = db_cursor_so(request)
+        cursor = db_cursor_so(self.request)
 
         data = lotes.queries.lote.get_lotes(cursor, op=op, order='o')
         if len(data) == 0:
@@ -60,22 +61,22 @@ class CorrigeSequenciamento(PermissionRequiredMixin, View):
             'data': data,
         })
 
-    def start(self, request):
-        self.request = request
-        self.context = {'titulo': self.title_name}
+    # def start(self, request):
+    #     self.request = request
+    #     self.context = {'titulo': self.title_name}
 
-    def end(self):
-        self.context['form'] = self.form
-        return render(self.request, self.template_name, self.context)
+    # def end(self):
+    #     self.context['form'] = self.form
+    #     return render(self.request, self.template_name, self.context)
 
-    def get(self, request, *args, **kwargs):
-        self.start(request)
-        self.form = self.Form_class()
-        return self.end()
+    # def get(self, request, *args, **kwargs):
+    #     self.start(request)
+    #     self.form = self.Form_class()
+    #     return self.end()
 
-    def post(self, request, *args, **kwargs):
-        self.start(request)
-        self.form = self.Form_class(self.request.POST)
-        if self.form.is_valid():
-            self.mount_context(request)
-        return self.end()
+    # def post(self, request, *args, **kwargs):
+    #     self.start(request)
+    #     self.form = self.Form_class(self.request.POST)
+    #     if self.form.is_valid():
+    #         self.mount_context()
+    #     return self.end()
