5e071f15f69cad950e8141d99f94830935eeec36
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2022-05-13 15:57:31 -0300

    seq_erro.view melhorada com links

--- src/lotes/views/ops/seq_erro.py
+++ src/lotes/views/ops/seq_erro.py
@@ -1,6 +1,7 @@
 from pprint import pprint
 
 from django.shortcuts import render
+from django.urls import reverse
 
 from fo2.connections import db_cursor_so
 
@@ -10,6 +11,18 @@ from lotes.queries.op import seq_erro
 def view(request):
     cursor = db_cursor_so(request)
     data = seq_erro.lista_op(cursor)
+
+    for row in data:
+        row['op|TARGET'] = '_blank'
+        row['op|LINK'] = reverse(
+            'producao:op__get',
+            args=[row['op']],
+        )
+        row['ref|TARGET'] = '_blank'
+        row['ref|LINK'] = reverse(
+            'produto:ref__get',
+            args=[row['ref']],
+        )
 
     context = {
         'titulo': 'OP com erro de sequência',
