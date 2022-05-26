ec463a5b4208492f759e6c01beb7c5cc830d9036
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2022-05-26 19:24:00 -0300

    model InventarioLoteprimeira versão

--- src/lotes/models/__init__.py
+++ src/lotes/models/__init__.py
@@ -1,4 +1,5 @@
 from .impresso import *
+from .inventario import *
 from .op import *
 from .parametro import *
 from .permissions import *
--- src/lotes/models/inventario.py
+++ src/lotes/models/inventario.py
@@ -5,42 +5,42 @@ from django.db import models
 from django.utils import timezone
 
 
-# class InventarioLote(models.Model):
-#     lote = models.CharField(
-#         db_index=True,
-#         max_length=9,
-#     )
-#     quantidade = models.IntegerField(
-#         default=0,
-#     )
-#     usuario = models.ForeignKey(
-#         User,
-#         on_delete=models.PROTECT,
-#         verbose_name='informado por',
-#     )
-#     quando = models.DateTimeField(
-#         verbose_name='informado em',
-#     )
-#     # versao = models.IntegerField(
-#     #     default=0,
-#     #     verbose_name='versão')
-#     # corrigido_por = models.ForeignKey(
-#     #     User,
-#     #     on_delete=models.PROTECT,
-#     #     null=True,
-#     #     blank=True,
-#     #     verbose_name='corrigido por',
-#     # )
-#     # corrigido_inicio = models.DateTimeField(
-#     #     null=True,
-#     #     blank=True,
-#     #     verbose_name='início da correção',
-#     # )
-#     # corrigido_fim = models.DateTimeField(
-#     #     null=True,
-#     #     blank=True,
-#     #     verbose_name='fim da correção',
-#     # )
+class InventarioLote(models.Model):
+    lote = models.CharField(
+        db_index=True,
+        max_length=9,
+    )
+    quantidade = models.IntegerField(
+        default=0,
+    )
+    usuario = models.ForeignKey(
+        User,
+        on_delete=models.PROTECT,
+        verbose_name='informado por',
+    )
+    quando = models.DateTimeField(
+        verbose_name='informado em',
+    )
+    # versao = models.IntegerField(
+    #     default=0,
+    #     verbose_name='versão')
+    # corrigido_por = models.ForeignKey(
+    #     User,
+    #     on_delete=models.PROTECT,
+    #     null=True,
+    #     blank=True,
+    #     verbose_name='corrigido por',
+    # )
+    # corrigido_inicio = models.DateTimeField(
+    #     null=True,
+    #     blank=True,
+    #     verbose_name='início da correção',
+    # )
+    # corrigido_fim = models.DateTimeField(
+    #     null=True,
+    #     blank=True,
+    #     verbose_name='fim da correção',
+    # )
 
 #     def save(self, *args, **kwargs):
 #         # # se tem "id" é alteração
