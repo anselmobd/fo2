35248eda79efea42c251973d0217e21eab8ca038
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2022-05-30 16:48:21 -0300

    estava atrapalhando o makemigrations

--- src/fo2/settings.py
+++ src/fo2/settings.py
@@ -191,12 +191,12 @@ DATABASES = {
         'USER': 'systextil',
         'PASSWORD': DBPASS_SH,
     },
-    'snd': {  # Systextil OC (nuvem) direto via internet
-        'ENGINE': 'django.db.backends.oracle',
-        'NAME': '152.67.55.216:1521/db_pdb1.sub02011943440.tussorvcn.oraclevcn.com',
-        'USER': 'systextil',
-        'PASSWORD': DBPASS_SH,
-    },
+    # 'snd': {  # Systextil OC (nuvem) direto via internet
+    #     'ENGINE': 'django.db.backends.oracle',
+    #     'NAME': '152.67.55.216:1521/db_pdb1.sub02011943440.tussorvcn.oraclevcn.com',
+    #     'USER': 'systextil',
+    #     'PASSWORD': DBPASS_SH,
+    # },
     'persona': {  # Nasajon
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': "nasajon_db3_20200617_163619",
