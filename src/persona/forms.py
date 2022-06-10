edb4d53b97e58a62049eb7f750ad4f4385a4c3d6
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2022-06-10 10:03:18 -0300

    CriaUsuarioForm utilizando FormWidgetAttrs

--- src/persona/forms.py
+++ src/persona/forms.py
@@ -2,21 +2,16 @@ from pprint import pprint
 
 from django import forms
 
+from o2.forms.custom import FormWidgetAttrs
+
 
 class CriaUsuarioForm(forms.Form):
-
-    number_attrs = {
-        'type': 'number;',
-        }
-
-    autofocus_attrs = {
-        'autofocus': 'autofocus;',
-        }
+    a = FormWidgetAttrs()
 
     codigo = forms.CharField(
         label='Código (matrícula)', required=True, max_length=5,
         widget=forms.TextInput(attrs={
-            **autofocus_attrs, **number_attrs}))
+            **a.autofocus, **a.number}))
 
     cpf = forms.CharField(
         label='CPF', required=False, max_length=11,
