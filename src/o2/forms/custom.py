5ff38e723879f0980b5bd024fdb66643c88d5648
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2022-06-10 09:54:51 -0300

    criado FormWidgetAttrs

---
+++ src/o2/forms/custom.py
@@ -0,0 +1,17 @@
+from pprint import pprint
+
+
+class FormWidgetAttrs():
+
+    string_upper = {
+        'type': 'string',
+        'style': 'text-transform:uppercase;',
+    }
+
+    autofocus = {
+        'autofocus': 'autofocus;',
+    }
+
+    placeholder_0 = {
+        'placeholder': '0...',
+    }
