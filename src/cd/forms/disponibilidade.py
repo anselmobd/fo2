e5f8072d4bb0c8fd361a90c12ae0e5eaea660d13
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2022-06-11 18:23:39 -0300

    reordenando campos e melhor clean_referencia

--- src/cd/forms/disponibilidade.py
+++ src/cd/forms/disponibilidade.py
@@ -7,6 +7,8 @@ from fo2.connections import db_cursor_so
 from comercial.queries import get_tabela_preco
 from systextil.models import Colecao
 
+from utils.functions.strings import is_only_digits
+
 
 class DisponibilidadeForm(forms.Form):
     CHOICES = [
@@ -25,6 +27,19 @@ class DisponibilidadeForm(forms.Form):
         label='Modelos da tabela de preços',
         required=False, initial=None)
 
+    modelo = forms.CharField(
+        required=False,
+        min_length=1,
+        max_length=5,
+        widget=forms.TextInput(
+            attrs={
+                'size': 5,
+                'type': 'number',
+                'placeholder': '0',
+            }
+        )
+    )
+
     referencia = forms.CharField(
         label='Referência',
         required=False,
@@ -36,19 +51,6 @@ class DisponibilidadeForm(forms.Form):
                 'type': 'string',
                 'style': 'text-transform:uppercase;',
                 'placeholder': '0...',
-            }
-        )
-    )
-
-    modelo = forms.CharField(
-        required=False,
-        min_length=1,
-        max_length=5,
-        widget=forms.TextInput(
-            attrs={
-                'size': 5,
-                'type': 'number',
-                'placeholder': '0',
             }
         )
     )
@@ -95,7 +97,18 @@ class DisponibilidadeForm(forms.Form):
 
     def clean_referencia(self):
         cleaned = self.cleaned_data['referencia']
-        cleaned = '' if len(cleaned) == 0 else cleaned.upper().zfill(5)
+        if len(cleaned) == 0:
+            cleaned = ''
+        else:
+            if (
+                len(cleaned) > 1 and 
+                cleaned[0].isalpha() and 
+                is_only_digits(cleaned[1:])
+            ):
+                cleaned = cleaned[0].upper() + cleaned[1:].upper().zfill(4)
+            else:
+                cleaned = cleaned.upper().zfill(5)
+
         data = self.data.copy()
         data['referencia'] = cleaned
         self.data = data
