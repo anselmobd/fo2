0c037dcbcea7d76031feeaf08a33d80fe5968400
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2022-06-17 15:01:05 -0300

    orimeira versão completa, ainda sem filtros

--- src/cd/queries/novo_modulo/estoque_ficticio.py
+++ src/cd/queries/novo_modulo/estoque_ficticio.py
@@ -13,6 +13,9 @@ def query(cursor):
           , l.ORDEM_CONFECCAO OC
           , l.CODIGO_ESTAGIO EST
           , l.QTDE_PROGRAMADA QTD_LOTE
+          , l.PROCONF_GRUPO REF
+          , l.PROCONF_SUBGRUPO TAM
+          , l.PROCONF_ITEM COR
           FROM PCPC_040 l 
           WHERE 1=1
             AND l.QTDE_DISPONIVEL_BAIXA > 0
@@ -31,6 +34,10 @@ def query(cursor):
           , l.PER
           , l.OC
           , l.QTD_LOTE
+          , lp.COD_CONTAINER PALETE
+          , l.REF
+          , l.TAM
+          , l.COR
           FROM lotes_605763 l
           LEFT JOIN ENDR_014 lp
             ON lp.ORDEM_PRODUCAO = l.OP
@@ -42,8 +49,40 @@ def query(cursor):
             )
         )
         SELECT 
-          l.*
+          l.OP
+        , l.PER
+        , l.OC
+        , l.PER * 100000 + l.OC LOTE
+        , l.QTD_LOTE
+        , l.PALETE
+        , l.REF
+        , l.TAM
+        , l.COR
+        , COALESCE(ec.COD_ENDERECO, '-') endereco
+        , COALESCE(
+            ( SELECT
+                LISTAGG(DISTINCT COALESCE(TO_CHAR(sl.SOLICITACAO), '#'), ', ')
+                WITHIN GROUP (ORDER BY sl.SOLICITACAO) colicitacoes
+              FROM pcpc_044 sl -- solicitação / lote 
+              WHERE 1=1
+                AND sl.ORDEM_PRODUCAO = l.OP
+                AND sl.ORDEM_CONFECCAO = l.OC
+            ),
+          '-'
+          ) solicitacoes
+        , COALESCE(
+            ( SELECT
+                SUM(sl.QTDE) qtd_sol
+              FROM pcpc_044 sl -- solicitação / lote 
+              WHERE 1=1
+                AND sl.ORDEM_PRODUCAO = l.OP
+                AND sl.ORDEM_CONFECCAO = l.OC
+            ),
+          0
+          ) qtd_emp
         FROM lotes_605763end l
+        LEFT JOIN ENDR_015 ec -- endereço/container 
+          ON ec.COD_CONTAINER = l.PALETE
     """
     debug_cursor_execute(cursor, sql)
     return dictlist_lower(cursor)
