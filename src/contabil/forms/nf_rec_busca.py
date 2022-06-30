89740e7d6a84059e9891fc0c7ccde2f52f0a6fbd
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2022-06-30 11:10:28 -0300

    BuscaNFRecebidaForm filtra por ref, tam e cor

---
+++ src/contabil/forms/nf_rec_busca.py
@@ -0,0 +1,89 @@
+from pprint import pprint
+
+from django import forms
+
+from o2.forms.widget_attrs import FormWidgetAttrs
+
+from utils.functions.strings import is_only_digits
+
+
+class BuscaNFRecebidaForm(forms.Form):
+    a = FormWidgetAttrs()
+
+    ref = forms.CharField(
+        label='Referência',
+        required=False,
+        min_length=1,
+        max_length=5,
+        widget=forms.TextInput(
+            attrs={
+                'size': 5,
+                **a.string_upper,
+                **a.placeholder_0,
+            }
+        )
+    )
+
+    cor = forms.CharField(
+        required=False,
+        min_length=1,
+        max_length=6,
+        widget=forms.TextInput(
+            attrs={
+                'size': 6,
+                **a.string_upper,
+                **a.placeholder_0,
+            }
+        )
+    )
+
+    tam = forms.CharField(
+        label='Tamanho',
+        required=False,
+        min_length=1,
+        max_length=3,
+        widget=forms.TextInput(
+            attrs={
+                'size': 3,
+                **a.string_upper,
+            }
+        )
+    )
+
+    def clean_ref(self):
+        cleaned = self.cleaned_data['ref']
+        if len(cleaned) == 0:
+            cleaned = ''
+        else:
+            if (
+                len(cleaned) > 1 and 
+                cleaned[0].isalpha() and 
+                is_only_digits(cleaned[1:])
+            ):
+                cleaned = cleaned[0].upper() + cleaned[1:].upper().zfill(4)
+            else:
+                cleaned = cleaned.upper().zfill(5)
+
+        data = self.data.copy()
+        data['ref'] = cleaned
+        self.data = data
+        return cleaned
+
+    def clean_cor(self):
+        cleaned = self.cleaned_data['cor']
+        if len(cleaned) == 0:
+            cleaned = ''
+        else:
+            cleaned = cleaned.upper().zfill(6)
+
+        data = self.data.copy()
+        data['cor'] = cleaned
+        self.data = data
+        return cleaned
+
+    def clean_tam(self):
+        cleaned = self.cleaned_data['tam'].upper()
+        data = self.data.copy()
+        data['tam'] = cleaned
+        self.data = data
+        return cleaned
