79c7c17432da08cce82307537c3582c912c4b690
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2022-06-30 16:02:28 -0300

    code

--- src/contabil/views/nf_rec_busca.py
+++ src/contabil/views/nf_rec_busca.py
@@ -6,37 +6,36 @@ from fo2.connections import db_cursor_so
 
 from base.views import O2BaseGetPostView
 from utils.table_defs import TableDefs
-from utils.views import totalize_data
 
-import contabil.forms.nf_rec_busca
+from contabil.forms.nf_rec_busca import BuscaNFRecebidaForm
 from contabil.queries import nf_rec_info
 
 
 class BuscaNFRecebida(O2BaseGetPostView):
 
+    table_defs = TableDefs(
+        {
+            'dt': ['Data'],
+            'nf': ['NF'],
+            'forn_cnpj_nome': ['Fornecedor'],
+            'nat': ['Nat.Op.'],
+            'cfop': ['CFOP', 'c'],
+            # 'nat_descr': ['nat_descr'],
+            'tran_est': ['Tran.est.', 'c'],
+            # 'tran_descr': ['tran_descr'],
+            'hist_cont': ['Hist.cont.', 'c'],
+            # 'hist_descr': ['hist_descr'],
+        },
+        ['header', '+style'],
+        style = {'_': 'text-align'},
+    )
+
     def __init__(self, *args, **kwargs):
         super(BuscaNFRecebida, self).__init__(*args, **kwargs)
-        self.Form_class = contabil.forms.nf_rec_busca.BuscaNFRecebidaForm
+        self.Form_class = BuscaNFRecebidaForm
         self.template_name = 'contabil/nf_rec_busca.html'
         self.title_name = "Busca NF recebida"
         self.cleaned_data2self = True
-
-        self.table_defs = TableDefs(
-            {
-                'dt': ['Data'],
-                'nf': ['NF'],
-                'forn_cnpj_nome': ['Fornecedor'],
-                'nat': ['Nat.Op.'],
-                'cfop': ['CFOP', 'c'],
-                # 'nat_descr': ['nat_descr'],
-                'tran_est': ['Tran.est.', 'c'],
-                # 'tran_descr': ['tran_descr'],
-                'hist_cont': ['Hist.cont.', 'c'],
-                # 'hist_descr': ['hist_descr'],
-            },
-            ['header', '+style'],
-            style = {'_': 'text-align'},
-        )
 
     def mount_context(self):
         cursor = db_cursor_so(self.request)
@@ -50,9 +49,7 @@ class BuscaNFRecebida(O2BaseGetPostView):
             cor=self.cor,
         )
         if len(data) == 0:
-            self.context.update({
-                'msg_erro': "Nota fiscal recebida não encontrada",
-            })
+            self.context['msg_erro'] = "Nota fiscal recebida não encontrada"
             return
 
         for row in data:
@@ -63,6 +60,4 @@ class BuscaNFRecebida(O2BaseGetPostView):
             )
 
         self.context.update(self.table_defs.hfs_dict())
-        self.context.update({
-            'data': data,
-        })
+        self.context['data'] = data
