77d3a0e889d398cfd62831caf356f4b614df35d0
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2022-06-30 11:11:36 -0300

    view BuscaNFRecebida primeira versão funcional

--- src/contabil/views/nf_rec_busca.py
+++ src/contabil/views/nf_rec_busca.py
@@ -9,7 +9,7 @@ from fo2.connections import db_cursor_so
 from base.views import O2BaseGetPostView
 from utils.views import totalize_data
 
-import contabil.forms.nf
+import contabil.forms.nf_rec_busca
 from contabil.queries import (
     nf_rec_info,
     nf_rec_itens,
@@ -20,20 +20,20 @@ class BuscaNFRecebida(O2BaseGetPostView):
 
     def __init__(self, *args, **kwargs):
         super(BuscaNFRecebida, self).__init__(*args, **kwargs)
-        self.Form_class = contabil.forms.nf.NotaFiscalForm
+        self.Form_class = contabil.forms.nf_rec_busca.BuscaNFRecebidaForm
         self.template_name = 'contabil/nf_rec_busca.html'
         self.title_name = "Busca NF recebida"
-        self.get_args = ['nf', 'empresa']
         self.cleaned_data2self = True
 
     def mount_context(self):
         cursor = db_cursor_so(self.request)
-        self.context = {
-            'titulo': self.title_name,
-        }
-
+        
         data = nf_rec_info.query(
-            cursor, self.nf, self.empresa)
+            cursor,
+            ref=self.ref,
+            tam=self.tam,
+            cor=self.cor,
+        )
         if len(data) == 0:
             self.context.update({
                 'msg_erro': "Nota fiscal recebida não encontrada",
@@ -71,34 +71,4 @@ class BuscaNFRecebida(O2BaseGetPostView):
                     'hist_descr',
                 ],
                 'data': data,
-            })
-
-            i_data = nf_rec_itens.query(cursor, self.nf, self.empresa)
-
-            if i_data:
-                totalize_data(i_data, {
-                    'sum': ['qtd'],
-                    'descr': {'ref': "Total:"},
-                    'row_style': 'font-weight: bold;',
-                })
-
-            self.context.update({
-                'i_headers': [
-                    "Nível",
-                    "ref",
-                    "tam",
-                    "cor",
-                    "qtd",
-                ],
-                'i_fields': [
-                    'niv',
-                    'ref',
-                    'tam',
-                    'cor',
-                    'qtd',
-                ],
-                'i_data': i_data,
-                'i_style': {
-                    5: 'text-align: right;',
-                },
             })
