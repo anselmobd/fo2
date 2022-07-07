8823a831374394c7a5d08359df64a3b6d2e8b47c
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2022-07-07 16:27:27 -0300

    criada query para buscar dados da NF para etiquetas

---
+++ src/logistica/queries/etiqueta_nf.py
@@ -0,0 +1,23 @@
+from django.db import connections
+
+from utils.functions.models.dictlist import dictlist_lower
+from utils.functions.queries import debug_cursor_execute
+
+__all_ = ['dados_nf']
+
+
+def dados_nf(cursor, nf):
+    sql = f"""
+        SELECT
+          f.NUM_NOTA_FISCAL nf_num
+        , f.SERIE_NOTA_FISC nf_ser
+        , f.QTDE_EMBALAGENS vols
+        , f.PESO_BRUTO peso_tot
+        , f.PEDIDO_VENDA ped
+        FROM FATU_050 f
+        WHERE f.CODIGO_EMPRESA = 1
+          AND f.NUM_NOTA_FISCAL = {nf}
+          AND f.NUMERO_CAIXA_ECF = 0
+    """
+    debug_cursor_execute(cursor, sql, [])
+    return dictlist_lower(cursor)
