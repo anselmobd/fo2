1e3216c216618816b8b320cb5939d043cdfe75e7
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2022-07-08 00:20:07 -0300

    view EtiquetaNf cadidato a versão final

--- src/logistica/views/etiqueta_nf.py
+++ src/logistica/views/etiqueta_nf.py
@@ -5,7 +5,10 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from fo2.connections import db_cursor_so
 
 from base.views import O2BaseGetPostView
+from utils.classes import TermalPrint
 from utils.table_defs import TableDefs
+
+import lotes.models
 
 from logistica.forms.nf import NfForm
 from logistica.queries.etiqueta_nf import get_dados_nf
@@ -54,17 +57,18 @@ class EtiquetaNf(LoginRequiredMixin, O2BaseGetPostView):
                 'msg_erro': 'NF não encontrada',
             })
             return
+        row = dados_nf[0]
         
         vol_inicial_val = int(self.vol_inicial) if self.vol_inicial else 1
-        vol_final_val = int(self.vol_final) if self.vol_final else dados_nf[0]['vols']
+        vol_final_val = int(self.vol_final) if self.vol_final else row['vols']
 
-        if not (1 <= vol_inicial_val <= dados_nf[0]['vols']):
+        if not (1 <= vol_inicial_val <= row['vols']):
             self.context.update({
                 'msg_erro': 'Caixa inicial inválida',
             })
             return
 
-        if not (vol_inicial_val <= vol_final_val <= dados_nf[0]['vols']):
+        if not (vol_inicial_val <= vol_final_val <= row['vols']):
             self.context.update({
                 'msg_erro': 'Caixa final inválida',
             })
@@ -77,3 +81,47 @@ class EtiquetaNf(LoginRequiredMixin, O2BaseGetPostView):
             'data': dados_nf,
             'qtd_vols': qtd_vols,
         })
+
+        if 'print' not in self.request.POST:
+            return
+
+        nome_impresso = 'etiqueta_nf'
+        try:
+            impresso = lotes.models.Impresso.objects.get(
+                nome=nome_impresso)
+        except lotes.models.Impresso.DoesNotExist:
+            self.context.update({
+                'msg_erro': 'Impresso não cadastrado',
+            })
+            return
+
+        try:
+            usuario_impresso = lotes.models.UsuarioImpresso.objects.get(
+                usuario=self.request.user, impresso=impresso)
+        except lotes.models.UsuarioImpresso.DoesNotExist:
+            self.context.update({
+                'msg_erro': 'Impresso não cadastrado para o usuário',
+            })
+            return
+
+        teg = TermalPrint(
+            usuario_impresso.impressora_termica.nome,
+            file_dir=f"impresso/{nome_impresso}/%Y/%m"
+        )
+        teg.template(usuario_impresso.modelo.gabarito, '\r\n')
+        teg.printer_start()
+        try:
+            for i in range(row['vols']):
+                row['vol'] = i + 1
+                teg.context(row)
+                try:
+                    teg.printer_send()
+                except Exception as e:
+                    self.context.update({
+                        'msg_erro': f'Erro ao imprimir <{e}>',
+                    })
+                    return
+        except Exception as e:
+            raise e
+        finally:
+            teg.printer_end()
