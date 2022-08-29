c94fa9f6881c5962fcf14b3690fb7878d9708cc0
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2022-08-29 12:27:57 -0300

    referencias_com_op pegando data de digitação da OP mais recente

--- src/systextil/queries/op/referencia.py
+++ src/systextil/queries/op/referencia.py
@@ -1,16 +1,20 @@
 from pprint import pprint
 
 from utils.functions.models.dictlist import dictlist_lower
+from utils.functions.queries import debug_cursor_execute
 
 
 def referencias_com_op(cursor):
     sql = f'''
-        SELECT distinct
-          op.REFERENCIA_PECA ref
+        SELECT
+          op.REFERENCIA_PECA REF
+        , max(op.DATA_PROGRAMACAO) DT_DIGITACAO 
         FROM PCPC_020 op
         WHERE op.COD_CANCELAMENTO = 0
+        GROUP BY
+          op.REFERENCIA_PECA
         ORDER BY 
           op.REFERENCIA_PECA
     '''
-    cursor.execute(sql)
+    debug_cursor_execute(cursor, sql)
     return dictlist_lower(cursor)
