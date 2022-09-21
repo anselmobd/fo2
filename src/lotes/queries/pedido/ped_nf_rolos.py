3dfb8a7c2c1e844faf0503d67bc0477ddb29eb37
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2022-09-21 16:52:25 -0300

    pode receber várias OPs; adiciona devolver série da NF e fornecedor

--- src/lotes/queries/pedido/ped_nf_rolos.py
+++ src/lotes/queries/pedido/ped_nf_rolos.py
@@ -1,3 +1,4 @@
+import collections.abc
 from pprint import pprint
 
 from utils.functions.queries import debug_cursor_execute
@@ -5,10 +6,18 @@ from utils.functions.models.dictlist import dictlist_lower
 
 
 def query(cursor, op):
+    if (not isinstance(op, collections.abc.Sequence)) or isinstance(op, str):
+        op = (op, )
+    test_op = f"rc.ORDEM_PRODUCAO IN ({', '.join(map(str, op))})"
+
     sql = f"""
         SELECT
           rc.ORDEM_PRODUCAO op
         , ro.NOTA_FISCAL_ENT nf
+        , ro.SERI_FISCAL_ENT serie
+        , ro.FORNECEDOR_CGC9 cnpj9
+        , ro.FORNECEDOR_CGC4 cnpj4
+        , ro.FORNECEDOR_CGC2 cnpj2
         , ro.PANOACAB_NIVEL99 nivel
         , ro.PANOACAB_GRUPO ref
         , ro.PANOACAB_SUBGRUPO tam
@@ -17,10 +26,15 @@ def query(cursor, op):
         FROM PCPT_020 ro -- cadastro de rolos
         LEFT JOIN PCPT_025 rc -- alocação de rolo para OP
           ON rc.CODIGO_ROLO = ro.CODIGO_ROLO
-        WHERE rc.ORDEM_PRODUCAO =  {op}
+        WHERE {test_op}
+          AND ro.NOTA_FISCAL_ENT > 0
         GROUP BY 
           rc.ORDEM_PRODUCAO
         , ro.NOTA_FISCAL_ENT
+        , ro.SERI_FISCAL_ENT
+        , ro.FORNECEDOR_CGC9
+        , ro.FORNECEDOR_CGC4
+        , ro.FORNECEDOR_CGC2
         , ro.PANOACAB_NIVEL99
         , ro.PANOACAB_GRUPO
         , ro.PANOACAB_SUBGRUPO
