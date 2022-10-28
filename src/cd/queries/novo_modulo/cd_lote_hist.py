0e5e4d3c709fcb313fda56f3e52b89882c467f26
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2022-10-27 22:01:12 -0300

    adiciona descrição do código da tela

--- src/cd/queries/novo_modulo/cd_lote_hist.py
+++ src/cd/queries/novo_modulo/cd_lote_hist.py
@@ -15,21 +15,41 @@ __atividade = {
 }
 
 __apex_tela = {
-    '49': "duom_fa01",
-    '50': "duom_fa02",
-    '51': "duom_fa03",
-    '52': "duom_fa04",
-    '53': "duom_fa05",
-    '54': "duom_fa06",
-    '55': "duom_fa07",
-    '56': "duom_fa08",
-    '57': "duom_fa09",
-    '58': "duom_fa10",
-    '59': "duom_fa11",
-    '60': "duom_fa12",
-    '61': "duom_fa13",
-    '62': "duom_fa14",
-    '63': "duom_fa15",
+    '49': 'duom_fa01',
+    '50': 'duom_fa02',
+    '51': 'duom_fa03',
+    '52': 'duom_fa04',
+    '53': 'duom_fa05',
+    '54': 'duom_fa06',
+    '55': 'duom_fa07',
+    '56': 'duom_fa08',
+    '57': 'duom_fa09',
+    '58': 'duom_fa10',
+    '59': 'duom_fa11',
+    '60': 'duom_fa12',
+    '61': 'duom_fa13',
+    '62': 'duom_fa14',
+    '63': 'duom_fa15',
+}
+
+__nome_tela = {
+    'endr_f014': "Associação de ordens de confecção",
+    'endr_f015': "Endereçamento de Container",
+    'duom_fa01': "Painel de Programação de Pedidos",
+    'duom_fa02': "Transfere estoques PAs para Afaturar",
+    'duom_fa03': "Troca de pedidos/lotes estoques e OCs",
+    'duom_fa04': "Nova Ordem de produção",
+    'duom_fa05': "Parametros do Processo de geração de ordens",
+    'duom_fa06': "Nível de qualidade aceitavel",
+    'duom_fa07': "Nivel de qualidade de cada ordem de produção",
+    'duom_fa08': "Proposta de Empenho de OC's de Confecção",
+    'duom_fa09': "Endereços",
+    'duom_fa10': "Containers",
+    'duom_fa11': "Tipo Container",
+    'duom_fa12': "Endereços das ordens de confecção",
+    'duom_fa13': "Atendimento da requisição de estoque",
+    'duom_fa14': "Estoque mínimo para depósito varejo",
+    'duom_fa15': "Histórico de endereçamento",
 }
 
 
@@ -75,7 +95,7 @@ def query(cursor, lote):
                 or row['usuario'].startswith('WS Associaca ')
             ):
                 row['sistema'] = 'Systextil'
-                row['tela'] = 'endr_f015'
+                row['tela'] = 'endr_f014 - Associação de ordens de confecção'
                 row['login'] = row['usuario'].split("-")[1]
             elif (
                 row['usuario'].startswith('python3@intranet ')
@@ -93,7 +113,7 @@ def query(cursor, lote):
                 row['sistema'] = 'APEX'
                 try:
                     pagina = row['usuario'].split()[1].split(':')[1]
-                    row['tela'] = __apex_tela[pagina]
+                    row['tela'] = f"{__apex_tela[pagina]} - {__nome_tela[__apex_tela[pagina]]}"
                 except Exception:
                     pass
             elif row['usuario_systextil']:
