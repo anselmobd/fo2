ad1e3b5f1ccfb432a0b7dd914741144b09927693
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2022-10-31 16:42:54 -0300

    transformando situacao_descr em _SITUACAO_DESCR

--- src/contabil/functions/nf.py
+++ src/contabil/functions/nf.py
@@ -5,21 +5,23 @@ __version__ = '0.1 220131.1443'
 __author__ = 'Oxigenai'
 
 
-def nf_situacao_descr(situacao, status):
-    situacao_descr = {
-        0: 'CALCULADA',
-        1: {
-            100: 'ATIVA',  # 'EMITIDA',
-            None: 'REJEITADA',
-        },
-        2: 'CANCELADA',
-        3: 'VERIFICAR',
-        4: 'CONFIRMADA',
-        5: 'INCOMPLETA',
-        6: 'INCOMPLETA',  # porém foi calculada a duplicata
-    }
+_SITUACAO_DESCR = {
+    0: 'CALCULADA',
+    1: {
+        100: 'ATIVA',  # 'EMITIDA',
+        None: 'REJEITADA',
+    },
+    2: 'CANCELADA',
+    3: 'VERIFICAR',
+    4: 'CONFIRMADA',
+    5: 'INCOMPLETA',
+    6: 'INCOMPLETA',  # porém foi calculada a duplicata
+}
 
-    descr = situacao_descr.get(situacao, "DESCONHECIDO")
+
+def nf_situacao_descr(situacao, status):
+
+    descr = _SITUACAO_DESCR.get(situacao, "DESCONHECIDO")
     if isinstance(descr, dict):
         try:
             descr = descr[status]
