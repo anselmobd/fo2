f64a4bda34feb1cbcc396fc32ed92ac67d574666
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2022-11-10 07:53:29 -0300

    view BuscaPedido - primeira versão funcional

--- src/beneficia/views/busca_pedido.py
+++ src/beneficia/views/busca_pedido.py
@@ -12,7 +12,7 @@ from utils.table_defs import TableDefsHpSD
 from utils.views import totalize_data
 
 from beneficia.forms.busca_pedido import Form as BuscaPedidoForm
-from beneficia.queries.pendente import query as pendente_query
+from beneficia.queries.busca_pedido import query as busca_pedido_query
 
 
 class BuscaPedido(O2BaseGetPostView):
@@ -25,77 +25,42 @@ class BuscaPedido(O2BaseGetPostView):
         self.form_class_has_initial = True
         self.cleaned_data2self = True
 
-    def get_pendente(self):
-        return pendente_query(
+    def get_pedidos(self):
+        return busca_pedido_query(
             self.cursor,
-            data_de=self.data_de,
-            data_ate=self.data_ate,
-            estagio=(self.estagio.codigo_estagio 
-                if self.estagio else None),
-            tipo=2,
+            emissao_de=self.data_de,
+            emissao_ate=self.data_ate,
         )
 
     def mount_titulo(self, data):
         return {
-            'titulo': 'OB2',
+            'titulo': 'Pedidos',
             'data': data,
-            'vazio': "Sem pendente",
+            'vazio': "Sem pedidos",
         }
 
     def mount_link(self, pendente):
         for row in pendente['data']:
-            row['ob|TARGET'] = '_blank'
-            row['ob|LINK'] = reverse(
-                'beneficia:ob__get',
-                args=[row['ob']],
+            row['pedido|TARGET'] = '_blank'
+            row['pedido|LINK'] = reverse(
+                'producao:pedido__get',
+                args=[row['pedido']],
             )
-            if row.get('op'):
-                op_link = reverse(
-                    'producao:op__get', args=['99999']
-                ).replace("99999", r"\1")
-                row['op'] = re.sub(
-                    r'([^, ]+)',
-                    fr'<a href="{op_link}" target="_blank">\1<span '
-                    'class="glyphicon glyphicon-link" '
-                    'aria-hidden="true"></span></a>',
-                    str(row['op']))
-            else:
-                row['op'] = '-'
-
-    def mount_total(self, pendente):
-        totalize_data(
-            pendente['data'],
-            {
-                'sum': ['quilos'],
-                'descr': {'ob': 'Total:'},
-                'row_style':
-                    "font-weight: bold;"
-                    "background-image: linear-gradient(#DDD, white);",
-                'flags': ['NO_TOT_1'],
-            }
-        )
 
     def mount_hfs(self, pendente):
         TableDefsHpSD({
-            'ob': ["OB2"],
-            'op': ["OP"],
-            'tipo_tecido': ["Tipo tecido"],
-            'cor': ["Cor"],
-            'quilos': ["Quilos", 'r', 3],
+            'pedido': ["Pedido"],
+            'dt_emissao': ["Emissão"],
         }).hfs_dict(context=pendente)
-        pendente.update({
-            'safe': ['op'],
-        })
 
     def mount_context(self):
         self.cursor = db_cursor_so(self.request)
 
-        pendente_data = self.get_pendente()
-        pendente = self.mount_titulo(pendente_data)
+        pedidos_data = self.get_pedidos()
+        pedidos = self.mount_titulo(pedidos_data)
 
-        if pendente_data:
-            self.mount_link(pendente)
-            self.mount_total(pendente)
-            self.mount_hfs(pendente)
+        if pedidos_data:
+            self.mount_link(pedidos)
+            self.mount_hfs(pedidos)
 
-        self.context['dados'] = pendente
+        self.context['pedidos'] = pedidos
