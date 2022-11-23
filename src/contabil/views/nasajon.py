408d08fff374598659a4fa0e23cd552ad2faf362
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2022-11-23 11:18:07 -0300

    todos os docuementos

--- src/contabil/views/nasajon.py
+++ src/contabil/views/nasajon.py
@@ -20,37 +20,78 @@ def view(request):
                         "Ordem de Servico - 4181.pdf",
                     'Cronograma Versão 1':
                         "TUSSOR CONFECCOES LTDA - Cronograma V1.pdf",
-                }
+                },
             },
             {
                 'titulo': "19/10/2022 - Levantamento fiscal",
+                'subdir': "2022-10-19 levantamento fiscal",
+                'docs': {
+                    'MAIS013 - Levantamento de Processos':
+                        "MAIS013 - Levantamento de Processos TUSSOR CONFECÇÕES.docx.pdf",
+                    'Ordem de Servico - 4301':
+                        "Ordem de Servico - 4301.pdf",
+                },
             },
             {
                 'titulo': "20/10/2022 - Levantamento contábil",
+                'subdir': "2022-10-20 levantamento contábil",
+                'docs': {
+                    'MAIS013 - Levantamento de Processos do Contábil':
+                        "MAIS013 - Levantamento de Processos do Contábil.pdf",
+                    'Ordem de Servico - 4324':
+                        "Ordem de Servico - 4324.pdf",
+                },
                 'files': {
                     '14h31': "kkq-rieh-qxp (2022-10-20 14 31 GMT-3).mp4",
                 },
             },
             {
                 'titulo': "24/10/2022 - Preparação de aderência",
+                'subdir': "2022-10-24 preparação aderência",
+                'docs': {
+                    'Ordem de Servico - 4359':
+                        "Ordem de Servico - 4359.pdf",
+                },
             },
             {
                 'titulo': "25/10/2022 - Apresentação aderência",
+                'subdir': "2022-10-25 apresentação aderência",
+                'docs': {
+                    'Ordem de Servico - 4381':
+                        "Ordem de Servico - 4381.pdf",
+                },
                 'files': {
                     '14h31': "kkq-rieh-qxp (2022-10-25 14 31 GMT-3).mp4",
                 },
             },
             {
                 'titulo': "26/10/2022 - Definição de processos e Scritta 1 de 3",
+                'subdir': "2022-10-26 def. processos e scritta 1 de 3",
+                'docs': {
+                    'MAIS014 - Definição de Processos Contábil':
+                        "MAIS014 - Definição de Processos Contábil.docx.pdf",
+                    'Ordem de Servico - 4393':
+                        "Ordem de Servico - 4393.pdf",
+                },
             },
             {
                 'titulo': "27/10/2022 - Scritta 2 de 3",
+                'subdir': "2022-10-27 scritta 2 de 3",
+                'docs': {
+                    'Ordem de Servico - 4411':
+                        "Ordem de Servico - 4411.pdf",
+                },
                 'files': {
                     '14h21': "kkq-rieh-qxp (2022-10-27 14 21 GMT-3).mp4",
                 },
             },
             {
                 'titulo': "01/11/2022 - Scritta 3 de 3",
+                'subdir': "2022-11-01 scritta 3 de 3",
+                'docs': {
+                    'Ordem de Servico - 4464':
+                        "Ordem de Servico - 4464.pdf",
+                },
                 'files': {
                     '14h14': "kkq-rieh-qxp (2022-11-01 14 14 GMT-3).mp4",
                     '15h48': "kkq-rieh-qxp (2022-11-01 15 48 GMT-3).mp4",
@@ -58,21 +99,41 @@ def view(request):
             },
             {
                 'titulo': "07/11/2022 - Contabil 1 de 3",
+                'subdir': "2022-11-07 contabil 1 de 3",
+                'docs': {
+                    'Ordem de Servico - 4508':
+                        "Ordem de Servico - 4508.pdf",
+                },
                 'files': {
                     '15h48': "kkq-rieh-qxp (2022-11-07 14 32 GMT-3).mp4",
                 },
             },
             {
                 'titulo': "10/11/2022 - Contábil 2 de 3",
+                'subdir': "2022-11-10 contábil 2 de 3",
+                'docs': {
+                    'Ordem de Servico - 4550':
+                        "Ordem de Servico - 4550.pdf",
+                },
                 'files': {
                     '14h32': "kkq-rieh-qxp (2022-11-10 14 21 GMT-3).mp4",
                 },
             },
             {
                 'titulo': "16/11/2022 - Contábil 3 de 3",
+                'subdir': "2022-11-16 contábil 3 de 3",
+                'docs': {
+                    'Ordem de Servico - 4583':
+                        "Ordem de Servico - 4583.pdf",
+                },
             },
             {
                 'titulo': "21/11/2022 - Simulação scritta",
+                'subdir': "2022-11-21 simulação scritta",
+                'docs': {
+                    'Ordem de Servico - 4636':
+                        "Ordem de Servico - 4636.pdf",
+                },
                 'files': {
                     '14h24': "kkq-rieh-qxp (2022-11-21 14 24 GMT-3).mp4",
                     '16h15': "kkq-rieh-qxp (2022-11-21 16 15 GMT-3).mp4",
@@ -81,6 +142,11 @@ def view(request):
             },
             {
                 'titulo': "22/11/2022 - Simulação contábil",
+                'subdir': "2022-11-22 simulação contábil",
+                'docs': {
+                    'Ordem de Servico - 4663':
+                        "Ordem de Servico - 4663.pdf",
+                },
             },
         ]
     }
