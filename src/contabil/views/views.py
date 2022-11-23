da6ff6098432282060c61f61ef757f70724aae0e
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2022-11-23 08:56:38 -0300

    lista de vídeos via context

--- src/contabil/views/views.py
+++ src/contabil/views/views.py
@@ -12,5 +12,19 @@ def index(request):
 
 def nasajon(request):
     template_name = 'contabil/nasajon.html'
-    context = {'titulo': "Nasajon"}
+    context = {
+        'titulo': "Nasajon",
+        'videos': [
+            {'file': "kkq-rieh-qxp (2022-10-20 14 31 GMT-3).mp4"},
+            {'file': "kkq-rieh-qxp (2022-10-25 14 31 GMT-3).mp4"},
+            {'file': "kkq-rieh-qxp (2022-10-27 14 21 GMT-3).mp4"},
+            {'file': "kkq-rieh-qxp (2022-11-01 14 14 GMT-3).mp4"},
+            {'file': "kkq-rieh-qxp (2022-11-01 15 48 GMT-3).mp4"},
+            {'file': "kkq-rieh-qxp (2022-11-07 14 32 GMT-3).mp4"},
+            {'file': "kkq-rieh-qxp (2022-11-10 14 21 GMT-3).mp4"},
+            {'file': "kkq-rieh-qxp (2022-11-21 14 24 GMT-3).mp4"},
+            {'file': "kkq-rieh-qxp (2022-11-21 16 15 GMT-3).mp4"},
+            {'file': "kkq-rieh-qxp (2022-11-21 16 58 GMT-3).mp4"},
+        ]
+    }
     return render(request, template_name, context)
