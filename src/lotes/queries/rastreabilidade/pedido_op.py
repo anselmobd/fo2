7f41eed442fb1238c9401c02887c5e4f1282550e
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2022-11-30 15:26:47 -0300

    criando queries.rastreabilidade.pedido_op

--- src/lotes/queries/rastreabilidade/pedido_op.py
+++ src/lotes/queries/rastreabilidade/pedido_op.py
@@ -4,30 +4,27 @@ from utils.functions.models.dictlist import dictlist_lower
 from utils.functions.queries import debug_cursor_execute
 from utils.functions.strings import lms
 
-__all__ = ['rastreabilidade_query']
+__all__ = ['query']
 
 
-def rastreabilidade_query(
+def query(
     cursor,
     empresa=1,
     pedido=None,
 ):
 
     filtra_pedido = lms(f"""\
-        AND ped.PEDIDO_VENDA = '{pedido}'
+        AND op.PEDIDO_VENDA = '{pedido}'
     """) if pedido else ''
 
     sql = lms(f"""\
-        SELECT
-          ped.PEDIDO_VENDA PEDIDO
-        , ped.DATA_EMIS_VENDA DT_EMISSAO
-        , ped.OBSERVACAO OBS
-        FROM PEDI_100 ped -- pedido de venda
+        SELECT 
+          op.ORDEM_PRODUCAO OP
+        -- , op.*
+        FROM PCPC_020 op
         WHERE 1=1
-          AND ped.CODIGO_EMPRESA = {empresa}
           {filtra_pedido} -- filtra_pedido
-        ORDER BY
-          ped.PEDIDO_VENDA
+          AND op.PEDIDO_VENDA = 33710
     """)
 
     debug_cursor_execute(cursor, sql)
