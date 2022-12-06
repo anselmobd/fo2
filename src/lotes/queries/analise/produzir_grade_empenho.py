bc912e4c00844390681c6e71d024b36db3c2277f
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2022-12-06 11:56:49 -0300

    utilizando op_nao_finalizada

--- src/lotes/queries/analise/produzir_grade_empenho.py
+++ src/lotes/queries/analise/produzir_grade_empenho.py
@@ -125,8 +125,8 @@ class MountProduzirGradeEmpenho():
         )
 
     def query(self):
-        debug_cursor_execute_prt_off()
-        p = Perf(id='MountProduzirGradeEmpenho.query', on=True)
+        # debug_cursor_execute_prt_off()
+        # p = Perf(id='MountProduzirGradeEmpenho.query', on=True)
 
         if self.cache_get():
             return self.mount_produzir
@@ -177,19 +177,8 @@ class MountProduzirGradeEmpenho():
         if not (self.gme or self.gmg):
             return self.mount_produzir
 
-        p.prt('antes')
-
-        g_header, g_fields, g_data, g_style, total_opa = \
-            lotes.queries.op.op_sortimentos(
-                self.cursor, tipo='a', descr_sort=False, modelo=self.modelo,
-                situacao='a', tipo_ref='v', tipo_alt='p', total='Total')
-
-        p.prt('op_sortimentos')
-
         g_header, g_fields, g_data, g_style, total_opa = \
             self.op_nao_finalizada(modelo=self.modelo)
-
-        p.prt('op_nao_finalizada')
 
         gopa = None
         if total_opa != 0:
@@ -545,6 +534,6 @@ class MountProduzirGradeEmpenho():
 
         self.cache_set()
 
-        debug_cursor_execute_prt_on()
+        # debug_cursor_execute_prt_on()
 
         return self.mount_produzir
