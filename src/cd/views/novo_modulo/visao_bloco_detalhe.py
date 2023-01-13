cc75fbaf1d76897c6b6d430e351ba12e9876d12f
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2023-01-13 19:26:54 -0300

    apresenta OP e agrupamento

--- src/cd/views/novo_modulo/visao_bloco_detalhe.py
+++ src/cd/views/novo_modulo/visao_bloco_detalhe.py
@@ -5,7 +5,10 @@ from pprint import pprint
 from fo2.connections import db_cursor_so
 
 from o2.views.base.get import O2BaseGetView
-from utils.views import totalize_data
+from utils.views import (
+    group_rowspan,
+    totalize_grouped_data,
+)
 
 from cd.classes.endereco import EnderecoCd
 from cd.queries.endereco import conteudo_local
@@ -19,7 +22,7 @@ class VisaoBlocoDetalhe(O2BaseGetView):
         self.title_name = 'Visão detalhada do bloco'
         self.get_args = ['bloco']
         self.get_args2self = True
-        self.Key = namedtuple('Key', 'local ref cor tam')
+        self.Key = namedtuple('Key', 'local op item')
 
     def mount_context(self):
         self.cursor = db_cursor_so(self.request)
@@ -38,53 +41,62 @@ class VisaoBlocoDetalhe(O2BaseGetView):
             row.update(ecd.details_dict)
 
         sort_field = 'order_ap' if local_field == 'endereco' else local_field
-        lotes.sort(key=operator.itemgetter(sort_field, 'ref', 'cor', 'tam'))
+        lotes.sort(key=operator.itemgetter(sort_field, 'op', 'ref', 'cor', 'tam'))
 
         itens = {}
         for lote in lotes:
             item = self.Key(
                 local=lote[local_field],
-                ref=lote['ref'],
-                cor=lote['cor'],
-                tam=lote['tam'],
+                op=lote['op'],
+                item=lote['item'],
             )
             if item not in itens:
                 itens[item] = {
+                    'lotes': set(),
                     'qtd': 0,
                 }
+            itens[item]['lotes'].add(lote['lote'])
             itens[item]['qtd'] += lote['qtd']
 
         dados = [
             {
                 local_field: item.local,
-                'ref': item.ref,
-                'cor': item.cor,
-                'tam': item.tam,
+                'op': item.op,
+                'item': item.item,
+                'lotes': len(itens[item]['lotes']),
                 'qtd': itens[item]['qtd'],
             }
             for item in itens
         ]
 
-        totalize_data(dados, {
-            'sum': ['qtd'],
+        group = [local_field]
+        totalize_grouped_data(dados, {
+            'group': group,
+            'sum': ['lotes', 'qtd'],
+            'count': [],
             'descr': {local_field: 'Totais:'},
+            'global_sum': ['lotes', 'qtd'],
+            'global_descr': {local_field: 'Totais gerais:'},
             'flags': ['NO_TOT_1'],
             'row_style': 'font-weight: bold;',
         })
+        group_rowspan(dados, group)
 
         fields = {
             local_field: 'Palete' if local_field == 'palete' else 'Endereço',
-            'ref': 'Referência',
-            'cor': 'cor',
-            'tam': 'tamanho',
-            'qtd': 'Qtd.',
+            'op': 'OP',
+            'item': 'Item',
+            'lotes': 'Lotes',
+            'qtd': 'Quant.',
         }
 
         self.context.update({
             'headers': fields.values(),
             'fields': fields.keys(),
             'data': dados,
+            'group': group,
             'style': {
+                4: 'text-align: right;',
                 5: 'text-align: right;',
             },
         })
