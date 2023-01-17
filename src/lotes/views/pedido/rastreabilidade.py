4be52323c40e47d9a0e7a14c442efb54d075fa1c
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2023-01-17 20:45:38 -0300

    data e obs de pedido filial-matriz

--- src/lotes/views/pedido/rastreabilidade.py
+++ src/lotes/views/pedido/rastreabilidade.py
@@ -180,6 +180,7 @@ class RastreabilidadeView(O2BaseGetPostView):
     def prep_rows_filial_pedido(self):
         for row in self.dados_filial_ped:
             row_field_str(row, 'ped')
+            row_field_date(row, 'data')
             row_field_str(row, 'nf')
 
     def table_filial_pedido(self):
@@ -189,6 +190,8 @@ class RastreabilidadeView(O2BaseGetPostView):
         )
         TableDefsHpSD({
             'ped': ["Pedido"],
+            'data': ["Data estágio 16", 'c'],
+            'obs': ["Observação"],
             'nf': ["NF"],
         }).hfs_dict_context(bloco)
         return bloco
