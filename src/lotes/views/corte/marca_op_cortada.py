8137ea433809d7a4ed424f4b81b323f9ac7c5359
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2023-02-06 07:07:20 -0300

    quase funcional

--- src/lotes/views/corte/marca_op_cortada.py
+++ src/lotes/views/corte/marca_op_cortada.py
@@ -1,28 +1,14 @@
-import re
 from pprint import pprint
 
 from django.http import JsonResponse
 from django.views import View
-
-from fo2.connections import db_cursor_so
-
-import lotes.queries
-from lotes.queries.pedido.ped_alter import (
-    altera_pedido,
-    altera_pedido_itens,
-)
-from lotes.queries.pedido.mensagem_nf import cria_mens_nf
-from lotes.queries.producao.romaneio_corte import producao_ops_finalizadas
-from lotes.views.prepara_pedido_compra_matriz import cria_pedido_compra_matriz
 
 from lotes.models.op import OpCortada
 
 
 class MarcaOpCortada(View):
 
-    def process(self, request, kwargs):
-        cursor = db_cursor_so(request)
-
+    def process(self, kwargs):
         op = kwargs['op']
 
         try:
@@ -30,23 +16,22 @@ class MarcaOpCortada(View):
         except OpCortada.DoesNotExist:
             op_object = None
 
-        try:
-            if op_object:
-                op_object.detete()
-                return 'DESMARCADA'
-            else:
-                op_object = OpCortada(op=op)
-                op_object.save()
-                return 'MARCADA'
-        except Exception:
-            return 'ERRO'
+        if op_object:
+            op_object.detete()
+            return 'DESMARCADA'
+        else:
+            op_object = OpCortada(op=op)
+            op_object.save()
+            return 'MARCADA'
 
-    def response(self, result):
-        status, message = result
+    def response(self, kwargs):
+        try:
+            status = self.process(kwargs)
+        except Exception:
+            status =  'ERRO'
         return {
             'status': status,
         }
 
     def get(self, request, *args, **kwargs):
-        result = self.process(request, kwargs)
-        return JsonResponse(self.response(result), safe=False)
+        return JsonResponse(self.response(kwargs), safe=False)
