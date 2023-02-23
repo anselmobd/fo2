4a338c451687997eccd0fcbfaf87382b7a7179a5
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2023-02-23 19:20:01 -0300

    filtra por lista de OPs e pega quantidade programada

--- src/lotes/queries/op/op_itens.py
+++ src/lotes/queries/op/op_itens.py
@@ -7,33 +7,51 @@ __all__ = ['query']
 
 
 def query(cursor, op):
+    """Busca itens de OP
+    Parametro:
+        op - uma OP ou uma lista de OPs
+    Retorno:
+        dictlist
+    """
+    
+    if isinstance(op, (tuple, list)):
+        ops = ", ".join(map(str, op))
+        condicao = f"IN ({ops})"
+    else:
+        condicao = f"= {op}"
+    filtra_op = f"""--
+        AND l.ORDEM_PRODUCAO {condicao}
+    """
+
     sql = f"""
         SELECT
-          l.ORDEM_PRODUCAO OP
-        , l.PROCONF_NIVEL99 NIVEL
+          l.PROCONF_NIVEL99 NIVEL
         , l.PROCONF_GRUPO REF
         , l.PROCONF_ITEM COR
         , t.ORDEM_TAMANHO
         , l.PROCONF_SUBGRUPO TAM
-        , sum(l.QTDE_EM_PRODUCAO_PACOTE) QTD
+        , sum(l.QTDE_PECAS_PROG) QTD
+        , l.ORDEM_PRODUCAO OP
         FROM PCPC_040 l -- lotes
         LEFT JOIN BASI_220 t -- tamanhos
           ON t.TAMANHO_REF = l.PROCONF_SUBGRUPO
-        WHERE l.ORDEM_PRODUCAO = {op}
+        WHERE 1=1
+          AND l.SEQUENCIA_ESTAGIO = 1
+          {filtra_op} -- filtra_op
         GROUP BY 
-          l.ORDEM_PRODUCAO
-        , l.PROCONF_NIVEL99
+          l.PROCONF_NIVEL99
         , l.PROCONF_GRUPO
         , l.PROCONF_ITEM
         , t.ORDEM_TAMANHO
         , l.PROCONF_SUBGRUPO
+        , l.ORDEM_PRODUCAO
         ORDER BY 
-          l.ORDEM_PRODUCAO
-        , l.PROCONF_NIVEL99
+          l.PROCONF_NIVEL99
         , l.PROCONF_GRUPO
         , l.PROCONF_ITEM
         , t.ORDEM_TAMANHO
         , l.PROCONF_SUBGRUPO
+        , l.ORDEM_PRODUCAO
     """
     debug_cursor_execute(cursor, sql)
     dados = dictlist_lower(cursor)
