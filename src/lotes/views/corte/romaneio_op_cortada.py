fe0f4eb3ff509ddb0723620d92bdeb64e5fdc557
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2023-02-23 19:21:06 -0300

    em desenvolvimento

--- src/lotes/views/corte/romaneio_op_cortada.py
+++ src/lotes/views/corte/romaneio_op_cortada.py
@@ -13,8 +13,13 @@ from utils.functions import untuple_keys_concat
 from utils.views import totalize_grouped_data, group_rowspan
 
 from lotes.forms.corte.romaneio_op_cortada import RomaneioOpCortadaForm
+from lotes.models.op import OpCortada
 from lotes.queries.producao.romaneio_corte import (
     producao_ops_finalizadas,
+)
+from lotes.queries.op import (
+    op_itens,
+    op_ped_cli,
 )
 
 
@@ -29,13 +34,96 @@ class RomaneioOpCortada(O2BaseGetPostView):
         self.get_args2context = True
         self.form_class_has_initial = True
 
+    def ped_cli_por_cliente(self, pedidos_ops, itens_ops):
+        # Separa OPs por clientes e pedidos
+        clientes = {}
+        for pedido_op in pedidos_ops:
+            slug = pedido_op['cliente_slug']
+            if slug not in clientes:
+                clientes[slug] = {
+                    'cliente': pedido_op['cliente'],
+                    'pedidos': {},
+                    'ops': set(),
+                    'itens': {},
+                }
+            pedidos_do_cliente = clientes[slug]['pedidos']
+            if pedido_op['ped_cli'] not in pedidos_do_cliente:
+                pedidos_do_cliente[pedido_op['ped_cli']] = {pedido_op['op']}
+            else:
+                pedidos_do_cliente[pedido_op['ped_cli']].add(pedido_op['op'])
+            clientes[slug]['ops'].add(pedido_op['op'])
+            itens_do_cliente = clientes[slug]['itens']
+            itens_op = [
+                item_ops
+                for item_ops in itens_ops
+                if item_ops['op'] == pedido_op['op']
+            ]
+            for item_op in itens_op:
+                try:
+                    itens_do_cliente[item_op['item']] += item_op['qtd']
+                except KeyError:
+                    itens_do_cliente[item_op['item']] = item_op['qtd']
+
+        # Monta observação para pedido de faturamento filial-matriz
+        for cli in clientes:
+            cliaux = clientes[cli]
+            if cli == 'estoque':
+                ops = ', '.join(map(str, cliaux['pedidos']['-']))
+                cliaux['obs'] = f"OP({ops})"
+            else:
+                cliaux['obs'] = ''
+                sep = ''
+                for ped in cliaux['pedidos']:
+                    ops = ', '.join(map(str, cliaux['pedidos'][ped]))
+                    cliaux['obs'] += sep + f"Pedido({ped})-OP({ops})"
+                    sep = ', '
+       
+        return clientes
+
     def mount_context(self):
         self.cursor = db_cursor_so(self.request)
         locale.setlocale(locale.LC_ALL, settings.LOCAL_LOCALE)
 
+        objs = OpCortada.objects.filter(when__date__lte=self.data)
+        print(objs.query)
+        dados_ops = objs.values()
+        pprint(dados_ops)
+        ops = [
+            row['op']
+            for row in dados_ops
+        ]
+        pprint(ops)
+
+        # for row_op in dados_ops:
+        #     itens_op = op_itens.query(self.cursor, op=row_op['op'])
+        #     pprint(itens_op)
+        #     ped_cli_op = op_ped_cli.query(self.cursor, op=row_op['op'])
+        #     pprint(ped_cli_op)
+
+        itens_ops = op_itens.query(self.cursor, op=ops)
+        pprint(itens_ops)
+
+        ped_cli_ops = op_ped_cli.query(self.cursor, op=ops)
+        pprint(ped_cli_ops)
+
+        clientes = self.ped_cli_por_cliente(ped_cli_ops, itens_ops)
+        pprint(clientes)
+
+        self.context['teste'] = {
+            'headers': [
+                'Cliente',
+                'Observação',
+            ],
+            'fields': [
+                'cliente',
+                'obs',
+            ],
+            'dados': clientes.values(),
+        }
+
         dados = []
         dados, clientes = producao_ops_finalizadas.query(self.cursor, self.data, para_nf=True)
-        pprint(dados)
+        pprint(dados[:2])
         pprint(clientes)
 
         if not dados:
