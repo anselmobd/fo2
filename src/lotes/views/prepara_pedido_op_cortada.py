bf245dcd48c4bfa5806279131734c06343099afd
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2023-02-24 18:31:48 -0300

    primeira versão funcional, mas ainda não gravando números de pedidos no model OpCortada

--- src/lotes/views/prepara_pedido_op_cortada.py
+++ src/lotes/views/prepara_pedido_op_cortada.py
@@ -7,14 +7,14 @@ from django.views import View
 
 from fo2.connections import db_cursor_so
 
-import lotes.queries
+from lotes.queries.pedido import ped_inform
+from lotes.models.op import OpCortada
 from lotes.queries.pedido.ped_alter import (
     altera_pedido,
     altera_pedido_itens,
 )
 from lotes.queries.op import ped_cli_por_cliente
 from lotes.queries.pedido.mensagem_nf import cria_mens_nf
-from lotes.queries.producao.romaneio_corte import producao_ops_finalizadas
 from lotes.views.prepara_pedido_compra_matriz import cria_pedido_compra_matriz
 
 
@@ -33,12 +33,10 @@ class PreparaPedidoOpCortada(View):
         clientes = ped_cli_por_cliente.get_cached(
             cursor, dt=data, cliente_slug=cliente)
 
-        pprint(clientes)
-
         if not clientes:
             return ('ERRO', f"Dados de '{cliente}' não encontrados!")
     
-        dados = lotes.queries.pedido.ped_inform(cursor, pedido, empresa=3)
+        dados = ped_inform(cursor, pedido, empresa=3)
         if not dados:
             return ('ERRO', "Pedido não encontrado!")
         if dados[0]['COD_CANCELAMENTO'] != 0:
@@ -49,36 +47,34 @@ class PreparaPedidoOpCortada(View):
             if re.search('^\[MPCFM\] ', dados[0]['OBSERVACAO']):
                 return ('ERRO', "Pedido já preparado!")
 
-        # dados, clientes = producao_ops_finalizadas.query(
-        #     cursor, data, para_nf=True, cliente_slug=cliente)
-
         #   exemplos de observações:
-        # MOPCFM - Movimentação de OPs Cortadas da Filial p/ Matriz; Data: 2022-03-16
-        # Produção para o cliente Renner. Pedido(5214524)-OP(34023), Pedido(5214547)-OP(34027)
+        # [MOPCFM] Movimentacao de OPs Cortadas da Filial para Matriz; Data: 2022-03-16
+        # Produção para estoque: OP(34082, 34307, 34339, 34262, 34297)
         #   ou
-        # MOPCFM - Movimentação de OPs Cortadas da Filial p/ Matriz; Data: 2022-03-16
-        # Produção para estoque. OP(34082, 34307, 34339, 34262, 34297)
+        # [MOPCFM] Movimentacao de OPs Cortadas da Filial para Matriz; Data: 2022-03-16
+        # Produção para o cliente Renner: Pedido(5214524)-OP(34023), Pedido(5214547)-OP(34027)
 
         if cliente == 'estoque':
             observacao = (
-                f"[MPCFM] Movimentacao de Pecas Cortadas da Filial para Matriz; Data: {data}",
+                f"[MOPCFM] Movimentacao de OPs Cortadas da Filial para Matriz; Data: {data}",
                 f"Producao para estoque: {clientes[cliente]['obs']}",
             )
         else:
             observacao = (
-                f"[MPCFM] Movimentacao de Pecas Cortadas da Filial para Matriz; Data: {data}",
+                f"[MOPCFM] Movimentacao de OPs Cortadas da Filial para Matriz; Data: {data}",
                 f"Producao para o cliente {cliente.capitalize()}: {clientes[cliente]['obs']}",
             )
 
         cria_mens_nf(cursor, pedido, observacao)
-        return ('ERRO', "Em desenvolvimento!")
 
-        altera_pedido_itens(cursor, pedido, 302, 'RJ', dados)
+        altera_pedido_itens(cursor, pedido, 302, 'RJ', clientes[cliente]['dados'])
 
         qtd_itens = 0
-        for row in dados:
+        for row in clientes[cliente]['dados']:
             qtd_itens += row['mov_qtd']
         altera_pedido(cursor, data, pedido, 3, qtd_itens, "\n".join(observacao))
+
+        # OpCortada
 
         if not cria_pedido_compra_matriz(cursor, pedido):
             return ('ERRO', "Algum erro ocorreu durante a criação do pedido de compra!")
