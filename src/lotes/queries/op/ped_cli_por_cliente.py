e50af7d183c8ed090e43b0a7b7ec8d2f7062c200
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2023-02-24 09:47:41 -0300

    separando função ped_cli_por_cliente

---
+++ src/lotes/queries/op/ped_cli_por_cliente.py
@@ -0,0 +1,74 @@
+from collections import OrderedDict
+from pprint import pprint
+
+from lotes.models.op import OpCortada
+
+from lotes.queries.op import (
+    op_itens,
+    op_ped_cli,
+)
+
+__all__ = ['mount']
+
+
+def ped_cli_por_cliente(pedidos_ops, itens_ops):
+    # Separa OPs por clientes e pedidos
+    clientes = {}
+    for pedido_op in pedidos_ops:
+        slug = pedido_op['cliente_slug']
+        if slug not in clientes:
+            clientes[slug] = {
+                'cliente': pedido_op['cliente'],
+                'pedidos': {},
+                'ops': set(),
+            }
+        pedidos_do_cliente = clientes[slug]['pedidos']
+        if pedido_op['ped_cli'] not in pedidos_do_cliente:
+            pedidos_do_cliente[pedido_op['ped_cli']] = {pedido_op['op']}
+        else:
+            pedidos_do_cliente[pedido_op['ped_cli']].add(pedido_op['op'])
+        clientes[slug]['ops'].add(pedido_op['op'])
+    # Monta dados para pedido de faturamento filial-matriz
+    for cli in clientes:
+        cli_dict = clientes[cli]
+        # Monta observação
+        if cli == 'estoque':
+            ops = ', '.join(map(str, cli_dict['pedidos']['-']))
+            cli_dict['obs'] = f"OP({ops})"
+        else:
+            cli_dict['obs'] = ''
+            sep = ''
+            for ped in cli_dict['pedidos']:
+                ops = ', '.join(map(str, cli_dict['pedidos'][ped]))
+                cli_dict['obs'] += sep + f"Pedido({ped})-OP({ops})"
+                sep = ', '
+        # Monta itens
+        itens_ops_cli = [
+            item_ops
+            for item_ops in itens_ops
+            if item_ops['op'] in cli_dict['ops']
+        ]
+        cli_dict['itens'] = OrderedDict()
+        for item_op in itens_ops_cli:
+            try:
+                cli_dict['itens'][item_op['item']] += item_op['qtd']
+            except KeyError:
+                cli_dict['itens'][item_op['item']] = item_op['qtd']
+    return clientes
+
+
+def mount(cursor, dt):
+    """Monta informações para pedidos para NF filial-matriz"""
+    
+    dados_ops = OpCortada.objects.filter(when__date__lte=dt)
+    dados_ops = dados_ops.values()
+    ops = [
+        row['op']
+        for row in dados_ops
+    ]
+
+    itens_ops = op_itens.query(cursor, op=ops)
+
+    ped_cli_ops = op_ped_cli.query(cursor, op=ops)
+
+    return ped_cli_por_cliente(ped_cli_ops, itens_ops)
--- src/lotes/views/corte/romaneio_op_cortada.py
+++ src/lotes/views/corte/romaneio_op_cortada.py
@@ -1,6 +1,5 @@
 import datetime
 import locale
-from collections import OrderedDict
 from pprint import pprint
 
 from django.conf import settings
@@ -14,10 +13,8 @@ from utils.functions import untuple_keys_concat
 from utils.views import totalize_grouped_data, group_rowspan
 
 from lotes.forms.corte.romaneio_op_cortada import RomaneioOpCortadaForm
-from lotes.models.op import OpCortada
 from lotes.queries.op import (
-    op_itens,
-    op_ped_cli,
+    ped_cli_por_cliente,
 )
 
 __all__ = ['RomaneioOpCortada']
@@ -34,67 +31,11 @@ class RomaneioOpCortada(O2BaseGetPostView):
         self.get_args2context = True
         self.form_class_has_initial = True
 
-    def ped_cli_por_cliente(self, pedidos_ops, itens_ops):
-        # Separa OPs por clientes e pedidos
-        clientes = {}
-        for pedido_op in pedidos_ops:
-            slug = pedido_op['cliente_slug']
-            if slug not in clientes:
-                clientes[slug] = {
-                    'cliente': pedido_op['cliente'],
-                    'pedidos': {},
-                    'ops': set(),
-                }
-            pedidos_do_cliente = clientes[slug]['pedidos']
-            if pedido_op['ped_cli'] not in pedidos_do_cliente:
-                pedidos_do_cliente[pedido_op['ped_cli']] = {pedido_op['op']}
-            else:
-                pedidos_do_cliente[pedido_op['ped_cli']].add(pedido_op['op'])
-            clientes[slug]['ops'].add(pedido_op['op'])
-        # Monta dados para pedido de faturamento filial-matriz
-        for cli in clientes:
-            cli_dict = clientes[cli]
-            # Monta observação
-            if cli == 'estoque':
-                ops = ', '.join(map(str, cli_dict['pedidos']['-']))
-                cli_dict['obs'] = f"OP({ops})"
-            else:
-                cli_dict['obs'] = ''
-                sep = ''
-                for ped in cli_dict['pedidos']:
-                    ops = ', '.join(map(str, cli_dict['pedidos'][ped]))
-                    cli_dict['obs'] += sep + f"Pedido({ped})-OP({ops})"
-                    sep = ', '
-            # Monta itens
-            itens_ops_cli = [
-                item_ops
-                for item_ops in itens_ops
-                if item_ops['op'] in cli_dict['ops']
-            ]
-            cli_dict['itens'] = OrderedDict()
-            for item_op in itens_ops_cli:
-                try:
-                    cli_dict['itens'][item_op['item']] += item_op['qtd']
-                except KeyError:
-                    cli_dict['itens'][item_op['item']] = item_op['qtd']
-        return clientes
-
     def mount_context(self):
         self.cursor = db_cursor_so(self.request)
         locale.setlocale(locale.LC_ALL, settings.LOCAL_LOCALE)
 
-        dados_ops = OpCortada.objects.filter(when__date__lte=self.data)
-        dados_ops = dados_ops.values()
-        ops = [
-            row['op']
-            for row in dados_ops
-        ]
-
-        itens_ops = op_itens.query(self.cursor, op=ops)
-
-        ped_cli_ops = op_ped_cli.query(self.cursor, op=ops)
-
-        clientes = self.ped_cli_por_cliente(ped_cli_ops, itens_ops)
+        clientes = ped_cli_por_cliente.mount(self.cursor, dt=self.data)
 
         cli_dados = []
         for cli_vals in clientes.values():
