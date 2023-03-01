3534db41b7502145360fd73e33b2c1705b07b9e9
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2023-03-01 10:43:37 -0300

    criada class Fo2ViewMethods para acomodar os métodos cleanned_fields_to_context e context_to_form_post

--- src/utils/functions/views.py
+++ src/utils/functions/views.py
@@ -15,3 +15,20 @@ def context_to_form_post(self):
     for field in self.context['form'].fields:
         if isinstance(self.context[field], datetime.date):
             self.context[field] = self.context[field].isoformat()
+
+
+class Fo2ViewMethods():
+
+    def cleanned_fields_to_context(self):
+        '''
+            Assume que há self.context['form'] e cria um
+            self.context['field'] para cada field no form
+        '''
+        for field in self.context['form'].fields:
+            self.context[field] = self.context['form'].cleaned_data[field]
+
+
+    def context_to_form_post(self):
+        for field in self.context['form'].fields:
+            if isinstance(self.context[field], datetime.date):
+                self.context[field] = self.context[field].isoformat()
