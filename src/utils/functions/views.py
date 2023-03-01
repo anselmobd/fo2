a4ff412e6aded432ceae4aca90fb22024a131813
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2023-03-01 11:12:23 -0300

    eliminando funções que passaram para dentro da classe Fo2ViewMethods

--- src/utils/functions/views.py
+++ src/utils/functions/views.py
@@ -2,19 +2,7 @@ import datetime
 from pprint import pprint
 
 
-def cleanned_fields_to_context(self):
-    '''
-        Assume que há self.context['form'] e cria um
-        self.context['field'] para cada field no form
-    '''
-    for field in self.context['form'].fields:
-        self.context[field] = self.context['form'].cleaned_data[field]
-
-
-def context_to_form_post(self):
-    for field in self.context['form'].fields:
-        if isinstance(self.context[field], datetime.date):
-            self.context[field] = self.context[field].isoformat()
+__all__ = ['Fo2ViewMethods']
 
 
 class Fo2ViewMethods():
