250f8c805df406f3cd053b9719737ea453308e75
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2023-03-10 09:21:51 -0300

    op_cortada.query preparada para receber filtro por uma OP

--- src/lotes/queries/producao/romaneio_corte/op_cortada.py
+++ src/lotes/queries/producao/romaneio_corte/op_cortada.py
@@ -4,10 +4,13 @@ from utils.functions.models.dictlist import dictlist_lower
 from utils.functions.queries import debug_cursor_execute
 
 
-def query(cursor, data_de=None, data_ate=None):
+def query(cursor, data_de=None, data_ate=None, op1=None):
     """Lista OPs com alguma movimentação no estágio 15
     na data ou período informado
     """
+
+    if not any([data_de, data_ate, op1]):
+        return []
 
     if not data_ate:
         data_ate = data_de
@@ -20,36 +23,41 @@ def query(cursor, data_de=None, data_ate=None):
         f"DATE '{data_ate}'"
     ) if data_ate else 'NULL'
 
+    op1_value = (
+        f"{op1}"
+    ) if op1 else 'NULL'
+
     sql = f'''
         WITH
-          filtro AS 
+          filtro AS
         (
-          SELECT 
+          SELECT
             15 EST
-          , {data_de_value} DT_DE 
-          , {data_ate_value} DT_ATE 
-          FROM dual 
-          WHERE {data_de_value} IS NOT NULL
+          , {data_de_value} DT_DE
+          , {data_ate_value} DT_ATE
+          , {op1_value} OP1
+          FROM dual
         )
-        , op_com_15 AS 
+        , op_com_15 AS
         -- seleciona OPs com estágio indicado no filtro e
         -- conta o total de lotes por OP
         (
-          SELECT DISTINCT 
+          SELECT DISTINCT
             l.ORDEM_PRODUCAO OP
           , COUNT(*) lotes
           FROM filtro, pcpc_040 l
           WHERE l.CODIGO_ESTAGIO = filtro.EST
-          GROUP BY 
+            AND (filtro.OP1 IS NULL OR l.ORDEM_PRODUCAO = filtro.OP1)
+          GROUP BY
             l.ORDEM_PRODUCAO
         )
         --SELECT * FROM op_com_15;
-        , op_corte_dt AS 
+        , op_corte_dt AS
         -- Para as OPs selecionas acima, conta quantos lotes tem corte
         -- no período de datas indicado no filtro e devolve apenas OPs com
         -- essa quantidade diferente de zero, lista também as datas em questão
         (
-          SELECT DISTINCT 
+          SELECT DISTINCT
             MAX(ml.DATA_PRODUCAO) DT_CORTE
           , op15.op
           , op15.lotes
@@ -62,22 +70,22 @@ def query(cursor, data_de=None, data_ate=None):
           JOIN pcpc_045 ml
             ON ml.ORDEM_PRODUCAO = op15.OP
            AND ml.PCPC040_ESTCONF = fi.EST
-           AND ml.DATA_PRODUCAO >= fi.DT_DE
-           AND ml.DATA_PRODUCAO <= fi.DT_ATE
+           AND (fi.DT_DE IS NULL OR ml.DATA_PRODUCAO >= fi.DT_DE)
+           AND (fi.DT_ATE IS NULL OR ml.DATA_PRODUCAO <= fi.DT_ATE)
           HAVING
             SUM(COALESCE(ml.QTDE_PRODUZIDA, 0)) > 0
-          GROUP BY 
+          GROUP BY
             op15.op
           , op15.lotes
           , fi.DT_DE
           , fi.DT_ATE
-          ORDER BY 
+          ORDER BY
             1 DESC  -- DT_CORTE
           , op15.op DESC
         )
         SELECT
           ocdt.*
-        , ( SELECT 
+        , ( SELECT
               COUNT(*)
             FROM filtro fi
             JOIN pcpc_040 l
@@ -90,7 +98,7 @@ def query(cursor, data_de=None, data_ate=None):
         , ped.COD_PED_CLIENTE PED_CLI
         , CASE WHEN c.NOME_CLIENTE IS NULL THEN
             NULL
-          ELSE 
+          ELSE
             c.NOME_CLIENTE
             || ' (' || lpad(c.CGC_9, 8, '0')
             || '/' || lpad(c.CGC_4, 4, '0')
