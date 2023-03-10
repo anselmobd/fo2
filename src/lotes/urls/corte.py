1f379ccfe6e026f9e189528f0ffd357676b753c9
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2023-03-10 17:40:23 -0300

    evitando repetição de string que identifica o tópico dos nomes das urls

--- src/lotes/urls/corte.py
+++ src/lotes/urls/corte.py
@@ -10,41 +10,46 @@ from lotes.views.corte import (
     romaneio_corte,
 )
 
+
+def name(name):
+    return f"corte-{name}"
+
+
 urlpatterns = [
     path(
         "ajax_marca_op_cortada/<int:op>/",
         ajax_marca_op_cortada.AjaxMarcaOpCortadaView.as_view(),
-        name='corte-ajax_marca_op_cortada',
+        name=name('ajax_marca_op_cortada'),
     ),
     path(
         "envio_insumo/",
         envio_insumo.EnvioInsumo.as_view(),
-        name='corte-envio_insumo',
+        name=name('envio_insumo'),
     ),
     path(
         "gera_pedido_op/",
         gera_pedido_op.GeraPedidoOpView.as_view(),
-        name='corte-gera_pedido_op',
+        name=name('gera_pedido_op'),
     ),
     re_path(
         r"^informa_nf_envio/(?P<empresa>[0-9]+)/(?P<nf>[0-9]+)/"
         r"(?P<nf_ser>[0-9]+)/(?P<cnpj>[^/]+)/$",
         informa_nf_envio.InformaNfEnvio.as_view(),
-        name='corte-informa_nf_envio',
+        name=name('informa_nf_envio'),
     ),
     re_path(
         r"^marca_op_cortada/(?P<data>[^/]+)?$",
         marca_op_cortada.MarcaOpCortadaView.as_view(),
-        name='corte-marca_op_cortada',
+        name=name('marca_op_cortada'),
     ),
     path(
         "pedidos_gerados/",
         pedidos_gerados.PedidosGeradosView.as_view(),
-        name='corte-pedidos_gerados',
+        name=name('pedidos_gerados'),
     ),
     path(
         "romaneio_corte/",
         romaneio_corte.RomaneioCorte.as_view(),
-        name='corte-romaneio_corte',
+        name=name('romaneio_corte'),
     ),
 ]
