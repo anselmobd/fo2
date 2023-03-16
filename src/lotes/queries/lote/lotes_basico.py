b4d6b0d84cd9f29d7e7557d380bb3f5b96099981
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2023-03-16 11:15:21 -0300

    lotes_basico.query faltavam campos que a tela "Etiqueta de caixa lotes" precisa

--- src/lotes/queries/lote/lotes_basico.py
+++ src/lotes/queries/lote/lotes_basico.py
@@ -23,7 +23,7 @@ def query(
               l.ORDEM_PRODUCAO
             , l.PROCONF_GRUPO
             , l.PROCONF_ITEM
-            , t.ORDEM_TAMANHO
+            , ot.ORDEM_TAMANHO
             , l.PERIODO_PRODUCAO
             , l.ORDEM_CONFECCAO
         '''
@@ -34,17 +34,30 @@ def query(
         , l.PERIODO_PRODUCAO PERIODO
         , l.ORDEM_CONFECCAO OC
         , r.COLECAO
+        , r.DESCR_REFERENCIA
         , l.PROCONF_GRUPO REF
         , l.PROCONF_SUBGRUPO TAM
-        , t.ORDEM_TAMANHO
+        , ot.ORDEM_TAMANHO
+        , t.DESCR_TAM_REFER DESCR_TAMANHO
         , l.PROCONF_ITEM COR
+        , i.DESCRICAO_15 DESCR_COR
+        , i.NARRATIVA
         , l.QTDE_PECAS_PROG QTD
         FROM PCPC_040 l
         JOIN BASI_030 r
           ON r.NIVEL_ESTRUTURA = '1'
          AND r.REFERENCIA = l.PROCONF_GRUPO
-        LEFT JOIN BASI_220 t
-          ON t.TAMANHO_REF = l.PROCONF_SUBGRUPO
+        JOIN BASI_020 t
+          ON t.BASI030_NIVEL030 = '1'
+         AND t.BASI030_REFERENC = l.PROCONF_GRUPO
+         AND t.TAMANHO_REF = l.PROCONF_SUBGRUPO
+        JOIN BASI_010 i
+          ON i.NIVEL_ESTRUTURA = '1'
+         AND i.GRUPO_ESTRUTURA = l.PROCONF_GRUPO
+         AND i.SUBGRU_ESTRUTURA = l.PROCONF_SUBGRUPO
+         AND i.ITEM_ESTRUTURA = l.PROCONF_ITEM
+        LEFT JOIN BASI_220 ot
+          ON ot.TAMANHO_REF = l.PROCONF_SUBGRUPO
         WHERE 1=1
           {filtra_op} -- filtra_op
           AND l.SEQUENCIA_ESTAGIO = 1
