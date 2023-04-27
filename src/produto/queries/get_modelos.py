394e6d46b8ba378e0b1e261727f049081a8f0a1b
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2023-04-26 21:02:16 -0300

    criada query get_modelos_query

---
+++ src/produto/queries/get_modelos.py
@@ -0,0 +1,20 @@
+from pprint import pprint
+
+from systextil.queries.produto.modelo import sql_sele_modeloint_ref
+from utils.functions.models.dictlist import dictlist_lower
+from utils.functions.queries import debug_cursor_execute
+
+
+def get_modelos_query(cursor):
+    sql = f"""
+        SELECT DISTINCT
+          {sql_sele_modeloint_ref('r.REFERENCIA')}
+        FROM BASI_030 r -- ref
+        WHERE r.NIVEL_ESTRUTURA = 1
+          AND r.DESCR_REFERENCIA NOT LIKE '-%'
+          AND r.REFERENCIA < 'C0000'
+        ORDER BY
+          1
+    """
+    debug_cursor_execute(cursor, sql)
+    return dictlist_lower(cursor)
