5f96f3fa6e02a6eb57103bcb2af5831397e48998
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2023-05-26 14:21:54 -0300

    view EnderecaGrupo montando lista de endereços

--- src/cd/views/endereca_grupo.py
+++ src/cd/views/endereca_grupo.py
@@ -29,6 +29,23 @@ class EnderecaGrupo(O2BaseGetPostView):
     def mount_context(self):
         cursor = db_cursor_so(self.request)
 
+        fixo, *variavel = self.endereco.split("|")
+        try:
+            inicio, *final = variavel[0].split("-")
+            inicio = int(inicio)
+            final = int(final[0])
+            incr = final - inicio
+            incr = incr // abs(incr)
+        except Exception:
+            self.context.update({
+                'msgerr': "Mascara de endereço inválida"
+            })
+            return
+
+        end_list = []
+        for num in range(inicio, final+incr, incr):
+            end_list.append(f"{fixo}{num}")
+
         palete_list = []
 
         for line in map(str.strip, self.paletes.split("\n")):
@@ -44,20 +61,27 @@ class EnderecaGrupo(O2BaseGetPostView):
                 palete = Plt().mount(line)
             palete_list.append(palete)
 
-        data = [
-            {
+        if len(end_list) != len(palete_list):
+            self.context.update({
+                'msgerr': "Número de paletes diferente do número de endereços"
+            })
+
+        data = []
+        for num in range(max(len(end_list), len(palete_list))):
+            data.append({
                 'num': num + 1,
-                'palete': palete,
-            }
-            for num, palete in enumerate(palete_list)
-        ]
+                'palete': palete_list[num] if num < len(palete_list) else "?",
+                'endereco': end_list[num] if num < len(end_list) else "?",
+            })
         headers = [
             '#',
             'Palete',
+            'Endereço',
         ]
         fields = [
             'num',
             'palete',
+            'endereco',
         ]
 
         self.context.update({
