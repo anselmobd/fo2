2d471ce296758c6111ae4d502891621188e0cde3
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2023-08-01 13:02:09 -0300

    split_numbers parâmetro negative

--- src/utils/functions/strings.py
+++ src/utils/functions/strings.py
@@ -106,16 +106,20 @@ def split_strip(text, sep):
     ]
 
 
-def split_numbers(text):
+def split_numbers(text, negative=False):
     groups = []
     number = ""
+    last_non_digit = ""
     for c in f"{text} ":
         if c.isdigit():
             number = f"{number}{c}"
         else:
             if number:
+                if negative and last_non_digit == "-":
+                    number = f"-{number}"
                 groups.append(number)
                 number = ""
+            last_non_digit = c
     return groups
 
 
