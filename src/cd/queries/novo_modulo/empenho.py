22b87e7d0bcdb936f317cdb4972c665b99bf398c
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2023-08-08 18:15:29 -0300

    criada função para excluir solicitação

--- src/cd/queries/novo_modulo/empenho.py
+++ src/cd/queries/novo_modulo/empenho.py
@@ -58,3 +58,33 @@ def insere(
         )
     """
     debug_cursor_execute(cursor, sql, exec=exec)
+
+
+def exclui(
+    cursor,
+    ordem_producao,
+    ordem_confeccao,
+    pedido_destino,
+    op_destino,
+    oc_destino,
+    grupo_destino,
+    alter_destino,
+    sub_destino,
+    cor_destino,
+    exec=True,
+):
+    sql = f"""
+        DELETE FROM SYSTEXTIL.PCPC_044
+        WHERE 1=1
+          AND ORDEM_PRODUCAO = {ordem_producao}
+          AND ORDEM_CONFECCAO = {ordem_confeccao}
+          AND PEDIDO_DESTINO = {pedido_destino}
+          AND OP_DESTINO = {op_destino}
+          AND OC_DESTINO = {oc_destino}
+          AND DEP_DESTINO = 0
+          AND GRUPO_DESTINO = '{grupo_destino}'
+          AND ALTER_DESTINO = {alter_destino}
+          AND SUB_DESTINO = '{sub_destino}'
+          AND COR_DESTINO = '{cor_destino}'
+    """
+    debug_cursor_execute(cursor, sql, exec=exec)
