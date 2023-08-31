2ce2898d58fbba8696353e87e954b93136600001
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2023-08-31 13:28:56 -0300

    DistribuicaoForm utilizando FormWidgetAttrs

--- src/cd/forms/distribuiccao.py
+++ src/cd/forms/distribuiccao.py
@@ -2,8 +2,12 @@ from pprint import pprint
 
 from django import forms
 
+from o2.forms.widget_attrs import FormWidgetAttrs
+
 
 class DistribuicaoForm(forms.Form):
+    a = FormWidgetAttrs()
+
     modelo = forms.CharField(
         required=False,
         min_length=1,
@@ -11,8 +15,8 @@ class DistribuicaoForm(forms.Form):
         widget=forms.TextInput(
             attrs={
                 'size': 5,
-                'type': 'number',
-                'placeholder': '0',
+                **a.number,
+                **a.autofocus,
             }
         )
     )
