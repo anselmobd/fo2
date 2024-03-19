238ce90c7993443ae7be93b79ff4bfbd57d24c5c
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2024-03-19 15:57:42 -0300

    query posicao_historico prevendo PCPC_045.USUARIO_SYSTEXTIL com ":" e indicação de PROCESSO_SYSTEXTIL correto ou alternativo

--- src/lotes/queries/lote/posicao_historico.py
+++ src/lotes/queries/lote/posicao_historico.py
@@ -2,7 +2,7 @@ from utils.functions.models.dictlist import dictlist
 
 
 def posicao_historico(cursor, periodo, ordem_confeccao):
-    sql = '''
+    sql = f'''
         SELECT
           TO_CHAR(h.DATA_INSERCAO, 'DD/MM/YYYY HH24:MI:SS') DT
         , h.DATA_PRODUCAO DT_PROD
@@ -14,22 +14,29 @@ def posicao_historico(cursor, periodo, ordem_confeccao):
         , h.QTDE_CONSERTO Q_C
         , h.QTDE_PERDAS Q_P
         , coalesce(u.USUARIO, ' ') USU
-        , coalesce(p.CODIGO_PROGRAMA, ' ') PRG
+        , CASE WHEN h.USUARIO_SYSTEXTIL LIKE '%:%'
+          THEN SUBSTR(h.USUARIO_SYSTEXTIL, INSTR(h.USUARIO_SYSTEXTIL, ':') + 1)
+          ELSE h.PROCESSO_SYSTEXTIL
+          END PRG
         , coalesce(p.DESCRICAO, ' ') PRG_DESCR
         FROM PCPC_045 h
         LEFT JOIN HDOC_036 p
-          ON p.CODIGO_PROGRAMA = h.PROCESSO_SYSTEXTIL
+          ON p.CODIGO_PROGRAMA =
+             CASE WHEN h.USUARIO_SYSTEXTIL LIKE '%:%'
+             THEN SUBSTR(h.USUARIO_SYSTEXTIL, INSTR(h.USUARIO_SYSTEXTIL, ':') + 1)
+             ELSE h.PROCESSO_SYSTEXTIL
+             END
          AND p.LOCALE = 'pt_BR'
          AND SUBSTR(h.USUARIO_SYSTEXTIL,0,1) != '*'
         LEFT JOIN HDOC_030 u
           ON u.EMPRESA = 1
          AND u.CODIGO_USUARIO = h.CODIGO_USUARIO
-        WHERE h.PCPC040_PERCONF = %s
-          AND h.PCPC040_ORDCONF = %s
+        WHERE h.PCPC040_PERCONF = {periodo}
+          AND h.PCPC040_ORDCONF = {ordem_confeccao}
         ORDER BY
           h.DATA_INSERCAO
         , h.PCPC040_ESTCONF
         , h.SEQUENCIA
     '''
-    cursor.execute(sql, [periodo, ordem_confeccao])
+    cursor.execute(sql)
     return dictlist(cursor)
