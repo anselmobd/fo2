9784c62810258d2148dc79ed593e7ad5ff32bf6c
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2024-03-19 20:01:14 -0300

    ProduzLote se não achar movimentações do estágio, busca anteriores

--- src/lotes/views/ajax/produz_lote.py
+++ src/lotes/views/ajax/produz_lote.py
@@ -10,6 +10,10 @@ from lotes.queries.lote import movimentacao_de_lote
 
 class ProduzLote(View):
 
+    ERROR_STATUS = "ERROR"
+    OK_STATUS = "OK"
+
+
     def __init__(self):
         self._cursor = None
 
@@ -19,27 +23,60 @@ class ProduzLote(View):
             self._cursor = db_cursor_so(self.request)
         return self._cursor 
 
-    def get_movimentacoes(self, kwargs):
-        self.movimentacoes = None
+    def get_movimentacoes(self, lote, estagio):
         try:
             self.movimentacoes = movimentacao_de_lote.get_movimentacoes(
-                self.cursor, kwargs['lote'], kwargs['estagio'])
+                cursor=self.cursor,
+                lote=lote,
+                estagio=estagio,
+            )
         except Exception as e:
-            return ('ERROR',  f"Erro ao buscar movimentações do lote no estágio: {e}")
+            return f"Erro ao buscar movimentações do lote no estágio: {e}"
+
+    def get_movimentacoes_estagio_anterior(self, lote, estagio):
+        try:
+            self.movimentacoes = movimentacao_de_lote.get_movimentacoes_estagio_anterior(
+                cursor=self.cursor,
+                lote=lote,
+                estagio=estagio,
+            )
+        except Exception as e:
+            return f"Erro ao buscar movimentações em estágios anteriores do lote no estágio: {e}"
 
     def process(self, kwargs):
-        self.get_movimentacoes()
+        self.movimentacoes = None
+
+        result_error = self.get_movimentacoes(
+            lote=kwargs['lote'],
+            estagio=kwargs['estagio'],
+        )
+        if result_error:
+            return (self.ERROR_STATUS, result_error)
+
+        if not self.movimentacoes:
+            result_error = self.get_movimentacoes_estagio_anterior(
+                lote=kwargs['lote'],
+                estagio=kwargs['estagio'],
+            )
+            if result_error:
+                return (self.ERROR_STATUS, result_error)
 
         if not self.movimentacoes:
             return (
                 'ERROR',
-                "Só é possível inserir movimentação se já houver movimentação no estágio",
+                "Só é possível inserir movimentação se já houver movimentação no lote",
             )
 
+        pprint(self.movimentacoes)
+
         try:
-            1/0
             movimentacao_de_lote.insere(
-                self.cursor, kwargs['lote'], kwargs['estagio'], kwargs['qtd'])
+                cursor=self.cursor,
+                lote=kwargs['lote'],
+                estagio=kwargs['estagio'],
+                qtd=kwargs['qtd'],
+                estagio_modelo=self.movimentacoes[-1]['pcpc040_estconf'],
+            )
             return ('OK', "OK!")
         except Exception as e:
             return ('ERROR',  f"Erro ao inserir movimentação de lote: {e}")
