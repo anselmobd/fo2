424a6d7fa2eca1cb691a63a4a891b06dad800f4d
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2024-03-19 20:39:42 -0300

    criado lotes.queries.lote.lote_estagios.get_estagios_str

--- src/lotes/queries/lote/lote_estagios.py
+++ src/lotes/queries/lote/lote_estagios.py
@@ -3,9 +3,14 @@ from pprint import pprint
 import lotes.queries
 
 
-def query(cursor, lote):
+def get_estagios(cursor, lote):
     dados = lotes.queries.lote.posicao_so_estagios(cursor, lote[:4], lote[4:])
     return [
       row['COD_EST']
       for row in dados
     ]
+
+
+def get_estagios_str(cursor, lote):
+    estagios = get_estagios(cursor, lote)
+    return list(map(str, estagios))
