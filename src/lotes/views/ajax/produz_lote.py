ad8aa05c30657764ae3a1aa84ac6e4698fc2b359
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2024-03-19 22:37:27 -0300

    fazendo verificações de estágios, inclusive quantidade disponível, e utilizando get_movimentacoes_estagios

--- src/lotes/views/ajax/produz_lote.py
+++ src/lotes/views/ajax/produz_lote.py
@@ -6,6 +6,7 @@ from django.views import View
 from fo2.connections import db_cursor_so
 
 from lotes.queries.lote import movimentacao_de_lote
+from lotes.queries.lote import lote_estagios
 
 
 class ProduzLote(View):
@@ -13,17 +14,42 @@ class ProduzLote(View):
     ERROR_STATUS = "ERROR"
     OK_STATUS = "OK"
 
+    def verifica_estagios(self):
+        self.estagios = lote_estagios.get_estagios(
+            self.cursor, self.lote, ['COD_EST', 'Q_AP', 'SEQ_EST'], cod_est_fn=str)
+        pprint(self.estagios)
+        if not self.estagios:
+            return "Lote não encontrado"
+        self.processa_estagios()
+        if self.estagio_selecionado['Q_AP'] < self.qtd:
+            return f"Estágio {self.estagio} do lote {self.lote} com quantidade menor do que {self.qtd}"
+
+    def processa_estagios(self):
+        self.estagio_selecionado = next(
+            estagio
+            for estagio in self.estagios
+            if estagio['COD_EST'] == self.estagio
+        )
+        pprint(self.estagio_selecionado['SEQ_EST'])
+        self.estagios_filtrados = [
+            estagio['COD_EST']
+            for estagio in self.estagios
+            if estagio['SEQ_EST'] <= self.estagio_selecionado['SEQ_EST']
+        ]
+        pprint(self.estagios_filtrados)
+
     def get_movimentacoes(self):
         try:
-            self.movimentacoes = movimentacao_de_lote.get_movimentacoes_ate_estagio(
+            self.movimentacoes = movimentacao_de_lote.get_movimentacoes_estagios(
                 cursor=self.cursor,
                 lote=self.lote,
-                estagio=self.estagio,
+                estagios=self.estagios_filtrados,
             )
         except Exception as e:
             return f"Erro ao buscar movimentações do lote no estágio: {e}"
         if not self.movimentacoes:
             return "Só é possível inserir movimentação se já houver movimentação no lote"
+        pprint(self.movimentacoes)
 
     def insere_movimentacao(self):
         try:
@@ -38,11 +64,13 @@ class ProduzLote(View):
             return f"Erro ao inserir movimentação de lote: {e}"
 
     def process(self):
-        result_error = self.get_movimentacoes()
+        result_error = self.verifica_estagios()
         if result_error:
             return (self.ERROR_STATUS, result_error)
 
-        pprint(self.movimentacoes)
+        result_error = self.get_movimentacoes()
+        if result_error:
+            return (self.ERROR_STATUS, result_error)
 
         result_error = self.insere_movimentacao()
         if result_error:
@@ -62,8 +90,7 @@ class ProduzLote(View):
         self.cursor = db_cursor_so(request)
         self.lote=kwargs['lote']
         self.estagio=kwargs['estagio']
-        self.qtd=kwargs['qtd']
-        self.movimentacoes = None
+        self.qtd=int(kwargs['qtd'])
 
     def get(self, request, *args, **kwargs):
         self.inicializa_variaveis(request, kwargs)
