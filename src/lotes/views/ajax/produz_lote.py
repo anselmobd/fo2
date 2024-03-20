f1c14973bfc32cbc5cf7ec94ff2f9f793145c2e8
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2024-03-19 21:13:38 -0300

    code

--- src/lotes/views/ajax/produz_lote.py
+++ src/lotes/views/ajax/produz_lote.py
@@ -13,51 +13,42 @@ class ProduzLote(View):
     ERROR_STATUS = "ERROR"
     OK_STATUS = "OK"
 
-
-    def __init__(self):
-        self._cursor = None
-
-    @property
-    def cursor(self):
-        if not self._cursor:
-            self._cursor = db_cursor_so(self.request)
-        return self._cursor 
-
-    def get_movimentacoes(self, lote, estagio):
+    def get_movimentacoes(self):
         try:
             self.movimentacoes = movimentacao_de_lote.get_movimentacoes_ate_estagio(
                 cursor=self.cursor,
-                lote=lote,
-                estagio=estagio,
+                lote=self.lote,
+                estagio=self.estagio,
             )
         except Exception as e:
             return f"Erro ao buscar movimentações do lote no estágio: {e}"
         if not self.movimentacoes:
             return "Só é possível inserir movimentação se já houver movimentação no lote"
 
-    def process(self, kwargs):
-        self.movimentacoes = None
+    def insere_movimentacao(self):
+        try:
+            movimentacao_de_lote.insere(
+                cursor=self.cursor,
+                lote=self.lote,
+                estagio=self.estagio,
+                qtd=self.qtd,
+                estagio_modelo=self.movimentacoes[-1]['pcpc040_estconf'],
+            )
+        except Exception as e:
+            return f"Erro ao inserir movimentação de lote: {e}"
 
-        result_error = self.get_movimentacoes(
-            lote=kwargs['lote'],
-            estagio=kwargs['estagio'],
-        )
+    def process(self):
+        result_error = self.get_movimentacoes()
         if result_error:
             return (self.ERROR_STATUS, result_error)
 
         pprint(self.movimentacoes)
 
-        try:
-            movimentacao_de_lote.insere(
-                cursor=self.cursor,
-                lote=kwargs['lote'],
-                estagio=kwargs['estagio'],
-                qtd=kwargs['qtd'],
-                estagio_modelo=self.movimentacoes[-1]['pcpc040_estconf'],
-            )
-            return ('OK', "OK!")
-        except Exception as e:
-            return ('ERROR',  f"Erro ao inserir movimentação de lote: {e}")
+        result_error = self.insere_movimentacao()
+        if result_error:
+            return (self.ERROR_STATUS, result_error)
+
+        return (self.OK_STATUS, "OK!")
             
 
     def response(self, result):
@@ -67,7 +58,13 @@ class ProduzLote(View):
             'message': message,
         }
 
+    def inicializa_variaveis(self, request, kwargs):
+        self.cursor = db_cursor_so(request)
+        self.lote=kwargs['lote']
+        self.estagio=kwargs['estagio']
+        self.qtd=kwargs['qtd']
+        self.movimentacoes = None
+
     def get(self, request, *args, **kwargs):
-        self.request = request
-        result = self.process(kwargs)
-        return JsonResponse(self.response(result), safe=False)
+        self.inicializa_variaveis(request, kwargs)
+        return JsonResponse(self.response(self.process()), safe=False)
