1d6bdc8061a8e003d144297b409d641a6c25f419
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2024-04-11 16:54:32 -0300

    bug quantidade_finalizada.query

--- src/lotes/queries/lote/quantidade_finalizada.py
+++ src/lotes/queries/lote/quantidade_finalizada.py
@@ -4,17 +4,16 @@ from utils.functions.models.dictlist import dictlist_lower
 from utils.functions.queries import debug_cursor_execute
 
 
-def query(cursor, periodo, ordem_confeccao):
+def query(cursor, lote):
     sql = f"""
         SELECT
           l.QTDE_PECAS_PROD QTD
         , l.CODIGO_ESTAGIO ULTIMO_ESTAGIO
         , l.SEQUENCIA_ESTAGIO ULTIMA_SEQUENCIA
         FROM PCPC_040 l
-          ON l.PERIODO_PRODUCAO = {periodo}
-         AND l.ORDEM_CONFECCAO = {ordem_confeccao}
-        WHERE l.SEQUENCIA_ESTAGIO
-              = (
+        WHERE l.PERIODO_PRODUCAO = {lote[:4]}
+          AND l.ORDEM_CONFECCAO = {lote[4:]}
+          AND l.SEQUENCIA_ESTAGIO = (
                 SELECT
                   max(ms.SEQUENCIA_ESTAGIO)
                 FROM PCPC_040 ms
